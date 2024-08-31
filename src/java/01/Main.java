33c31a73e0b7e6c12629c859e671834c5fee34d7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-31 13:49:44 +0700

    Commit

--- src/java/01/Main.java
+++ src/java/01/Main.java
@@ -1,4 +1,4 @@
-public class Two {
+class Main {
     public static void main(String[] args) {
         
     }
--- src/java/02/Main.java
+++ src/java/02/Main.java
@@ -1,4 +1,4 @@
-public class One {
+class Main {
     public static void main(String[] args) {
         
     }
