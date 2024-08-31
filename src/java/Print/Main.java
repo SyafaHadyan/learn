feaad47851fad5032c88045e82cfb7364a2b2e6b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-31 14:49:23 +0700

    chore: delete Main.class and Main.java

--- src/java/Print/Main.java
+++ src/java/Print/Main.java
@@ -1,8 +1,13 @@
-package java;
+package java.Print;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // Start
+
+        /*
+         * Testing
+         * Print
+         */
         System.out.println("First");
         System.out.println("\t Second");
         System.out.println("\"Third\"");
---
+++ src/java/Variable/Main.java
@@ -0,0 +1,9 @@
+package java.Variable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
