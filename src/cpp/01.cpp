57499294d6d707ef37e068bba619ecbb66e9058f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-31 13:07:32 +0700

    Add: Number

---
+++ src/cpp/01.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+
+double Number;
+
+int Number() {
+    cout << "Enter Number" << ":" << " " << endl;
+    cin >> Number;
+    cout << "Number" << ":" << " " << Number << endl;
+
+    return 0;
+}
