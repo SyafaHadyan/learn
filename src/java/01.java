bf01f958760f7aba37010087f44dd156c36a101b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-31 13:30:23 +0700

    Add

---
+++ src/java/01.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+
+class 01 {
+    public static void main(String[] args) {
+        String Input;
+        Scanner Input = new Scanner(Input);
+        System.out.println("Enter a string" + ":" + " ");
+        Input = Input.nextLine();
+    }
+}
