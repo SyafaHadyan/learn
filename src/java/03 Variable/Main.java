f0313045b1f4edb8528b96eb7068ae478b322e26
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-01 09:04:18 +0700

    feat: create Main.class and Main.java

---
+++ src/java/03 Variable/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String first_name;
+        String middle_name;
+        String last_name;
+        
+        first_name = "Syafa";
+        middle_name = "Hadyan";
+        last_name = "Rasendriya";
+        
+        System.out.print("Name" + ":" + " " + first_name + " " + middle_name + " " + last_name);
+    }
+}
