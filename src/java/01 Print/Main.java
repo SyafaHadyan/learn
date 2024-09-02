5b6fcda8725d7a58222c93dbcf65710f162271eb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-02 08:24:45 +0700

    Add: my name

--- src/java/01 Print/Main.java
+++ src/java/01 Print/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Test");
-        System.out.print("This");
+        System.out.println("Hello World");
+        System.out.print("Syafa Hadyan Rasendriya");
     }
 }
