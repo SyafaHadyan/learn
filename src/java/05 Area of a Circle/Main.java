65efc738df3cec533654dc142f73317eaf4035d9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-02 08:25:31 +0700

    Add: comments

--- src/java/05 Area of a Circle/Main.java
+++ src/java/05 Area of a Circle/Main.java
@@ -4,14 +4,19 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Declare
         double result;
 
+        //User input
         Scanner input = new Scanner(System.in);
+
+        //Output
         System.out.print("Enter Radius" + ":" + " ");
         double radius = input.nextDouble();
         input.close();
 
+        //Output result
         result = radius * radius * 22 / 7;
-        System.out.print("Result" + ":" + " " + result);
-    }
+        System.out.println("Result" + ":" + " " + result);
+    }    
 }
