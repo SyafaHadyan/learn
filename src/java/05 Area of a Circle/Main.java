cd741e32a3d350f25359d36fa73be5312f3e63e9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-03 07:16:07 +0700

    Add: Main.java on 05 Area of a Circle

--- src/java/05 Area of a Circle/Main.java
+++ src/java/05 Area of a Circle/Main.java
@@ -4,19 +4,48 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Declare
-        double result;
+        
+        do{
+            //Declare variables
+            double result;
+            boolean repeat;
+            String confirmrepeat;
 
-        //User input
-        Scanner input = new Scanner(System.in);
+            //User input
+            Scanner input = new Scanner(System.in);
 
-        //Output
-        System.out.print("Enter Radius" + ":" + " ");
-        double radius = input.nextDouble();
-        input.close();
+            //Output
+            System.out.print("Enter Radius" + ":" + " ");
+            double radius = input.nextDouble();
+            input.close();
 
-        //Output result
-        result = radius * radius * 22 / 7;
-        System.out.println("Result" + ":" + " " + result);
+            //Calculate area of a circle
+            result = radius * radius * 22 / 7;
+            
+            if (result > 0);
+            {
+                System.out.println("Result" + ":" + " " + result);
+                repeat = true;
+            }
+            elif (result <= 0);
+            {
+                System.out.println("Radius cannnot be less or equal to 0.");
+                repeat = false;
+                Scanner repeatinput = new Scanner(System.in);
+                System.out.println("Do you want to try again? (Y/N)");
+                String confirmrepeat = repeatinput.next();
+                if ( confirmrepeat == 'Y' || confirmrepeat == 'y')
+                {
+                    repeat = true;
+                }
+                else if (confirmrepeat == 'N' || confirmrepeat == 'n')
+                {
+                    repeat = false;
+                }
+            }
+
+        } while (repeat == true);
+
+
     }    
 }
