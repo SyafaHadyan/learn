8725236771a0b3b39c0b6071d71decf68cf231b9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-08 13:37:27 +0700

    Fixed but got another error

--- src/java/05 Area of a Circle/Main.java
+++ src/java/05 Area of a Circle/Main.java
@@ -4,50 +4,42 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double result;
+        boolean repeatcalculation = false;
         do{
-            //Declare variables
-            double result;
-            boolean repeat;
-            String confirmrepeat;
-
-            //User input
             Scanner input = new Scanner(System.in);
 
-            //Output
+            //Ask user
             System.out.print("Enter Radius" + ":" + " ");
             double radius = input.nextDouble();
             input.close();
 
             //Calculate area of a circle
             result = radius * radius * 22 / 7;
-            
 
-            // TODO: fix this
-            if (result > 0);
+            //Check user input
+            if (radius > 0)
             {
                 System.out.println("Result" + ":" + " " + result);
-                repeat = true;
+                repeatcalculation = true;
             }
-            elif (result <= 0);
+            else if (radius <= 0)
             {
                 System.out.println("Radius cannnot be less or equal to 0.");
-                repeat = false;
+                repeatcalculation = false;
                 Scanner repeatinput = new Scanner(System.in);
                 System.out.println("Do you want to try again? (Y/N)");
                 String confirmrepeat = repeatinput.next();
-                if ( confirmrepeat == 'Y' || confirmrepeat == 'y')
+                input.close();
+                if ( confirmrepeat == "Y" || confirmrepeat == "y")
                 {
-                    repeat = true;
+                    repeatcalculation = true;
                 }
-                else if (confirmrepeat == 'N' || confirmrepeat == 'n')
+                else if (confirmrepeat == "N" || confirmrepeat == "n")
                 {
-                    repeat = false;
+                    repeatcalculation = false;
                 }
             }
-
-        } while (repeat == true);
-
-
+        }while (true);
     }    
 }
