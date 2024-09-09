fde55eacae3fae3eb01fc3a1bdcd4c142da21157
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-10 06:32:40 +0700

    Add login

--- src/java/00 Testing/MP.java
+++ src/java/00 Testing/MP.java
@@ -5,27 +5,39 @@ public class MP
 {
     public static void main(String[] args)
     {
-        Scanner login = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Welcome to Microsoft Privacy Center" + "\n" + "Please log in to your account");
         System.out.print("\n" + "Username" + ":" + " ");
-        String username = login.next();
+        String username = input.next();
         System.out.println("\n" + "Password" + ":" + " ");
-        String password = login.next();
+        String password = input.next();
         TimeUnit.SECONDS.sleep(1);
         System.out.println("Logging in");
         TimeUnit.SECONDS.sleep(5);
         System.out.println("We never sell your user data");
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("Successfully logged in");
         
         if (username == "admin" && password == "admin");
         {
-            System.out.println("\n" + "Welcome to Admin Center" + "\n" + "(0) View user data" + "\n" + );
+            System.out.println("\n" + "Successfully logged in");
+            System.out.println("\n" + "Welcome to Admin Center" + "\n" + "(0) Sell users' data");
+            String select = input.next();
+            if (select == "0")
+            {
+                
+            }
+        }
+        else if (username.equalsIgnoreCase("admin"))
+        {
+            System.out.println("\n" + "Failed to log in");
+            System.out.println("User did not exist, please enter a valid username");
         }
         else
         {
+            System.out.println("\n" + "Successfully logged in");
             System.out.println("\n" + "Welcome" + username + "!")
+
         }
-        login.close();
+        input.close();
     }
 }
