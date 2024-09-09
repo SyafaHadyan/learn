8a847e796dacbb6f92d1400063386c8c782b1d01
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-09 07:58:44 +0700

    Add: Formatting.java

---
+++ src/java/Testing/Formatting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Formatting
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("%c\n", 69);
+        System.out.printf("%20c%50c%75c\n",10,20,15);
+        System.out.printf("%-10c%-10c%-10c\n",65,66,67);
+    }
+}
