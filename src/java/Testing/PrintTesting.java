9a1860ded501476ebc4ca0e0cba60dbeb1d32683
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-09 07:45:22 +0700

    Add: PrintTesting.java

---
+++ src/java/Testing/PrintTesting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PrintTesting
+{
+    public static void main(String[] args)
+    {
+        System.out.print("15 x 3" + " " + "=" + " ");
+        System.out.print(15*3);
+    }
+}
