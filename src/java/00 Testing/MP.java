e3903ae3e4337648bda769190aa8d21702f72b03
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-10 07:09:15 +0700

    Add: some options for users

--- src/java/00 Testing/MP.java
+++ src/java/00 Testing/MP.java
@@ -10,7 +10,6 @@ public class MP
 
         while (repeat)
         {
-            int loadingtime = ThreadLocalRandom.current().nextInt(1, 5);
             System.out.print("Welcome to Microsoft Privacy Center" + "\n" + "Please log in to your account");
             System.out.print("\n" + "Username" + ":" + " ");
             String username = input.next();
@@ -26,6 +25,7 @@ public class MP
             {
                 System.out.println("\n" + "Successfully logged in");
                 System.out.println("\n" + "Welcome to Admin Center" + "\n" + "(0) Sell users' data");
+                System.out.print("Enter" + ":" + " ");
                 String select = input.next();
                 if (select == "0")
                 {
@@ -35,7 +35,7 @@ public class MP
                     TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
                     System.out.println("Don't forget to promote Windows 11!");
                     TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
-                    System.out.println("");
+                    System.out.println("...");
                     TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
                     System.out.println("Successfully sold" + " " + ThreadLocalRandom.current().nextInt(1000, 5000) + " " + "users' data");
                     TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
@@ -55,10 +55,34 @@ public class MP
                 System.out.println("\n" + "Welcome" + username + "!");
                 System.out.println("We never sell your data to any third party");
                 System.out.println("Please choose an option" + "\n" + "(0) View your personal information" + "\n" + "(1) Log out");
+                System.out.print("Enter" + ":" + " ");
                 String choose = input.next();
-                if(choose == "0")
+                if (choose == "0")
                 {
+                    String usrchs = input.next();
+                    System.out.println("Select an option" + "\n" + "(0) Delete your data" + "");
+                    System.out.print("Enter" + ":" + " ");
                     
+                    if (usrchs == "0")
+                    {
+                        //Don't let users delete their data
+                        System.out.println("\n" + "Our website is on high demand, please try again");
+                        repeat = true;
+                    }
+                    else if (usrchs == "1")
+                    {
+                        System.out.println("\n" + "Logging out" + "...");
+                        TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
+                        System.out.println("Sucessfully logged out");
+                        repeat = false;
+                    }
+                }
+                else if (choose == "1")
+                {
+                    System.out.println("Logging out" + "...");
+                    TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 5));
+                    System.out.println("Successfully logged out");
+                    repeat = false;
                 }
             }
         }   
