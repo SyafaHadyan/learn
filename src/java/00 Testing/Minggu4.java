d31e51f027980ddb433ae14312133caa98c69b1c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-10 07:46:02 +0700

    Add: Minggu4.java

---
+++ src/java/00 Testing/Minggu4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Minggu4
+{
+    public static void main(String[] args)
+    {
+        
+        int inta = 7;
+        int intb = 2;
+        double douba = 7;
+        double doubb = 2;
+
+
+        System.out.println("Integer" + " " + "A / B" + ":" + " " + inta/intb);
+        System.out.print("Dobule" + " " + "A / B" + ":" + " " + douba/doubb);
+    }
+}
