eb29f426b1def595083ffef2fde002fd43d9cc95
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-11 11:39:01 +0700

    Add: Latihan 03

---
+++ src/java/Latihan Pemrograman Dasar/03/Latihan.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Latihan
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 6;
+        int z = 25;
+
+        x *= 5 + y;
+        y += y &= x;
+        z += x>>2 & y<<1;
+        
+        System.out.printf("x = %d\ny = %d\nz = %d\n",x,y,z);
+
+        /*
+         * Output:
+         * 
+         * x = 110
+         * y = 12
+         * z = 49
+         */
+    }
+}
