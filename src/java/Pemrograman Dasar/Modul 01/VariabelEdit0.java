36f35742ea995fe5974eac05c393317d9c89d20e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 06:45:14 +0700

    Add: VariabelEdit0.java

---
+++ src/java/Pemrograman Dasar/Modul 01/VariabelEdit0.java
@@ -0,0 +1,13 @@
+public class VariabelEdit0
+{
+    public static void main(String[] args)
+    {
+        int nilai = 10;
+        double nilai_2 = 5.3;
+        int hasil; // Tidak terpakai
+        String s = "Belajar Java";
+        System.out.println(nilai - nilai_2);
+        System.out.println(nilai + nilai_2);
+        System.out.println("Kita sedang " + s);
+    }
+}
