5a98e63b4025b7fca99b248b225b3d3ad7c0e836
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-11 11:26:55 +0700

    Move files and add multiple outputs

---
+++ src/java/Latihan Pemrograman Dasar/01/Latihan.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Latihan 
+{
+    public static void main(String[] args)
+    {
+        int a_0;
+        int a_1;
+        int a_2;
+        int a_3;
+        int a_4;
+        int b = 23;
+        int c = 12;
+        int d = 32;
+        int e = 0;
+
+        //a = b&&e; // Error
+        a_0 = b & c; // 4
+        //a = c || d; // Error
+        a_1 = b | d; // 55
+        //a = b > 2; // Error
+        a_2 = b >> 2; // 5
+        //a = c < 3; // Error
+        a_3 = c << 3; // 96
+        a_4 = b = c; // 12
+        //a = b == c; // Error
+
+        System.out.println(a_0);
+        System.out.println(a_1);
+        System.out.println(a_2);
+        System.out.println(a_3);
+        System.out.println(a_4);
+    }
+}
