a905726cc08b588df96172854420f8d029fefde8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-11 16:52:53 +0700

    Add: Latihan.java

--- src/java/Latihan Pemrograman Dasar/04/Latihan.java
+++ src/java/Latihan Pemrograman Dasar/04/Latihan.java
@@ -4,6 +4,28 @@ public class Latihan
 {
     public static void main(String[] args)
     {
-        System.out.println();
+        double x;
+
+        x = 7/2;
+        System.out.printf("x = %f\n",x);
+        x = 7.0/2;
+        System.out.printf("x = %f\n",x);
+        x = 7/2.0;
+        System.out.printf("x = %f\n",x);
+        x = 7.0/2.0;
+        System.out.printf("x = %f\n",x);
+        x = (float)7/2;
+        System.out.printf("x = %f\n",x);
+
+        /*
+         * Output:
+         * 
+         * x = 3.000000
+         * x = 3.500000
+         * x = 3.500000
+         * x = 3.500000
+         * x = 3.500000
+         * 
+         */ 
     }
 }
