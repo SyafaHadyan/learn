85f552914c5d43272a0abe8784f4381384ed5661
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 06:58:53 +0700

    Add: VariabelEdit3.java

---
+++ src/java/Pemrograman Dasar/Modul 01/VariabelEdit3.java
@@ -0,0 +1,18 @@
+public class VariabelEdit3
+{
+    public static void main(String[] args)
+    {
+        int nilai = 10;
+        double nilai_2 = 5.3;
+        int nilai_3 = 100;
+        int nilai3 = 100;
+        double hasil = nilai + nilai_2;
+        String s = "Belajar Java";
+        System.out.println(nilai - nilai_2);
+        System.out.println(nilai + nilai_2);
+        System.out.println(nilai_3 / hasil);
+        System.out.println(nilai3 / nilai);
+        System.out.println("Kita sedang " + s);
+        // char ch = 'Belajar Java' // Error, karena char tidak bisa memuat lebih dari 1
+    }
+}
