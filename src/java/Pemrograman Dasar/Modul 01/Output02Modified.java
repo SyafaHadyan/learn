b99edd0a9cee460ec38bf0f28e82febdc2292365
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 13:04:11 +0700

    Modified

--- src/java/Pemrograman Dasar/Modul 01/Output02Modified.java
+++ src/java/Pemrograman Dasar/Modul 01/Output02Modified.java
@@ -1,8 +1,8 @@
-public class Output02Modified
+public class Output02
 {
     public static void main(String[] args)
     {
         System.out.println("Belajar ");
-        System.out.println("Pemrograman Java");
+        System.out.println("Pemrograman "  "Java"); // Error karena tanda '+' diperlukan untuk menyambung teks atau karakter
     }
 }
