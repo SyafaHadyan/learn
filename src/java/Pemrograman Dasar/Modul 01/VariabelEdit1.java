c4b2cf189a71d9130e70ba7aeba7588b1726b7e5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 16:31:10 +0700

    Edit comments

--- src/java/Pemrograman Dasar/Modul 01/VariabelEdit1.java
+++ src/java/Pemrograman Dasar/Modul 01/VariabelEdit1.java
@@ -5,10 +5,19 @@ public class VariabelEdit1
         int nilai = 10;
         double nilai_2 = 5.3;
         int hasil; // Tidak terpakai
+        System.out.println(nilai - nilai_2); // Mengurangi variabel "nilai" dengan "nilai_2"
         String s = "Belajar Java";
-        System.out.println(nilai - nilai_2);
         System.out.println(nilai + nilai_2);
         System.out.println("Kita sedang " + s);
-        char ch = 'Belajar Java' // Error, karena char tidak bisa memuat lebih dari 1
+        //char ch = s; // Tidak bisa mengubah string ke char dan tipe data char hanya bisa memuat satu karakter
+        //Program error dan tidak bisa berjalan
+
+        /*
+         * Output:
+         * 
+         * 4.7
+         * 15.3
+         * Kita sedang Belajar Java
+         */
     }
 }
