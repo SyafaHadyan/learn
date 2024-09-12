c8cb428705befe17f04bdf2ad1d54ce5b4b57e6d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 16:47:16 +0700

    Edit output

--- src/java/Pemrograman Dasar/Modul 01/VariabelEdit3.java
+++ src/java/Pemrograman Dasar/Modul 01/VariabelEdit3.java
@@ -4,15 +4,25 @@ public class VariabelEdit3
     {
         int nilai = 10;
         double nilai_2 = 5.3;
-        int nilai_3 = 100;
         int nilai3 = 100;
-        double hasil = nilai + nilai_2;
+        int hasil = nilai3 / nilai; // Pembagian int nilai3 dan int nilai
         String s = "Belajar Java";
-        System.out.println(nilai - nilai_2);
-        System.out.println(nilai + nilai_2);
-        System.out.println(nilai_3 / hasil);
-        System.out.println(nilai3 / nilai);
+        System.out.println(nilai - nilai_2); // Mengurangi variabel "nilai" dengan "nilai_2"
+        System.out.println(nilai + nilai_2); // Penjumlahan int nilai dan double nilai_2
+        System.out.println(hasil); // Print nilai hasil
+        System.out.println(nilai3 / nilai); // Pembagian variabel "nilai3" dengan "nilai" tanpa harus disimpan ke dalam variabel
         System.out.println("Kita sedang " + s);
-        // char ch = 'Belajar Java' // Error, karena char tidak bisa memuat lebih dari 1
+        //char ch = s; // Tidak bisa mengubah string ke char dan tipe data char hanya bisa memuat satu karakter
+        //Program error dan tidak bisa berjalan
+
+        /*
+         * Output:
+         * 
+         * 4.7
+         * 15.3
+         * 10
+         * 10
+         * Kita sedang Belajar Java
+         */
     }
 }
