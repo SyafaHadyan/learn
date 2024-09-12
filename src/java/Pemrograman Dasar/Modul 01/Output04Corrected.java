02f289eefb2918dac684abab55e7361747a7e89a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 12:48:59 +0700

    Comment

--- src/java/Pemrograman Dasar/Modul 01/Output04Corrected.java
+++ src/java/Pemrograman Dasar/Modul 01/Output04Corrected.java
@@ -2,7 +2,7 @@ public class Output04Corrected
 {
     public static void main(String[] args)
     {
-        float x = 7654.123456789f; // Kesalahan pada `F` yang seharusnya `7654.123456789f;
+        float x = 7654.123456789f; // Kesalahan pada ` F` yang seharusnya `7654.123456789f tanpa spasi;
         System.out.printf("%d\t%3d\t%8d\n", 1234, -567, 8910);
         System.out.printf("%d\t%3d\t%+8d\n", 1234, -567, 8910);
         System.out.printf("%f\t%15f\t%15.3f\n", x, x, x); // Escape sequance tidak valid pada `\ n` yang seharusnya `\n`
