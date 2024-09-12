11d9914d06374f9079ce5791632a10348776d8d2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 16:25:40 +0700

    Add comment and move System.out.println

--- src/java/Pemrograman Dasar/Modul 01/VariabelEdit0.java
+++ src/java/Pemrograman Dasar/Modul 01/VariabelEdit0.java
@@ -5,9 +5,17 @@ public class VariabelEdit0
         int nilai = 10;
         double nilai_2 = 5.3;
         int hasil; // Tidak terpakai
+        System.out.println(nilai - nilai_2); // Mengurangi variabel "nilai" dengan "nilai_2"
         String s = "Belajar Java";
-        System.out.println(nilai - nilai_2);
         System.out.println(nilai + nilai_2);
         System.out.println("Kita sedang " + s);
+
+        /*
+         * Output:
+         * 
+         * 4.7
+         * 15.3
+         * Kita sedang Belajar Java
+         */
     }
 }
