371357c3ae3f55d0e2ae4e565678bbee2aa067f1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-12 13:31:13 +0700

    Edit comment

--- src/java/Pemrograman Dasar/Modul 01/Output03Edit.java
+++ src/java/Pemrograman Dasar/Modul 01/Output03Edit.java
@@ -2,7 +2,8 @@ public class Output03Edit
 {
     public static void main(String[] args)
     {
-        System.out.printf("%s%c\n", "Pemrograman Java", 'ab'); // error karena bukan char
+        // Error karena 'ab' bukan char dan progarm tidak bisa berjalan
+        System.out.printf("%s%c\n", "Pemrograman Java", 'ab');
         System.out.printf("%30s%c\n", "Pemrograman Java ", '|');
         System.out.printf("%-30s%c\n", "Pemrograman Java ", '|');
         System.out.printf("%30.5s%c\n", "Pemrograman Java ", '|');
