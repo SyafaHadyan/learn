450f8ba03f7c2dccc0265d3d162f4bf94205d577
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-13 07:21:52 +0700

    Add payment option, actual payment processing is not yet available

--- src/java/Pemrograman Dasar/Modul 01/TugasPraktikum1.java
+++ src/java/Pemrograman Dasar/Modul 01/TugasPraktikum1.java
@@ -21,6 +21,8 @@ public class TugasPraktikum1
         double tariflistrik = beban * (k_final - k_initial);
         double pajak = tariflistrik * ppj / 100;
         double total = tariflistrik + pajak;
+        double cicilan = total / 12;
+        double transferbank = total + 2500;
         String header = "\n" + "=========================" + ' ' + "PLN Kelurahan" + ' ' + kelurahan + ' ' + "=========================";
         System.out.printf("%.87s%s%s",header,"\n","\n");
         System.out.printf("%-25s%c%c%s\n","Nama lengkap",':',' ',fullname);
@@ -30,6 +32,8 @@ public class TugasPraktikum1
         System.out.printf("%-25s%c%c%s%c%.2f\n","PPJ",':',' ',"Rp",' ',pajak);
         System.out.printf("%-25s%c%c%s%c%.2f\n","Tarif Listrik",':',' ',"Rp",' ',total);
         System.out.print("\n" + "================================ Portal Pembayaran PLN ================================");
+        System.out.printf("%s%s%s%s%-45s%c%c%s%c%.2f%s%-45s%c%c%s%c%.2f%s%-45s%c%c%s%c%.2f%c%s","\n","\n","Pilih opsi pembayaran","\n","(0) Tunai",':',' ',"Rp",' ',total,"\n","(1) Transfer Bank + Biaya Admin (Rp 2500)",':',' ',"Rp",' ',transferbank,"\n","(2) Cicilan 12 bulan",':',' ',"Rp",' ',cicilan,' ',"Per bulan");
+        //String pembayaran = input.nextLine();
         input.close();
     }
 }
