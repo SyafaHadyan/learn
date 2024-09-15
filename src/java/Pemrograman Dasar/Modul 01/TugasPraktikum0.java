c336abebd54f343cc95758ffc5f7166f56e4198f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-15 16:24:27 +0700

    Changed - to = and added program author

--- src/java/Pemrograman Dasar/Modul 01/TugasPraktikum0.java
+++ src/java/Pemrograman Dasar/Modul 01/TugasPraktikum0.java
@@ -9,11 +9,12 @@ public class TugasPraktikum0
         double first = input.nextDouble();
         System.out.printf("%-30s%-1c%-1c","Masukkan Operator Kedua",':',' ');
         double second = input.nextDouble();
-        System.out.println("--------------------------------------------------");
+        System.out.println("==================================================");
         System.out.printf("%-30s%-1c%-1c%1.3f\n","Hasil Penjumlahan",':',' ',first + second);
         System.out.printf("%-30s%-1c%-1c%1.3f\n","Hasil Pengurangan",':',' ',first - second);
         System.out.printf("%-30s%-1c%-1c%1.3f\n","Hasil Perkalian",':',' ',first * second);
         System.out.printf("%-30s%-1c%-1c%1.3f","Hasil Pembagian",':',' ',first / second);
+        System.out.printf("%s%s%-25s%c%c%s%s%-25s%c%c%s","\n","\n","Program made by",':',' ',"Syafa Hadyan Rasendriya","\n","NIM",':',' ',"245150207111047");
         input.close();
     }
 }
