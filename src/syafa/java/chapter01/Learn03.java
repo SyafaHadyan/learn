a1535aa834acf5ac5a387a5f44ec1dc13f1462b6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-16 17:12:43 +0700

    Add repeat

--- src/syafa/java/chapter01/Learn03.java
+++ src/syafa/java/chapter01/Learn03.java
@@ -7,9 +7,18 @@ public class Learn03
 {
     public static void main(String[] args)
     {
+        boolean Repeat = true;
         Scanner Input = new Scanner(System.in);
         System.out.println("Welcome to RPS!");
         System.out.println("(0) Play once" + "\n" + "(1) Play indefinitely");
+        int PlayDuration = Integer.parseInt(Input.nextLine());
+        while (repeat)
+        {
+            if (PlayDuration = 0)
+            {
+
+            }
+        }
         System.out.println("Please choose an option" + "\n" + "(0) Rock" + "\n" + "(1) Paper" + "\n" + "(2) Scissor");
         int User = Integer.parseInt(Input.nextLine());
         String [] Option = {"Rock", "Paper", "Scissor"};
