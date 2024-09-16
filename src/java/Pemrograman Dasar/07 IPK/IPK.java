4d15bf45a38d41b670f87448013fbafc625acc99
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-17 06:41:16 +0700

    Check IPK

--- src/java/Pemrograman Dasar/07 IPK/IPK.java
+++ src/java/Pemrograman Dasar/07 IPK/IPK.java
@@ -4,37 +4,48 @@ public class IPK
 {
     public static void main(String[] args)
     {
+        boolean CheckIPK = true;
         Scanner Input = new Scanner(System.in);
         System.out.print("Masukkan IPK anda" + ':' + ' ');
         float IPK = Float.parseFloat(Input.nextLine());
         System.out.print("\n");
-        if (IPK <= 3.5 && IPK >= 4)
+        while (CheckIPK)
         {
-            System.out.println("Sangat Memuaskan");
-        }
-        else if (IPK <= 3.0 && IPK >= 3.4)
-        {
-            System.out.println("Memuaskan");
-        }
-        else if (IPK <= 2.5 && IPK >= 2.9)
-        {
-            System.out.println("Baik Sekali");
-        }
-        else if (IPK <= 2.0 && IPK >= 2.4)
-        {
-            System.out.println("Baik");
-        }
-        else if (IPK <= 1.9 && IPK > 0)
-        {
-            System.out.println("Kurang");
-        }
-        else if (IPK == 0)
-        {
-            System.out.println("Anda tidak lulus");
-        }
-        else if (IPK < 0)
-        {
-            System.out.println("IPK tidak valid. Harap ulang kembali");
+            if (IPK <= 3.5 && IPK >= 4)
+            {
+                System.out.println("Sangat Memuaskan");
+                CheckIPK = false;
+            }
+            else if (IPK <= 3.0 && IPK >= 3.4)
+            {
+                System.out.println("Memuaskan");
+                CheckIPK = false;
+            }
+            else if (IPK <= 2.5 && IPK >= 2.9)
+            {
+                System.out.println("Baik Sekali");
+                CheckIPK = false;
+            }
+            else if (IPK <= 2.0 && IPK >= 2.4)
+            {
+                System.out.println("Baik");
+                CheckIPK = false;
+            }
+            else if (IPK <= 1.9 && IPK > 0)
+            {
+                System.out.println("Kurang");
+                CheckIPK = false;
+            }
+            else if (IPK == 0)
+            {
+                System.out.println("Anda tidak lulus");
+                CheckIPK = false;
+            }
+            else if (IPK < 0 || IPK > 4)
+            {
+                System.out.println("IPK tidak valid. Harap ulang kembali");
+                CheckIPK = true;
+            }
         }
         Input.close();
     }
