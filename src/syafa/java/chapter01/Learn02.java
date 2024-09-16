6c5e6eac14118912e62429cdf0b3ddd7f9fca275
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-16 13:42:33 +0700

    Finished

--- src/syafa/java/chapter01/Learn02.java
+++ src/syafa/java/chapter01/Learn02.java
@@ -14,11 +14,19 @@ public class Learn02
         int BatuBata = Keliling * 113;
         int Pilar = (Keliling / 10) * 37;
         int TotalBatuBata = BatuBata + Pilar;
+        input.close();
+        if (Panjang < 0 || Panjang > 1000000)
+        {
+            return;
+        }
+        else if (Lebar < 0 || Lebar > 1000000)
+        {
+            return;
+        }
         System.out.printf("%-25s%c%c%s%c%s%s","Nama istana",':',' ',"Istana",' ',Istana,"\n");
         System.out.printf("%-25s%c%c%d%c%s%s","Batu bata",':',' ',TotalBatuBata,' ',"buah","\n");
         System.out.printf("%-25s%c%c%d%c%s%s","Panjang",':',' ',Panjang,' ',"meter","\n");
         System.out.printf("%-25s%c%c%d%c%s%s","Lebar",':',' ',Lebar,' ',"meter","\n");
         System.out.printf("%-25s%c%c%d%c%s","Keliling",':',' ',Keliling,' ',"meter");
-        input.close();
     }
 }
