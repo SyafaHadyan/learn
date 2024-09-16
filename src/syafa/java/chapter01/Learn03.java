5c8023d9dbf4276ab6610bc10ea37b880f7fc4eb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-16 17:11:20 +0700

    Add duration option

--- src/syafa/java/chapter01/Learn03.java
+++ src/syafa/java/chapter01/Learn03.java
@@ -9,6 +9,7 @@ public class Learn03
     {
         Scanner Input = new Scanner(System.in);
         System.out.println("Welcome to RPS!");
+        System.out.println("(0) Play once" + "\n" + "(1) Play indefinitely");
         System.out.println("Please choose an option" + "\n" + "(0) Rock" + "\n" + "(1) Paper" + "\n" + "(2) Scissor");
         int User = Integer.parseInt(Input.nextLine());
         String [] Option = {"Rock", "Paper", "Scissor"};
