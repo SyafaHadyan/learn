8c15108df04d36127bb317c361440fb387ad7ed7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-16 17:09:42 +0700

    Add options

---
+++ src/syafa/java/chapter01/Learn03.java
@@ -0,0 +1,19 @@
+package syafa.java.chapter01;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Learn03
+{
+    public static void main(String[] args)
+    {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Welcome to RPS!");
+        System.out.println("Please choose an option" + "\n" + "(0) Rock" + "\n" + "(1) Paper" + "\n" + "(2) Scissor");
+        int User = Integer.parseInt(Input.nextLine());
+        String [] Option = {"Rock", "Paper", "Scissor"};
+        Random rand = new Random();
+        String Choose = Option[rand.nextInt(Option.length)];
+        System.out.println(Choose);
+    }
+}
