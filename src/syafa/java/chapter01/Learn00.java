6189cf916ba14e770ef2bc37350cedab666ed59a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-16 14:00:47 +0700

    Compiled Learn00.java

---
+++ src/syafa/java/chapter01/Learn00.java
@@ -0,0 +1,18 @@
+package syafa.java.chapter01;
+
+import java.util.*;
+
+public class Learn00
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double first = Double.parseDouble(input.nextLine());
+        double second = Double.parseDouble(input.nextLine());
+        System.out.printf("%-26s%-1c%-1c%1.0f\n","Hasil penjumlahan",':',' ',first + second);
+        System.out.printf("%-26s%-1c%-1c%1.0f\n","Hasil pengurangan",':',' ',first - second);
+        System.out.printf("%-26s%-1c%-1c%1.0f\n","Hasil perkalian",':',' ',first * second);
+        System.out.printf("%-26s%-1c%-1c%1.1f","Hasil pembagian",':',' ',first / second);
+        input.close();
+    }
+}
