ffb3541369db971e5bf38486d0053048c7238a87
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-17 07:51:43 +0700

    Finished

--- src/java/Pemrograman Dasar/08/DiskonMember.java
+++ src/java/Pemrograman Dasar/08/DiskonMember.java
@@ -4,22 +4,33 @@ public class DiskonMember
 {
     public static void main(String[] args)
     {
+        boolean CheckInput = true;
         Scanner Input = new Scanner(System.in);
-        System.out.printf("%s%-30s%c%c","Masukkan total belanja",':',' ');
-        int TotalBelanja = Integer.parseInt(Input.nextLine());
-        System.out.println("Apakah anda memiliki member?" + "(Y/N)");
-        String CheckMember = Input.nextLine();
-        if (CheckMember.equalsIgnoreCase("Y"))
+        System.out.printf("%-30s%c%c","Masukkan total belanja",':',' ');
+        double TotalBelanja = Double.parseDouble(Input.nextLine());
+        System.out.println("\n" + "Apakah anda memiliki member?" + "(Y/N)");
+        String CheckMember = Input.nextLine();        
+        while (CheckInput)
         {
-            System.out.println();
-        }
-        else if (CheckMember.equalsIgnoreCase("N"))
-        {
-            System.out.println();
-        }
-        else
-        {
-            System.out.println("Pilihan yang anda masukkan salah, silahkan coba lagi");
+            if (CheckMember.equalsIgnoreCase("Y"))
+            {
+                double Diskon = 0.1 * TotalBelanja;
+                double HargaTotal = TotalBelanja - Diskon;
+                System.out.printf("%-30s%c%c%s%c%1.2f%s","Diskon anda adalah",':',' ',"Rp",' ',Diskon,"\n");
+                System.out.printf("%-30s%c%c%s%c%1.2f%s","Total belanja anda adalah",':',' ',"Rp",' ',HargaTotal,"\n");
+                System.out.print("Terimakasih telah berbelanja");
+                CheckInput = false;
+            }
+            else if (CheckMember.equalsIgnoreCase("N"))
+            {
+                System.out.print("Terimakasih telah berbelanja");
+                CheckInput = false;
+            }
+            else
+            {
+                System.out.println("Pilihan yang anda masukkan salah, silahkan coba lagi");
+                CheckInput = true;
+            }
         }
     }
 }
