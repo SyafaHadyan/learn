404d4d207db07402ff9dd367299592ff82275664
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-19 17:10:25 +0700

    add

---
+++ src/syafa/java/chapter01/Class.java
@@ -0,0 +1,13 @@
+public class Class
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        int y = 5;
+        while (x < y)
+        {
+            System.out.println("Hello");
+            x++; 
+        }
+    }
+}
