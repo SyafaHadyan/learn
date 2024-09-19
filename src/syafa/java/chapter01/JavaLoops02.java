b1681d12aeaf0d0aaf62c83228dca76d782734ec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-19 16:56:11 +0700

    Edit

--- src/syafa/java/chapter01/JavaLoops02.java
+++ src/syafa/java/chapter01/JavaLoops02.java
@@ -5,21 +5,20 @@ public class JavaLoops02
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int a = Integer.parseInt(input.nextLine());
-        int b = input.nextInt();
-        int c = input.nextInt();
-        int d = input.nextInt();
-        input.nextLine();
-        int e = input.nextInt();
-        int f = input.nextInt();
-        int g = input.nextInt();
+        int q = input.nextInt();
+        for (int i = 0; i < q; i++)
+        {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int n = input.nextInt();
+            series(a, b, n);
+        }
+        
         input.close();
-        series(a, b, c, d);
-        series(e, f, g, 0);
     }
-    private static void series(int a, int b, int c, int d)
+    
+    private static void series(int a, int b, int n)
     {
-        int n = d;
         int current = a;
         for (int i = 0; i < n; i++) {
             current = current + (int) Math.pow(2, i) * b;
