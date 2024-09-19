318bed938ac8da995557f33cfa267fbf9fec9be4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-19 16:31:23 +0700

    Test

--- src/syafa/java/chapter01/JavaLoops02.java
+++ src/syafa/java/chapter01/JavaLoops02.java
@@ -8,14 +8,18 @@ public class JavaLoops02
          * (a + b^c * d) * e
          */
         Scanner input = new Scanner(System.in);
-        int a = Integer.parseInt(input.nextLine());
-        int b = input.nextInt();
-        int c = input.nextInt();
-        int d = input.nextInt();
-        input.nextLine();
-        int e = input.nextInt();
-        int f = input.nextInt();
-        int g = input.nextInt();
+        //int a = Integer.parseInt(input.nextLine());
+        //int b = input.nextInt();
+        //int c = input.nextInt();
+        //int d = input.nextInt();
+        //input.nextLine();
+        //int e = input.nextInt();
+        //int f = input.nextInt();
+        //int g = input.nextInt();
         input.close();
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println("Test");
+        }
     }
 }
