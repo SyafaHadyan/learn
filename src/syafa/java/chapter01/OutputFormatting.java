0f844d92a8d3bd0cc784dfd39aa318e65b97237f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-19 15:58:52 +0700

    Scanner initialization

---
+++ src/syafa/java/chapter01/OutputFormatting.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class OutputFormatting
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String first_string = input.next();
+        int first_int = input.nextInt();
+        input.nextLine();
+        String second_string = input.next();
+        int second_int = input.nextInt();
+        input.nextLine();
+        String third_string = input.next();
+        int third_int = input.nextInt();
+        input.nextLine();
+    }
+}
