1a869e8d9ffbcf5b9aca601209d6d92d660a3a68
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-20 07:40:05 +0700

    Initialize input

---
+++ src/java/syafa/java/section0/JavaDateAndTime.java
@@ -0,0 +1,13 @@
+package syafa.java.section0;
+import java.util.*;
+
+public class JavaDateAndTime
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int month = input.nextInt();
+        int day = input.nextInt();
+        int year = input.nextInt();
+    }
+}
