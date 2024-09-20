0438fe5f7c3cbf9f3da90d30c38fca5721b677d2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-20 07:32:16 +0700

    Add scanner close and fix tab space

--- src/java/syafa/java/section0/SimpleCalculator.java
+++ src/java/syafa/java/section0/SimpleCalculator.java
@@ -3,21 +3,21 @@ import java.util.*;
 
 public class SimpleCalculator
 {
-    public static void main(String[] args)
-    {
+	public static void main(String[] args)
+	{
 		Scanner input = new Scanner(System.in);
-        System.out.printf("%-25s%-3s","Enter first number",":");
-        double first = Double.parseDouble(input.nextLine());
-        System.out.printf("%-25s%-3s","Enter second number",":");
-        double second = Double.parseDouble(input.nextLine());
-        double add = first + second;
-        double substract = first - second;
-        double multiply = first * second;
-        double divide = first / second;
-
-        System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," + ",second," = ",add);
-        System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," - ",second," = ",substract);
-        System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," x ",second," = ",multiply);
-        System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," / ",second," = ",divide);
+		System.out.printf("%-25s%-3s","Enter first number",":");
+		double first = Double.parseDouble(input.nextLine());
+		System.out.printf("%-25s%-3s","Enter second number",":");
+		double second = Double.parseDouble(input.nextLine());
+		double add = first + second;
+		double substract = first - second;
+		double multiply = first * second;
+		double divide = first / second;
+		input.close();
+		System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," + ",second," = ",add);
+		System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," - ",second," = ",substract);
+		System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," x ",second," = ",multiply);
+		System.out.printf("%-5.0f%-5s%-5.0f%s%.8f",first," / ",second," = ",divide);
 	}
 }
