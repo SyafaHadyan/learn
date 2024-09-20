ecc5c076396e8cba8e8284d861e6e61be1760fab
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-20 18:31:26 +0700

    Test

--- src/java/syafa/java/section0/SLWhileLoops.java
+++ src/java/syafa/java/section0/SLWhileLoops.java
@@ -7,6 +7,7 @@ public class SLWhileLoops
     {
         Scanner input = new Scanner(System.in);
         int number = 0;
+        int factorial;
         int factorial_array[] = new int[number - 1];
         for (int i = 0; i < number; i++)
         {
@@ -15,8 +16,9 @@ public class SLWhileLoops
         }
         for (int i = 0; i < factorial_array.length; i++)
         {
-            
+            factorial = i + 1;
         }
+        System.out.println(factorial);
         input.close();
     }
 }
