0543f80b5979360aa7dbe8a0022e83d5d4d22bee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-20 18:22:08 +0700

    Initialize input

---
+++ src/java/syafa/java/section0/SLWhileLoops.java
@@ -0,0 +1,12 @@
+package syafa.java.section0;
+import java.util.*;
+
+public class SLWhileLoops
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number = Integer.parseInt(input.nextLine());
+        input.close();
+    }
+}
