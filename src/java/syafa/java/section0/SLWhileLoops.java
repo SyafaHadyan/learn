5048f218fbe88b7407984936970e5b5b0eb667b4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-20 18:29:32 +0700

    Renamed variable

--- src/java/syafa/java/section0/SLWhileLoops.java
+++ src/java/syafa/java/section0/SLWhileLoops.java
@@ -7,11 +7,15 @@ public class SLWhileLoops
     {
         Scanner input = new Scanner(System.in);
         int number = 0;
-        int factorial[] = new int[number - 1];
+        int factorial_array[] = new int[number - 1];
         for (int i = 0; i < number; i++)
         {
             number = Integer.parseInt(input.nextLine());
-            factorial[i] = i + 1;
+            factorial_array[i] = i + 1;
+        }
+        for (int i = 0; i < factorial_array.length; i++)
+        {
+            
         }
         input.close();
     }
