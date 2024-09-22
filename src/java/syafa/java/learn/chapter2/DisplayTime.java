39d7e912f46d46ffda46838f321d2f2f64c4d75e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 06:35:23 +0700

    Initialize input

---
+++ src/java/syafa/java/learn/chapter2/DisplayTime.java
@@ -0,0 +1,11 @@
+package syafa.java.learn.chapter2;
+import java.util.*;
+
+public class DisplayTime
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int initialTime = Integer.parseInt(input.nextLine());
+    }
+}
