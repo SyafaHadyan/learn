235225407fbb664f8175606c7746b4eb48cad322
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 06:39:48 +0700

    Prompt user and begin calculation

--- src/java/syafa/java/learn/chapter2/DisplayTime.java
+++ src/java/syafa/java/learn/chapter2/DisplayTime.java
@@ -6,6 +6,9 @@ public class DisplayTime
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Enter time (in second(s))",':');
         int initialTime = Integer.parseInt(input.nextLine());
+        input.close();
+        int minute = initialTime / 60;
     }
 }
