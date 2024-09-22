cba351c8acc73ea31bdf80adcfcf2f149820a21b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 06:58:13 +0700

    Prompt user

--- src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
+++ src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
@@ -5,8 +5,11 @@ public class NilaiMatakuliah
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan nilai tugas",':');
         double nilaiTugas = Double.parseDouble(input.nextLine());
+        System.out.printf("%-30s%-2c","Masukkan nilai kuis",':');
         double nilaiKuis = Double.parseDouble(input.nextLine());
+        System.out.printf("%-30s%-2c","Masukkan nilai UAS",':');
         double nilaiUAS = Double.parseDouble(input.nextLine());
         input.close();
     }
