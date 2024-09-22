7ec12198c829a02ff412f22677bb283179826ea7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-22 20:34:57 +0700

    Edit

---
+++ src/java/syafa/java/learn/chapter2/CalculateAverage.java
@@ -0,0 +1,10 @@
+package syafa.java.learn.chapter2;
+import java.util.*;
+
+public class CalculateAverage
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+    }
+}
--- src/java/syafa/java/learn/chapter2/ComputeAverage.java
+++
@@ -1,8 +0,0 @@
-package syafa.java.learn.chapter2;
-import java.util.*;
-
-public class ComputeAverage
-{
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter first number" + ':' + ' ');
-}
