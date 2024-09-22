b37933adc3b328e17e0c208c34da9b36732ba973
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-22 21:06:47 +0700

    FixError2.5

---
+++ src/java/syafa/java/learn/chapter2/FixError2_5.java
@@ -0,0 +1,11 @@
+package syafa.java.learn.chapter2;
+
+public class FixError2_5
+{
+    public static void main(String[] args)
+    {
+        int k = 2;
+        int i = k + 2;
+        System.out.println(i);
+    }
+}
