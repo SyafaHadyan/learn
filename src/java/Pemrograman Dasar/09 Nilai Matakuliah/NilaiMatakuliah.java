8a57f02c95e3ea3db2a44c0271e7189bc2d11ca3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 06:55:16 +0700

    Input

--- src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
+++ src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
@@ -4,6 +4,10 @@ public class NilaiMatakuliah
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        double nilaiTugas = Double.parseDouble(input.nextLine());
+        double nilaiKuis = Double.parseDouble(input.nextLine());
+        double nilaiUAS = Double.parseDouble(input.nextLine());
+        input.close();
     }
 }
