8cd0cc887f776a5fba68ec95239e970243a04fab
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-22 21:10:06 +0700

    Fixed error

--- src/java/syafa/java/learn/chapter2/FixError2_6.java
+++ src/java/syafa/java/learn/chapter2/FixError2_6.java
@@ -4,6 +4,8 @@ public class FixError2_6
 {
     public static void main(String[] args)
     {
+        int k;
+        int j;
         int i = j = k = 2;
         System.out.println(i + " " + j + " " + k);
     }
