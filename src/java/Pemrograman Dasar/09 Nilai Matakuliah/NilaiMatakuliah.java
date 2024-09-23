fc86218370bc0f2eb59c83e980a1e6dafa83b62e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 07:01:52 +0700

    Finished calculation

--- src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
+++ src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
@@ -12,5 +12,13 @@ public class NilaiMatakuliah
         System.out.printf("%-30s%-2c","Masukkan nilai UAS",':');
         double nilaiUAS = Double.parseDouble(input.nextLine());
         input.close();
+        if (nilaiKuis > nilaiUAS)
+        {
+            double nilaiAkhir = (40 / 100 * nilaiKuis) + (40 / 100 * nilaiUAS) + (20 / 100 * nilaiTugas);
+        }
+        else if (nilaiUAS >= nilaiKuis)
+        {
+            double nilaiAkhir = (30 / 100 * nilaiKuis) + (50 / 100 * nilaiUAS) + (20 / 100 * nilaiTugas);
+        }
     }
 }
