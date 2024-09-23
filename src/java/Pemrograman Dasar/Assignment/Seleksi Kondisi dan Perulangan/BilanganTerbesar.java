33f6c313b1b08ee5cd5f247afff7f6eb734b3610
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 20:26:35 +0700

    Initalize input

--- src/java/Pemrograman Dasar/Assignment/Seleksi Kondisi dan Perulangan/BilanganTerbesar.java
+++ src/java/Pemrograman Dasar/Assignment/Seleksi Kondisi dan Perulangan/BilanganTerbesar.java
@@ -4,6 +4,9 @@ public class BilanganTerbesar
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int bilanganPertama = Integer.parseInt(input.nextLine());
+        int bilanganKedua = Integer.parseInt(input.nextLine());
+        int bilanganKetiga = Integer.parseInt(input.nextLine());
     }
 }
