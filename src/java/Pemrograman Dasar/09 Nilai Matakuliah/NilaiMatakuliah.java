da9ee889a68cb27caf9e76f2ec5acfb6912ad1fb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 07:11:57 +0700

    Edit input

--- src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
+++ src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
@@ -5,14 +5,26 @@ public class NilaiMatakuliah
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.printf("%-30s%-2c","Masukkan nilai tugas",':');
-        double nilaiTugas = Double.parseDouble(input.nextLine());
-        System.out.printf("%-30s%-2c","Masukkan nilai kuis",':');
-        double nilaiKuis = Double.parseDouble(input.nextLine());
-        System.out.printf("%-30s%-2c","Masukkan nilai UAS",':');
-        double nilaiUAS = Double.parseDouble(input.nextLine());
-        input.close();
+        boolean cekNilai = true;
         double nilaiAkhir = 0;
+        while (cekNilai)
+        {
+            System.out.printf("%-30s%-2c","Masukkan nilai tugas",':');
+            double nilaiTugas = Double.parseDouble(input.nextLine());
+            System.out.printf("%-30s%-2c","Masukkan nilai kuis",':');
+            double nilaiKuis = Double.parseDouble(input.nextLine());
+            System.out.printf("%-30s%-2c","Masukkan nilai UAS",':');
+            double nilaiUAS = Double.parseDouble(input.nextLine());
+            input.close();
+            if (nilaiTugas <= 100 && nilaiKuis <= 100 && nilaiUAS <= 100)
+            {
+                cekNilai = false;
+            }
+            else
+            {
+                System.out.println("Nilai tidak valid, harap masukkan ulang nilai");
+            }
+        }
         if (nilaiKuis > nilaiUAS)
         {
             nilaiAkhir = (nilaiKuis * 40 / 100) + (nilaiUAS * 40 / 100) + (nilaiTugas * 20 / 100);
