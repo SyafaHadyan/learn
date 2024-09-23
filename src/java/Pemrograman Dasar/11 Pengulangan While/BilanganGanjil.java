e774d26a26175e58fc1fbeaa44ef10a6d98dfd88
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 06:53:35 +0700

    Prompt user

--- src/java/Pemrograman Dasar/11 Pengulangan While/BilanganGanjil.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/BilanganGanjil.java
@@ -5,7 +5,9 @@ public class BilanganGanjil
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-25s%-2c","Masukkan bilangan awal",':');
         int bilnanganAwal = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan batas bilangan",':');
         int batasBilangan = Integer.parseInt(input.nextLine());
         input.close();
     }
