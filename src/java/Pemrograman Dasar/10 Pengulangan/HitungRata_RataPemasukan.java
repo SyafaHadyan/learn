924707fdb667c0cd627975983a7f8162f24f8d99
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 08:09:33 +0700

    Edit

--- src/java/Pemrograman Dasar/10 Pengulangan/HitungRata_RataPemasukan.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/HitungRata_RataPemasukan.java
@@ -7,9 +7,13 @@ public class HitungRata_RataPemasukan
         Scanner input = new Scanner(System.in);
         System.out.printf("%-30s%-2c","Masukkan jumlah pemasukan",':');
         int jumlahPemasukan = Integer.parseInt(input.nextLine());
+        double pemasukan = 0;
+        double totalPemasukan = 0;
+        double rataRata = 0;
         for (int i = jumlahPemasukan; i <= jumlahPemasukan; i++)
         {
-            
+            pemasukan = Double.parseDouble(input.nextLine());
+            totalPemasukan = totalPemasukan + pemasukan;
         }
     }
 }
