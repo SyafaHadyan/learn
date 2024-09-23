028f0a2ca44fcc2d5ba7ac1d7f3db1d5d6d8647b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 17:06:59 +0700

    User prompt

--- src/java/Pemrograman Dasar/10 Pengulangan/DeretAritmatika.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/DeretAritmatika.java
@@ -5,8 +5,11 @@ public class DeretAritmatika
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-25s%-2c","Masukkan suku pertama",':');
         int sukuPertama = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan beda",':');
         int beda = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan batas deret",':');
         int batasDeret = Integer.parseInt(input.nextLine());
         input.close();
     }
