e002f6099e9dfaacf809843aacb9d48eb503430a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 08:13:58 +0700

    Edit

--- src/java/Pemrograman Dasar/10 Pengulangan/HitungRata_RataPemasukan.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/HitungRata_RataPemasukan.java
@@ -10,11 +10,13 @@ public class HitungRata_RataPemasukan
         double pemasukan = 0;
         double totalPemasukan = 0;
         double rataRata = 0;
-        for (int i = jumlahPemasukan; i <= jumlahPemasukan; i++)
+        for (int i = 1; i <= jumlahPemasukan; i++)
         {
+            System.out.printf("%-30s%d%-2c","Pemasukan ke ",i,':');
             pemasukan = Double.parseDouble(input.nextLine());
             totalPemasukan = totalPemasukan + pemasukan;
         }
+        input.close();
         rataRata = totalPemasukan / jumlahPemasukan;
         System.out.printf("%-30s%-2c%s%.2f","Total pemasukan",':',"Rp ",totalPemasukan);
         System.out.printf("%-30s%-2c%s%.2f","Rata-rata pemasukan",':',"Rp ",rataRata);
