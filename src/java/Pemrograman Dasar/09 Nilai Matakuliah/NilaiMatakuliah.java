87d4c6d98363b05890ea02e8ec38be90ed1defac
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 07:35:50 +0700

    Edit

--- src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
+++ src/java/Pemrograman Dasar/09 Nilai Matakuliah/NilaiMatakuliah.java
@@ -19,7 +19,6 @@ public class NilaiMatakuliah
             nilaiKuis = Double.parseDouble(input.nextLine());
             System.out.printf("%-30s%-2c","Masukkan nilai UAS",':');
             nilaiUAS = Double.parseDouble(input.nextLine());
-            double ipk = Double.parseDouble(input.nextLine());
             if (nilaiTugas <= 100 && nilaiKuis <= 100 && nilaiUAS <= 100)
             {
                 cekNilai = false;
