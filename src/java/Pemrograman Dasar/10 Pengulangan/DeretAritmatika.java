fc064e2086dcf38bacabf789bdf3ea4aa4b323bb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 17:05:27 +0700

    Initialize input

--- src/java/Pemrograman Dasar/10 Pengulangan/DeretAritmatika.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/DeretAritmatika.java
@@ -4,6 +4,10 @@ public class DeretAritmatika
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int sukuPertama = Integer.parseInt(input.nextLine());
+        int beda = Integer.parseInt(input.nextLine());
+        int batasDeret = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
