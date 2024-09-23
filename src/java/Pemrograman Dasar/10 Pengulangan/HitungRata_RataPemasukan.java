fcbfae83dfa8cb24bf625cb9f74ad1a0eaecca43
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 08:04:52 +0700

    Initialize

---
+++ src/java/Pemrograman Dasar/10 Pengulangan/HitungRata_RataPemasukan.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class HitungRata_RataPemasukan
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
