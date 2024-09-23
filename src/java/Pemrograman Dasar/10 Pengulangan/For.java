5a13a6d10fa1e53d2ef9a928b65bae9d973a87db
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 07:58:29 +0700

    Added sum of printed numbers

--- src/java/Pemrograman Dasar/10 Pengulangan/For.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/For.java
@@ -7,10 +7,13 @@ public class For
         Scanner input = new Scanner(System.in);
         System.out.printf("%-30s%-2c","Masukkan jumlah pengulangan",':');
         int jumlahPengulangan = Integer.parseInt(input.nextLine());
+        int total = 0;
         input.close();
         for (int i = 1; i <= jumlahPengulangan; i++)
         {
             System.out.println(i);
+            total = total + i;
         }
+        System.out.printf("%-7s%-2c%d","Total",':',total);
     }
 }
