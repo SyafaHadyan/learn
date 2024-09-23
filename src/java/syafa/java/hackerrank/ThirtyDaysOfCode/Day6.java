e5ead407c9b05ae667a2130229cf288b82958553
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 06:42:23 +0700

    Input

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6.java
@@ -5,6 +5,11 @@ public class Day6
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < args.length; i++)
+        {
+            String str = input.nextLine();
+        }
     }
 }
