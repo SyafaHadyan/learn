9017c718cf5d191fc2db1c3a1f8637697c5b556b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 07:51:09 +0700

    Finished

--- src/java/Pemrograman Dasar/10 Pengulangan/For.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/For.java
@@ -5,6 +5,7 @@ public class For
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan jumlah pengulangan",':');
         int jumlahPengulangan = Integer.parseInt(input.nextLine());
         input.close();
         for (int i = 1; i <= jumlahPengulangan; i++)
