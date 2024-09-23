afd775c951076dcb893c0b8c4a88a8b94f1a93a4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 17:14:19 +0700

    Initialize input

--- src/java/Pemrograman Dasar/10 Pengulangan/DeretGeometri.java
+++ src/java/Pemrograman Dasar/10 Pengulangan/DeretGeometri.java
@@ -4,6 +4,13 @@ public class DeretGeometri
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%-25s%-2c","Masukkan suku pertama",':');
+        int sukuPertama = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan rasio",':');
+        int rasio = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan batas deret",':');
+        int batasDeret = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
