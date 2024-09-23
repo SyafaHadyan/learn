055d09b8c4d5d78211439d204627040d160ffac2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-23 16:55:45 +0700

    Finished

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day7.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day7.java
@@ -7,16 +7,12 @@ public class Day7
     {
         Scanner input = new Scanner(System.in);
         int arrayLength = Integer.parseInt(input.nextLine());
-        int numberArray[] = new int [arrayLength];
-        for (int i = 0; i < arrayLength; i++)
-        {
-            int number = Integer.parseInt(input.nextLine());
-            numberArray[i] = number;
-        }
+        String inputArrays = input.nextLine();
+        String[] numberArray = inputArrays.split(" ");
         input.close();
         for (int i = 1; i <= numberArray.length ; i++)
         {
-            int numberArrayAt = numberArray[arrayLength - i];
+            String numberArrayAt = numberArray[arrayLength - i];
             System.out.println(numberArrayAt);
         }
     }
