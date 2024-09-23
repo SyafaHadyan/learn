412b2a2787a740ac55a508c8dffc99545dcce4fa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 06:52:05 +0700

    Initialize input

--- src/java/Pemrograman Dasar/11 Pengulangan While/BilanganGanjil.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/BilanganGanjil.java
@@ -4,6 +4,9 @@ public class BilanganGanjil
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int bilnanganAwal = Integer.parseInt(input.nextLine());
+        int batasBilangan = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
