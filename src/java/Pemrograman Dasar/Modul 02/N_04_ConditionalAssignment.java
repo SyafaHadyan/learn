c5df08a391bfe524bfb7e774d7432354ff8d7208
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 15:52:35 +0700

    Edit output spacing

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -5,14 +5,14 @@ public class N_04_ConditionalAssignment
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.printf("%-25s%-2c","Masukkan nama anda",':');
+        System.out.printf("%-30s%-2c","Masukkan nama anda",':');
         String nama = input.nextLine();
-        System.out.printf("%-25s%-2c","Masukkan password anda",':');
+        System.out.printf("%-30s%-2c","Masukkan password anda",':');
         String password = input.nextLine();
         boolean dataValid = false;
-        System.out.printf("%-25s%-2c","Masukkan ulang nama anda",':');
+        System.out.printf("%-30s%-2c","Masukkan ulang nama anda",':');
         String cekNama = input.nextLine();
-        System.out.printf("%-25s%-2c","Masukkan ulang password anda",':');
+        System.out.printf("%-30s%-2c","Masukkan ulang password anda",':');
         String cekPassword = input.nextLine();
         if (nama == cekNama && password == cekPassword)
         {
