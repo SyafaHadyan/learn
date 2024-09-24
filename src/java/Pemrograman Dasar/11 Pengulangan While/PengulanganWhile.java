1d334ad39d1a264de6b676c6661065d2768427b3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 07:39:30 +0700

    Prompt user

--- src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
@@ -5,13 +5,15 @@ public class PengulanganWhile
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan angka awal",':');
         int angkaAwal = Integer.parseInt(input.nextLine());
+        System.out.printf("%-30s%-2c","Masukkan angka maksimal",':');
         int angkaMaksimal = Integer.parseInt(input.nextLine());
         input.close();
         while (angkaAwal <= angkaMaksimal)
         {
             System.out.println(angkaAwal);
-            angkaAwal++
+            angkaAwal++;
         }
     }
 }
