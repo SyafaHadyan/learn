f0c5f1c5218ab8235696e594887888659b8fd79b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 07:14:39 +0700

    Prompt user

--- src/java/Pemrograman Dasar/11 Pengulangan While/HitunganHari.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/HitunganHari.java
@@ -5,6 +5,7 @@ public class HitunganHari
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan jumlah hari",':');
         int jumlahHari = Integer.parseInt(input.nextLine());
         input.close();
     }
