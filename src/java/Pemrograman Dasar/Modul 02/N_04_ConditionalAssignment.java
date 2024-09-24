21b9d48d9915b91c334bcf2fde59cd2d14b9a4f7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 15:39:16 +0700

    Initialize input

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -4,6 +4,9 @@ public class N_04_ConditionalAssignment
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String nama = input.nextLine();
+        String password = input.nextLine();
+        input.close();
     }
 }
