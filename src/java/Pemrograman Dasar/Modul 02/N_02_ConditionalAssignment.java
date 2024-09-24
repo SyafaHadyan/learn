66d6eaaf55f975f31460e81ee6d1d7c23d0d4e16
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 14:55:09 +0700

    Add 02

---
+++ src/java/Pemrograman Dasar/Modul 02/N_02_ConditionalAssignment.java
@@ -0,0 +1,10 @@
+public class N_02_ConditionalAssignment
+{
+    public static void main(String[] args)
+    {
+        String s = "filkom";
+        //int val = (s=="filkom")?"Brawijaya": "null";
+        // Error: Tidak bisa mengubah String menjadi integer.
+        //System.out.println(s+" "+val);
+    }
+}
