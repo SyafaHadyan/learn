bd43969e2185b4c1b9e93680f08de0413ac5fb09
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 07:37:02 +0700

    Initialize input

--- src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
@@ -4,6 +4,8 @@ public class PengulanganWhile
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int angka = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
