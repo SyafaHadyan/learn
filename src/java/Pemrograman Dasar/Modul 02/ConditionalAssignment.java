7d096c8de359d5b1295c3575476cfadb65b8ef07
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 14:48:21 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/ConditionalAssignment.java
@@ -1,13 +1,9 @@
-import java.util.*;
-
 public class ConditionalAssignment
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        String s = input.nextLine();
-        input.close();
-        String val = (s.equalsIgnoreCase("Filkom"))? "Brawijaya" : "null";
-        System.out.println(s + ' ' + val);
+        String s = "filkom";
+        String val = (s=="filkom")?"Brawijaya": "null";
+        System.out.println(s+" "+val);
     }
 }
