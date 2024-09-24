ab428855e46659c86755d8dc6315b5a0707cf7d5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 06:27:00 +0700

    Prompt user to enter score

---
+++ src/java/Pemrograman Dasar/Modul 02/N_06_IfElse.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; public class N_06_IfElse
+{
+    public static void main(String[] args)
+    {
+        boolean ulang = true;
+        while (ulang)
+        {
+            Scanner in = new Scanner(System.in); System.out.print("masukkan nilai: ");
+            System.out.printf("%-30s%-2c","Masukkan nilai anda (-1) untuk keluar",':');
+            int nilai = in.nextInt();
+            if (nilai > 60)
+            {
+                System.out.println("Anda lulus");
+            }
+            else if (nilai >= 40)
+            {
+                System.out.println("Anda harus mengulang !");
+            }
+            else if (nilai == -1)
+            {
+                ulang = false;
+                in.close();
+            }
+            else
+            {
+                System.out.println("Anda gagal");
+            }
+        }
+    }
+}
