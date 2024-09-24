09f6baa6dabfaf05377a550fd018acfd2e4fba0a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 07:43:52 +0700

    Add sum of printed numbers

--- src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/PengulanganWhile.java
@@ -5,15 +5,18 @@ public class PengulanganWhile
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.printf("%-30s%-2c","Masukkan angka awal",':');
+        System.out.printf("%-25s%-2c","Masukkan angka awal",':');
         int angkaAwal = Integer.parseInt(input.nextLine());
-        System.out.printf("%-30s%-2c","Masukkan angka maksimal",':');
+        System.out.printf("%-25s%-2c","Masukkan angka maksimal",':');
         int angkaMaksimal = Integer.parseInt(input.nextLine());
         input.close();
+        int totalAngka = 0;
         while (angkaAwal <= angkaMaksimal)
         {
             System.out.println(angkaAwal);
+            totalAngka += angkaAwal;
             angkaAwal++;
         }
+        System.out.printf("%-25s%-2c%d","Total angka",':',totalAngka);
     }
 }
