c5c5755610a1f5aa6bf0e341c3e65a24cede0fe1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 14:59:28 +0700

    Initialize scanner

---
+++ src/java/Pemrograman Dasar/Modul 02/N_03_ConditionalAssignment.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class N_03_ConditionalAssignment
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        boolean cekInput = true;
+        String s = "filkom";
+        String val = (s=="filkom")?"Brawijaya": "null";
+        System.out.println(s+" "+val);
+
+    }
+}
