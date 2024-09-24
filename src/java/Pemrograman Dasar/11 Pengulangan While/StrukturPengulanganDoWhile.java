d75c86b0eee399dd2a57be060fcc97a92c38ec7d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 07:50:24 +0700

    Initialize input

--- src/java/Pemrograman Dasar/11 Pengulangan While/StrukturPengulanganDoWhile.java
+++ src/java/Pemrograman Dasar/11 Pengulangan While/StrukturPengulanganDoWhile.java
@@ -4,6 +4,11 @@ public class StrukturPengulanganDoWhile
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%-25s%-2c","Masukkan angka awal",':');
+        int angkaAwal = Integer.parseInt(input.nextLine());
+        System.out.printf("%-25s%-2c","Masukkan angka maksimal",':');
+        int angkaMaksimal = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
