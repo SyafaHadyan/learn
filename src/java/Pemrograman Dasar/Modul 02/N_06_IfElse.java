2b2cfacbe31820d7267472b8001acdef2bcaae8e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 06:29:16 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/N_06_IfElse.java
+++ src/java/Pemrograman Dasar/Modul 02/N_06_IfElse.java
@@ -7,6 +7,7 @@ import java.util.Scanner; public class N_06_IfElse
         {
             Scanner in = new Scanner(System.in);
             System.out.printf("%-30s%-2c","Masukkan nilai anda (-1) untuk keluar",':');
+            int nilai = in.nextInt();
             /*
             try
             {
