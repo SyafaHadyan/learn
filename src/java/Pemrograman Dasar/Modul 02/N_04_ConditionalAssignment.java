c56e5b145d5ac9f69008ac251600f82e4380f26b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 15:55:49 +0700

    Move

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -9,12 +9,12 @@ public class N_04_ConditionalAssignment
         String nama = input.nextLine();
         System.out.printf("%-30s%-2c","Masukkan password anda",':');
         String password = input.nextLine();
-        boolean dataValid = false;
         System.out.printf("\n%-30s%-2c","Masukkan ulang nama anda",':');
         String cekNama = input.nextLine();
         System.out.printf("%-30s%-2c","Masukkan ulang password anda",':');
         String cekPassword = input.nextLine();
         input.close();
+        boolean dataValid = false;
         if (nama == cekNama && password == cekPassword)
         {
             dataValid = true;
