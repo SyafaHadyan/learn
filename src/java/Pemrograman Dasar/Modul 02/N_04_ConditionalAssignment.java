2dc29992fe5b1d1c87ef28c6abc3556ade6481f2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 15:42:16 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -5,13 +5,14 @@ public class N_04_ConditionalAssignment
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-25s%-2c","Masukkan nama anda",':');
+        String nama = input.nextLine();
+        System.out.printf("%-25s%-2c","Masukkan password anda",':');
+        String password = input.nextLine();
         boolean cekInput = true;
         while (cekInput)
         {
-            System.out.printf("%-25s%-2c","Masukkan nama anda",':');
-            String nama = input.nextLine();
-            System.out.printf("%-25s%-2c","Masukkan password anda",':');
-            String password = input.nextLine();
+            
         }
     }
 }
