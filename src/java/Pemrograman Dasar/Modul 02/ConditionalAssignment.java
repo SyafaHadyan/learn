9bca0c045e478048fd158c0f638475e0850f2282
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-24 14:42:00 +0700

    Initial file

---
+++ src/java/Pemrograman Dasar/Modul 02/ConditionalAssignment.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ConditionalAssignment
+{
+    public static void main(String[] args)
+    {
+        String s = "filkom";
+        String val = (s=="filkom")?"Brawijaya": "null";
+        System.out.println(s+" "+val);
+    }
+}
