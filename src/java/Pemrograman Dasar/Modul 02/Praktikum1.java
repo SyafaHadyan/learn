53a3e182a855222ad2963823841d4f1eba389d22
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 18:45:23 +0700

    Initialize input

--- src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Praktikum1
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        double beratBadan = Double.parseDouble(input.nextLine());
+        double tinggiBadan = Double.parseDouble(input.nextLine());
     }
 }
