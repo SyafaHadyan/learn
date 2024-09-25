5a7d7c58e0f6879ba7dc3f45b756d2a0d21f0f0a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 18:47:26 +0700

    Prompt user

--- src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
@@ -4,7 +4,10 @@ public class Praktikum1
 {
     public static void main(String[] args)
     {
+        String unitBerat = "(kg)";
+        String unitTinggi = "(m)";
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-17s%-2c","Berat badan",' ',unitBerat,':');
         double beratBadan = Double.parseDouble(input.nextLine());
         double tinggiBadan = Double.parseDouble(input.nextLine());
     }
