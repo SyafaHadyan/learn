27283f7eb54c92f94a8355f2441973e4cddd955f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 18:50:26 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
@@ -4,18 +4,19 @@ public class Praktikum1
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         String unitBerat = "(kg)";
         String unitTinggi = "(m)";
         String outBerat = "Berat badan" + ' ' + unitBerat;
         String outTinggi = "Tinggi badan" + ' ' + unitTinggi;
         boolean ulang = true;
-        Scanner input = new Scanner(System.in);
         while (ulang)
         {
             System.out.printf("%-17s%-2c",outBerat,':');
             double beratBadan = Double.parseDouble(input.nextLine());
             System.out.printf("%-17s%-2c",outTinggi,':');
             double tinggiBadan = Double.parseDouble(input.nextLine());
+            
         }
     }
 }
