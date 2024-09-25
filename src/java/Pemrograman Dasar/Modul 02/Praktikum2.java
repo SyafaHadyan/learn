6edbc426693cca813f75380a1859186016a78c37
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 20:21:46 +0700

    Move input to loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -5,9 +5,14 @@ public class Praktikum2
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int jamKerja = Integer.parseInt(input.nextLine());
+        int jamKerja = 0;
         int upah = 0;
         int lembur = 0;
         int denda = 0;
+        input.close();
+        while (true)
+        {
+            jamKerja = Integer.parseInt(input.nextLine());
+        }
     }
 }
