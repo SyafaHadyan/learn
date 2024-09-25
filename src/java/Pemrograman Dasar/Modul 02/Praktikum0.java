fcc65cbb5a46875ef76058e1c9e8a7fa04638a82
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 11:30:40 +0700

    input loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
@@ -5,11 +5,6 @@ public class Praktikum0
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("1. menghitung luas dan keliling persegi panjang");
-        System.out.println("2. menghitung luas dan keliling lingkaran");
-        System.out.println("3`. menghitung luas dan keliling segitiga");
-        System.out.printf("%s%-2c","Pilihan anda",':');
-        int pilihanKonversi = Integer.parseInt(input.nextLine());
         double hasilKeliling = 0;
         double hasilLuas = 0;
         String namaPilihan = "Bentuk";
@@ -18,40 +13,48 @@ public class Praktikum0
         final String unitLuas = "cm2"; // Ganti untuk unit lain
         boolean outputHasilPerhitungan = true;
         boolean inputValid = true;
-        switch (pilihanKonversi)
+        while(inputValid)
         {
-            case 1:
-                namaPilihan = "persegi panjang";
-                System.out.printf("%s%-2c","Masukkan sisi 1",':');
-                int sisiSatu = Integer.parseInt(input.nextLine());
-                System.out.printf("%s%-2c","Masukkan sisi 2",':');
-                int sisiDua = Integer.parseInt(input.nextLine());
-                hasilKeliling = (sisiSatu * 2) + (sisiDua * 2);
-                hasilLuas = sisiSatu * sisiDua;
-                break;
-            case 2:
-                namaPilihan = "lingkaran";
-                System.out.printf("%s%-2c","Masukkan radius",':');
-                int radius = Integer.parseInt(input.nextLine());
-                hasilKeliling = 2 * PI * radius;
-                hasilLuas = radius * radius * PI;
-                break;
-            case 3:
-                namaPilihan = "segitiga";
-                System.out.printf("%s%-2c","Masukkan a",':');
-                int a = Integer.parseInt(input.nextLine());
-                System.out.printf("%s%-2c","Masukkan b",':');
-                int b = Integer.parseInt(input.nextLine());
-                System.out.printf("%s%-2c","Masukkan r",':');
-                int c = Integer.parseInt(input.nextLine());
-                hasilKeliling = a + b + c;
-                hasilLuas = a * b;
-                break;
-            default:
-                System.out.print("Data tak ditemukan, program dihentikan ...");
-                outputHasilPerhitungan = false;
-                inputValid = false;
-                break;
+            System.out.println("1. menghitung luas dan keliling persegi panjang");
+            System.out.println("2. menghitung luas dan keliling lingkaran");
+            System.out.println("3`. menghitung luas dan keliling segitiga");
+            System.out.printf("%s%-2c","Pilihan anda",':');
+            int pilihanKonversi = Integer.parseInt(input.nextLine());
+            switch (pilihanKonversi)
+            {
+                case 1:
+                    namaPilihan = "persegi panjang";
+                    System.out.printf("%s%-2c","Masukkan sisi 1",':');
+                    int sisiSatu = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan sisi 2",':');
+                    int sisiDua = Integer.parseInt(input.nextLine());
+                    hasilKeliling = (sisiSatu * 2) + (sisiDua * 2);
+                    hasilLuas = sisiSatu * sisiDua;
+                    break;
+                case 2:
+                    namaPilihan = "lingkaran";
+                    System.out.printf("%s%-2c","Masukkan radius",':');
+                    int radius = Integer.parseInt(input.nextLine());
+                    hasilKeliling = 2 * PI * radius;
+                    hasilLuas = radius * radius * PI;
+                    break;
+                case 3:
+                    namaPilihan = "segitiga";
+                    System.out.printf("%s%-2c","Masukkan a",':');
+                    int a = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan b",':');
+                    int b = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan r",':');
+                    int c = Integer.parseInt(input.nextLine());
+                    hasilKeliling = a + b + c;
+                    hasilLuas = a * b;
+                    break;
+                default:
+                    System.out.print("Data tak ditemukan, program dihentikan ...");
+                    outputHasilPerhitungan = false;
+                    inputValid = false;
+                    break;
+            }
         }
         if (outputHasilPerhitungan)
         {
