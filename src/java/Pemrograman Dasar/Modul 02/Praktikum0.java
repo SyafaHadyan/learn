271f8121ab5e4de9999734a90cf157c7853888c1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 11:01:00 +0700

    Prompt user

--- src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
@@ -5,6 +5,9 @@ public class Praktikum0
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.println("1. menghitung luas dan keliling persegi panjang");
+        System.out.println("1. menghitung luas dan keliling lingkaran");
+        System.out.println("1. menghitung luas dan keliling segitiga");
         int pilihanKonversi = Integer.parseInt(input.nextLine());
     }
 }
