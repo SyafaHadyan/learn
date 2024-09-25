2f7230ef9e23b51ae24806af3d873a932d224ec3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 08:42:16 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -15,15 +15,16 @@ public class N_04_ConditionalAssignment
         String cekPassword = input.nextLine();
         input.close();
         String headerFooter = "============================================================";
+        String sideBorder = "|";
         double nim = 245150207111047.0;
         double IPK = 3.98;
         if (nama.equals(cekNama) && password.equals(cekPassword))
         {
             System.out.printf("%.20s%c%s%c%.20s\n\n",headerFooter,' ',"Biodata Mahasiswa",' ',headerFooter);
-            System.out.printf("%-30s%-2c%s\n","Nama mahasiswa",':',nama);
-            System.out.printf("%-30s%-2c%s\n","Password",':',password);
-            System.out.printf("%-30s%-2c%.0f\n","NIM mahasiswa",':',nim);
-            System.out.printf("%-30s%-2c%.2f\n","IPK mahasiswa",':',IPK);
+            System.out.printf("%s%-30s%-2c%s\n",sideBorder,"Nama mahasiswa",':',nama);
+            System.out.printf("%s%-30s%-2c%s\n",sideBorder,"Password",':',password);
+            System.out.printf("%s%-30s%-2c%.0f\n",sideBorder,"NIM mahasiswa",':',nim);
+            System.out.printf("%s%-30s%-2c%.2f\n",sideBorder,"IPK mahasiswa",':',IPK);
             System.out.printf("\n%.59s\n",headerFooter);
         }
         else if (!nama.equals(cekNama) || !password.equals(cekPassword))
