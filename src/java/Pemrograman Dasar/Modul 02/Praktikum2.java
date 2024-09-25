69e005232bc80f9490b7427c77e7cfa720c53053
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 20:20:37 +0700

    Fix

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -5,7 +5,7 @@ public class Praktikum2
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int jamKerja = Integer.parseInt(input.nextLine())
+        int jamKerja = Integer.parseInt(input.nextLine());
         int upah = 0;
         int lembur = 0;
         int denda = 0;
