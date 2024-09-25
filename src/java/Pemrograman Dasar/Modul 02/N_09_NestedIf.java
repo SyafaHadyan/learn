a33dc579c363c2e634eba968671da35f18ffb8dd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 07:37:18 +0700

    Add comments

--- src/java/Pemrograman Dasar/Modul 02/N_09_NestedIf.java
+++ src/java/Pemrograman Dasar/Modul 02/N_09_NestedIf.java
@@ -18,7 +18,8 @@ public class N_09_NestedIf
             if (nilai % 2 == 1)
             {
                 System.out.println("UB");
-            } else
+            }
+            else
             {
                 System.out.println("Brawijaya");
             }
@@ -35,5 +36,18 @@ public class N_09_NestedIf
                 System.out.println("Brawijaya");
             }
         }
+        /*
+         * Input pertama: 5
+         * Nilai jika dikali 2 kurang dari 50, maka nilai = nilai + 10 = 15
+         * Nilai kurang dari sama dengan 20, kemudian print "Filkom", dan modulo pembagian dari nilai 15 bersisa 1, maka print "UB"
+         * 
+         * Input kedua: 20
+         * Nilai jika dikali 2 kurang dari 50, maka nilai = nilai + 10 = 30
+         * Nilai tidak kurang dari sama dengan 20, kemudian print "PTIIK" dan modulo pembagian dari nilai 30 tidak bersisa 1, maka print "Brawijaya"
+         * 
+         * Input ketiga: 30
+         * Nilai jika dikali 2 tidak kurang dari 50
+         * Nilai tidak kurang dari sama dengan 20, kemudian print "PTIIK" dan modulo pembagian dari nilai 50 tidak bersisa 1, maka print "Brawijaya"
+         */
     }
 }
