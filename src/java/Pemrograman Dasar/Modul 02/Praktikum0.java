0daa1a4c22c347b449104379cfdd028cea80e9dd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 11:00:03 +0700

    Initialize input

--- src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
@@ -1,7 +1,10 @@
+import java.util.*;
+
 public class Praktikum0
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int pilihanKonversi = Integer.parseInt(input.nextLine());
     }
 }
