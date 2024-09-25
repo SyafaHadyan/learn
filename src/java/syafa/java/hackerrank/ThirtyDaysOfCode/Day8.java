b300ad8a2b4ed36860fb940f07a795e7d980bb1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-26 06:27:32 +0700

    Initalize input

---
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8.java
@@ -0,0 +1,10 @@
+package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
+public class Day8
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int phoneBookInput = Integer.parseInt(input.nextLine());
+    }
+}
