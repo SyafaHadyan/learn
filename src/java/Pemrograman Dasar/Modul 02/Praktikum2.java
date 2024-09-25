f0106911704ec09afb76e82ee189c846557ea6f5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 20:32:14 +0700

    Remove

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -10,7 +10,6 @@ public class Praktikum2
         int lemburPerJam = 6000;
         int dendaPerJam = 0;
         int upahTotal = 0;
-        int lemburTotal = 0;
         int dendaTotal = 0;
         input.close();
         while (true)
