2756aa46189016a6cab4ad2d652ab477ded1f9c8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 08:56:24 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_04_ConditionalAssignment.java
@@ -4,28 +4,32 @@ public class N_04_ConditionalAssignment
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        System.out.printf("%-30s%-2c","Masukkan nama anda",':');
-        String nama = input.nextLine();
-        System.out.printf("%-30s%-2c","Masukkan password anda",':');
-        String password = input.nextLine();
-        System.out.printf("\n%-30s%-2c","Masukkan ulang nama anda",':');
-        String cekNama = input.nextLine();
-        System.out.printf("%-30s%-2c","Masukkan ulang password anda",':');
-        String cekPassword = input.nextLine();
-        input.close();
+        String nama = "";
+        String password = "";
+        String cekNama = "";
+        String cekPassword = "";
         String headerFooter = "============================================================";
         String sideBorder = "|";
         double nim = 245150207111047.0;
         double IPK = 3.98;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Masukkan nama anda",':');
+        nama = input.nextLine();
+        System.out.printf("%-30s%-2c","Masukkan password anda",':');
+        password = input.nextLine();
+        System.out.printf("\n%-30s%-2c","Masukkan ulang nama anda",':');
+        cekNama = input.nextLine();
+        System.out.printf("%-30s%-2c","Masukkan ulang password anda",':');
+        cekPassword = input.nextLine();
+        input.close();
         if (nama.equals(cekNama) && password.equals(cekPassword))
         {
             System.out.printf("\n%.20s%c%s%c%.20s\n",headerFooter,' ',"Biodata Mahasiswa",' ',headerFooter);
             System.out.printf("%s%58s\n",sideBorder,sideBorder);
-            System.out.printf("%-5s%58s%-30s%-2c%s%58s\n",sideBorder,sideBorder,"Nama mahasiswa",':',nama);
-            System.out.printf("%-5s%58s%-30s%-2c%s%58s\n",sideBorder,sideBorder,"Password",':',password);
-            System.out.printf("%-5s%58s%-30s%-2c%.0f%58s\n",sideBorder,sideBorder,"NIM mahasiswa",':',nim);
-            System.out.printf("%-5s%58s%-30s%-2c%.2f%58s\n",sideBorder,sideBorder,"IPK mahasiswa",':',IPK);
+            System.out.printf("%-5s%-30s%-2c%s%58s\n",sideBorder,"Nama mahasiswa",':',nama,sideBorder);
+            System.out.printf("%-5s%-30s%-2c%s%58s\n",sideBorder,"Password",':',password,sideBorder);
+            System.out.printf("%-5s%-30s%-2c%.0f%58s\n",sideBorder,"NIM mahasiswa",':',nim,sideBorder);
+            System.out.printf("%-5s%-30s%-2c%.2f%58s\n",sideBorder,"IPK mahasiswa",':',IPK,sideBorder);
             System.out.printf("%s%58s\n",sideBorder,sideBorder);
             System.out.printf("%.59s\n",headerFooter);
         }
