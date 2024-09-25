966c8839028d6ed3939a1256b8e9d14d31932837
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-26 06:30:21 +0700

    Create empty array

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8.java
@@ -6,6 +6,8 @@ public class Day8
     {
         Scanner input = new Scanner(System.in);
         int phoneBookInput = Integer.parseInt(input.nextLine());
+        String nameArray[] = new String[phoneBookInput];
+        int phoneNumber[] = new int[phoneBookInput];
         for (int i = 1; i <= phoneBookInput; i++)
         {
             String name = input.next();
