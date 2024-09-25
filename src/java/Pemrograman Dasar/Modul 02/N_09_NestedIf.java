f979ba4acaa64a0d12cb8c512c66f6aecbe106dd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 07:24:18 +0700

    Add file

---
+++ src/java/Pemrograman Dasar/Modul 02/N_09_NestedIf.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class N_09_NestedIf
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("masukkan nilai: ");
+        int nilai = in.nextInt();
+        in.close();
+        if (nilai * 2 < 50)
+        {
+            nilai += 10;
+        }
+        if (nilai <= 20)
+        {
+            System.out.println("Filkom");
+            if (nilai % 2 == 1)
+            {
+                System.out.println("UB");
+            } else
+            {
+                System.out.println("Brawijaya");
+            }
+        }
+        else
+        {
+            System.out.println("PTIIK");
+            if (nilai % 2 == 1)
+            {
+                System.out.println("UB");
+            }
+            else
+            {
+                System.out.println("Brawijaya");
+            }
+        }
+    }
+}
