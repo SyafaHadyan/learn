30f665539babe4d452b01d632bcc0f9767fd0f2f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 20:30:55 +0700

    Under 50 hours

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -8,6 +8,7 @@ public class Praktikum2
         int jamKerja = 0;
         int upahPerJam = 5000;
         int lemburPerJam = 6000;
+        int dendaPerJam = 0;
         int upahTotal = 0;
         int lemburTotal = 0;
         int dendaTotal = 0;
@@ -25,7 +26,7 @@ public class Praktikum2
             }
             else if (jamKerja < 50)
             {
-                
+                upahTotal = upahPerJam * jamKerja;
             }
         }
     }
