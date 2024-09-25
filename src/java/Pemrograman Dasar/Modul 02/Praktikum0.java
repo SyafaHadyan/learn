6cf1ae0f4ac4cc404cc80529d5fd3e53eaeef5ff
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 11:31:41 +0700

    Move option outside of loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
@@ -13,11 +13,11 @@ public class Praktikum0
         final String unitLuas = "cm2"; // Ganti untuk unit lain
         boolean outputHasilPerhitungan = true;
         boolean inputValid = true;
+        System.out.println("1. menghitung luas dan keliling persegi panjang");
+        System.out.println("2. menghitung luas dan keliling lingkaran");
+        System.out.println("3. menghitung luas dan keliling segitiga");
         while(inputValid)
         {
-            System.out.println("1. menghitung luas dan keliling persegi panjang");
-            System.out.println("2. menghitung luas dan keliling lingkaran");
-            System.out.println("3. menghitung luas dan keliling segitiga");
             System.out.printf("%s%-2c","Pilihan anda",':');
             int pilihanKonversi = Integer.parseInt(input.nextLine());
             switch (pilihanKonversi)
