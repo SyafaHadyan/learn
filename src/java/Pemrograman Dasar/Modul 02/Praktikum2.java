5e7950b0bf2d811d0d8dd5d33e5c3ab4eec92908
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-25 20:20:26 +0700

    Intialize input

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -4,6 +4,10 @@ public class Praktikum2
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int jamKerja = Integer.parseInt(input.nextLine())
+        int upah = 0;
+        int lembur = 0;
+        int denda = 0;
     }
 }
