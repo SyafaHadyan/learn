00eac7bf38d3f3ed8e548264e39ef2075cf3ffd8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-26 13:58:49 +0700

    Edit

--- src/java/Pemrograman Dasar/Modul 02/N_03_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_03_ConditionalAssignment.java
@@ -5,22 +5,18 @@ public class N_03_ConditionalAssignment
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        String s = "filkom";
-        String val = (s=="filkom")?"Brawijaya": "null";
+        String s = "Filkom";
+        String val = (s.equalsIgnoreCase("Filkom"))?"Brawijaya": "null";
         System.out.println(s+" "+val);
         boolean cekInput = true;
-        String namaMahasiswa = "Nama";
-        String nimMahasiswa = "0";
+        String namaMahasiswa = "";
+        String nimMahasiswa = "";
         while (cekInput)
         {
             System.out.printf("%-20s%-2c","Masukkan nama anda",':');
             namaMahasiswa = input.nextLine();
-            if (namaMahasiswa == "" || namaMahasiswa.equalsIgnoreCase("Korlap"))
+            if (namaMahasiswa == "")
             {
-                if(namaMahasiswa.equalsIgnoreCase("Korlap"))
-                {
-                    namaMahasiswa = "Does not exist";
-                }
                 System.out.printf("%s%c%c%s%c%c%s\n","Nama yang anda masukkan",' ','[',namaMahasiswa,']',' ',"Salah, harap masukkan nama kembali");
                 cekInput = true;
             }
@@ -38,14 +34,14 @@ public class N_03_ConditionalAssignment
             {
                 cekInput = false;
             }
-            else
+            else if (nimMahasiswa.length() != 15)
             {
-                System.out.printf("%s%c%c%s%c%c%s\n","Nim yang anda masukkan",' ','[',nimMahasiswa,']',' ',"Salah, harap masukkan nim kembali");
+                System.out.printf("%s%c%c%s%c%c%s\n","Nim yang anda masukkan",' ','[',nimMahasiswa,']',' ',"Salah, harap masukkan nim kembali dan cek jumlah digit nim anda");
                 cekInput = true;
             }
         }
+        input.close();
         System.out.printf("\n%-20s%-2c%s\n","Nama anda adalah",':',namaMahasiswa);
         System.out.printf("%-20s%-2c%s","Nim anda adalah",':',nimMahasiswa);
-        input.close();
     }
 }
