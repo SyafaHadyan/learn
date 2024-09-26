17053cdeef8604ec7875d1841997e1a8fa899b04
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-26 13:44:14 +0700

    Add suprress warning

--- src/java/Pemrograman Dasar/Modul 02/N_02_ConditionalAssignment.java
+++ src/java/Pemrograman Dasar/Modul 02/N_02_ConditionalAssignment.java
@@ -2,6 +2,7 @@ public class N_02_ConditionalAssignment
 {
     public static void main(String[] args)
     {
+        @SuppressWarnings("unused")
         String s = "filkom";
         //int val = (s=="filkom")?"Brawijaya": "null";
         // Error: Tidak bisa mengubah String menjadi integer.
