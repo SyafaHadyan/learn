177a7d4a0ab40d05bc7753b337fdae01834abbf8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-27 18:35:30 +0700

    Removed while-loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -9,53 +9,48 @@ public class Praktikum2
         int upahPerJam = 5000;
         int lemburPerJam = 6000;
         int dendaPerJam = 1000;
-        boolean ulang = true;
-        boolean valid = true;
-        while (ulang)
+        int jamKerja = 0;
+        int upahTotal = 0;
+        int lemburTotal = 0;
+        int dendaTotal = 0;
+        int total = 0;
+        System.out.printf("%-20s%-2c","Jam kerja",':');
+        try
         {
-            int jamKerja = 0;
-            int upahTotal = 0;
-            int lemburTotal = 0;
-            int dendaTotal = 0;
-            int total = 0;
-            System.out.printf("%-20s%-2c","Jam kerja",':');
-            try
-            {
-                jamKerja = Integer.parseInt(input.nextLine());
-            }
-            catch (NumberFormatException e)
-            {
-                input.close();
-                return;
-            }
-            if (jamKerja >= 50 && jamKerja <= 60)
-            {
-                upahTotal = upahPerJam * jamKerja;
-                total = upahTotal + lemburTotal - dendaTotal;
-            }
-            else if (jamKerja > 60)
-            {
-                upahTotal = (upahPerJam * 60);
-                lemburTotal = lemburPerJam * (jamKerja - 60);
-                total = upahTotal + lemburTotal - dendaTotal;
-            }
-            else if (jamKerja < 50)
-            {
-                upahTotal = upahPerJam * jamKerja;
-                dendaTotal = dendaPerJam * (50 - jamKerja);
-                total = upahTotal + lemburTotal - dendaTotal;
-            }
-            if (valid)
-            {
-                System.out.printf("%-20s%c%c%s%c%d\n","Upah",'=',' ',mataUang,' ',upahTotal);
-                System.out.printf("%-20s%c%c%s%c%d\n","Lembur",'=',' ',mataUang,' ',lemburTotal);
-                System.out.printf("%-20s%c%c%s%c%d\n","Denda",'=',' ',mataUang,' ',dendaTotal);
-                System.out.println("---------------------");
-                System.out.printf("%-20s%c%c%s%c%d\n\n","Total",'=',' ',mataUang,' ',total);
-                valid = true;
-                ulang = true;
-            }
+            jamKerja = Integer.parseInt(input.nextLine());
+        }
+        catch (NumberFormatException e)
+        {
+            input.close();
+            return;
         }
         input.close();
+        if (jamKerja >= 50 && jamKerja <= 60)
+        {
+            upahTotal = upahPerJam * jamKerja;
+            total = upahTotal + lemburTotal - dendaTotal;
+        }
+        else if (jamKerja > 60)
+        {
+            upahTotal = (upahPerJam * 60);
+            lemburTotal = lemburPerJam * (jamKerja - 60);
+            total = upahTotal + lemburTotal - dendaTotal;
+        }
+        else if (jamKerja < 50)
+        {
+            upahTotal = upahPerJam * jamKerja;
+            dendaTotal = dendaPerJam * (50 - jamKerja);
+            total = upahTotal + lemburTotal - dendaTotal;
+        }
+        if (valid)
+        {
+            System.out.printf("%-20s%c%c%s%c%d\n","Upah",'=',' ',mataUang,' ',upahTotal);
+            System.out.printf("%-20s%c%c%s%c%d\n","Lembur",'=',' ',mataUang,' ',lemburTotal);
+            System.out.printf("%-20s%c%c%s%c%d\n","Denda",'=',' ',mataUang,' ',dendaTotal);
+            System.out.println("---------------------");
+            System.out.printf("%-20s%c%c%s%c%d\n\n","Total",'=',' ',mataUang,' ',total);
+            valid = true;
+            ulang = true;
+        }
     }
 }
