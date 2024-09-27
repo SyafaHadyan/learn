d59bbd4b050c6f6a1be27f3006815aa33203b861
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-27 18:36:35 +0700

    Moved calculation outside of if-else statements

--- src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum2.java
@@ -28,29 +28,21 @@ public class Praktikum2
         if (jamKerja >= 50 && jamKerja <= 60)
         {
             upahTotal = upahPerJam * jamKerja;
-            total = upahTotal + lemburTotal - dendaTotal;
         }
         else if (jamKerja > 60)
         {
             upahTotal = (upahPerJam * 60);
             lemburTotal = lemburPerJam * (jamKerja - 60);
-            total = upahTotal + lemburTotal - dendaTotal;
         }
         else if (jamKerja < 50)
         {
             upahTotal = upahPerJam * jamKerja;
             dendaTotal = dendaPerJam * (50 - jamKerja);
-            total = upahTotal + lemburTotal - dendaTotal;
         }
-        if (valid)
-        {
-            System.out.printf("%-20s%c%c%s%c%d\n","Upah",'=',' ',mataUang,' ',upahTotal);
-            System.out.printf("%-20s%c%c%s%c%d\n","Lembur",'=',' ',mataUang,' ',lemburTotal);
-            System.out.printf("%-20s%c%c%s%c%d\n","Denda",'=',' ',mataUang,' ',dendaTotal);
-            System.out.println("---------------------");
-            System.out.printf("%-20s%c%c%s%c%d\n\n","Total",'=',' ',mataUang,' ',total);
-            valid = true;
-            ulang = true;
-        }
+        System.out.printf("%-20s%c%c%s%c%d\n","Upah",'=',' ',mataUang,' ',upahTotal);
+        System.out.printf("%-20s%c%c%s%c%d\n","Lembur",'=',' ',mataUang,' ',lemburTotal);
+        System.out.printf("%-20s%c%c%s%c%d\n","Denda",'=',' ',mataUang,' ',dendaTotal);
+        System.out.println("---------------------");
+        System.out.printf("%-20s%c%c%s%c%d\n\n","Total",'=',' ',mataUang,' ',total);
     }
 }
