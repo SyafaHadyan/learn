260f68daca8ca2f3041b432e07df22d675c2340b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-27 09:48:26 +0700

    Removed loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum0.java
@@ -11,7 +11,6 @@ public class Praktikum0
         final double PI = 3.14159265358979323846;
         final String unitKeliling = "cm";
         final String unitLuas = unitKeliling + '2';
-        boolean inputValid = true;
         int pilihanKonversi = 0;
         int sisiSatu = 0;
         int sisiDua = 0;
@@ -22,76 +21,73 @@ public class Praktikum0
         System.out.println("1. menghitung luas dan keliling persegi panjang");
         System.out.println("2. menghitung luas dan keliling lingkaran");
         System.out.println("3. menghitung luas dan keliling segitiga" + "\n");
-        while(inputValid)
+        try
         {
-            try
-            {
-                System.out.printf("%s%-2c","Pilihan anda",':');
-                pilihanKonversi = Integer.parseInt(input.nextLine());
-            }
-            catch (NumberFormatException e)
-            {
-                input.close();
-                return;
-            }
-            switch (pilihanKonversi)
-            {
-                case 1:
-                    try
-                    {
-                        System.out.printf("%s%-2c","Masukkan sisi 1",':');
-                        sisiSatu = Integer.parseInt(input.nextLine());
-                        System.out.printf("%s%-2c","Masukkan sisi 2",':');
-                        sisiDua = Integer.parseInt(input.nextLine());   
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        input.close();
-                        return;
-                    }
-                    namaPilihan = "persegi panjang";
-                    hasilKeliling = (sisiSatu * 2) + (sisiDua * 2);
-                    hasilLuas = sisiSatu * sisiDua;
-                    break;
-                case 2:
-                    try
-                    {
-                        System.out.printf("%s%-2c","Masukkan radius",':');
-                        radius = Integer.parseInt(input.nextLine());
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        input.close();
-                        return;
-                    }
-                    namaPilihan = "lingkaran";;
-                    hasilKeliling = 2 * PI * radius;
-                    hasilLuas = radius * radius * PI;
-                    break;
-                case 3:
-                    try
-                    {
-                        System.out.printf("%s%-2c","Masukkan a",':');
-                        a = Integer.parseInt(input.nextLine());
-                        System.out.printf("%s%-2c","Masukkan b",':');
-                        b = Integer.parseInt(input.nextLine());
-                        System.out.printf("%s%-2c","Masukkan r",':');
-                        r = Integer.parseInt(input.nextLine());
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        input.close();
-                        return;
-                    }
-                    namaPilihan = "segitiga";
-                    hasilKeliling = a + b + r;
-                    hasilLuas = a * b;
-                    break;
-                default:
-                    System.out.print("Data tak ditemukan, program dihentikan ...");
+            System.out.printf("%s%-2c","Pilihan anda",':');
+            pilihanKonversi = Integer.parseInt(input.nextLine());
+        }
+        catch (NumberFormatException e)
+        {
+            input.close();
+            return;
+        }
+        switch (pilihanKonversi)
+        {
+            case 1:
+                try
+                {
+                    System.out.printf("%s%-2c","Masukkan sisi 1",':');
+                    sisiSatu = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan sisi 2",':');
+                    sisiDua = Integer.parseInt(input.nextLine());   
+                }
+                catch (NumberFormatException e)
+                {
                     input.close();
                     return;
-            }
+                }
+                namaPilihan = "persegi panjang";
+                hasilKeliling = (sisiSatu * 2) + (sisiDua * 2);
+                hasilLuas = sisiSatu * sisiDua;
+                break;
+            case 2:
+                try
+                {
+                    System.out.printf("%s%-2c","Masukkan radius",':');
+                    radius = Integer.parseInt(input.nextLine());
+                }
+                catch (NumberFormatException e)
+                {
+                    input.close();
+                    return;
+                }
+                namaPilihan = "lingkaran";;
+                hasilKeliling = 2 * PI * radius;
+                hasilLuas = radius * radius * PI;
+                break;
+            case 3:
+                try
+                {
+                    System.out.printf("%s%-2c","Masukkan a",':');
+                    a = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan b",':');
+                    b = Integer.parseInt(input.nextLine());
+                    System.out.printf("%s%-2c","Masukkan r",':');
+                    r = Integer.parseInt(input.nextLine());
+                }
+                catch (NumberFormatException e)
+                {
+                    input.close();
+                    return;
+                }
+                namaPilihan = "segitiga";
+                hasilKeliling = a + b + r;
+                hasilLuas = a * b;
+                break;
+            default:
+                System.out.print("Data tak ditemukan, program dihentikan ...");
+                input.close();
+                return;
             System.out.printf("\n%s%c%s%c%-2c%.0f%c%s\n","Keliling",' ',namaPilihan,' ',':',hasilKeliling,' ',unitKeliling);
             System.out.printf("%s%c%s%c%-2c%.0f%c%s\n\n","Luas",' ',namaPilihan,' ',':',hasilLuas,' ',unitLuas);
         }
