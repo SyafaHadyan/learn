147196bbf1bee3ba0fe03445169d059986843ae0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-27 18:20:13 +0700

    Removed while-loop

--- src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
+++ src/java/Pemrograman Dasar/Modul 02/Praktikum1.java
@@ -13,39 +13,36 @@ public class Praktikum1
         double beratBadan;
         double tinggiBadan;
         boolean ulang = true;
-        while (ulang)
+        try
         {
-            try
-            {
-                System.out.printf("%-17s%-2c",outBerat,':');
-                beratBadan = Double.parseDouble(input.nextLine());
-                System.out.printf("%-17s%-2c",outTinggi,':');
-                tinggiBadan = Double.parseDouble(input.nextLine());
-            }
-            catch (NumberFormatException e)
-            {
-                input.close();
-                return;
-            }
-            IMT = beratBadan / (tinggiBadan * tinggiBadan);
-            if (IMT <= 18.5)
-            {
-                System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk kurus");
-            }
-            else if (IMT > 18.5 && IMT <= 25)
-            {
-                System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk normal");
-            }
-            else if (IMT > 25 && IMT <= 30)
-            {
-                System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk gemuk");
-            }
-            else if (IMT > 30)
-            {
-                System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk kegemukan");
-            }
-            System.out.print("\n\n");
+            System.out.printf("%-17s%-2c",outBerat,':');
+            beratBadan = Double.parseDouble(input.nextLine());
+            System.out.printf("%-17s%-2c",outTinggi,':');
+            tinggiBadan = Double.parseDouble(input.nextLine());
+        }
+        catch (NumberFormatException e)
+        {
+            input.close();
+            return;
         }
         input.close();
+        IMT = beratBadan / (tinggiBadan * tinggiBadan);
+        if (IMT <= 18.5)
+        {
+            System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk kurus");
+        }
+        else if (IMT > 18.5 && IMT <= 25)
+        {
+            System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk normal");
+        }
+        else if (IMT > 25 && IMT <= 30)
+        {
+            System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk gemuk");
+        }
+        else if (IMT > 30)
+        {
+            System.out.printf("%s%c%c%c%.2f%c%s","IMT",' ','=',' ',IMT,' ',"Termasuk kegemukan");
+        }
+        System.out.print("\n\n");
     }
 }
