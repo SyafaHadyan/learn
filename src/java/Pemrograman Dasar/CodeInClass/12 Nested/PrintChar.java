55f71956534e56740ee4d8ac89f74edfefb35170
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 19:00:24 +0700

    Close scanner

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/PrintChar.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/PrintChar.java
@@ -7,6 +7,7 @@ public class PrintChar
         Scanner input = new Scanner(System.in);
         String gambar = input.nextLine();
         int perulangan = Integer.parseInt(input.nextLine());
+        input.close();
         for (int i = perulangan; i >= 0; i--)
         {
             for (int j = i; j >=0; j--)
