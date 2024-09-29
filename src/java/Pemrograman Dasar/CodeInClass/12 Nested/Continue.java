ee33ee2aaad556706321f632238e28e50f3e8863
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 20:21:54 +0700

    Continue

---
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Continue.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Continue
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%-10s%-2c","Mulai",':');
+        int mulai = Integer.parseInt(input.nextLine());
+        System.out.printf("%-10s%-2c","Akhir",':');
+        int akhir = Integer.parseInt(input.nextLine());
+        System.out.printf("%-10s%-2c","Continue",':');
+        int keluar = Integer.parseInt(input.nextLine());
+        input.close();
+        while (mulai <= akhir)
+        {
+            if (mulai == keluar)
+            {
+                System.out.println("Skip");
+                break;
+            }
+            System.out.print(mulai);
+            System.out.println(' ' + "Terus");
+            mulai++;
+        }
+    }
+}
