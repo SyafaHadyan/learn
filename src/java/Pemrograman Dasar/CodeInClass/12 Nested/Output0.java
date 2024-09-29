355b37c7c25173f08046e6d71f121e86bc0badbe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 06:48:50 +0700

    For-i

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
@@ -8,11 +8,14 @@ public class Output0
         int x = Integer.parseInt(input.nextLine());
         int y = Integer.parseInt(input.nextLine());
         int maksimalX = Integer.parseInt(input.nextLine());
-        int maksimalY = Integer.parseInt(input.nextLine());
+        int minimalY = Integer.parseInt(input.nextLine());
         input.close();
-        for (int i = x; i < args.length; i++)
+        for (int i = x; i <= maksimalX; i++)
         {
-            
+            for (int j = y; j >= minimalY; j--)
+            {
+                System.out.printf("%d%d",x,y);
+            }
         }
     }
 }
