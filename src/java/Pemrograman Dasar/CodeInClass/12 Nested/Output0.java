98e543280429bdf023b31dd7aa2f78dd7d56ac18
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 06:47:29 +0700

    For-i and max x y

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
@@ -7,6 +7,12 @@ public class Output0
         Scanner input = new Scanner(System.in);
         int x = Integer.parseInt(input.nextLine());
         int y = Integer.parseInt(input.nextLine());
+        int maksimalX = Integer.parseInt(input.nextLine());
+        int maksimalY = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = x; i < args.length; i++)
+        {
+            
+        }
     }
 }
