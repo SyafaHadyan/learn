15ebe6a5e2ebda72c76aee25e94e9df55fa141e6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 19:58:53 +0700

    Fix calculation

--- src/java/syafa/java/learn/chapter2/FahrenheitToCelcius.java
+++ src/java/syafa/java/learn/chapter2/FahrenheitToCelcius.java
@@ -9,7 +9,7 @@ public class FahrenheitToCelcius
         System.out.printf("%-15s%-2c","Fahrenheit",':');
         double fahrenheit = Double.parseDouble(input.nextLine());
         input.close();
-        double celcius = (5 / 9) * (fahrenheit- 32);
+        double celcius = (fahrenheit- 32) * (5.0 / 9.0);
         System.out.printf("%-15s%-2c%.5f","Celcius",':',celcius);
     }
 }
