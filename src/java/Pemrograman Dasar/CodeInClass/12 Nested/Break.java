e6abdc72d92529e51ac8dc503b8ab9420f4af86d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 20:12:28 +0700

    Initialize input

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Break.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Break.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class Break
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int mulai = Integer.parseInt(input.nextLine());
+        int akhir = Integer.parseInt(input.nextLine());
+        int keluar = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
