238e6d587c51f74c0b12a90c0e5b8a368263293a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 19:04:56 +0700

    Output formatting

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Draw.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Draw.java
@@ -5,9 +5,9 @@ public class Draw
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.printf("%-20s%-2c","Masukkan gambar yang digambar",':');
+        System.out.printf("%-30s%-2c","Masukkan gambar yang digambar",':');
         String gambar = input.nextLine();
-        System.out.printf("%-20s%-2c","Masukkan perulangan",':');
+        System.out.printf("%-30s%-2c","Masukkan perulangan",':');
         int perulangan = Integer.parseInt(input.nextLine());
         input.close();
         for (int i = perulangan; i > 0; i--)
