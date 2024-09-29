68ff55f56307cd93e43308d19d75ffd5e95c3308
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 20:13:24 +0700

    Prompt user

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Break.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Break.java
@@ -5,8 +5,11 @@ public class Break
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-10s%-2c","Mulai",':');
         int mulai = Integer.parseInt(input.nextLine());
+        System.out.printf("%-10s%-2c","Akhir",':');
         int akhir = Integer.parseInt(input.nextLine());
+        System.out.printf("%-10s%-2c","Break",':');
         int keluar = Integer.parseInt(input.nextLine());
         input.close();
     }
