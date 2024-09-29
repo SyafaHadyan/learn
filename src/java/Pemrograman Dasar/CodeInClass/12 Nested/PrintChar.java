2002373fe69bf7a9d9d8855949b6710c26b30170
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 19:01:39 +0700

    Fix calculation

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/PrintChar.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/PrintChar.java
@@ -8,9 +8,9 @@ public class PrintChar
         String gambar = input.nextLine();
         int perulangan = Integer.parseInt(input.nextLine());
         input.close();
-        for (int i = perulangan; i >= 0; i--)
+        for (int i = perulangan; i > 0; i--)
         {
-            for (int j = i; j >=0; j--)
+            for (int j = i; j > 0; j--)
             {
                 System.out.print(gambar);
             }
