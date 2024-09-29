268efa9f247ed9eb247adcce7eba319d57db9c3f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-29 19:52:19 +0700

    Initialize scannner

--- src/java/syafa/java/learn/chapter2/FahrenheitToCelcius.java
+++ src/java/syafa/java/learn/chapter2/FahrenheitToCelcius.java
@@ -1,9 +1,10 @@
 package syafa.java.learn.chapter2;
+import java.util.*;
 
 public class FahrenheitToCelcius
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
     }
 }
