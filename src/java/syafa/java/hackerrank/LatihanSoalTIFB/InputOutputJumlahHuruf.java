d46c2ecbe394f927f1de8739ab7dca1174113a61
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 20:04:09 +0700

    add

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/InputOutputJumlahHuruf.java
@@ -0,0 +1,13 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+  
+public class InputOutputJumlahHuruf
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String masuk = input.nextLine();
+        input.close();
+        System.out.print(masuk.length());
+    }
+}
