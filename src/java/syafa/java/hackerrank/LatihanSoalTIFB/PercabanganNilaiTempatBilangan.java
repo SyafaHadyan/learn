55d8a183f4d55b55a8533755c89ff179e46674cc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 20:39:55 +0700

    edit

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganNilaiTempatBilangan.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganNilaiTempatBilangan.java
@@ -8,8 +8,32 @@ public class PercabanganNilaiTempatBilangan
         Scanner input = new Scanner(System.in);
         int nomor = Integer.parseInt(input.nextLine());
         input.close();
-        //[Satuan, Puluhan, Ratusan, Ribuan, Puluhan Ribu, Ratusan Ribu, Jutaan]
         int panjang = (int)(Math.log10(nomor) + 1);
-        if 
+        switch (panjang)
+        {
+            case 1:
+                System.out.print("Satuan");
+                break;
+            case 2:
+                System.out.print("Puluhan");
+                break;
+            case 3:
+                System.out.print("Ratusan");
+                break;
+            case 4:
+                System.out.print("Ribuan");
+                break;
+            case 5:
+                System.out.print("Puluhan Ribu");
+                break;
+            case 6:
+                System.out.print("Ratusan Ribu");
+                break;
+            case 7:
+                System.out.print("Jutaan");
+                break;
+            default:
+                break;
+        }
     }
 }
