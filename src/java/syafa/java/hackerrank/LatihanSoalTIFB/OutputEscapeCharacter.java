0ec43291a73c4383f5f5d55d8a3cc370b15c38c0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 19:44:21 +0700

    ESC

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/OutputEscapeCharacter.java
@@ -0,0 +1,12 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+
+public class OutputEscapeCharacter
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Ali menjawab, \"Saya bisa\"" + "\n" + 
+                            "Hari Jum'at ada kegiatan" + "\n" +
+                            "Anak itu menjawab dengan emoji --\\UwU/--" + "\n" +
+                            "Gunakan\ttab\tdi\tsetiap\tkata");
+    }
+}
