375caf0e0ef93cb1ea92cf8b5713668aed17c423
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 06:58:34 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganSimbolDiagonal.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganSimbolDiagonal.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganSimbolDiagonal
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String simbol = input.nextLine();
+        int perulangan = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
