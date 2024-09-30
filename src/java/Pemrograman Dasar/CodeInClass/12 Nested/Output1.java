8497e2ff0b3333c62e22c884cec25b861f1cfe43
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 07:13:24 +0700

    Initialize input

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
@@ -1,7 +1,10 @@
+import java.util.*;
+
 public class Output1
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
