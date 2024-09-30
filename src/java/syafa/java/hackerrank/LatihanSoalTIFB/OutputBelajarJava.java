a6717f8a6fbafd83069040303d1fca1c3cf396eb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 19:37:12 +0700

    add

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/OutputBelajarJava.java
@@ -0,0 +1,9 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+
+public class OutputBelajarJava
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Halo, aku seorang programmer baru\nDi sini aku belajar bahasa Java");
+    }
+}
