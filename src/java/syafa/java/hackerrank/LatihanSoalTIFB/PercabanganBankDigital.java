4a3320cafdce7bb2066481fe6da34f45318a48b2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 22:57:46 +0700

    cant think of a commit msg

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBankDigital.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBankDigital.java
@@ -5,6 +5,10 @@ public class PercabanganBankDigital
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int saldo = Integer.parseInt(input.nextLine());
+        String fungsi = input.nextLine();
+        int transaksi = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
