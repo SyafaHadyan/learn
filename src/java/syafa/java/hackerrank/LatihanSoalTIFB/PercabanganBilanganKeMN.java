2fff2d4c05bb5b00704f00324e22659644c5b5af
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 06:40:47 +0700

    Get digit at

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBilanganKeMN.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBilanganKeMN.java
@@ -12,6 +12,8 @@ public class PercabanganBilanganKeMN
         input.close();
         String stringZ = Long.toString(z);
         int jumlahDigit = stringZ.length();
+        int digitKeM = jumlahDigit - m - 1;
+        int digitKeN = jumlahDigit - n - 1;
         //int digitkeM = Integer.parseInt(stringZ);
         //int digitkeM = stringZ.charAt(digitkeM - m - 1);
     }
