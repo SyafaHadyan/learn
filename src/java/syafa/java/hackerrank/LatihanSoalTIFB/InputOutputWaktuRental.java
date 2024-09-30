2ae043e47c513b2823279cc67d4f5ec0fb03e58e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 22:39:19 +0700

    Edit

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/InputOutputWaktuRental.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/InputOutputWaktuRental.java
@@ -11,11 +11,11 @@ public class InputOutputWaktuRental
         int selesai = Integer.parseInt(input.nextLine());
         int hargaPerJam = Integer.parseInt(input.nextLine());
         input.close();
-        int total = selesai - mulai;
         if (selesai > 23)
         {
-            selesai = selesai % 23;
+            selesai = selesai % 24;
         }
+        int total = selesai - mulai;
         System.out.println("Halo" + ' ' + nama);
         System.out.println("Anda merental Game Corner pada jam" + ' ' + mulai + ' ' + '-' + ' ' + selesai);
         System.out.println("Total durasi" + ' ' + total + ' ' + "jam");
