b55b498e8c9ad6157cbf7285c7874c3e78e7a7dc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 06:27:37 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBilanganKeMN.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBilanganKeMN.java
@@ -1,9 +1,14 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PercabanganBilanganKeMN
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        long z = Long.parseLong(input.nextLine());
+        int m = Integer.parseInt(input.nextLine());
+        int n = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
