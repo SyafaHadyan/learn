c291ae0845112d6888c7bdc0475cd504a812313a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 07:14:25 +0700

    Input variables

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
@@ -5,6 +5,9 @@ public class Output1
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int x = Integer.parseInt(input.nextLine());
+        int y = Integer.parseInt(input.nextLine());
+        int z = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
