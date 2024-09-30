2a2e552f571605db2e99b70768917b9a3c1ca906
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 19:47:50 +0700

    add

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/InputInputPertama.java
@@ -0,0 +1,13 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+
+public class InputInputPertama
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String stringInput = input.nextLine();
+        input.close();
+        System.out.print(stringInput);
+    }
+}
