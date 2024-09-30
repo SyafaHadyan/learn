0a18c55835a5570e1ebaaff862da88f31caf017c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 07:30:26 +0700

    Preview code

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output1.java
@@ -5,8 +5,10 @@ public class Output1
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("Kode yang akan dijalankan" + "\n" + 
-                            "\n" );
+        System.out.print("Kode yang akan dijalankan" + "\n" + "\n" + 
+                            "int x, y, z;" + "\n" + 
+                            "for (x = x awal; x < x maksimal; x++) {" + "\n" + 
+                            "\t" + "for (y = y awal; y < y maksimal; y++) {" + "\n");
         System.out.printf("%-30s%-2c","Masukkan nilai x",':');
         int x = Integer.parseInt(input.nextLine());
         System.out.printf("%-30s%-2c","Masukkan nilai y",':');
