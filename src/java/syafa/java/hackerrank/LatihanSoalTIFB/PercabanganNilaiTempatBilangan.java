92318aa80b5c052c9f3d4e5149b1c11b501d842a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 20:30:05 +0700

    Edit

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganNilaiTempatBilangan.java
@@ -0,0 +1,15 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+
+public class PercabanganNilaiTempatBilangan
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int nomor = Integer.parseInt(input.nextLine());
+        input.close();
+        //[Satuan, Puluhan, Ratusan, Ribuan, Puluhan Ribu, Ratusan Ribu, Jutaan]
+        int panjang = (int)(Math.log10(nomor) + 1);
+        if 
+    }
+}
