8bf62c3d6d32aff76a2c54287e1382b41d66aa5d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 20:45:44 +0700

    Add

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganBesarKecil.java
@@ -0,0 +1,21 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+
+public class PercabanganBesarKecil
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String masuk = input.nextLine();
+        String perintah = input.nextLine();
+        input.close();
+        if (perintah.equalsIgnoreCase("Besar"))
+        {
+            System.out.print(masuk.toUpperCase());
+        }
+        else if (perintah.equalsIgnoreCase("Kecil"))
+        {
+            System.out.print(masuk.toLowerCase());
+        }
+    }
+}
