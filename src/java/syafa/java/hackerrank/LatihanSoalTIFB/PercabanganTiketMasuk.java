809f82e01f1dfe7557b2617ea29cc154ef66fcbe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 20:23:02 +0700

    Add

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PercabanganTiketMasuk.java
@@ -0,0 +1,21 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+
+public class PercabanganTiketMasuk
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int harga = Integer.parseInt(input.nextLine());
+        int saldo = Integer.parseInt(input.nextLine());
+        input.close();
+        if (harga < saldo)
+        {
+            System.out.print("Selamat bermain");
+        }
+        else if (harga > saldo)
+        {
+            System.out.print("Maaf, saldo tidak cukup");
+        }
+    }
+}
