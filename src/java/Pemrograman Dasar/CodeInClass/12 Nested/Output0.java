64b35a0ccebd99579e2e79119460ad9ea867c0d7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-30 07:06:12 +0700

    Fix new line output

--- src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
+++ src/java/Pemrograman Dasar/CodeInClass/12 Nested/Output0.java
@@ -29,6 +29,6 @@ public class Output0
                 System.out.printf("%d%d",x,y);
             }
         }
-        System.out.printf("%s%2c\n%d","Nilai yang keluar",':',tebak);
+        System.out.printf("\n%s%2c\n%d","Nilai yang anda tebak",':',tebak);
     }
 }
