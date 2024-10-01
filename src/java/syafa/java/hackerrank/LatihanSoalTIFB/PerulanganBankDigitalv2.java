c9447e73f8a6222225f1095f11d00774ff6d99b6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 15:08:35 +0700

    Remove reference comment

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganBankDigitalv2.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganBankDigitalv2.java
@@ -43,7 +43,6 @@ public class PerulanganBankDigitalv2
             }
             else if (inputPertama.equalsIgnoreCase("Transfer"))
             {
-                //Transfer Rp. 5000 ke Iqbal berhasil
                 if (inputKedua > saldo)
                 {
                     output = "Transfer Rp." + ' ' + inputKedua + ' ' + "ke" + ' ' + inputKetiga + ' ' + "Gagal";
