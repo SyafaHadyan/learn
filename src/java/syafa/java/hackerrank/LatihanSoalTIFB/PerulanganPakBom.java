ce8d01e0aa7e8918978bd4bec4955b42f91cd35f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 16:58:29 +0700

    Initalize scanner

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganPakBom.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganPakBom.java
@@ -1,9 +1,15 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganPakBom
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int barisSatu = Integer.parseInt(input.nextLine());
+        int barisDuaSatu = input.nextInt();
+        int barisDuaDua = input.nextInt();
+        input.nextLine();
+        input.close();
     }
 }
