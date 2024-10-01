294c250e8986c87810270e2523c4fe0430cbd5be
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 06:56:33 +0700

    Initialize scanner and input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganDiamonds.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganDiamonds.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganDiamonds
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int ukuranDiamond = Integer.parseInt(input.nextLine());
+        input.close();
+        char simbol = '*';
     }
 }
