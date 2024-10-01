dadf10c8ddb0b9c63e6e44499b7aa40351c2c659
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 16:40:23 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganCekPrima.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganCekPrima.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganCekPrima
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int cekBilangan = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
