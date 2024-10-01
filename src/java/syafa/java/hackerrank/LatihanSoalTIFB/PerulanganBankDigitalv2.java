ba59f2b82b58cac93cdf9be18a2f6a186d7e48b9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-01 07:13:55 +0700

    Initialize basic input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganBankDigitalv2.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganBankDigitalv2.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganBankDigitalv2
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String nama = input.nextLine();
+        int saldo = Integer.parseInt(input.nextLine());
     }
 }
