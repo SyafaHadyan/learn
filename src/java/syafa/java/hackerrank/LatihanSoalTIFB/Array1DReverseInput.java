fa5ad115dcf4b0d1f6464e330929547bb8950049
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 06:42:45 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array1DReverseInput.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array1DReverseInput.java
@@ -1,9 +1,15 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class Array1DReverseInput
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int panjangArray = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < args.length; i++)
+        {
+            //
+        }
     }
 }
