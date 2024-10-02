a4dd6265e7f291e6e89b075a30dbc18877e8268d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 20:07:09 +0700

    Initialize file

---
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodHaloDunia.java
@@ -0,0 +1,15 @@
+package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
+
+public class MethodHaloDunia
+{
+    public static void main(String[] args)
+    {
+        // Tuliskan kode kalian di sini
+        printHaloDunia(/* Sesuaikan */) // Jangan hapus baris ini (boleh mengedit method sesuai method yang di bawah)
+    }
+  	
+    public static void printHaloDunia(/* Isi/Kosongi */) { // Boleh mengganti nama method, return type, dan parameter
+        // Tuliskan kode kalian di sini
+    }
+}
