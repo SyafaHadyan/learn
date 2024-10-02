2f662e029f1302efc5f4d547b3c00d3e231d3304
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 06:47:32 +0700

    Change height input to nextLine then parse to int

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodBuatPeta.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodBuatPeta.java
@@ -7,12 +7,12 @@ public class MethodBuatPeta
     {
         Scanner input = new Scanner(System.in);
         int tinggiPeta = input.nextInt();
-        int lebarPeta = input.nextInt();
+        int lebarPeta = Integer.parseInt(input.nextLine());
         String simbol = input.nextLine();
         input.close();
         for (int i = 1; i <= tinggiPeta; i++)
         {
-            for (int j = 1; i <= lebarPeta; j++)
+            for (int j = 1; j <= lebarPeta; j++)
             {
                 System.out.print(simbol);
                 if (j != lebarPeta)
