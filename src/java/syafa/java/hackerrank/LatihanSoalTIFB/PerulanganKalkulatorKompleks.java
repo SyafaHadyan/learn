1d5c23558db0fd2e5af5690fe2862c17f1670fe7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 13:44:13 +0700

    Put output into outputArray and printf 2 digits precision

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
@@ -8,6 +8,7 @@ public class PerulanganKalkulatorKompleks
         Scanner input = new Scanner(System.in);
         int jumlahOperasi = Integer.parseInt(input.nextLine());
         String outputArray[] = new String[jumlahOperasi];
+        double outputHasil[] = new double[jumlahOperasi];
         double hasil = 0d;
         String output = "";
         for (int i = 0; i < jumlahOperasi; i++)
@@ -19,38 +20,44 @@ public class PerulanganKalkulatorKompleks
             if (operasi.equalsIgnoreCase("Tambah"))
             {
                 hasil = x + y;
-                output = x + " + " + y + " = " + hasil;
+                output = x + " + " + y + " = ";
                 outputArray[i] = output;
+                outputHasil[i] = hasil;
             }
             else if (operasi.equalsIgnoreCase("Kurang"))
             {
                 hasil = x - y;
-                output = x + " - " + y + " = " + hasil;
+                output = x + " - " + y + " = ";
                 outputArray[i] = output;
+                outputHasil[i] = hasil;
             }
             else if (operasi.equalsIgnoreCase("Kali"))
             {
                 hasil = x * y;
-                output = x + " * " + y + " = " + hasil;
+                output = x + " * " + y + " = ";
                 outputArray[i] = output;
+                outputHasil[i] = hasil;
             }
             else if (operasi.equalsIgnoreCase("Bagi"))
             {
                 hasil = x / y;
-                output = x + " / " + y + " = " + hasil;
+                output = x + " / " + y + " = ";
                 outputArray[i] = output;
+                outputHasil[i] = hasil;
             }
             else if (operasi.equalsIgnoreCase("Mod"))
             {
                 hasil = x % y;
-                output = x + " % " + y + " = " + hasil;
+                output = x + " % " + y + " = ";
                 outputArray[i] = output;
+                outputHasil[i] = hasil;
             }
         }
         input.close();
         for (int i = 0; i < jumlahOperasi; i++)
         {
             System.out.print(outputArray[i]);
+            System.out.printf("%.2f",outputHasil[i]);
             if (i != jumlahOperasi - 1)
             {
                 System.out.print("\n");
