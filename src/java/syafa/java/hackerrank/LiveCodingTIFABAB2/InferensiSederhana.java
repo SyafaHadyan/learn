c2b8f23ac196bdd7a6913010a25b9d9762872f3f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 23:00:34 +0700

    Edit input

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/InferensiSederhana.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/InferensiSederhana.java
@@ -5,6 +5,14 @@ public class InferensiSederhana
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int masuk = Integer.parseInt(input.nextLine());
+        String operasi = input.nextLine();
+        int elemen;
+        for (int i = 1; i <= 10; i++)
+        {
+            elemen = input.nextInt();
+        }
+        input.close();
     }
 }
