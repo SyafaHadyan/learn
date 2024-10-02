d684c7a5a68d25d92f34e674b14c5750211526a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 13:06:37 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
@@ -1,9 +1,15 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class PerulanganKalkulatorKompleks
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int jumlahOperasi = Integer.parseInt(input.nextLine());
+        String operasi = input.next();
+        int x = input.nextInt();
+        int y = input.nextInt();
+        input.nextLine();
     }
 }
