ef6469deae9783c57bafd931d8fa26c6cbf108f5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 22:26:24 +0700

    If tie

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/DengklekvsSengklek.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/DengklekvsSengklek.java
@@ -1,6 +1,23 @@
 package syafa.java.hackerrank.LiveCodingTIFABAB2;
+import java.util.*;
 
 public class DengklekvsSengklek
 {
-
+    public static void main(String[] args)
+    {
+        int hasil[] = new int[4];
+        Scanner input = new Scanner(System.in);
+        int skorDengklek = 0;
+        int skorSengklek = 0;
+        for (int i = 1; i <= 4; i++)
+        {
+            String dengklek = input.next();
+            String sengklek = input.next();
+            input.nextLine();
+            if (dengklek.equalsIgnoreCase(sengklek))
+            {
+                //
+            }
+        }
+    }
 }
