7764d032c78e2b85c88f29f4e07db4465176eab8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 19:34:19 +0700

    Put num a and b into array

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganKalkulatorKompleks.java
@@ -7,7 +7,8 @@ public class PerulanganKalkulatorKompleks
     {
         Scanner input = new Scanner(System.in);
         int jumlahOperasi = Integer.parseInt(input.nextLine());
-        double bilanganPertamaKedua[] = new double[jumlahOperasi];
+        double bilanganPertama[] = new double[jumlahOperasi];
+        double bilanganKedua[] = new double[jumlahOperasi];
         String outputArray[] = new String[jumlahOperasi];
         double outputHasil[] = new double[jumlahOperasi];
         double x = 0d;
