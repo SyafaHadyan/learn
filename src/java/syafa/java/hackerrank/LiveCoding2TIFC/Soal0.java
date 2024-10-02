f674d75d658d99ab551e5ed18e93a320bbd2d29a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-02 23:34:15 +0700

    Create files

---
+++ src/java/syafa/java/hackerrank/LiveCoding2TIFC/Soal0.java
@@ -0,0 +1,11 @@
+package syafa.java.hackerrank.LiveCoding2TIFC;
+import java.util.*;
+
+public class Soal0
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
