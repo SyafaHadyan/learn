e156f0d1a3647fc175f067296d0d0583e76b697b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 06:52:57 +0700

    Create empty arrays to put input

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodPrintMultifungsi.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodPrintMultifungsi.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class MethodPrintMultifungsi
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int jumlahInput = Integer.parseInt(input.nextLine());
+        String kalimatArray[] = new String[jumlahInput];
+        String simbolArray[] = new String[jumlahInput];
     }
 }
