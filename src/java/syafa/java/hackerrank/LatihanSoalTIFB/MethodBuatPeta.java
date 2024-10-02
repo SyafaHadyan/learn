176948fed9250e3af2a8595379e2562a2bd1144f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 06:40:40 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodBuatPeta.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodBuatPeta.java
@@ -1,9 +1,14 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class MethodBuatPeta
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int tinggiPeta = input.nextInt();
+        int lebarPeta = input.nextInt();
+        String simbol = input.nextLine();
+        input.close();
     }
 }
