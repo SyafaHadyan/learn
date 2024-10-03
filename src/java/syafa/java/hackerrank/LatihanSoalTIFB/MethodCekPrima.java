4a5ee257b9c34a43f31eacc11b9151db2f479fc6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 07:35:24 +0700

    Add input for n times checking

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
@@ -1,9 +1,28 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
 
+import java.util.Scanner;
+
 public class MethodCekPrima
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int jumlahPengecekan = Integer.parseInt(input.nextLine());
+        int cekBilangan = Integer.parseInt(input.nextLine());
+        input.close();
+        String cekPrima = "Prima";
+        for (int i = 2; i < cekBilangan; i++)
+        {
+            if (cekBilangan % i == 0)
+            {
+                cekPrima = "Bukan prima";
+                break;
+            }
+        }
+        if (cekBilangan == 1)
+        {
+            cekPrima = "Bukan prima";
+        }
+        System.out.print(cekPrima);
     }
 }
