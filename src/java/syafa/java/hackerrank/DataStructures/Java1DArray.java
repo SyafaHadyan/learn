c50fab3bc765fddbe163c1c45784f34f9055238d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 14:36:48 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/DataStructures/Java1DArray.java
+++ src/java/syafa/java/hackerrank/DataStructures/Java1DArray.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.DataStructures;
+import java.util.*;
 
 public class Java1DArray
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
