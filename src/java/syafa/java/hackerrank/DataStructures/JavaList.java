192680fce03fa121d3be859ced7affb8d47eb66d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 15:27:38 +0700

    Create empty arraylist

--- src/java/syafa/java/hackerrank/DataStructures/JavaList.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaList.java
@@ -1,9 +1,16 @@
 package syafa.java.hackerrank.DataStructures;
+import java.util.*;
 
 public class JavaList
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int initialArrayLength = Integer.parseInt(input.nextLine());
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        for (int i = 0; i < initialArrayLength; i++)
+        {
+            
+        }
     }
 }
