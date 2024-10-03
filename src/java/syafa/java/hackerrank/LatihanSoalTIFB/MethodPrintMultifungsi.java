52f3a225b2f22fb6e13c7854caf1840cf1c04fce
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 07:01:30 +0700

    Put output into outputArray

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodPrintMultifungsi.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodPrintMultifungsi.java
@@ -9,12 +9,12 @@ public class MethodPrintMultifungsi
         int jumlahInput = Integer.parseInt(input.nextLine());
         String outputArray[] = new String[jumlahInput];
         String output = "";
-        for (int i = 1; i <= jumlahInput; i++)
+        for (int i = 0; i < jumlahInput; i++)
         {
             String kalimatArray[] = input.nextLine().split(" ");
             String simbolArray[] = input.nextLine().split(" ");
             output = simbolArray[0] + kalimatArray[0] + simbolArray[1] + kalimatArray[1] +simbolArray[1] + kalimatArray[3] + simbolArray[2];
-
+            outputArray[i] = output;
         }
     }
 }
