b1ad2a27343e6b8a90f6f35a8c2121858d8d4fe4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 15:32:09 +0700

    Input

--- src/java/syafa/java/hackerrank/DataStructures/JavaList.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaList.java
@@ -6,16 +6,19 @@ public class JavaList
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int indexAt = 0;
+        int operationInput = 0;
         int initialArrayLength = Integer.parseInt(input.nextLine());
         ArrayList<Integer> numberArray = new ArrayList<Integer>();
         for (int i = 0; i < initialArrayLength; i++)
         {
-            numberArray.add(input.nextInt());
+            numberArray.add(Integer.parseInt(input.nextLine()));
         }
         String operation = input.nextLine();
         if (operation.equalsIgnoreCase("Insert"))
         {
-            //
+            indexAt = input.nextInt();
+            numberArray.add(Integer.parseInt(input.nextLine()));
         }
         else if (operation.equalsIgnoreCase("Delete"))
         {
