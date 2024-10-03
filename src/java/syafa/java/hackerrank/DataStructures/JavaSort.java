bfddbcc06f6ce771a56ea8b8a5e34ca0127908c8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 14:52:01 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
@@ -1,9 +1,15 @@
 package syafa.java.hackerrank.DataStructures;
+import java.util.*;
 
 public class JavaSort
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int amountOfStudents = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
     }
 }
