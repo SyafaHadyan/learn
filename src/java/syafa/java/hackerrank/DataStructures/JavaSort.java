9e78f0d69f88c845481d7464ce51153d04f3db80
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 14:55:56 +0700

    Create arrays with the size of amountOfStudents

--- src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
@@ -10,9 +10,12 @@ public class JavaSort
         int studentID = 0;
         String studentName = "";
         double CGPA = 0d;
+        int studentIDArray[] = new int[amountOfStudents];
+        String studentNamearray[] = new String[amountOfStudents];
+        double CGPAArray[] = new double[amountOfStudents];
         for (int i = 0; i < amountOfStudents; i++)
         {
-            
+            studentID = input.nextInt();
         }
     }
 }
