d9c6a969f9fc931b5a9899de683f4db8ee24826c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 12:34:46 +0700

    Temporary fix

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganDiamonds.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/PerulanganDiamonds.java
@@ -22,7 +22,7 @@ public class PerulanganDiamonds
                 {
                     System.out.print(' ');
                 }
-                //spasi--;
+                spasi--;
                 System.out.print(simbol);
                 if (j != i)
                 {
@@ -42,14 +42,15 @@ public class PerulanganDiamonds
         System.out.print("\n");
         for (int i = panjangSebelumTengah; i >= 1; i--)
         {
-            for (int j = spasi; j < 0; j++)
-            {
-                System.out.print(spasiArray[0]);
-            }
             for (int j = i; j >= 1; j--)
             {
+                for (int k = spasi; k >= 1; j--)
+                {
+                    System.out.print(' ');
+                }
+                spasi++;
                 System.out.print(simbol);
-                if (j != 1)
+                if (i != j)
                 {
                     System.out.print(' ');
                 }
