23a2d6b13e14566c26171a47b91518a69b943e8d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 16:25:09 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.ExceptionHandling;
+import java.util.*;
 
 public class JavaExceptionHandlingTrycatch
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int firstNumber = Integer.parseInt(input.nextLine());
+        int secondNumber = Integer.parseInt(input.nextLine());
     }
 }
