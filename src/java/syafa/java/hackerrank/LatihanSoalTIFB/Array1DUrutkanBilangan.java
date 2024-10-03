ce844fef1fd1e025d94f645a61486673eee386d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 09:24:31 +0700

    Initialize scanner and put input to a String array

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array1DUrutkanBilangan.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array1DUrutkanBilangan.java
@@ -1,9 +1,14 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class Array1DUrutkanBilangan
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int jumlahBilangan = Integer.parseInt(input.nextLine());
+        String bilanganMasuk = input.nextLine();
+        String masuk[] = new String[jumlahBilangan];
+        input.close();
     }
 }
