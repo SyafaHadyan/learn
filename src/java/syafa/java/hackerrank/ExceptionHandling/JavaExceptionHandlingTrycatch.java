c0b657d8457d17d0ca117e997dcaa972fcfe114a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 16:26:12 +0700

    First exception handling

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
@@ -6,7 +6,14 @@ public class JavaExceptionHandlingTrycatch
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int firstNumber = Integer.parseInt(input.nextLine());
-        int secondNumber = Integer.parseInt(input.nextLine());
+        try
+        {
+            int firstNumber = Integer.parseInt(input.nextLine());
+            int secondNumber = Integer.parseInt(input.nextLine());
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.print("java.util.InputMismatchException");
+        }
     }
 }
