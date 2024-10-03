413c0e89c70c863a5f26fd714184e966a1dc4ede
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 15:09:12 +0700

    Input to array then split by space

--- src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaSort.java
@@ -7,13 +7,13 @@ public class JavaSort
     {
         Scanner input = new Scanner(System.in);
         int amountOfStudents = Integer.parseInt(input.nextLine());
-        int studentID = 0;
-        String studentName = "";
-        double CGPA = 0d;
         int studentIDArray[] = new int[amountOfStudents];
         String studentNamearray[] = new String[amountOfStudents];
         double CGPAArray[] = new double[amountOfStudents];
-        String 
+        String dataInputArray[] = input.nextLine().split(" ");
+        int studentID = 0;
+        String studentName = "";
+        double CGPA = 0d;
         for (int i = 0; i < amountOfStudents; i++)
         {
             studentIDArray[i] = input.nextInt();
@@ -21,6 +21,5 @@ public class JavaSort
             CGPAArray[i] = input.nextDouble();
         }
         Arrays.sort(studentIDArray);
-        Arrays.sort
     }
 }
