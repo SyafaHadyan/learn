e96ee8ecf1e15b13b19cfa430eb36d8d044a31d2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-03 16:28:10 +0700

    Handle division exception

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandlingTrycatch.java
@@ -6,14 +6,26 @@ public class JavaExceptionHandlingTrycatch
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int firstNumber = 0;
+        int secondNumber = 0;
+        int divide = 0;
         try
         {
-            int firstNumber = Integer.parseInt(input.nextLine());
-            int secondNumber = Integer.parseInt(input.nextLine());
+            firstNumber = Integer.parseInt(input.nextLine());
+            secondNumber = Integer.parseInt(input.nextLine());
+            input.close();
         }
         catch (InputMismatchException e)
         {
             System.out.print("java.util.InputMismatchException");
+        }
+        try
+        {
+            divide = firstNumber / secondNumber;
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
         }
     }
 }
