3f2443094c381ad0abb83ba00f814755310a3c09
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 09:41:24 +0700

    put into string

--- src/java/syafa/java/hackerrank/Strings/JavaStringReverse.java
+++ src/java/syafa/java/hackerrank/Strings/JavaStringReverse.java
@@ -8,10 +8,13 @@ public class JavaStringReverse
         Scanner input = new Scanner(System.in);
         String initial = input.nextLine();
         input.close();
+        String reversedInitial = "";
         char initialLengthArray[] = new char[initial.length()];
         for (int i = initial.length() - 1 ; i >= 0; i--)
         {
             initialLengthArray[i] = initial.charAt(i);
+            reversedInitial += initialLengthArray[i];
         }
+
     }
 }
