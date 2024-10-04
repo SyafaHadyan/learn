1912f3b49a2db20a3c53f02670c705adfff01063
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 12:53:09 +0700

    Input from scanner

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
@@ -6,6 +6,8 @@ public class JavaExceptionHandling
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int firstFirst = input.nextInt();
+        int firstSecond = input.nextInt();
         input.close();
     }
 }
