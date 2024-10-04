f7d5013a8ef2d0474252cd72cf80c988f0d347e7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 20:34:51 +0700

    Create arraylist

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
@@ -7,6 +7,7 @@ public class Day6LetsReview
     {
         Scanner input = new Scanner(System.in);
         int number = Integer.parseInt(input.nextLine());
+        ArrayList<String> inputString = new ArrayList<String>();
         for (int i = 0; i < number; i++)
         {
             
