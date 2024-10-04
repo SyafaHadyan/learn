98591e5ee24c2c2e431cb22655fbc1a7dbf1a5dc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 18:07:26 +0700

    Create emtpy arraylist

--- src/java/syafa/java/hackerrank/Strings/PatternSyntaxChecker.java
+++ src/java/syafa/java/hackerrank/Strings/PatternSyntaxChecker.java
@@ -7,6 +7,7 @@ public class PatternSyntaxChecker
     {
         Scanner input = new Scanner(System.in);
         int numberOfInput = Integer.parseInt(input.nextLine());
+        ArrayList<String> validArray = new ArrayList<String>();
         String check;
         String valid;
         for (int i = 0; i < numberOfInput; i++)
