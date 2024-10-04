21d608cb8d965dc4059bb376863765f1dbe5562b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 17:09:20 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/JavaStringTokens.java
+++ src/java/syafa/java/hackerrank/Strings/JavaStringTokens.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaStringTokens
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String regularString = input.nextLine();
+        input.close();
     }
 }
