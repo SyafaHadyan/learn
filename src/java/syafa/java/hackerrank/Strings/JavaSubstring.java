f6902323cbf030b64cfbff81c7678f350b040904
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 14:58:22 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/JavaSubstring.java
+++ src/java/syafa/java/hackerrank/Strings/JavaSubstring.java
@@ -1,9 +1,14 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaSubstring
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String singleString = input.nextLine();
+        int start = input.nextInt();
+        int end = input.nextInt();
+        input.close();
     }
 }
