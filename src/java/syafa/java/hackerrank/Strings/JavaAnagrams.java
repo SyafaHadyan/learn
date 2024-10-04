72b966736dc0df35edb911bc1d376483c48d5259
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 15:04:37 +0700

    Intialize input

--- src/java/syafa/java/hackerrank/Strings/JavaAnagrams.java
+++ src/java/syafa/java/hackerrank/Strings/JavaAnagrams.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaAnagrams
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String first = input.nextLine();
+        String second = input.nextLine();
+        input.close();
     }
 }
