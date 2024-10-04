31270920c69568dc5c4676239bf98836a8529379
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 15:50:50 +0700

    Create empty char array list

--- src/java/syafa/java/hackerrank/Strings/JavaSubstringComparisons.java
+++ src/java/syafa/java/hackerrank/Strings/JavaSubstringComparisons.java
@@ -9,5 +9,10 @@ public class JavaSubstringComparisons
         String string = input.nextLine();
         int split = Integer.parseInt(input.nextLine());
         input.close();
+        ArrayList<Character>splitAt = new ArrayList<Character>();
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
     }
 }
