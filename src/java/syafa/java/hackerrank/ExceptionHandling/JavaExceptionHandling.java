e6cc10f379446ca081f078af555bba5e82b5986c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 12:55:30 +0700

    Initialize var outside of loop

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
@@ -6,10 +6,20 @@ public class JavaExceptionHandling
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
         for (int i = 1; i <= 5; i++)
         {
-            int first = input.nextInt();
-            int second = input.nextInt();
+            try
+            {
+                
+            }
+            catch (Exception e)
+            {
+                // TODO: handle exception
+            }
+            first = input.nextInt();
+            second = input.nextInt();
             input.nextLine();
         }
         input.close();
