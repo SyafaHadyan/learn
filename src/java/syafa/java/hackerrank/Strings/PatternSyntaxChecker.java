d18f7f79f6378de652a53dc82e5afe04088d5527
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 18:04:43 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/PatternSyntaxChecker.java
+++ src/java/syafa/java/hackerrank/Strings/PatternSyntaxChecker.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class PatternSyntaxChecker
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int numberOfInput = Integer.parseInt(input.nextLine());
     }
 }
