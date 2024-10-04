4c7b04da1c5c65f47b76f04263cddb651bf9aa6a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 20:33:59 +0700

    Initialize scanner and for-i

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
@@ -1,9 +1,15 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
 public class Day6LetsReview
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int number = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < number; i++)
+        {
+            
+        }
     }
 }
