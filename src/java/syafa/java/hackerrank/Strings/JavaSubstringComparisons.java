359437fa05265a719a1d071b6aa651782a097aa8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 15:48:58 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/JavaSubstringComparisons.java
+++ src/java/syafa/java/hackerrank/Strings/JavaSubstringComparisons.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaSubstringComparisons
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        input.close();
     }
 }
