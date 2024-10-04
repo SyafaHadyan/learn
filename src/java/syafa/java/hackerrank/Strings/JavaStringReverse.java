eed5fbe18db07592d0013d91c07a0d09b74743ef
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 09:37:33 +0700

    Create empty array with the size of input string length

--- src/java/syafa/java/hackerrank/Strings/JavaStringReverse.java
+++ src/java/syafa/java/hackerrank/Strings/JavaStringReverse.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaStringReverse
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String initial = input.nextLine();
+        input.close();
+        String initialLengthArray[] = new String[initial.length()];
     }
 }
