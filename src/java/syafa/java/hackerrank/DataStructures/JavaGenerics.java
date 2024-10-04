c572178ce4b48685dc84f473439f5f289b7f5942
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 19:42:00 +0700

    Create ArrayList

--- src/java/syafa/java/hackerrank/DataStructures/JavaGenerics.java
+++ src/java/syafa/java/hackerrank/DataStructures/JavaGenerics.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.DataStructures;
+import java.util.*;
 
 public class JavaGenerics
 {
     public static void main(String[] args)
     {
-        
+        ArrayList<String> outputString = new ArrayList<String>();
+        ArrayList<Integer> outputInteger = new ArrayList<Integer>();
     }
 }
