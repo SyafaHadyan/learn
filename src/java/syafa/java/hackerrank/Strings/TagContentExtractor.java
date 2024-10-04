20c61e9bfb10f954cba36e0525491d02f21f5c33
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 19:14:17 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/TagContentExtractor.java
+++ src/java/syafa/java/hackerrank/Strings/TagContentExtractor.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class TagContentExtractor
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int amount = Integer.parseInt(input.nextLine());
     }
 }
