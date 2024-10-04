1f908320fff26804c3d4bf32254d8d117e2b1e99
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 19:54:17 +0700

    Convert to string

--- src/java/syafa/java/hackerrank/BigNumber/JavaPrimalityTest.java
+++ src/java/syafa/java/hackerrank/BigNumber/JavaPrimalityTest.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.BigNumber;
+import java.util.*;
 
 public class JavaPrimalityTest
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        double checkNumber = Double.parseDouble(input.nextLine());
+        String convertString = String.format("%.0f",checkNumber);
     }
 }
