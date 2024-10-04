ff7f00d5273483946657549e4dcf5184bf05b247
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 20:36:48 +0700

    Create arraylist character

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day6LetsReview.java
@@ -8,6 +8,7 @@ public class Day6LetsReview
         Scanner input = new Scanner(System.in);
         int number = Integer.parseInt(input.nextLine());
         ArrayList<String> inputString = new ArrayList<String>();
+        ArrayList<Character> inputCharacter = new ArrayList<Character>();
         for (int i = 0; i < number; i++)
         {
             inputString.add(input.nextLine());
