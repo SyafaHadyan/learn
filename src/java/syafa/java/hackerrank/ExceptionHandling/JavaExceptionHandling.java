8699308da1291cec74b0f9bf67cf95a68034b694
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 12:54:06 +0700

    Move input to for-i

--- src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
+++ src/java/syafa/java/hackerrank/ExceptionHandling/JavaExceptionHandling.java
@@ -6,8 +6,12 @@ public class JavaExceptionHandling
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int firstFirst = input.nextInt();
-        int firstSecond = input.nextInt();
+        for (int i = 1; i <= 5; i++)
+        {
+            int firstFirst = input.nextInt();
+            int firstSecond = input.nextInt();
+            input.nextLine();
+        }
         input.close();
     }
 }
