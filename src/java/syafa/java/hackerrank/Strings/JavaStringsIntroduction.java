c7e00b7f44592390372466f4f967bab4c9c464a9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-04 10:23:05 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Strings/JavaStringsIntroduction.java
+++ src/java/syafa/java/hackerrank/Strings/JavaStringsIntroduction.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.Strings;
+import java.util.*;
 
 public class JavaStringsIntroduction
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        String firstString = input.nextLine();
+        String secondString = input.nextLine();
+        input.close();
     }
 }
