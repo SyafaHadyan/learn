eb4363ed6ed9299bd5fbcc00609cfc3d46016154
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 02:18:14 +0700

    Use counter

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
@@ -7,10 +7,13 @@ public class PlusMinus
     {
         Scanner input = new Scanner(System.in);
         int size = Integer.parseInt(input.nextLine());
+        double positiveNumber = 0;
+        double negativeNumber = 0;
+        double zeroNumber = 0; 
         ArrayList<String> numberString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         for (int i = 0; i < numberString.size(); i++)
         {
-            if (Double.parseDouble(numberString.get(i)) == 0)
+            if (Double.parseDouble(numberString.get(i)) > 0)
             {
 
             }
