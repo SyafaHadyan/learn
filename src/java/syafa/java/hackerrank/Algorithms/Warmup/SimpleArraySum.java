f67363c3e047b7a1114b4ef6ebdfaaca5abe0386
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 15:00:39 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/SimpleArraySum.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/SimpleArraySum.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
+import java.util.*;
 
 public class SimpleArraySum
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0,useless.length());
     }
 }
