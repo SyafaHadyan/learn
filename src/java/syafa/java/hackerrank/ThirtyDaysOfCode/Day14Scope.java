a8c5b02844d972845f7733eed427ef814c4cbf47
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 14:24:06 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day14Scope.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day14Scope.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
 public class Day14Scope
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
