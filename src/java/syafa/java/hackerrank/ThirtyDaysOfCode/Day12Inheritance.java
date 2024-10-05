ee6abf2438356298e7acfb27514220786cd33baf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 13:51:50 +0700

    studentDetail arrlist split by space

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
@@ -6,7 +6,7 @@ public class Day12Inheritance
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        ArrayList<String> studentDetail = new ArrayList<String>(Arrays.asList(input.nextLine()));
+        ArrayList<String> studentDetail = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> studentGrade = new ArrayList<String>();
         int amountOfGrades = Integer.parseInt(input.nextLine());
         for (int i = 0; i < amountOfGrades; i++)
