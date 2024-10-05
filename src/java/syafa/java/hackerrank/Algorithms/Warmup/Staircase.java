19f698b50589bbf27b8daa22084423fee91e5a74
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 02:29:25 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/Staircase.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/Staircase.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
+import java.util.*;
 
 public class Staircase
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int staricaseSize = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
