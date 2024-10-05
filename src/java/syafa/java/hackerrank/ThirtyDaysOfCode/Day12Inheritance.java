aa69f6b4372d9539cfc2a14b6d4c91bb7b7537b6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 13:37:49 +0700

    Create arrlist for student

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
 public class Day12Inheritance
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> studentDetail = new ArrayList<String>();
+        ArrayList<Integer> studentGrade = new ArrayList<Integer>();
+        input.close();
     }
 }
