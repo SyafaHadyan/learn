c70b574a58ecd70d4e3e8690903b633c6c3ea7a3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 13:54:37 +0700

    Create parsedstudentdetail

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
@@ -9,10 +9,7 @@ public class Day12Inheritance
         ArrayList<String> studentDetail = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         int amountOfGrades = Integer.parseInt(input.nextLine());
         ArrayList<String> studentGrade = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-        for (int i = 0; i < amountOfGrades; i++)
-        {
-            studentGrade.add(input.nextLine());
-        }
+        ArrayList<String> parsedStudentDetail = new ArrayList<String>();
         input.close();
     }
 }
