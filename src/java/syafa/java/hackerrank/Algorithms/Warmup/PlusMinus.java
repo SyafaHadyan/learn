b0a2948283d5ca9d031481a008fd1aaeafc65460
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 02:12:04 +0700

    Input size

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
+import java.util.*;
 
 public class PlusMinus
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int size = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
