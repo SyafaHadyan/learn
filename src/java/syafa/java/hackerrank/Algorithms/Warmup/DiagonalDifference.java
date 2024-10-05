9c46f9d3679ef9669c625988aced77d5f213570a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 16:36:56 +0700

    Initialize file

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/DiagonalDifference.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/DiagonalDifference.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
 
-public class DiagonalDifference {
-
+public class DiagonalDifference
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
