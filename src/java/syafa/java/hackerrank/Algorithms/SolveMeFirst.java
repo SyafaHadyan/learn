6665aa0afb6969cde81dc5ba025c5b384d6b786e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 14:56:11 +0700

    Input

--- src/java/syafa/java/hackerrank/Algorithms/SolveMeFirst.java
+++ src/java/syafa/java/hackerrank/Algorithms/SolveMeFirst.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.Algorithms;
+import java.util.*;
 
 public class SolveMeFirst
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int first = Integer.parseInt(input.nextLine());
+        int second = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
