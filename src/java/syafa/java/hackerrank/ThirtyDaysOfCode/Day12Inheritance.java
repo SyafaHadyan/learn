c5392285612e81af6d4b06abc764a3a25d7c8156
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 13:53:01 +0700

    Direct student grade input to arrlist

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
@@ -7,8 +7,8 @@ public class Day12Inheritance
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> studentDetail = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-        ArrayList<String> studentGrade = new ArrayList<String>();
         int amountOfGrades = Integer.parseInt(input.nextLine());
+        ArrayList<String> studentGrade = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         for (int i = 0; i < amountOfGrades; i++)
         {
             studentGrade.add(input.nextLine());
