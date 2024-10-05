f0579d052e5680e2b2f014396a82fd17fe0c7055
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-05 13:57:00 +0700

    Get val form arrlist then print

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day12Inheritance.java
@@ -11,6 +11,7 @@ public class Day12Inheritance
         ArrayList<String> studentGrade = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> parsedStudentDetail = new ArrayList<String>();
         char gradeLetter = 'O';
+        System.out.println("Name" + ':' + ' ' + studentDetail.get(1) + ',' + ' ' + studentDetail.get(0));
         input.close();
     }
 }
