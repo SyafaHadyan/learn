4bc36d23b6e55862394c845b234145f3a79c4b23
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 02:13:13 +0700

    Input to arrlist str

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/PlusMinus.java
@@ -7,6 +7,7 @@ public class PlusMinus
     {
         Scanner input = new Scanner(System.in);
         int size = Integer.parseInt(input.nextLine());
+        ArrayList<String> numbers = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
