bba3a1a53d17bf6c7ea282194f59107cabe8d2be
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 20:11:29 +0700

    Fixed

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
@@ -23,7 +23,7 @@ public class BirthdayCakeCandles
        for (int i = 0; i < inputCandleDouble.size(); i++)
        {
             //Integer.parseInt(inputCandleString.get(i)) == Integer.parseInt(inputCandleString.get(inputCandleString.size()))
-            if (inputCandleDouble.get(i) == inputCandleDouble.getLast())
+            if (inputCandleDouble.get(i).equals(inputCandleDouble.getLast()))
             {
                 highestMatch++;
                 //System.out.println(highestMatch);
