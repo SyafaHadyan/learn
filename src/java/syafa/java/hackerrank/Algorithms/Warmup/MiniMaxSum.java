efe3bfdb11c17ef20cfc619e00685408468d4390
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 19:16:13 +0700

    Arrilst int

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
@@ -7,5 +7,10 @@ public class MiniMaxSum
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> inputString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<Integer> inputInteger = new ArrayList<Integer>();
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
     }
 }
