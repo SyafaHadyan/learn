37df15911bde2b265d0d627bd1385f19a2d763a1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 19:15:17 +0700

    Input to arrlist as str

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
+import java.util.*;
 
 public class MiniMaxSum
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> inputString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
     }
 }
