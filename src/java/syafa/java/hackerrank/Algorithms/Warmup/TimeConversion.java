520223b830abae686ea8ebeadc404f161a504b36
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 20:41:11 +0700

    Arrlist str split by :

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/TimeConversion.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/TimeConversion.java
@@ -6,6 +6,7 @@ public class TimeConversion
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> inputTimeString = new ArrayList<String>(Arrays.asList(input.nextLine().split(":")));
         input.close();
     }
 }
