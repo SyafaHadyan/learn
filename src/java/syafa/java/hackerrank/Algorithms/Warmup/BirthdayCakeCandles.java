78f353260aafa09a055432f1c3ebc6b2c0e04465
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 19:32:37 +0700

    Input as str stored in arrlist split by space

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
@@ -6,6 +6,7 @@ public class BirthdayCakeCandles
     public static void main(String[] args)
     {
        Scanner input = new Scanner(System.in);
+       ArrayList<String> inputCandleString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
        input.close();
     }
 }
