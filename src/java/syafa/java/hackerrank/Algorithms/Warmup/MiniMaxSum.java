f2867a2266e81fbc10b8ee370bcccc68522585ae
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 19:16:54 +0700

    Put val then parse as int

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/MiniMaxSum.java
@@ -8,9 +8,9 @@ public class MiniMaxSum
         Scanner input = new Scanner(System.in);
         ArrayList<String> inputString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<Integer> inputInteger = new ArrayList<Integer>();
-        for (int i = 0; i < args.length; i++)
+        for (int i = 0; i < inputString.size(); i++)
         {
-            
+            inputInteger.add(Integer.parseInt(inputString.get(i)));
         }
     }
 }
