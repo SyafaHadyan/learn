76bb784f05d230ce05861d9f961c4da9420331e1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-06 19:31:51 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
+++ src/java/syafa/java/hackerrank/Algorithms/Warmup/BirthdayCakeCandles.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Warmup;
+import java.util.*;
 
 public class BirthdayCakeCandles
 {
     public static void main(String[] args)
     {
-       
+       Scanner input = new Scanner(System.in);
+       input.close();
     }
 }
