7a4ba454ff581763d010e4262023af850d9e1534
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 21:53:42 +0700

    Add file

---
+++ src/java/syafa/java/Praktikum/Bab3/N09LoopingDoWhlie.java
@@ -0,0 +1,14 @@
+package syafa.java.Praktikum.Bab3;
+
+public class N09LoopingDoWhlie
+{
+    public static void main(String[] args)
+    {
+        int nilai=1;
+        do
+        {
+            System.out.println(nilai); nilai++;
+        }
+        while(nilai<=10);
+    }
+}
