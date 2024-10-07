e09678bd6eab4c608b907673f1d47f13a9d0e17e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 16:43:50 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
 
-public class Array2DTicTacToe {
-
+public class Array2DTicTacToe
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
