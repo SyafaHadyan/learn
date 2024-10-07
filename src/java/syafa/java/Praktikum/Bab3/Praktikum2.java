d92b9f1e5fa8f493f9f3aae5dfbd298667d2e7c7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 06:34:21 +0700

    Printf

--- src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
@@ -6,6 +6,7 @@ public class Praktikum2
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%s\n%s%s%s%s%s%s\n\n%s%-2c");
         input.close();
     }
 }
