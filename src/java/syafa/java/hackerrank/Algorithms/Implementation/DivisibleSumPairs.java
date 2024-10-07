5058da530a1eb78914984b127d71fd2b1b84ac00
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 08:16:32 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DivisibleSumPairs.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DivisibleSumPairs.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class DivisibleSumPairs {
-
+public class DivisibleSumPairs
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
