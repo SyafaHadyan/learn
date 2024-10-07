d09031eddd273d896a1d358783a75f526a010336
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 11:07:09 +0700

    Input items to arrlist str

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BillDivision.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BillDivision.java
@@ -6,6 +6,7 @@ public class BillDivision
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
