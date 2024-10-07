468e3c4e9cceebd6e1e147a0b4a23ea2b5e36c68
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 12:23:34 +0700

    Print

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
@@ -14,7 +14,9 @@ public class TheHurdleRace
             if (Integer.parseInt(jump.get(1) + Integer.parseInt(hurdle.get(i))) < Integer.parseInt(hurdle.get(i + 1)))
             {
                 System.out.print((Integer.parseInt(hurdle.get(i + 1)) - (Integer.parseInt(jump.get(1)) + Integer.parseInt(hurdle.get(i)))));
+                return;
             }
         }
+        System.out.print(0);
     }
 }
