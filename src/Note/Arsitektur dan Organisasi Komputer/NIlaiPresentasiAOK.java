0ad493391e31fdf59a2fcbdb91b39e4b62d7bf23
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 17:55:23 +0700

    Edit

--- src/Note/Arsitektur dan Organisasi Komputer/NIlaiPresentasiAOK.java
+++ src/Note/Arsitektur dan Organisasi Komputer/NIlaiPresentasiAOK.java
@@ -1,10 +1,12 @@
-import java.util.*;
-
 public class NIlaiPresentasiAOK
 {
     public static void main(String[] args)
     {
         int nilaiKKM = 90;
-        
+        for (int i = nilaiKKM; i >= 0; i--)
+        {
+            nilaiKKM = i;
+        }
+        System.out.print("Nilai presentasi anda adalah" + ' ' + nilaiKKM);
     }
 }
