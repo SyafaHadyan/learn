c92689e0803afdb03477059161659bb542e41063
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 17:03:32 +0700

    Third condition

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
@@ -39,7 +39,7 @@ public class Array2DTicTacToe
                 }
             }
         }
-        
+
         // First condition O
         if (first[0].equalsIgnoreCase("O"))
         {
@@ -88,6 +88,64 @@ public class Array2DTicTacToe
             if (first[0].equalsIgnoreCase("O"))
             {
                 if (first[2].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+        }
+
+        // Third condition X
+        if (first[2].equalsIgnoreCase("X"))
+        {
+            if (first[1].equalsIgnoreCase("X"))
+            {
+                if (first[0].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+            if (second[2].equalsIgnoreCase("X"))
+            {
+                if (third[2].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+            if (second[1].equalsIgnoreCase("X"))
+            {
+                if (third[0].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+        }
+
+        // Third condition O
+        if (first[2].equalsIgnoreCase("O"))
+        {
+            if (first[1].equalsIgnoreCase("O"))
+            {
+                if (first[0].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+            if (second[2].equalsIgnoreCase("O"))
+            {
+                if (third[2].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+            if (second[1].equalsIgnoreCase("O"))
+            {
+                if (third[0].equalsIgnoreCase("O"))
                 {
                     System.out.print("O Menang");
                     return;
