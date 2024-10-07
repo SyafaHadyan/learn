ffcfb60959d1a6f77c48f96e5fa6c5cb88e6adbe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 06:25:27 +0700

    Already did

--- src/java/syafa/java/Praktikum/Bab3/Praktikum0.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum0.java
@@ -6,7 +6,24 @@ public class Praktikum0
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System..in);
-        int staircaseHeight = Integer.parseInt(input.nextLine());
+        int staircaseSize = Integer.parseInt(input.nextLine());
+        int space = staircaseSize - 1;
         input.close();
+        for (int i = 0; i < staircaseSize; i++)
+        {
+            for (int j = space; j > 0; j--)
+            {
+                System.out.print(' ');
+            }
+            space--;
+            for (int j = 0; j < i + 1; j++)
+            {
+                System.out.print('#');
+            }
+            if (i < staircaseSize)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
