5a5fd5ecfae0ca26df2af39922d9b1a1ad5b5b1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 12:46:40 +0700

    Edit

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
@@ -12,11 +12,14 @@ public class TheHurdleRace
         int dose = 0;
         for (int i = 0; i < hurdle.size() - 1; i++)
         {
-            System.out.println("Next" + ';' + " " + Integer.parseInt(hurdle.get(i + 1)));
+            System.out.println("Next" + ':' + " " + Integer.parseInt(hurdle.get(i + 1)));
             if ((Integer.parseInt(jump.get(1)) + Integer.parseInt(hurdle.get(i))) < Integer.parseInt(hurdle.get(i + 1)))
             {
                 System.out.println("Dose needed" + ':' + ' ' + (Integer.parseInt(hurdle.get(i + 1)) - Integer.parseInt(jump.get(1))));
-                dose += Integer.parseInt(hurdle.get(i + 1)) - Integer.parseInt(jump.get(1));
+                if (Integer.parseInt(hurdle.get(i + 1)) - Integer.parseInt(jump.get(1)) > dose)
+                {
+                    dose = Integer.parseInt(hurdle.get(i + 1)) - Integer.parseInt(jump.get(1));
+                }
                 System.out.println("Current dose" + ':' + ' ' + dose);
             }
 
