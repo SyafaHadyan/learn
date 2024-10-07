dc9cdd848eecf45cad264545e2c9e623c89385f8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 08:05:15 +0700

    Temp remove

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BreakingTheRecords.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BreakingTheRecords.java
@@ -6,14 +6,13 @@ public class BreakingTheRecords
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        StringBuilder integer = new StringBuilder(input.nextLine());
+        //StringBuilder integer = new StringBuilder(input.nextLine());
         ArrayList<String> inputString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
         int min = 0;
         int max = 0;
         int currentLow = 0;
         int currentHigh = 0;
-        ArrayList<Integer> score = new ArrayList<Integer>();
         for (int i = 0; i < inputString.size(); i++)
         {
             if (i == 0)
