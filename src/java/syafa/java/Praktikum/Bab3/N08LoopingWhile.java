d6e21662c63fb717eb6912fbdbee995323adc517
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 21:39:22 +0700

    Edit to mult of 2

--- src/java/syafa/java/Praktikum/Bab3/N08LoopingWhile.java
+++ src/java/syafa/java/Praktikum/Bab3/N08LoopingWhile.java
@@ -4,11 +4,11 @@ public class N08LoopingWhile
 {
     public static void main(String[] args)
     {
-        int nilai = 1;
+        int nilai = 2;
         do
         {
             System.out.println(nilai);
-            nilai++;
+            nilai += 2;
         }
         while(nilai <= 10);
     }
