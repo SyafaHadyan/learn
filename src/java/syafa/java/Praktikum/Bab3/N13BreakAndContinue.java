795ae690f4ed6fe4f22bea1d84702d632ef864c3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 05:14:40 +0700

    Edit

--- src/java/syafa/java/Praktikum/Bab3/N13BreakAndContinue.java
+++ src/java/syafa/java/Praktikum/Bab3/N13BreakAndContinue.java
@@ -4,6 +4,30 @@ public class N13BreakAndContinue
 {
     public static void main(String[] args)
     {
-        
+        int nilai;
+        System.out.println("pernyataan break batas 10");
+        for(nilai = 1; nilai<=10; nilai++)
+        {
+            if(nilai == 5)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println(nilai);
+            }
+        }
+        System.out.println("\n" + "pernyataan continue batas 10");
+        for(nilai = 1;nilai<=10;nilai++)
+        {
+            if(nilai==5)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println(nilai);
+            }
+        }
     }
 }
