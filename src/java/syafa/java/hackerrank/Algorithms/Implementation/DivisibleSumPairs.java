3c4fdff99c3939d8e2914272f2945a0d0e2176a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 08:30:03 +0700

    Test

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DivisibleSumPairs.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DivisibleSumPairs.java
@@ -14,9 +14,10 @@ public class DivisibleSumPairs
         {
             for (int j = 0; j < pairs.size(); j++)
             {
-                if (Integer.parseInt(pairs.get(i)) + Integer.parseInt(pairs.get(j)) % Integer.parseInt(initialValue.get(1)) == 0)
+                System.out.println(Integer.parseInt(pairs.get(i)) + " " + Integer.parseInt(pairs.get(j)) + " " + Integer.parseInt(initialValue.get(1)));
+                if ((Integer.parseInt(pairs.get(i)) + Integer.parseInt(pairs.get(j))) % Integer.parseInt(initialValue.get(1)) == 0)
                 {
-                    System.out.println(Integer.parseInt(pairs.get(i)) + " " + Integer.parseInt(pairs.get(j)) + " " + Integer.parseInt(initialValue.get(1)));
+                    //System.out.println(Integer.parseInt(pairs.get(i)) + " " + Integer.parseInt(pairs.get(j)) + " " + Integer.parseInt(initialValue.get(1)));
                     divisiblePairs++;
                 }
             }
