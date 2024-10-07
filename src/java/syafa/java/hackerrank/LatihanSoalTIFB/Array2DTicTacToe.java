2cd0a4e12959eb02f5da906a0c4e1f2eef1f8a8d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 16:45:18 +0700

    Input str arr split by space

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
@@ -6,6 +6,9 @@ public class Array2DTicTacToe
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[] first = input.nextLine().split(" ");
+        String[] second = input.nextLine().split(" ");
+        String[] third = input.nextLine().split(" ");
         input.close();
     }
 }
