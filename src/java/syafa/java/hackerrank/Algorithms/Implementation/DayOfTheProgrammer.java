e950f130c13315c4be967e3baee57ed4e796d5de
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 19:20:28 +0700

    Add .

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
@@ -10,9 +10,9 @@ public class DayOfTheProgrammer
         input.close();
         if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
         {
-            System.out.print("12.09" + year);
+            System.out.print("12.09" + '.' + year);
             return;
         }
-        System.out.print("13.09" + year);
+        System.out.print("13.09" + '.' + year);
     }
 }
