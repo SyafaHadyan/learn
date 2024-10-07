797a63f32d38e9303b8a67909c2e95ae5cb58dfe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 07:20:00 +0700

    Amount of grades

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/GradingStudents.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/GradingStudents.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
 
 public class GradingStudents
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int amountOfGrades = Integer.parseInt(input.nextLine());
     }
 }
