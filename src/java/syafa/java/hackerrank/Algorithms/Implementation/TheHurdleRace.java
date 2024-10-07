874d5d29b7dbf084e3d85825fdd092b942e5e144
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 12:17:25 +0700

    Arrlist str jump split by space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
@@ -6,6 +6,7 @@ public class TheHurdleRace
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> jump = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
