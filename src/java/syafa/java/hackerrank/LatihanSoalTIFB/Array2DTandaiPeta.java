bd5fc3df305008e1e86a3c3d45c43e8c180f2101
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 15:43:10 +0700

    Create str arr

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
@@ -6,8 +6,9 @@ public class Array2DTandaiPeta
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int arrayLength = Integer.parseInt(input.nextLine());
+        int arrayHeight = Integer.parseInt(input.nextLine());
         int arrayWidth = Integer.parseInt(input.nextLine());
         int amountOfMarks = Integer.parseInt(input.nextLine());
+        String[][] map = new String[arrayHeight][arrayWidth];
     }
 }
