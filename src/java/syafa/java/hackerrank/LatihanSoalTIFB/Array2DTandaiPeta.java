b735fc6c3eef8b545e1d2cb26b2691913406f0a3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 15:41:07 +0700

    Input arr

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class Array2DTandaiPeta
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int arrayWidth = Integer.parseInt(input.nextLine());
     }
 }
