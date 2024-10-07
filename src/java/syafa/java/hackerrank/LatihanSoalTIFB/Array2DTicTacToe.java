e178e154a03817ef565e920a79992545c3a1645c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 18:21:36 +0700

    Sixth condition

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTicTacToe.java
@@ -181,6 +181,64 @@ public class Array2DTicTacToe
             System.out.print("O Menang");
             return;
         }
+
+        // Sixth condition X
+        if (third[0].equalsIgnoreCase("X"))
+        {
+            if (second[0].equalsIgnoreCase("X"))
+            {
+                if (first[0].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+            if (third[1].equalsIgnoreCase("X"))
+            {
+                if (third[2].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+            if (second[1].equalsIgnoreCase("X"))
+            {
+                if (first[2].equalsIgnoreCase("X"))
+                {
+                    System.out.print("X Menang");
+                    return;
+                }
+            }
+        }
+
+        // Sixth condition O
+        if (third[0].equalsIgnoreCase("O"))
+        {
+            if (second[0].equalsIgnoreCase("O"))
+            {
+                if (first[0].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+            if (third[1].equalsIgnoreCase("O"))
+            {
+                if (third[2].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+            if (second[1].equalsIgnoreCase("O"))
+            {
+                if (first[2].equalsIgnoreCase("O"))
+                {
+                    System.out.print("O Menang");
+                    return;
+                }
+            }
+        }
         System.out.print("Draw");
     }
 }
