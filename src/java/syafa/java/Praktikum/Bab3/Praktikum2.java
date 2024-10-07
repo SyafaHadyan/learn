70135a8178bba3b2067baa02511628fa9ddd01b1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 06:33:08 +0700

    Initialize input

--- src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
@@ -1,9 +1,11 @@
 package syafa.java.Praktikum.Bab3;
+import java.util.*;
 
 public class Praktikum2
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
