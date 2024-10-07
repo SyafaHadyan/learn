fdba72bff13a2848bf963d6161d8b7cee01bc95f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 12:15:57 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheHurdleRace.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class TheHurdleRace {
-
+public class TheHurdleRace
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
