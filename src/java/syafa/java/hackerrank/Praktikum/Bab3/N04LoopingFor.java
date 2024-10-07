d1486d93474c1894fec19168a6778c8c24254814
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 21:11:22 +0700

    Add file

---
+++ src/java/syafa/java/hackerrank/Praktikum/Bab3/N04LoopingFor.java
@@ -0,0 +1,35 @@
+package syafa.java.hackerrank.Praktikum.Bab3;
+import java.util.Scanner;
+
+public class N04LoopingFor
+{
+    public static void main(String[] args)
+    {
+        /*
+         * for (inisialisasi; kondisi; ekspresi_iterasi) {
+         * statement1;
+         * statement2;
+         * ...
+         * }
+         */
+
+        // instansiasi scanner
+        Scanner input = new Scanner(System.in);
+
+        // input dari user
+        int n = input.nextInt(); 
+        input.close();
+        
+        // inisialisasi dengan nilai awal i = 0
+      	// looping selama nilai i <= n
+        for (int i = 0; i <= n; /*i++*/)
+        {
+            // setiap iterasi, n bertambah 1
+            System.out.print(i);
+            if (i <= n)
+            {
+                System.out.print("\n");
+            }
+        }
+    }
+}
