5d64720505cc1e06273f83628a055c6fae4d5298
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 11:19:17 +0700

    Test val

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BillDivision.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BillDivision.java
@@ -18,9 +18,10 @@ public class BillDivision
                 continue;
             }
             totalPrice += Integer.parseInt(itemPrices.get(i));
+            System.out.println(totalPrice);
         }
-        System.out.println(charge + "\n" + (totalPrice / 2));
-        if (charge == totalPrice / 2)
+        System.out.println(totalPrice + "\n" + (totalPrice / 2));
+        if (!(charge == (totalPrice / 2)))
         {
             System.out.print(charge - (totalPrice / 2));
             return;
