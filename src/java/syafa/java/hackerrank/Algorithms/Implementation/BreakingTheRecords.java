a58584c658415c99e0d477018a67c4f491a6cc1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 07:57:04 +0700

    Increment min

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BreakingTheRecords.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BreakingTheRecords.java
@@ -20,6 +20,10 @@ public class BreakingTheRecords
             {
                 continue;
             }
+            else if (Integer.parseInt(inputString.get(i)) < 0 currentLow)
+            {
+                min++;
+            }
         }
     }
 }
