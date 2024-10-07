2ec3e85788316fbb7f6537508c573b0c21ee14b3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 06:38:38 +0700

    Fixed printf

--- src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
@@ -6,7 +6,7 @@ public class Praktikum2
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n\n%s%-2c",
+        System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%-20s%-2c",
                             "MENU",
                             "0. KELUAR",
                             "1. HITUNG VOLUME BALOK",
@@ -14,7 +14,7 @@ public class Praktikum2
                             "3. HITUNG VOLUME KERUCUT",
                             "4. HITUNG VOLUME SILINDER",
                             "5. HITUNG VOLUME LIMAS SEGITIGA",
-                            "MASUKKAN PILIHAN ANDA");
+                            "MASUKKAN PILIHAN ANDA",':');
         input.close();
     }
 }
