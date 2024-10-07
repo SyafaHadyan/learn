d57345747202d23b3546faef0907aea48c4159d0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 21:26:01 +0700

    Add file

---
+++ src/java/syafa/java/Praktikum/Bab3/N05LoopingWhile.java
@@ -0,0 +1,15 @@
+package syafa.java.Praktikum.Bab3;
+
+public class N05LoopingWhile
+{
+    public static void main(String[] args)
+    {
+        int nilai=1;
+        do
+        {
+            System.out.println(nilai);
+            nilai++;
+        }
+        while(nilai <= 10);
+    }
+}
