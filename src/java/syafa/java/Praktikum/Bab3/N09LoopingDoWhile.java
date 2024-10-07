fdc43c0a0d3d9ea9d06579b022cd19fd1c9cd44c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 04:50:29 +0700

    Edit

--- src/java/syafa/java/Praktikum/Bab3/N09LoopingDoWhile.java
+++ src/java/syafa/java/Praktikum/Bab3/N09LoopingDoWhile.java
@@ -4,11 +4,16 @@ public class N09LoopingDoWhile
 {
     public static void main(String[] args)
     {
-        int nilai=1;
+        int nilai = 1;
         do
         {
-            System.out.println(nilai); nilai++;
+            System.out.println(nilai);
+            if (nilai < 10)
+            {
+                System.out.print("\n");
+            }
+            nilai++;
         }
-        while(nilai<=10);
+        while(nilai <= 10);
     }
 }
