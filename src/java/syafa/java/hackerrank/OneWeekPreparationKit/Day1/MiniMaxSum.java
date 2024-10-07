276c8107efe417b4889c3b3ffe152a8c15c24456
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 19:08:31 +0700

    Add file

---
+++ src/java/syafa/java/hackerrank/OneWeekPreparationKit/Day1/MiniMaxSum.java
@@ -0,0 +1,29 @@
+package syafa.java.hackerrank.OneWeekPreparationKit.Day1;
+import java.util.*;
+
+public class MiniMaxSum
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> inputString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        input.close();
+        ArrayList<Double> inputDouble = new ArrayList<Double>();
+        double minSum = 0d;
+        double maxSum = 0d;
+        for (int i = 0; i < inputString.size(); i++)
+        {
+            inputDouble.add(Double.parseDouble(inputString.get(i)));
+        }
+        Collections.sort(inputDouble);
+        for (int i = 0; i < inputDouble.size() - 1; i++)
+        {
+            minSum += inputDouble.get(i);
+        }
+        for (int i = 1; i < inputDouble.size(); i++)
+        {
+            maxSum += inputDouble.get(i);
+        }
+        System.out.printf("%.0f%s%.0f",minSum," ",maxSum);
+    }
+}
