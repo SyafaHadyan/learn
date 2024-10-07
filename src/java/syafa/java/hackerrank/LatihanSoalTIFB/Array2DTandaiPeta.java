cae7758ff6bab9f89da765017dd368d29b43cc30
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 15:51:41 +0700

    Edit get

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DTandaiPeta.java
@@ -8,20 +8,20 @@ public class Array2DTandaiPeta
         Scanner input = new Scanner(System.in);
         ArrayList<String> arraySize = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         int amountOfMarks = Integer.parseInt(input.nextLine());
-        String[][] map = new String[arrayHeight][arrayWidth];
+        String[][] map = new String[Integer.parseInt(arraySize.get(0))][Integer.parseInt(arraySize.get(1))];
         for (int i = 0; i < amountOfMarks; i++)
         {
             String[] markAt = input.nextLine().split(" ");
             map[Integer.parseInt(markAt[0])][Integer.parseInt(markAt[1])] = "x";
         }
         input.close();
-        for (int i = 0; i < arrayHeight; i++)
+        for (int i = 0; i < Integer.parseInt(arraySize.get(0)); i++)
         {
-            for (int j = 0; j < arrayWidth; j++)
+            for (int j = 0; j < Integer.parseInt(arraySize.get(1)); j++)
             {
                 System.out.print(map[i][j]);
             }
-            if (i < arrayHeight)
+            if (i < Integer.parseInt(arraySize.get(0)))
             {
                 System.out.print("\n");
             }
