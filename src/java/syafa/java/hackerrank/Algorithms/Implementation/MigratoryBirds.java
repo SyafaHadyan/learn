720d8ede31986d99a5423bc5a8d2b098e2300a1c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-07 14:50:44 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class MigratoryBirds {
-
+public class MigratoryBirds
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
