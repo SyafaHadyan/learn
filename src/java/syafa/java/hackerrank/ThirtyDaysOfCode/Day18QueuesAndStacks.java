6c2fe5dba3aaa3370582a612e1f23b5d3e298be9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:49:19 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day18QueuesAndStacks.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day18QueuesAndStacks.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 
-public class Day18QueuesAndStacks {
-
+public class Day18QueuesAndStacks
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
