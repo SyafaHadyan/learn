6fbcfdca93e6170e6cd95bc930022068fc0d168e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 07:02:14 +0700

    Input

--- src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum2.java
@@ -58,7 +58,14 @@ public class Praktikum2
             }
             else if (selection == 5)
             {
-                //
+                System.out.printf("-25s%-2c","Masukkan radius",':');
+                radius = Double.parseDouble(input.nextLine());
+                System.out.printf("-25s%-2c","Masukkan tinggi",':');
+                height = Double.parseDouble(input.nextLine());
+                System.out.printf("-25s%-2c","Masukkan sisi pertama",':');
+                firstSide = Double.parseDouble(input.nextLine());
+                System.out.printf("-25s%-2c","Masukkan sisi kedua",':');
+                secondSide = Double.parseDouble(input.nextLine());
             }
             else
             {
