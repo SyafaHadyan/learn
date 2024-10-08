96aeded60c249596c96bc8e1e147c27fdff68c0d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 19:47:39 +0700

    Use arrlist

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
@@ -7,8 +7,10 @@ public class Day112DArrays
     {
         Scanner input = new Scanner(System.in);
         Integer[][] hourGlass = new Integer[6][6];
+        ArrayList<Integer> hourGlassArrList = new ArrayList<Integer>();
         for (int i = 0; i < 6; i++)
         {
+            hourGlassArrList.add(Integer.parseInt(Arrays.toString(input.nextLine().split(" "))));
             hourGlass[i][] = Integer.parseInt(Arrays.toString(input.nextLine().split(" ")));
         }
         input.close();
