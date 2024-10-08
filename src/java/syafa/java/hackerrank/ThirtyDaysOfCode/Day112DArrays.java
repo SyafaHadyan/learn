446bce2956e0e2c7583ae1973200825a91bb0767
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:25:15 +0700

    Suppress warnings

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Day112DArrays
 {
+    @SuppressWarnings("unused")
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
