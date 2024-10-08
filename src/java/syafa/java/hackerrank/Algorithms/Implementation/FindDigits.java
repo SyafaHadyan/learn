b38fe34d5015d8bd259e177cd34a3b284c35dee4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 06:34:37 +0700

    Int value check

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/FindDigits.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/FindDigits.java
@@ -6,6 +6,11 @@ public class FindDigits
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int valueCheck = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < args.length; i++)
+        {
+            //
+        }
         input.close();
     }
 }
