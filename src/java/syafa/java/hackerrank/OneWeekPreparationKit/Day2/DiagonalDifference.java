53630dfd8fcab37e1a942a934e4f46c85e0becc7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 08:43:22 +0700

    Add file from another folder

---
+++ src/java/syafa/java/hackerrank/OneWeekPreparationKit/Day2/DiagonalDifference.java
@@ -0,0 +1,31 @@
+package syafa.java.hackerrank.OneWeekPreparationKit.Day2;
+import java.util.*;
+
+public class DiagonalDifference
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int size = Integer.parseInt(input.nextLine());
+        int result0 = 0;
+        int result1 = 0;
+        int result = 0;
+        int[][] inputInteger = new int[size][size];
+        for (int i = 0; i < size; i++)
+        {
+            String inputArray[] = input.nextLine().split(" ");
+            for (int j = 0; j < size; j++)
+            {
+                inputInteger[i][j] = Integer.parseInt(inputArray[j]);
+            }
+        }
+        input.close();
+        for (int i = 0; i < size; i++)
+        {
+            result0 += inputInteger[i][i];
+            result1 += inputInteger[i][size - i - 1];
+        }
+        result = Math.abs(result0 - result1);
+        System.out.print(result);
+    }
+}
