43b086e6ff4a4e4e0b38e05ebf261a54ac6db4b1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:38:26 +0700

    Check if any number is negative

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day17MoreExceptions.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day17MoreExceptions.java
@@ -11,7 +11,7 @@ public class Day17MoreExceptions
         for (int i = 0; i < recursion; i++)
         {
             ArrayList<String> number = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-            //
+            if ((Integer.parseInt(number.get(0)) < 0) || (Integer.parseInt(number.get(1)) < 0))
         }
         input.close();
     }
