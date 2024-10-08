35bcffc1c6731c6ef33ad8b7d0047d2c5dfff39c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 06:36:58 +0700

    Numstr str

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/FindDigits.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/FindDigits.java
@@ -7,11 +7,15 @@ public class FindDigits
     {
         Scanner input = new Scanner(System.in);
         int valueCheck = Integer.parseInt(input.nextLine());
-        int number = 0;
+        String numberString = "";
         //ArrayList<Integer> number = new ArrayList<Integer>();
         for (int i = 0; i < valueCheck; i++)
         {
-            number = Integer.parseInt(input.nextLine());
+            numberString = input.nextLine();
+            for (int j = 0; j < args.length; j++)
+            {
+                
+            }
         }
         input.close();
     }
