efb58b5a8178da618862e9d7921224ca55c73676
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:38:55 +0700

    If any number is negative

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day17MoreExceptions.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day17MoreExceptions.java
@@ -12,6 +12,9 @@ public class Day17MoreExceptions
         {
             ArrayList<String> number = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
             if ((Integer.parseInt(number.get(0)) < 0) || (Integer.parseInt(number.get(1)) < 0))
+            {
+                result.add("n and p should be non-negative");
+            }
         }
         input.close();
     }
