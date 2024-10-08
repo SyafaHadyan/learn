58a123a74931c2d6b616df27bee54ee00999cf4b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 13:01:39 +0700

    Input

--- src/java/syafa/java/hackerrank/Algorithms/Strings/FunnyString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/FunnyString.java
@@ -8,6 +8,15 @@ public class FunnyString
         Scanner input = new Scanner(System.in);
         int queries = Integer.parseInt(input.nextLine());
         ArrayList<String> checkFunny = new ArrayList<String>();
+        String checkString = "";
+        for (int i = 0; i < queries; i++)
+        {
+            checkString = input.nextLine();
+            for (int j = 0; j < args.length; j++)
+            {
+                //
+            }
+        }
         input.close();
     }
 }
