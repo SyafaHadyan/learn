ec8cda7c847ef13372307149874857fd9ebf937d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:21:10 +0700

    Input

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day15LinkedList.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day15LinkedList.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
 public class Day15LinkedList
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int nodes = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
