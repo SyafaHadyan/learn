613137476055aa22af2805c54d19f9d37d4eec8c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 19:41:39 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
 public class Day112DArrays
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
