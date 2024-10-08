ae5835e0e29515d4cf58db76be001cd45105d69b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 11:58:01 +0700

    Test val

--- src/java/syafa/java/Praktikum/Bab3/Praktikum1.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum1.java
@@ -6,6 +6,8 @@ public class Praktikum1
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        char letter = input.nextLine().charAt(0);
         input.close();
+        System.out.print(letter);
     }
 }
