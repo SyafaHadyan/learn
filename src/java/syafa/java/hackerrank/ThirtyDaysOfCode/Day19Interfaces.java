e9bd26e95de084f612422b9d6ad647b572b4316d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 21:03:49 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day19Interfaces.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day19Interfaces.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 
-public class Day19Interfaces {
-
+public class Day19Interfaces
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
