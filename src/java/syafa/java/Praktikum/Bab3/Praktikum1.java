61f987feeb7bff2f3be84dc22d2f0f5f69d462d5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 14:27:26 +0700

    Removed

--- src/java/syafa/java/Praktikum/Bab3/Praktikum1.java
+++ src/java/syafa/java/Praktikum/Bab3/Praktikum1.java
@@ -1,28 +1,9 @@
 package syafa.java.Praktikum.Bab3;
-import java.util.*;
 
 public class Praktikum1
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        char letter = input.nextLine().charAt(0);
-        input.close();
-        ArrayList<String> A = new ArrayList<String>(Arrays.asList( "       A",
-                                                                        "      A A",
-                                                                        "     A   A",
-                                                                        "    A     A",
-                                                                        "   A A A A A",
-                                                                        "  A         A",
-                                                                        " A           A",
-                                                                        "A             A"));
-        for (int i = 0; i < A.size(); i++)
-        {
-            System.out.print(A.get(i));
-            if (i < A.size())
-            {
-                System.out.print("\n");
-            }
-        }
+
     }
 }
