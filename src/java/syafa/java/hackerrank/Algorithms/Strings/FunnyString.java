198e774c9d082573e0e0c9ef2b2529cd5f789db6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 13:03:51 +0700

    Test

--- src/java/syafa/java/hackerrank/Algorithms/Strings/FunnyString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/FunnyString.java
@@ -7,14 +7,13 @@ public class FunnyString
     {
         Scanner input = new Scanner(System.in);
         int queries = Integer.parseInt(input.nextLine());
-        ArrayList<String> checkFunny = new ArrayList<String>();
         String checkString = "";
         for (int i = 0; i < queries; i++)
         {
             checkString = input.nextLine();
             for (int j = checkString.length() - 1; j >= 0; j--)
             {
-                System.out.println(checkString.charAt(j));
+                
             }
         }
         input.close();
