ebf824588b698589fb354e9e84ae742287814d2c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 19:43:13 +0700

    Int arr 6 x 6

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day112DArrays.java
@@ -6,6 +6,11 @@ public class Day112DArrays
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        Integer[][] hourGlass = new Integer[6][6];
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
         input.close();
     }
 }
