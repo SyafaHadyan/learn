ea97e6208ebeea78ba65a77a69f4f1a30ef3cf3b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 20:25:56 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day16ExceptionsStringToInteger.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day16ExceptionsStringToInteger.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 
-public class Day16ExceptionsStringToInteger {
-
+public class Day16ExceptionsStringToInteger
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
