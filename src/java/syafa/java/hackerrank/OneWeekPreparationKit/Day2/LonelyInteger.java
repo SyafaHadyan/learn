3a0b7284c54414d7f847f9241a21ee5ab382ad06
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-08 08:36:09 +0700

    Initialize input

--- src/java/syafa/java/hackerrank/OneWeekPreparationKit/Day2/LonelyInteger.java
+++ src/java/syafa/java/hackerrank/OneWeekPreparationKit/Day2/LonelyInteger.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.OneWeekPreparationKit.Day2;
+import java.util.*;
 
 public class LonelyInteger
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
