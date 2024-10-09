7749c6bcdd8b4472009011fac6470d73bda4d9b6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 16:36:39 +0700

    Compress line size

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
@@ -7,29 +7,9 @@ public class SelisihWaktu
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> month = new ArrayList<String>(Arrays.asList("Jan",
-                                                                        "Feb",
-                                                                        "Mar",
-                                                                        "Apr",
-                                                                        "May",
-                                                                        "Jun",
-                                                                        "Jul",
-                                                                        "Aug",
-                                                                        "Sep",
-                                                                        "Oct",
-                                                                        "Nov",
-                                                                        "Dec"));
+        "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"));
         ArrayList<Integer> monthValue = new ArrayList<Integer>(Arrays.asList(31,
-                                                                                28,
-                                                                                31,
-                                                                                30,
-                                                                                31,
-                                                                                30,
-                                                                                31,
-                                                                                31,
-                                                                                30,
-                                                                                31,
-                                                                                30,
-                                                                                31));
+        28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31));
         ArrayList<String> firstDate = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> secondDate = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
