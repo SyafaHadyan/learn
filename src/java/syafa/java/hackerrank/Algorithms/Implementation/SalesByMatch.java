0988780f95eb1924816063c2556530b549f10fea
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 09:27:13 +0700

    Arrlist int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SalesByMatch.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SalesByMatch.java
@@ -8,6 +8,7 @@ public class SalesByMatch
         Scanner input = new Scanner(System.in);
         int AmountOfSales = Integer.parseInt(input.nextLine());
         ArrayList<String> sales = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<Integer> salesSorted = new ArrayList<Integer>();
         input.close();
         for (int i = 0; i < sales.size(); i++)
         {
