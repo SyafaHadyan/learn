97538fcc8153ef775a204b3d557d3c46f35b88c4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 13:19:44 +0700

    If out of bounds

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -29,7 +29,7 @@ public class MigratoryBirds
                     {
                         
                     }
-                    catch (Exception e)
+                    catch (IndexOutOfBoundsException e)
                     {
                         // TODO: handle exception
                     }
