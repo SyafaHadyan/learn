597777595132424d4d989806cf3f8b47d6020a2a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 15:52:37 +0700

    Arrlist str month

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
@@ -6,6 +6,18 @@ public class SelisihWaktu
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> month = new ArrayList<String>(Arrays.asList("Jan",
+                                                                            "Feb",
+                                                                            "Mar",
+                                                                            "Apr",
+                                                                            "May",
+                                                                            "Jun",
+                                                                            "Jul",
+                                                                            "Aug",
+                                                                            "Sep",
+                                                                            "Oct",
+                                                                            "Nov",
+                                                                            "Dec"));
         input.close();
     }
 }
