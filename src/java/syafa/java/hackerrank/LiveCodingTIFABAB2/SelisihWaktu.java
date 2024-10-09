56ee55b42779c60f62c2a9d96c8ea1eb86381b6e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 16:19:12 +0700

    Edit day counter

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
@@ -33,7 +33,7 @@ public class SelisihWaktu
         ArrayList<String> firstDate = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> secondDate = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
-        int day = 0;
+        int day = 1;
         int hour = 0;
         int minute = 0;
         for (int i = 0; i < month.size(); i++)
