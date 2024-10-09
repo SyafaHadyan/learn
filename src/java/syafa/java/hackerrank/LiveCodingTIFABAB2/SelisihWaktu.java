8221935c34d7f2539267f1824c9524819d4a96ee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 15:49:57 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/SelisihWaktu.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.LiveCodingTIFABAB2;
+import java.util.*;
 
 public class SelisihWaktu
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
