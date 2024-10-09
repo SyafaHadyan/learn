ffb47f9b0e18f1d9719e982453385da9d916d3ee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 10:47:31 +0700

    Remove var

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/UtopianTree.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/UtopianTree.java
@@ -8,7 +8,6 @@ public class UtopianTree
         Scanner input = new Scanner(System.in);
         int AmountOfChecks = Integer.parseInt(input.nextLine());
         int treeHeight = 0;
-        int currentHeight = 0;
         ArrayList<Integer> heightResult = new ArrayList<Integer>();
         for (int i = 0; i < AmountOfChecks; i++)
         {
