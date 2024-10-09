39a588938509c0b06bdc0ea4b8984aea4ea2acd3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 08:19:28 +0700

    Input int cuday at

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ViralAdvertising.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ViralAdvertising.java
@@ -6,6 +6,7 @@ public class ViralAdvertising
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int cumulativeAtDay = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
