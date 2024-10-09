a81494b5457185f4c404c450e66b113707267572
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 10:24:36 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/UtopianTree.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/UtopianTree.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class UtopianTree {
-
+public class UtopianTree
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
