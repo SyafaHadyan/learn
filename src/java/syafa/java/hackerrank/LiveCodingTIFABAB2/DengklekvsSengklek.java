78ec169aa62e1f702163b29c91b6b72f455e649d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-09 15:09:01 +0700

    Rename arrlist and add arrlist str

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/DengklekvsSengklek.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB2/DengklekvsSengklek.java
@@ -6,67 +6,68 @@ public class DengklekvsSengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> match = new ArrayList<String>();
         ArrayList<String> matchResult = new ArrayList<String>();
         String hasilPertandingan = "";
         for (int i = 0; i < 4; i++)
         {
-            matchResult.add(Arrays.toString(input.nextLine().split(" ")));
-            if (matchResult.get(0).equalsIgnoreCase(matchResult.get(1)))
+            match.add(Arrays.toString(input.nextLine().split(" ")));
+            if (match.get(0).equalsIgnoreCase(match.get(1)))
             {
                 hasil[i] = "Seri";
             }
-            else if (matchResult.get(0).equalsIgnoreCase("Batu"))
+            else if (match.get(0).equalsIgnoreCase("Batu"))
             {
-                if (matchResult.get(1).equalsIgnoreCase("Gunting"))
+                if (match.get(1).equalsIgnoreCase("Gunting"))
                 {
                     hasil[i] = "Dengklek menang";
-                    matchResult.get(0)++;
+                    match.get(0)++;
                 }
-                else if (matchResult.get(1).equalsIgnoreCase("Kertas"))
+                else if (match.get(1).equalsIgnoreCase("Kertas"))
                 {
                     hasil[i] = "Sengklek menang";
-                    matchResult.get(1)++;
+                    match.get(1)++;
                 }
             }
-            else if (matchResult.get(0).equalsIgnoreCase("Gunting"))
+            else if (match.get(0).equalsIgnoreCase("Gunting"))
             {
-                if (matchResult.get(1).equalsIgnoreCase("Batu"))
+                if (match.get(1).equalsIgnoreCase("Batu"))
                 {
                     hasil[i] = "Sengklek menang";
-                    matchResult.get(1)++;
+                    match.get(1)++;
                 }
-                else if (matchResult.get(1).equalsIgnoreCase("Kertas"))
+                else if (match.get(1).equalsIgnoreCase("Kertas"))
                 {
                     hasil[i] = "Dengklek menang";
-                    matchResult.get(0)++;
+                    match.get(0)++;
                 }
             }
-            else if (matchResult.get(0).equalsIgnoreCase("Kertas"))
+            else if (match.get(0).equalsIgnoreCase("Kertas"))
             {
-                if (matchResult.get(1).equalsIgnoreCase("Batu"))
+                if (match.get(1).equalsIgnoreCase("Batu"))
                 {
                     hasil[i] = "Dengklek menang";
-                    matchResult.get(0)++;
+                    match.get(0)++;
                 }
-                else if (matchResult.get(1).equalsIgnoreCase("Gunting"))
+                else if (match.get(1).equalsIgnoreCase("Gunting"))
                 {
                     hasil[i] = "Sengklek menang";
-                    matchResult.get(1)++;
+                    match.get(1)++;
                 }
             }
         }
         input.close();
-        if (matchResult.get(0) == matchResult.get(1))
+        if (match.get(0) == match.get(1))
         {
             hasilPertandingan = "Perlombaan seri, maka tidak ada yang menang dan hadiah buat panitia.";
         }
-        else if (matchResult.get(0) > matchResult.get(1))
+        else if (match.get(0) > match.get(1))
         {
-            hasilPertandingan = "Perlombaan dimenangkan oleh Dengklek dengan skor " + matchResult.get(0) + " vs " + matchResult.get(1) + '.';
+            hasilPertandingan = "Perlombaan dimenangkan oleh Dengklek dengan skor " + match.get(0) + " vs " + match.get(1) + '.';
         }
-        else if (matchResult.get(1) > matchResult.get(0))
+        else if (match.get(1) > match.get(0))
         {
-            hasilPertandingan = "Perlombaan dimenangkan oleh Sengklek dengan skor " + matchResult.get(1) + " vs " + matchResult.get(0) + '.';
+            hasilPertandingan = "Perlombaan dimenangkan oleh Sengklek dengan skor " + match.get(1) + " vs " + match.get(0) + '.';
         }
         for (String output : hasil)
         {
