7dc3933dbb746bcd518f45cebf4bf51a6ddf36e3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 17:57:12 +0700

    For-i array size

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/BigSorting.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/BigSorting.java
@@ -6,6 +6,11 @@ public class BigSorting
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < arraySize; i++)
+        {
+            
+        }
         input.close();
     }
 }
