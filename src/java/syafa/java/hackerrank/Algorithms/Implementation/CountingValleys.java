bf413e1d2ffca88c574b701a75892ca6b521f046
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 16:25:55 +0700

    For-i

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CountingValleys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CountingValleys.java
@@ -9,5 +9,9 @@ public class CountingValleys
         int numberOfSteps = Integer.parseInt(input.nextLine());
         ArrayList<String> step = new ArrayList<String>(Arrays.asList(input.nextLine().split("(?!^)")));
         input.close();
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
     }
 }
