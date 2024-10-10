25aa99a3194af933936b0fe439e6bfcc55559365
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 16:22:58 +0700

    Int steps

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CountingValleys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CountingValleys.java
@@ -6,6 +6,7 @@ public class CountingValleys
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberOfSteps = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
