b45bba22cf8195087d72e76ddb80a5cca68c1489
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 14:41:46 +0700

    Fix

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day9Recursion3.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day9Recursion3.java
@@ -1,28 +1,25 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 import java.util.*;
 
-class Result
-{
-    public static int factorial(int number)
-    {
-        int factorialResult = 0;
-        for (int i = number; i >= 1; i--)
-        {
-            factorialResult *= i;
-            number = number - 1;
-        }
-        return factorialResult;
-    }
-}
-
 public class Day9Recursion3
 {
+    private static int factorial(int number)
+{
+    int factorialResult = 1;
+    for (int i = number; i >= 1; i--)
+    {
+        factorialResult *= i;
+        number = number - 1;
+    }
+    System.out.print(factorialResult);
+    return factorialResult;
+}
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int number = Integer.parseInt(input.nextLine());
         input.close();
-        int result = Result.factorial(number);
-        System.out.print(result);
+        factorial(number);
+        System.out.print(factorial(number));
     }
 }
