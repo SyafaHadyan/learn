b1a19650dc0fcea3f5571a761d4f83a679e49469
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 19:18:29 +0700

    Input arrsize int

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MarcsCakewalk.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MarcsCakewalk.java
@@ -6,6 +6,7 @@ public class MarcsCakewalk
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
