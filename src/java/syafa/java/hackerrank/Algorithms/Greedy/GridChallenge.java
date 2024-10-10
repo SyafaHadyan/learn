57622e3f052bf0565231a7e3f352ad3b3629c047
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 21:16:09 +0700

    Input test case

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/GridChallenge.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/GridChallenge.java
@@ -6,6 +6,7 @@ public class GridChallenge
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
