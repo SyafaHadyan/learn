52bf1e38a62cc560acbf7fbba53283bce6848dfd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 16:43:02 +0700

    Arrlist str split by space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CatsAndAMouse.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CatsAndAMouse.java
@@ -7,6 +7,7 @@ public class CatsAndAMouse
     {
         Scanner input = new Scanner(System.in);
         int queries = Integer.parseInt(input.nextLine());
+        ArrayList<String> position = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         for (int i = 0; i < queries; i++)
         {
             
