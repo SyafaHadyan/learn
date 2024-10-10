9ab6d98fc4828655f1926a5bf5779ded4e67f991
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 17:37:27 +0700

    Delete unused input

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/IntroToTutorialChallenges.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/IntroToTutorialChallenges.java
@@ -5,14 +5,10 @@ public class IntroToTutorialChallenges
 {
     public static void main(String[] args)
     {
-        /*
-         * 4 = Number to search
-         * 6 = Array size
-         * 1 4 5 7 9 12 = Array given through input
-         */
         Scanner input = new Scanner(System.in);
         int numberToSearch = Integer.parseInt(input.nextLine());
-        int arraySize = Integer.parseInt(input.nextLine());
+        StringBuilder arraySize = new StringBuilder(input.nextLine());
+        arraySize.delete(0,arraySize.length());
         ArrayList<String> numbers = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
         for (int i = 0; i < numbers.size(); i++)
