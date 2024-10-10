f1bf1d283c0c7a86e2c733ddc6f100fafaf5c7d1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 16:53:30 +0700

    Print result

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CatsAndAMouse.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CatsAndAMouse.java
@@ -42,7 +42,11 @@ public class CatsAndAMouse
             {
                 for (int j = 0; j < result.size(); j++)
                 {
-                    
+                    System.out.print(result.get(i));
+                    if (j != result.size() - 1)
+                    {
+                        System.out.print("\n");
+                    }
                 }
             }
         }
