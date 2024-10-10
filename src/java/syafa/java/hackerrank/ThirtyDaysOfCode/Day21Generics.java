f243ab056ea9b0fc410ff8ad09a9959fd929404f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 14:07:16 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day21Generics.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day21Generics.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 
-public class Day21Generics {
-
+public class Day21Generics
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
