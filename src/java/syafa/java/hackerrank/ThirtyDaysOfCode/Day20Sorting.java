bc712d129653041797620c138eb4d668a86ab32a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 15:27:58 +0700

    Input to arrlist str split space

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
@@ -6,6 +6,8 @@ public class Day20Sorting
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
+        ArrayList<String> inputArrayString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
