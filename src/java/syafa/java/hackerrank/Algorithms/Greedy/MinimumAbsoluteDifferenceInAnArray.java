06271161f46086677e61b8217ecafe775366e8db
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 20:03:43 +0700

    Compare from previous

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MinimumAbsoluteDifferenceInAnArray.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MinimumAbsoluteDifferenceInAnArray.java
@@ -9,9 +9,18 @@ public class MinimumAbsoluteDifferenceInAnArray
         int arrayLength = Integer.parseInt(input.nextLine());
         ArrayList<String> numberString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
+        double currentLowestDiffAbs = 0d;
         ArrayList<Integer> numberInteger = new ArrayList<Integer>();
         for (int i = 0; i < numberString.size(); i++)
         {
+            try
+            {
+                if ((Double.parseDouble(numberString.get(i)) - Double.parseDouble(numberString.get(i + 1))) < currentLowestDiffAbs)
+            }
+            catch (IndexOutOfBoundsException e)
+            {
+                // TODO: handle exception
+            }
             numberInteger.add(Integer.parseInt(numberString.get(i)));
         }
         Collections.sort(numberInteger,Comparator.comparingInt(Math::abs));
