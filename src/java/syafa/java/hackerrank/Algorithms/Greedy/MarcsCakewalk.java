5b3503c316a7ca64b248f48842be2a15168feae2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-10 19:26:50 +0700

    User arrsize instead to store calc val

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MarcsCakewalk.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MarcsCakewalk.java
@@ -7,6 +7,7 @@ public class MarcsCakewalk
     {
         Scanner input = new Scanner(System.in);
         int arraySize = Integer.parseInt(input.nextLine());
+        arraySize = 0;
         ArrayList<String> numberString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
         ArrayList<Integer> numberInteger = new ArrayList<Integer>();
@@ -18,7 +19,7 @@ public class MarcsCakewalk
         Collections.reverse(numberInteger);
         for (int i = 0; i < args.length; i++)
         {
-            numberInteger.set(i,(int)(Math.pow(2,i)) * numberInteger.get(i));
+            arraySize += (int)(Math.pow(2,i)) * numberInteger.get(i);
         }
     }
 }
