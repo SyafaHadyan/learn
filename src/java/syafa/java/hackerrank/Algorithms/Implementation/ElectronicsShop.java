804601f3dd8706a604d2407e8b3a05ec7a810809
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:28:23 +0700

    Input budget split space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -6,6 +6,7 @@ public class ElectronicsShop
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<Strin> budget = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
