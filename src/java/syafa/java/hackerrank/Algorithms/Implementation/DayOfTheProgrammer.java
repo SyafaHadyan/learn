9283656a7a7519c2e71ae12ef66c5da7f8a00dd2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 15:15:45 +0700

    Edit

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
@@ -9,7 +9,7 @@ public class DayOfTheProgrammer
         Scanner input = new Scanner(System.in);
         int year = Integer.parseInt(input.nextLine());
         input.close();
-        if (LocalDate.of(year, 01, 01).isLeapYear())
+        if (LocalDate.of(year, 01, 01).isLeapYear() || year == 1700 || year == 1800 || year == 1900 || year == 1918)
         {
             System.out.print("12.09." + year);
             return;
