5fbbcca77689f94ebf9851ca8521b84f6fd5e7e2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:34:33 +0700

    Sort

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -10,11 +10,17 @@ public class ElectronicsShop
         ArrayList<String> keyboardPricesString = new ArrayList<String>(Arrays.asList((input.nextLine().split(" "))));
         ArrayList<String> usbPricesString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
+        Collections.sort(keyboardPricesString);
+        Collections.sort(usbPricesString);
         ArrayList<Integer> keyboardPrices = new ArrayList<Integer>();
         ArrayList<Integer> usbPrices = new ArrayList<Integer>();
         for (int i = 0; i < keyboardPricesString.size(); i++)
         {
             keyboardPrices.add(Integer.parseInt(keyboardPricesString.get(i)));
+            if (i == keyboardPricesString.size() - 1)
+            {
+                Collections.sort(keyboardPrices);
+            }
         }
     }
 }
