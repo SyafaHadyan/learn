e235badc6f5227fe7710970d719f4c194cc273a1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 10:27:52 +0700

    Exit at end

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DesignerPDFViewer.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DesignerPDFViewer.java
@@ -24,6 +24,7 @@ public class DesignerPDFViewer
                 if (i == word.length() - 1)
                 {
                     System.out.print(currentMaxHeight * word.length());
+                    return;
                 }
             }
         }
