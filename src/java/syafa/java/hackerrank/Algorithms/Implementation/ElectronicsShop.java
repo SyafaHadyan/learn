f8248b806b0a4f1cac328c6df2a64fb2523b28b9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:33:03 +0700

    Arrlist int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -7,8 +7,14 @@ public class ElectronicsShop
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> budget = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-        ArrayList<String> keyboardPrices = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-        ArrayList<String> usbPrices = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<String> keyboardPricesString = new ArrayList<String>(Arrays.asList((input.nextLine().split(" "))));
+        ArrayList<String> usbPricesString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
+        ArrayList<Integer> keyboardPrices = new ArrayList<Integer>();
+        ArrayList<Integer> usbPrices = new ArrayList<Integer>();
+        for (int i = 0; i < keyboardPricesString.size(); i++)
+        {
+            
+        }
     }
 }
