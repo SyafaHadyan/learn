cd74b0a72b5edc7be7b2969541851d16ea5b9ecb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 13:44:27 +0700

    Check if whole number

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
@@ -8,12 +8,18 @@ public class BeautifulDaysAtTheMovies
         Scanner input = new Scanner(System.in);
         ArrayList<String> days = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
+        int validDay = 0;
         for (int i = 0; i < Integer.parseInt(days.get(0)) - Integer.parseInt(days.get(1)) + 1; i++)
         {
             String reversedDay = "";
+            int checkDay = 0;
             for (int j = days.get(i).length() - 1; j >= 0; j++)
             {
                 reversedDay += days.get(i).charAt(j);
+            }
+            if (Math.floor(Math.abs(Integer.parseInt(days.get(i)) - Integer.parseInt(reversedDay))) == Math.abs(Integer.parseInt(days.get(i)) - Integer.parseInt(reversedDay)))
+            {
+                validDay++;
             }
         }
     }
