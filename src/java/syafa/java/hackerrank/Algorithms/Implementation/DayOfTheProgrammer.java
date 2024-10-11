3635b2b17302c906a3c192ecf4ee88355d3b518e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 15:03:06 +0700

    Delete

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DayOfTheProgrammer.java
@@ -8,24 +8,5 @@ public class DayOfTheProgrammer
         Scanner input = new Scanner(System.in);
         int year = Integer.parseInt(input.nextLine());
         input.close();
-        if ((year % 400 == 0) && (year % 100 == 0))
-        {
-            System.out.print("12.09" + '.' + year);
-            return;
-        }
-        else if (year % 400 == 0)
-        {
-            System.out.print("12.09" + '.' + year);
-            return;
-        }
-        else if (year % 4 == 0)
-        {
-            if (year % 100 != 0)
-            {
-                System.out.print("12.09" + '.' + year);
-                return;
-            }
-        }
-        System.out.print("13.09" + '.' + year);
     }
 }
