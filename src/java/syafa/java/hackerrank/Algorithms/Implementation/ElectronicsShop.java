a9caaeda2b24f24fc9a0386ab7d144f238cda25c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:59:22 +0700

    Use from str parse int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -7,29 +7,17 @@ public class ElectronicsShop
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> budget = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
-        ArrayList<String> keyboardPricesString = new ArrayList<String>(Arrays.asList((input.nextLine().split(" "))));
-        ArrayList<String> usbPricesString = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<String> keyboardPrices = new ArrayList<String>(Arrays.asList((input.nextLine().split(" "))));
+        ArrayList<String> usbPrices = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
-        ArrayList<Integer> keyboardPrices = new ArrayList<Integer>();
-        ArrayList<Integer> usbPrices = new ArrayList<Integer>();
         int currentHigh = 0;
-        for (int i = 0; i < keyboardPricesString.size(); i++)
-        {
-            keyboardPrices.add(Integer.parseInt(keyboardPricesString.get(i)));
-        }
-        for (int i = 0; i < usbPricesString.size(); i++)
-        {
-            usbPrices.add(Integer.parseInt(usbPricesString.get(i)));
-        }
-        //Collections.sort(keyboardPrices);
-        //Collections.sort(usbPrices);
         for (int i = 0; i < keyboardPrices.size(); i++)
         {
             for (int j = 0; j < usbPrices.size(); j++)
             {
-                if ((keyboardPrices.get(i) + usbPrices.get(j)) <= Integer.parseInt(budget.get(0)) && (keyboardPrices.get(i) + usbPrices.get(j)) > currentHigh)
+                if ((Integer.parseInt(keyboardPrices.get(i)) + Integer.parseInt(usbPrices.get(j))) <= Integer.parseInt(budget.get(0)) && (Integer.parseInt(keyboardPrices.get(i)) + Integer.parseInt(usbPrices.get(i))) > currentHigh)
                 {
-                    currentHigh = (keyboardPrices.get(i) + usbPrices.get(j));
+                    currentHigh = (Integer.parseInt(keyboardPrices.get(i)) + Integer.parseInt(usbPrices.get(i)));
                 }
                 if (i + j - keyboardPrices.size() - usbPrices.size() == -2)
                 {
