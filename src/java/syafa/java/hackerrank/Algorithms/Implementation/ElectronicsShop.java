ac710d5d3bce264e8d594c24597d545ca4e925c9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:29:14 +0700

    Input keyboard prices sep space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -6,7 +6,8 @@ public class ElectronicsShop
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        ArrayList<Strin> budget = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<String> budget = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
+        ArrayList<String> keyboardPrices = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
