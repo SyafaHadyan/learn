d0ca315c7b2896f58050442a7e1a1eff3f0fe4d2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 15:46:11 +0700

    Edit remove

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CutTheSticks.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CutTheSticks.java
@@ -9,6 +9,7 @@ public class CutTheSticks
         int arrLength = Integer.parseInt(input.nextLine());
         ArrayList<Integer> sticksLeft = new ArrayList<Integer>();
         ArrayList<Integer> sticks = new ArrayList<Integer>();
+        int currentLowestStick = 0;
         for (int i = 0; i < arrLength; i++)
         {
             sticks.add(input.nextInt());
@@ -17,15 +18,19 @@ public class CutTheSticks
         Collections.sort(sticks);
         for (int i = 0; i < sticks.size(); i++)
         {
+            if (i == 0)
+            {
+                currentLowestStick = sticks.get(0);
+            }
             sticks.set(i,sticks.get(i) - sticks.get(0));
+            if (sticks.get(i) <= 0)
+            {
+                sticks.remove(i);
+            }
             if (i == sticks.size() - 1)
             {
                 for (int j = 0; j < sticks.size(); j++)
                 {
-                    if (sticks.get(j) <= 0)
-                    {
-                        sticks.remove(j);
-                    }
                 }
             }
             sticksLeft.add(sticks.size());
