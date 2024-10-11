2fcc1304d71dcc48ac0bd3b1d95f89abae2bb0a6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 19:46:05 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/AppleAndOrange.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/AppleAndOrange.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class AppleAndOrange {
-
+public class AppleAndOrange
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
