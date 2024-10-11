5e4da8f2e6b5c50d88a640083082f7bf0b6468d9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 09:26:12 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ElectronicsShop.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class ElectronicsShop {
-
+public class ElectronicsShop
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
