1ac108b73951ba1ae946f26f7baecca1411bf965
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 10:44:06 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/AngryProfessor.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/AngryProfessor.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class AngryProfessor {
-
+public class AngryProfessor
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
