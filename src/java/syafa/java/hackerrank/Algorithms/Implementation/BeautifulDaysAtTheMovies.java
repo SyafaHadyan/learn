773e0d8069693d903f896930128738d65f1c6f1f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 13:55:38 +0700

    Fix for

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
@@ -10,14 +10,14 @@ public class BeautifulDaysAtTheMovies
         input.close();
         ArrayList<Integer> firstLastDay = new ArrayList<Integer>();
         int validDay = 0;
-        for (int i = 0; i < Integer.parseInt(days.get(0)) - Integer.parseInt(days.get(1)) + 1; i++)
+        for (int i = 0; i < Integer.parseInt(days.get(1)) - Integer.parseInt(days.get(0)) + 1; i++)
         {
             firstLastDay.add(Integer.parseInt(days.get(0)) + i);
         }
         for (int i = 0; i < firstLastDay.size(); i++)
         {
             String reversedDay = "";
-            for (int j = String.valueOf(firstLastDay.get(i)).length() - 1; j >= 0; j++)
+            for (int j = String.valueOf(firstLastDay.get(i)).length() - 1; j >= 0; j--)
             {
                 reversedDay += String.valueOf(firstLastDay.get(i)).charAt(j);
             }
