f56e1e9d0be78dbd6ced616792ba0b34346cc7c9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 10:12:57 +0700

    Input arrlist str split space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DesignerPDFViewer.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DesignerPDFViewer.java
@@ -6,6 +6,7 @@ public class DesignerPDFViewer
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> letterHeight = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
