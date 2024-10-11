f30a1eae3378e3718e99180562562fc892108f40
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 13:33:56 +0700

    Arrlist str split space

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulDaysAtTheMovies.java
@@ -6,6 +6,7 @@ public class BeautifulDaysAtTheMovies
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> days = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
