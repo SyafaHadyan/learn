ed3e7e60657749543ee7990e58290b3b758699a3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-11 19:47:45 +0700

    Input arrlist str house

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/AppleAndOrange.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/AppleAndOrange.java
@@ -6,6 +6,7 @@ public class AppleAndOrange
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> houseDomain = new ArrayList<String>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
     }
 }
