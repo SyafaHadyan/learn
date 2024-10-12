31355db8fd5f3c2cfc4df59b7333772908bda9c2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:12:44 +0700

    Arrlist int date

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -6,6 +6,12 @@ public class Day26NestedLogic
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<Integer> dateReturn = new ArrayList<>();
+        ArrayList<Integer> dateDue = new ArrayList<>();
+        for (int i = 0; i < 3; i++)
+        {
+            //
+        }
         input.close();
     }
 }
