d6906132b74897c89664c0daddf965c931031f99
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:34:36 +0700

    Move to end

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -9,14 +9,7 @@ public class Day26NestedLogic
         ArrayList<String> dateReturn = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> dateDue = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
         input.close();
-        if ((Integer.parseInt(dateReturn.get(0)) <= Integer.parseInt(dateDue.get(0))) &&
-        (Integer.parseInt(dateReturn.get(1)) <= Integer.parseInt(dateDue.get(1))) &&
-        (Integer.parseInt(dateReturn.get(2)) <= Integer.parseInt(dateDue.get(2))))
-        {
-            System.out.print(0);
-            return;
-        }
-        else if ((Integer.parseInt(dateReturn.get(0)) > Integer.parseInt(dateDue.get(0))) &&
+        if ((Integer.parseInt(dateReturn.get(0)) > Integer.parseInt(dateDue.get(0))) &&
         (Integer.parseInt(dateReturn.get(1)) == Integer.parseInt(dateDue.get(1))) &&
         (Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2))))
         {
@@ -27,6 +20,13 @@ public class Day26NestedLogic
         (Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2))))
         {
             System.out.print(500 * (Integer.parseInt(dateReturn.get(1)) - Integer.parseInt(dateDue.get(1))));
+            return;
+        }
+        if ((Integer.parseInt(dateReturn.get(0)) <= Integer.parseInt(dateDue.get(0))) ||
+        (Integer.parseInt(dateReturn.get(1)) <= Integer.parseInt(dateDue.get(1))) ||
+        (Integer.parseInt(dateReturn.get(2)) <= Integer.parseInt(dateDue.get(2))))
+        {
+            System.out.print(0);
             return;
         }
         System.out.print(10000);
