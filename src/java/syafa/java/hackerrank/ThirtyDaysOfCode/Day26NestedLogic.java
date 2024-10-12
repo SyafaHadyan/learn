17d8e6e1627de9fe5131eb7a8572fd1978858ee1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:39:14 +0700

    Fix month check

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -16,7 +16,7 @@ public class Day26NestedLogic
             System.out.print(15 * (Integer.parseInt(dateReturn.get(0)) - Integer.parseInt(dateDue.get(0))));
             return;
         }
-        else if ((Integer.parseInt(dateReturn.get(1)) != Integer.parseInt(dateDue.get(1))) &&
+        else if ((Integer.parseInt(dateReturn.get(1)) > Integer.parseInt(dateDue.get(1))) &&
         (Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2))))
         {
             System.out.print(500 * (Integer.parseInt(dateReturn.get(1)) - Integer.parseInt(dateDue.get(1))));
