ced5927450438c9758fe7b80682e92bb6faf6576
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 19:16:09 +0700

    For calc var

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day11_2DArrays.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day11_2DArrays.java
@@ -14,6 +14,13 @@ public class Day11_2DArrays
                 hourGlass[i][j] = Integer.parseInt(input.next());
             }
         }
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                //
+            }
+        }
         input.close();
     }
 }
