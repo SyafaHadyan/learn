1f2f1b34815e800cfe791877298e91bed3a3a04b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:17:58 +0700

    If late but same month and year

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -10,5 +10,11 @@ public class Day26NestedLogic
         ArrayList<String> dateDue = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
         int lateCount = 0;
         input.close();
+        if (Integer.parseInt(dateReturn.get(0)) > Integer.parseInt(dateDue.get(0)) &&
+        Integer.parseInt(dateReturn.get(1)) == Integer.parseInt(dateDue.get(1)) &&
+        Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2)))
+        {
+            //
+        }
     }
 }
