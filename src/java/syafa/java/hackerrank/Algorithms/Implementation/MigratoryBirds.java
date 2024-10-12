906b8027fbc82dd96a4612587c651353a1c3e5a2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-13 02:39:22 +0700

    Temp if check

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -14,13 +14,23 @@ public class MigratoryBirds
          * TODO: mostSightings must be lowest type of most seen bird
          */
         boolean resetCounter = true;
-        int mostType = Integer.MIN_VALUE;
-        int currentMostType = Integer.MIN_VALUE;
-        int mostSightings = Integer.MIN_VALUE;
+        int checkType = Integer.MIN_VALUE; // get i
+        int checkSightings = Integer.MIN_VALUE; // check get i
+        int currentMostType = Integer.MIN_VALUE; // if true
+        int mostSightings = Integer.MIN_VALUE; //
         for (int i = 0; i < arrayLength; i++)
         {
             sightings.add(Integer.parseInt(input.next()));
         }
         Collections.sort(sightings);
+        for (int i = 0; i < sightings.size(); i++)
+        {
+            checkType = sightings.get(i);
+            if (checkType == sightings.get(i))
+            {
+                //
+            }
+            //if ((sightings.get(i) > currentMostType) && )
+        }
     }
 }
