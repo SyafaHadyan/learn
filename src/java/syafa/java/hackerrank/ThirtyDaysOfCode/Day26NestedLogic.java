ff3591e3fd0f8e43fd3aace30a317faf0782e580
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:36:31 +0700

    Change when year is more than

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -22,13 +22,11 @@ public class Day26NestedLogic
             System.out.print(500 * (Integer.parseInt(dateReturn.get(1)) - Integer.parseInt(dateDue.get(1))));
             return;
         }
-        if ((Integer.parseInt(dateReturn.get(0)) <= Integer.parseInt(dateDue.get(0))) ||
-        (Integer.parseInt(dateReturn.get(1)) <= Integer.parseInt(dateDue.get(1))) ||
-        (Integer.parseInt(dateReturn.get(2)) <= Integer.parseInt(dateDue.get(2))))
+        else if ((Integer.parseInt(dateReturn.get(2)) > Integer.parseInt(dateDue.get(2))))
         {
-            System.out.print(0);
+            System.out.print(100000);
             return;
         }
-        System.out.print(10000);
+        System.out.print(0);
     }
 }
