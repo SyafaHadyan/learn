77b7ae65258885e0b9e05b1abe6b6eee8539b170
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-12 17:27:58 +0700

    Reduce var used

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day26NestedLogic.java
@@ -8,22 +8,20 @@ public class Day26NestedLogic
         Scanner input = new Scanner(System.in);
         ArrayList<String> dateReturn = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
         ArrayList<String> dateDue = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
-        int lateCount = 0;
         input.close();
         if ((Integer.parseInt(dateReturn.get(0)) > Integer.parseInt(dateDue.get(0))) &&
         (Integer.parseInt(dateReturn.get(1)) == Integer.parseInt(dateDue.get(1))) &&
         (Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2))))
         {
-            lateCount = 15 * (Integer.parseInt(dateReturn.get(0)) - Integer.parseInt(dateDue.get(0)));
-            System.out.print(lateCount);
+            System.out.print(15 * (Integer.parseInt(dateReturn.get(0)) - Integer.parseInt(dateDue.get(0))));
             return;
         }
         else if ((Integer.parseInt(dateReturn.get(1)) != Integer.parseInt(dateDue.get(1))) &&
         (Integer.parseInt(dateReturn.get(2)) == Integer.parseInt(dateDue.get(2))))
         {
-            lateCount = 500 * (Integer.parseInt(dateReturn.get(1)) - Integer.parseInt(dateDue.get(1)));
-            System.out.print(lateCount);
+            System.out.print(500 * (Integer.parseInt(dateReturn.get(1)) - Integer.parseInt(dateDue.get(1))));
             return;
         }
+        System.out.print(10000);
     }
 }
