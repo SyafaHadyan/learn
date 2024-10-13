6ae7af09c0b096ce210e1790afd2f80a2a611c70
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 06:33:39 +0700

    For-i request

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
@@ -8,9 +8,14 @@ public class CircularArrayRotation
         Scanner input = new Scanner(System.in);
         String[] request = input.nextLine().split(" ");
         int[] integerArray = new int[Integer.parseInt(request[0])];
+        int[] queries = new int[Integer.parseInt(request[2])];
         for (int i = 0; i < Integer.parseInt(request[0]); i++)
         {
             integerArray[i] = Integer.parseInt(input.next());
+        }
+        for (int i = 0; i < Integer.parseInt(request[2]); i++)
+        {
+            
         }
         input.close();
     }
