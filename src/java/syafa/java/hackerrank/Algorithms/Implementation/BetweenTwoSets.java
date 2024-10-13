381f3cc8f0235bc7b34980900497d4cf2fa7d720
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-13 17:54:28 +0700

    Arr int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BetweenTwoSets.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BetweenTwoSets.java
@@ -7,6 +7,12 @@ public class BetweenTwoSets
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> arrayLength = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
+        int[] arrayA = new int[Integer.parseInt(arrayLength.get(0))];
+        int[] arrayB = new int[Integer.parseInt(arrayLength.get(1))];
+        for (int i = 0; i < Integer.parseInt(arrayLength.get(0)); i++) 
+        {
+            //
+        }
         input.close();
     }
 }
