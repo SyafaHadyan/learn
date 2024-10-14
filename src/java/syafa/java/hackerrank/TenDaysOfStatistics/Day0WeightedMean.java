515fc54cd1c70854aac5e071b4a7095aa9486ab1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 20:39:30 +0700

    Input arrlen

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0WeightedMean.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0WeightedMean.java
@@ -6,6 +6,7 @@ public class Day0WeightedMean
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
