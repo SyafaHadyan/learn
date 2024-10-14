f335d3315843cbdb24fd2fc2f4c69bf27d32c728
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 18:23:17 +0700

    Initizlize scanner

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.LatihanSoalTIFB;
+import java.util.*;
 
 public class MethodCekPrima
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
