dc2b7f42032043e4066df7c35329663399ef6083
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 11:32:25 +0700

    Initialize mean median and mode

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
@@ -8,6 +8,9 @@ public class Day0MeanMedianAndMode
         Scanner input = new Scanner(System.in);
         int dataLength = Integer.parseInt(input.nextLine());
         int[] data = new int[dataLength];
+        double mean = -Double.MAX_VALUE;
+        double median = -Double.MAX_VALUE;
+        int mode = Integer.MIN_VALUE;
         for (int i = 0; i < dataLength; i++)
         {
             data[i] = Integer.parseInt(input.next());
