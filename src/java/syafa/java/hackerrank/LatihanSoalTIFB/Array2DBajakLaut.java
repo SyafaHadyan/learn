b48be46084c9cf027cd278d0666131ff25f6d310
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 16:48:48 +0700

    Input arr size

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DBajakLaut.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DBajakLaut.java
@@ -6,6 +6,7 @@ public class Array2DBajakLaut
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[] arraySize = input.nextLine().split(" ");
         input.close();
     }
 }
