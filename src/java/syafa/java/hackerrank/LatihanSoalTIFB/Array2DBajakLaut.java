769292db1cfa62fe14584ee19e875b8c14a45271
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 16:57:56 +0700

    Move

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DBajakLaut.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/Array2DBajakLaut.java
@@ -11,14 +11,6 @@ public class Array2DBajakLaut
         int obstacleAmount = Integer.parseInt(input.nextLine());
         int[] obstacleCoordinate = new int[obstacleAmount * 2];
         char[][] map = new char[Integer.parseInt(arraySize[0])][Integer.parseInt(arraySize[1])];
-        for (int i = 0; i < Integer.parseInt(arraySize[0]); i++)
-        {
-            for (int j = 0; j < Integer.parseInt(arraySize[1]); j++)
-            {
-                map[i][j] = '.';
-            }
-        }
-        map[Integer.parseInt(treasureCoordinate[0])][Integer.parseInt(treasureCoordinate[1])] = 'X';
         for (int i = 0; i < obstacleAmount; i++)
         {
             for (int j = 0; j < 2; j++)
@@ -28,5 +20,13 @@ public class Array2DBajakLaut
         }
         String[] playerMovement = input.nextLine().split(" ");
         input.close();
+        for (int i = 0; i < Integer.parseInt(arraySize[0]); i++)
+        {
+            for (int j = 0; j < Integer.parseInt(arraySize[1]); j++)
+            {
+                map[i][j] = '.';
+            }
+        }
+        map[Integer.parseInt(treasureCoordinate[0])][Integer.parseInt(treasureCoordinate[1])] = 'X';
     }
 }
