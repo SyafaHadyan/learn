700278f1762fb2d397ea5ede7a263da6d4148ffe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 18:26:34 +0700

    Initialize int var

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
@@ -7,6 +7,7 @@ public class MethodCekPrima
     {
         Scanner input = new Scanner(System.in);
         int check = Integer.parseInt(input.nextLine());
+        int numberCheck = Integer.MIN_VALUE;
         input.close();
     }
 }
