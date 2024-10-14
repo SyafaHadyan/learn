e3d0626301398a559fea2354161e748842ae7bec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 11:27:30 +0700

    Int data length

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
 
 public class Day0MeanMedianAndMode
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int dataLength = Integer.parseInt(input.nextLine());
+        input.close();
     }
 }
