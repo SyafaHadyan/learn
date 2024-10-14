8917f8ce23c535c0c5298f23e808e3410f0ca570
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 18:26:09 +0700

    Amount to check

--- src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
+++ src/java/syafa/java/hackerrank/LatihanSoalTIFB/MethodCekPrima.java
@@ -6,6 +6,7 @@ public class MethodCekPrima
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int check = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
