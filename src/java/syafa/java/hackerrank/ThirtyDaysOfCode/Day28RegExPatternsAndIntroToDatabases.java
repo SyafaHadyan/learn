d9bfd4dffd893441c8a95341f2732bed4dcc0cc6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-14 10:40:06 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day28RegExPatternsAndIntroToDatabases.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day28RegExPatternsAndIntroToDatabases.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 
-public class Day28RegExPatternsAndIntroToDatabases {
-
+public class Day28RegExPatternsAndIntroToDatabases
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
