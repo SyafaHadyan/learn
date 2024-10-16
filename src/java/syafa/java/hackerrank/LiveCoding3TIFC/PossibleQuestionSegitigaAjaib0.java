bc69d3961ed920b195ae9ea4bc1480cc7a2e34d1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 14:06:48 +0700

    Input staricase size and print space

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib0.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib0.java
@@ -6,6 +6,12 @@ public class PossibleQuestionSegitigaAjaib0
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int staricaseSize = Integer.parseInt(input.nextLine());
+        int space = staricaseSize - 1;
         input.close();
+        for (int i = 0; i < space; i++)
+        {
+            System.out.print(' ');
+        }
     }
 }
