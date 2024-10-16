9cdeda3f78592e2c8edad0b1c6beda514d32ebc6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:38:24 +0700

    Fixed calc

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -8,23 +8,13 @@ public class PossibleQuestionSegitigaAjaib10
         Scanner input = new Scanner(System.in);
         int fibonacci = Integer.parseInt(input.nextLine());
         input.close();
-        int previous = 1;
+        int previous = 0;
         int current = 1;
         int nextSequence = 0;
         for (int i = 0; i <= fibonacci; i++)
         {
-            if (i == 0)
-            {
-                nextSequence = 0;
-                continue;
-            }
-            if (i == 1)
-            {
-                nextSequence = 1;
-                continue;
-            }
-            nextSequence = previous + current;
-            previous = nextSequence;
+            nextSequence = current + previous;
+            previous = current;
             current = nextSequence;
             /*
              * 0
