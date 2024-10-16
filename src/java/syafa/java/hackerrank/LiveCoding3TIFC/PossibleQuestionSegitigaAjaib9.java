12e41fdca9e9e2484cde99c7d655ab3d84004beb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 19:21:30 +0700

    Apply space at all sector

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib9.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib9.java
@@ -92,7 +92,7 @@ public class PossibleQuestionSegitigaAjaib9
                     System.out.print(' ');
                 }
             }
-            for (int j = 0; j < (staricaseSize * 3); j++)
+            for (int j = 0; j < (staricaseSize * 4) - 1; j++)
             {
                 System.out.print('n');
             }
