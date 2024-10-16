5cb45bf8de497f3b3d3747932cb95f03c3ba0e9a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 21:59:53 +0700

    Fix

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
@@ -93,6 +93,6 @@ public class TankiAir
             }
         }
         input.close();
-        Arrays.stream(waterTank).forEach(System.out::println);;
+        Arrays.stream(result).forEach(System.out::println);;
     }
 }
