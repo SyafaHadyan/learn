e11745288fbd396773585d7808d44d8b74b581d0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 15:32:53 +0700

    Get from previous type

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib4.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib4.java
@@ -7,13 +7,29 @@ public class PossibleQuestionSegitigaAjaib4
     {
         Scanner input = new Scanner(System.in);
         int staricaseSize = Integer.parseInt(input.nextLine());
+        int space = 0;
         input.close();
         for (int i = staricaseSize; i > 0; i--)
         {
-            for (int j = 0; j < args.length; j++)
+            for (int j = 0; j < space; j++)
             {
-                //
+                System.out.print(' ');
+                if (space - j != 0)
+                {
+                    System.out.print(' ');
+                }
             }
+            space++;
+            for (int j = 1; j >= 0; j++)
+            {
+                System.out.print('*');
+                if ((j - staricaseSize != 0) && (i != 0))
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (staricaseSize - i != 1)
+            System.out.print("\n");
         }
     }
 }
