d534d586bcbd988573543adc365aa7144564c4ec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:11:32 +0700

    Boilerplate

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.LiveCoding3TIFC;
 
-public class PossibleQuestionSegitigaAjaib10 {
-
+public class PossibleQuestionSegitigaAjaib10
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
