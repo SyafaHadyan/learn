b3e640c7403469a2d5da800217825f6fb310161e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 14:01:54 +0700

    Calculate total number

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionTotalBilangan.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionTotalBilangan.java
@@ -6,6 +6,12 @@ public class PossibleQuestionTotalBilangan
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberInput = Integer.parseInt(input.nextLine());
         input.close();
+        int totalNumber = 0;
+        for (int i = 1; i <= numberInput; i++)
+        {
+            totalNumber += i;
+        }
     }
 }
