113982586a968dd53205b28f6dc5df648f07a280
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 21:35:45 +0700

    Initialize check

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
@@ -9,6 +9,15 @@ public class TankiAir
         int testCase = Integer.parseInt(input.nextLine());
         int waterTank = 0;
         int[] result = new int[testCase];
+        /*
+         * phase0 = normal = inc 3
+         * phase1 = rain = inc 5
+         * phase2 = heat = inc 3
+         * 
+         * phase0 = normal = dec -4
+         * phase1 = rain = dec -4
+         * phase2 = heat = dec -6
+         */
         for (int i = 0; i < testCase; i++)
         {
             waterTank = Integer.parseInt(input.nextLine());
@@ -19,7 +28,19 @@ public class TankiAir
             }
             if (waterTank > 100)
             {
-                //
+                if (i >= 0 && i <= 4)
+                {
+                    //
+                }
+                else if (i >= 5 && i <= 9)
+                {
+                    //
+                }
+                else if (i >= 10 && i <= 14)
+                {
+                    //
+                    i = -1;
+                }
             }
             if (waterTank < 100)
             {
