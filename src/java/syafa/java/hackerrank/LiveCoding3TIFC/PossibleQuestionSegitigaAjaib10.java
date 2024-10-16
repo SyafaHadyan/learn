a9de91aa0a1d67b37e05d67d8ca65fd17d66d951
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:13:09 +0700

    For-i fibonacci

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -6,6 +6,11 @@ public class PossibleQuestionSegitigaAjaib10
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int fibonacci = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = 0; i <= fibonacci; i++)
+        {
+            //
+        }
     }
 }
