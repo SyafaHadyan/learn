b1c05fcd2d8047e2dbe813354fb4a9615ab4a427
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 21:29:24 +0700

    Initialize for loop

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB3/TankiAir.java
@@ -6,6 +6,24 @@ public class TankiAir
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        int waterTank = 0;
+        for (int i = 0; i < testCase; i++)
+        {
+            waterTank = Integer.parseInt(input.nextLine());
+            if (waterTank == 100)
+            {
+                //
+            }
+            if (waterTank > 100)
+            {
+                //
+            }
+            if (waterTank < 100)
+            {
+                //
+            }
+        }
         input.close();
     }
 }
