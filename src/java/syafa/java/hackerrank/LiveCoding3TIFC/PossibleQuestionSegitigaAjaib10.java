0146a95995e78c4baab7907eb10a89f101fb6ac1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:42:53 +0700

    Fix indexing

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -13,17 +13,22 @@ public class PossibleQuestionSegitigaAjaib10
         int nextSequence = 0;
         for (int i = 2; i < fibonacci; i++)
         {
-            if (fibonacci == 0)
+            if (i == 2)
             {
                 nextSequence = 0;
+                i--;
             }
             if (fibonacci == 1)
             {
                 nextSequence = 1;
+                i--;
             }
-            nextSequence = current + previous;
-            previous = current;
-            current = nextSequence;
+            else
+            {
+                nextSequence = current + previous;
+                previous = current;
+                current = nextSequence;
+            }
             System.out.println(nextSequence);
             /*
              * 0
