00d76ab8c1e3fa4317e962648f91a078eaaffcd2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:11:59 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.LiveCoding3TIFC;
+import java.util.*;
 
 public class PossibleQuestionSegitigaAjaib10
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
