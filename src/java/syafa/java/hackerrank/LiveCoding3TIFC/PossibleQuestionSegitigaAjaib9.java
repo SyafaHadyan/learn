dbb903329a03a064083885fb01da66424efcfec2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 17:14:04 +0700

    Add left side from another file

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib9.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib9.java
@@ -33,5 +33,55 @@ public class PossibleQuestionSegitigaAjaib9
                 System.out.print("\n");
             }
         }
+        System.out.print("\n");
+        space = staricaseSize - 1;
+        for (int i = 0; i < staricaseSize; i++)
+        {
+            for (int j = 0; j < space; j++)
+            {
+                System.out.print(' ');
+                if (space - j != 0)
+                {
+                    System.out.print(' ');
+                }
+            }
+            space--;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print('*');
+                if ((j - staricaseSize != 0) && (i != 0))
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (staricaseSize - i != 1)
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        space = 1;
+        for (int i = staricaseSize - 1; i > 0; i--)
+        {
+            for (int j = space; j > 0; j--)
+            {
+                System.out.print(' ');
+                if (j != 0)
+                {
+                    System.out.print(' ');
+                }
+            }
+            space++;
+            for (int j = i; j > 0; j--)
+            {
+                System.out.print('*');
+                if (j != 1)
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (i != 1)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
