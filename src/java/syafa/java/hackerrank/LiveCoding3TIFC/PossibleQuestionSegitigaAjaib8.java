d89c18084ca3ecb34632cf7d8f287abc88a7284a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 16:20:35 +0700

    Add from another file

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib8.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib8.java
@@ -31,5 +31,31 @@ public class PossibleQuestionSegitigaAjaib8
             if (staricaseSize - i != 1)
             System.out.print("\n");
         }
+        System.out.print("\n");
+        space = 0;
+        for (int i = staricaseSize; i > 0; i--)
+        {
+            for (int j = space; j > 0; j--)
+            {
+                System.out.print(' ');
+                if (j != 0)
+                {
+                    System.out.print(' ');
+                }
+            }
+            space++;
+            for (int j = i; j > 0; j--)
+            {
+                System.out.print('*');
+                if (j != 1)
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (i != 1)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
