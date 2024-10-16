8fd29750875173b7512787aa25230dda28825582
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 20:17:29 +0700

    Add comment

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib10.java
@@ -8,9 +8,21 @@ public class PossibleQuestionSegitigaAjaib10
         Scanner input = new Scanner(System.in);
         int fibonacci = Integer.parseInt(input.nextLine());
         input.close();
+        int currentSeq = 0;
+        int currentIncr = 1;
         for (int i = 0; i <= fibonacci; i++)
         {
-            //
+            currentSeq += currentIncr;
+            /*
+             * 0
+             * 1
+             * 1
+             * 2
+             * 3
+             * 5
+             * 8
+             */
         }
+        System.out.print(currentSeq);
     }
 }
