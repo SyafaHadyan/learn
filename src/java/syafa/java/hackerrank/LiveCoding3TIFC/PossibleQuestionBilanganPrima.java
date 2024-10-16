967ed1731529a2fbe7b36cfadf564ee5f2871a1d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 13:54:25 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionBilanganPrima.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionBilanganPrima.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.LiveCoding3TIFC;
 
-public class PossibleQuestionBilanganPrima {
-
+public class PossibleQuestionBilanganPrima
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
