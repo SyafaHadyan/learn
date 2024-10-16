41697db71357ece3c63e7f583906023f7db7a641
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 13:56:40 +0700

    If not prime

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionBilanganPrima.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionBilanganPrima.java
@@ -6,6 +6,15 @@ public class PossibleQuestionBilanganPrima
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberCheck = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = 1; i < numberCheck; i++)
+        {
+            if (numberCheck % i == 0)
+            {
+                System.out.print("Bukan prima");
+                return;
+            }
+        }
     }
 }
