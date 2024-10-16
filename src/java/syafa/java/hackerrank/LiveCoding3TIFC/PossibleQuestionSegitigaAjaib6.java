f693261988dd47ed100e18baf55e9838b04d9421
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 16:03:31 +0700

    Add from another file

---
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib6.java
@@ -0,0 +1,37 @@
+package syafa.java.hackerrank.LiveCoding3TIFC;
+import java.util.*;
+
+public class PossibleQuestionSegitigaAjaib6
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int staricaseSize = Integer.parseInt(input.nextLine());
+        int space = 0;
+        input.close();
+        for (int i = staricaseSize; i > 0; i--)
+        {
+            for (int j = space; j > 0; j--)
+            {
+                System.out.print(' ');
+                if (j != 0)
+                {
+                    System.out.print(' ');
+                }
+            }
+            space++;
+            for (int j = i; j > 0; j--)
+            {
+                System.out.print('*');
+                if (j != 1)
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (i != 1)
+            {
+                System.out.print("\n");
+            }
+        }
+    }
+}
