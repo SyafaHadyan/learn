9fc8785a5858a1b04619fb3ad4cbce1b523cb0fd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-16 15:30:48 +0700

    For loop reversed staricase

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib4.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib4.java
@@ -6,6 +6,14 @@ public class PossibleQuestionSegitigaAjaib4
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int staricaseSize = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = staricaseSize; i > 0; i--)
+        {
+            for (int j = 0; j < args.length; j++)
+            {
+                //
+            }
+        }
     }
 }
