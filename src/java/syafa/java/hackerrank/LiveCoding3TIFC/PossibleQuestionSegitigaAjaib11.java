537b0bdb2ad505f95321fc172aa818f2ed8136b9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-17 07:10:34 +0700

    Testing add

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib11.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib11.java
@@ -8,6 +8,7 @@ public class PossibleQuestionSegitigaAjaib11
         Scanner input = new Scanner(System.in);
         int triangleLevel = Integer.parseInt(input.nextLine());
         input.close();
+        int space = triangleLevel - 1;
         int[] values = new int[triangleLevel + 1];
         int[] prevValues = new int[triangleLevel + 1];
         /*
@@ -19,9 +20,13 @@ public class PossibleQuestionSegitigaAjaib11
          * 1 5 10 10 5 1
          */
         /*
+         *     1
+         *    1 1
+         *   1 2 1
          *  1 3 3 1
          * 1 4 6 4 1
          */
+        /*
         prevValues[0] = 1;
         for (int i = 0; i < triangleLevel; i++)
         {
@@ -51,6 +56,27 @@ public class PossibleQuestionSegitigaAjaib11
                         }
                     }
                 }
+            }
+        }
+        */
+        for (int i = 0; i < triangleLevel; i++)
+        {
+            for (int j = 0; j < space; j++)
+            {
+                System.out.print(' ');
+            }
+            space--;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print('*');
+                if ((j - triangleLevel != 0) && (i != 0))
+                {
+                    System.out.print(' ');
+                }
+            }
+            if (triangleLevel - i != 1)
+            {
+                System.out.print("\n");
             }
         }
     }
