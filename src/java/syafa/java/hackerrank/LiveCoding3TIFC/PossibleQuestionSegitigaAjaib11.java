45f2da4c9fdc36d9b840fd8137a74a41705a7938
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-17 07:53:59 +0700

    Temp

--- src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib11.java
+++ src/java/syafa/java/hackerrank/LiveCoding3TIFC/PossibleQuestionSegitigaAjaib11.java
@@ -9,8 +9,8 @@ public class PossibleQuestionSegitigaAjaib11
         int triangleLevel = Integer.parseInt(input.nextLine());
         input.close();
         int space = triangleLevel - 1;
-        int[] values = new int[triangleLevel + 1];
-        int[] prevValues = new int[triangleLevel + 1];
+        int[][] values = new int[triangleLevel + 1][triangleLevel + 1];
+        int[][] prevValues = new int[triangleLevel + 1][triangleLevel + 1];
         /*
          * 1
          * 1 1
@@ -27,8 +27,7 @@ public class PossibleQuestionSegitigaAjaib11
          * 1 4 6 4 1
          */
         
-        prevValues[0] = 1;
-        
+        //prevValues[0] = 1;
         for (int i = 0; i < triangleLevel; i++)
         {
             for (int j = 0; j < triangleLevel; j++)
@@ -38,28 +37,28 @@ public class PossibleQuestionSegitigaAjaib11
                     //System.out.print(values[j]);
                     try
                     {
-                        if (k == 0)
+                        if (k == 0 || k == 1)
                         {
-                            values[k + 1] = prevValues[k] + prevValues[k];
+                            values[j][k] = 1;
                         }
                         else
                         {
-                            values[k + 1] = prevValues[k] + prevValues[k + 1];
+                            values[j][k] = values[j - 1][k - 1] + values[j - 1][k];
                         }
                     }
                     catch (IndexOutOfBoundsException e)
                     {
+                        /*
                         for (int l = 0; l < values.length; l++)
                         {
-                            /*
                             prevValues[k] = values[j];
                             System.out.print(values[k]);
                             if (values.length - k != 1)
                             {
                                 System.out.println("\n");
                             }
-                            */
                         }
+                        */
                     }
                 }
             }
@@ -70,15 +69,19 @@ public class PossibleQuestionSegitigaAjaib11
             space--;
             for (int j = 0; j <= i; j++)
             {
-                System.out.print(values[j]);
-                if ((j - triangleLevel != 0) && (i != 0))
+                for (int k = 0; k < j; k++)
                 {
-                    System.out.print(' ');
+                    System.out.print(values[j][k]);
+                    if ((j - triangleLevel != 0) && (i != 0))
+                    {
+                        System.out.print(' ');
+                    }
+                    if (triangleLevel - i != 1)
+                    {
+                        //System.out.print("\n");
+                    }
                 }
-            }
-            if (triangleLevel - i != 1)
-            {
-                System.out.print("\n");
+                System.out.println();
             }
         }
     }
