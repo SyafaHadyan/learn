1b12b15e3fe712222c0b42d78517826ec8db6235
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 10:01:08 +0700

    Print summary

--- src/java/syafa/java/codeinclass/Array1D.java
+++ src/java/syafa/java/codeinclass/Array1D.java
@@ -8,15 +8,18 @@ public class Array1D
         Scanner input = new Scanner(System.in);
         int arraySize = Integer.parseInt(input.nextLine());
         input.close();
+        int totalSum = 0;
         int[] array = new int[arraySize];
         for (int i = 0; i < arraySize; i++)
         {
             array[i] = i;
             System.out.print(array[i]);
+            totalSum += i;
             if (arraySize - i != 1)
             {
                 System.out.print("\n");
             }
         }
+        System.out.printf("\n%-15s%-2c%d\n%-15s%-2c%d","Array size",':',arraySize,"Total sum",':',totalSum);
     }
 }
