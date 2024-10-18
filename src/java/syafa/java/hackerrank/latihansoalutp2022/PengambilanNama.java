8bba8cf7bd67787e22876f9ac53e07dcf7e16333
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 19:55:20 +0700

    Fix space

--- src/java/syafa/java/hackerrank/latihansoalutp2022/PengambilanNama.java
+++ src/java/syafa/java/hackerrank/latihansoalutp2022/PengambilanNama.java
@@ -9,5 +9,22 @@ public class PengambilanNama
         String[] firstName = input.nextLine().split(" ");
         String[] secondName = input.nextLine().split(" ");
         input.close();
+        for (int i = 0; i < firstName.length - 1; i++)
+        {
+            System.out.print(firstName[i + 1]);
+            if (firstName.length - i != 2)
+            {
+                System.out.print(' ');
+            }
+        }
+        System.out.print("\n");
+        for (int i = 0; i < secondName.length - 1; i++)
+        {
+            System.out.print(secondName[i + 1]);
+            if (secondName.length - i != 2)
+            {
+                System.out.print(' ');
+            }
+        }
     }
 }
