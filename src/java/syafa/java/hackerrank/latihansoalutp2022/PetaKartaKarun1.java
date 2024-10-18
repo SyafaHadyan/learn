daef944718c8c10203cbccab6af443d3661c0f50
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 19:58:10 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/latihansoalutp2022/PetaKartaKarun1.java
+++ src/java/syafa/java/hackerrank/latihansoalutp2022/PetaKartaKarun1.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.latihansoalutp2022;
+import java.util.*;
 
 public class PetaKartaKarun1
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
