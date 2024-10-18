ca44cfe5d99e383ddd18cdcf18d3b5573accaac0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 09:44:57 +0700

    Input to arr

--- src/java/syafa/java/codeinclass/Array1D.java
+++ src/java/syafa/java/codeinclass/Array1D.java
@@ -1,4 +1,5 @@
 package syafa.java.codeinclass;
+import java.lang.classfile.constantpool.IntegerEntry;
 import java.util.*;
 
 public class Array1D
@@ -6,6 +7,12 @@ public class Array1D
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
         input.close();
+        int[] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = i;
+        }
     }
 }
