213cdd3633336086e16491632c22e832080d4f45
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 13:46:50 +0700

    Edit to enter at new line

--- src/java/syafa/java/codeinclass/array/Matrix.java
+++ src/java/syafa/java/codeinclass/array/Matrix.java
@@ -9,7 +9,7 @@ public class Matrix
         Scanner input = new Scanner(System.in);
         System.out.printf("%-55s%-2c","Input matrix size [Row Column] (Separate by space)",':');
         int[] matrixSize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.out.printf("%-55s%-2c","Input first matrix [Row Column] (Separate by space)",':');
+        System.out.printf("%-55s%c\n","Input first matrix [Row Column] (Separate by space)",':');
         int[][] firstMatrix = new int[matrixSize[0]][];
         for (int i = 0; i < matrixSize[0]; i++)
         {
