f32e6f8994893fc253ce688fef4a9260ae9b4e76
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 09:43:29 +0700

    Initialize scanner

--- src/java/syafa/java/codeinclass/Array1D.java
+++ src/java/syafa/java/codeinclass/Array1D.java
@@ -1,9 +1,11 @@
 package syafa.java.codeinclass;
+import java.util.*;
 
 public class Array1D
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
