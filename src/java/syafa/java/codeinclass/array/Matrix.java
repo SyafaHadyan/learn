935997ed4904b1ee93110a98a19c768806e1e7bf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 13:39:33 +0700

    Prompt user

--- src/java/syafa/java/codeinclass/array/Matrix.java
+++ src/java/syafa/java/codeinclass/array/Matrix.java
@@ -7,6 +7,7 @@ public class Matrix
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-55s%-2c","Input matrix size [Row Column] (Separate by space)",':');
         int[] matrixSize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
