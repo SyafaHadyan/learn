6178ad301f131ea61e82633838c22f0bb4aad677
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 13:37:46 +0700

    Input matrix size

--- src/java/syafa/java/codeinclass/array/Matrix.java
+++ src/java/syafa/java/codeinclass/array/Matrix.java
@@ -1,11 +1,13 @@
 package syafa.java.codeinclass.array;
 import java.util.*;
+import java.util.stream.*;
 
 public class Matrix
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] matrixSize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
