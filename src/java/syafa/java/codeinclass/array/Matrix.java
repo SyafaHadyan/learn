630d5774e4785c4efd4bf7b3ae2817e6f9c88baa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 13:35:14 +0700

    Initialize scanner

--- src/java/syafa/java/codeinclass/array/Matrix.java
+++ src/java/syafa/java/codeinclass/array/Matrix.java
@@ -1,9 +1,11 @@
 package syafa.java.codeinclass.array;
+import java.util.*;
 
 public class Matrix
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
