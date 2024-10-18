3d8fa4e13578563d3fc92462ef245e3e6ffa9c30
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 14:04:42 +0700

    Edit printf

--- src/java/syafa/java/codeinclass/array/Matrix.java
+++ src/java/syafa/java/codeinclass/array/Matrix.java
@@ -18,15 +18,15 @@ public class Matrix
         boolean possibleMultiplication = true;
         String possibleMultiplicationOption = "";
         Scanner input = new Scanner(System.in);
-        System.out.printf("%-55s%-2c","Input matrix size [Row]",':');
+        System.out.printf("%-50s%-2c","Input matrix sizes [First Row] [Second Row]",':');
         int[] matrixSize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.out.printf("%-55s%c\n","Input first matrix [Column] (Separate by space)",':');
+        System.out.printf("%-50s%c\n","Input first matrix [Column] (Separate by space)",':');
         int[][] firstMatrix = new int[matrixSize[0]][];
         for (int i = 0; i < matrixSize[0]; i++)
         {
             firstMatrix[i] = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        System.out.printf("%-55s%c\n","Input second matrix [Column] (Separate by space)",':');
+        System.out.printf("%-50s%c\n","Input second matrix [Column] (Separate by space)",':');
         int[][] secondMatrix = new int[matrixSize[0]][];
         for (int i = 0; i < matrixSize[0]; i++)
         {
@@ -37,7 +37,7 @@ public class Matrix
             possibleMultiplication = false;
             possibleMultiplicationOption = " [Not possible due to matrix size]";
         }
-        System.out.printf("%-55s%c\n%s\n%s\n%s%s","Choose operation",':',"Add","Substract","Multiply",possibleMultiplicationOption);
+        System.out.printf("%-50s%c\n%s\n%s\n%s%s","Choose operation",':',"Add","Substract","Multiply",possibleMultiplicationOption);
         input.close();
     }
 }
