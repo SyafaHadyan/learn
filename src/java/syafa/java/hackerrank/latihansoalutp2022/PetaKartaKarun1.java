a8676803c468af76e6921f55240b9ee772465b77
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 20:01:04 +0700

    Arr str result

--- src/java/syafa/java/hackerrank/latihansoalutp2022/PetaKartaKarun1.java
+++ src/java/syafa/java/hackerrank/latihansoalutp2022/PetaKartaKarun1.java
@@ -6,6 +6,17 @@ public class PetaKartaKarun1
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String step = input.nextLine();
+        String[] result = new String[step.length()];
         input.close();
+        for (int i = 0; i < step.length(); i++)
+        {
+            //
+        }
+        /*
+         * U05U10B05B10
+         * 
+         * Pak Dengklek akan berjalan ke arah utara sebanyak 5 langkah, ke arah utara sebanyak 10 langkah, ke arah barat sebanyak 5 langkah, ke arah barat sebanyak 10 langkah.
+         */
     }
 }
