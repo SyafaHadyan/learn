1c399f5cdc4ff5947d16e81ea1f2baf2c1525dc3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 10:25:53 +0700

    Get split space

--- src/java/syafa/java/codeinclass/Array1DSort.java
+++ src/java/syafa/java/codeinclass/Array1DSort.java
@@ -1,11 +1,13 @@
 package syafa.java.codeinclass;
 import java.util.*;
+import java.util.stream.*;
 
 public class Array1DSort
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
