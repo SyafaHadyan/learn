5df1920eb02638c31eb3a3b4f61ca818bb4ecabc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 19:52:20 +0700

    Store input in arr str

--- src/java/syafa/java/hackerrank/latihansoalutp2022/PengambilanNama.java
+++ src/java/syafa/java/hackerrank/latihansoalutp2022/PengambilanNama.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.latihansoalutp2022;
+import java.util.*;
 
-public class PengambilanNama {
-
+public class PengambilanNama
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String[] firstName = input.nextLine().split(" ");
+        String[] secondName = input.nextLine().split(" ");
+        input.close();
+    }
 }
