75bf6dd22f44d306e15bf03a8e8468ea62d5eb33
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-18 10:35:48 +0700

    Sort if yes

--- src/java/syafa/java/codeinclass/Array1DSort.java
+++ src/java/syafa/java/codeinclass/Array1DSort.java
@@ -7,7 +7,15 @@ public class Array1DSort
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-30s%-2c","Input integers split by space",':');
         int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.printf("%-30s%-2c","Sort array (Y/N)",':');
+        String confirm = input.nextLine();
+        boolean sort = (confirm.equalsIgnoreCase("Y"))? (sort = true) : (sort = false);
+        if (sort)
+        {
+            Arrays.sort(array);
+        }
         input.close();
         for (int i = 0; i < array.length; i++)
         {
