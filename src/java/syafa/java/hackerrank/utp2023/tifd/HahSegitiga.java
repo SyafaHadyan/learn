3d2fba7368f491e08c3227966ceb009ef6bc7916
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 11:16:00 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/utp2023/tifd/HahSegitiga.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/HahSegitiga.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.utp2023.tifd;
+import java.util.*;
 
 public class HahSegitiga
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
