9090aea399cb333256b7205370df0c67ddeba450
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 07:52:53 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/sampletest/FizzBuzz.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/sampletest/FizzBuzz.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.certification.problemsolvingbasic.sampletest;
+import java.util.*;
 
 public class FizzBuzz
 {
     public static void main(String[] args) 
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
