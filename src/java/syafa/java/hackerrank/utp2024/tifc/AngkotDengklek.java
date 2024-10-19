8916c0176dd224a4373a4fe9d999050afafd83fa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 18:22:43 +0700

    Int var input

--- src/java/syafa/java/hackerrank/utp2024/tifc/AngkotDengklek.java
+++ src/java/syafa/java/hackerrank/utp2024/tifc/AngkotDengklek.java
@@ -6,6 +6,9 @@ public class AngkotDengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int start = Integer.parseInt(input.nextLine());
+        int end = Integer.parseInt(input.nextLine());
+        int passengerAmount = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
