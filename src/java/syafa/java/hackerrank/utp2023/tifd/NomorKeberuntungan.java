c4bbb5311878f41358b8a3bce22f841640c6b59c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 12:06:39 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.utp2023.tifd;
+import java.util.*;
+import java.util.stream.*;
 
 public class NomorKeberuntungan
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
