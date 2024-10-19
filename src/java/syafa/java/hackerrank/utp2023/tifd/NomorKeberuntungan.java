b46413547f961e26f1ede1e0966a2164c69053ef
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 12:33:23 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
@@ -6,27 +6,6 @@ public class NomorKeberuntungan
 {
     public static void main(String[] args)
     {
-        /*
-         * 1 2 7
-         * 
-         * Terbesar 7
-         * Terkecil 1
-         * Selisih 7 - 1 = 6
-         * Habis dibagi 2 3
-         * Belum hoki!
-         * 
-         * 11 2 19
-         * 
-         * Terbesar 19
-         * Terkecil 2
-         * Selisih 19 - 2 = 17
-         * Tidak ada
-         * Jackpot!!!
-         * 
-         * 2 3 5 7 11
-         * if divisible by 1-4 of num, not jack
-         * else if not divisible by any of num or divisible by all of num, jack
-         */
         Scanner input = new Scanner(System.in);
         int[] magicNumber = {2, 3, 5, 7, 11};
         int[] number = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
