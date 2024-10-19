815ca9e29aa086ed5247af098b2828a704b66a10
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 23:31:17 +0700

    Use arrlist

--- src/java/syafa/java/hackerrank/Algorithms/Strings/Pangrams.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/Pangrams.java
@@ -6,12 +6,16 @@ public class Pangrams
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        char[] word = input.nextLine().toCharArray();
-        Arrays.sort(word);
+        //char[] word = input.nextLine().toCharArray();
+        ArrayList<String> word = new ArrayList<>(Arrays.asList(input.nextLine().split("")));
+        Collections.sort(word);
         input.close();
-        for (int i = 0; i < word.length; i++)
+        for (int i = 0; i < word.size(); i++)
         {
-            
+            if (word.get(i) == word.get(i + 1))
+            {
+                //
+            }
         }
     }
 }
