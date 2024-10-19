b5cc4cb02d2e19fe6d20d1048af038721b1bc28e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 12:09:26 +0700

    Add comment

--- src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/NomorKeberuntungan.java
@@ -6,6 +6,19 @@ public class NomorKeberuntungan
 {
     public static void main(String[] args)
     {
+        /*
+         * 1 2 7
+         * 
+         * Terbesar 7
+         * Terkecil 1
+         * Selisih 7 - 1 = 6
+         * Habis dibagi 2 3
+         * Belum hoki!
+         * 
+         * 2 3 5 7 11
+         * if divisible by 1-4 of num, not jack
+         * else if not divisible by any of num or divisible by all of num, jack
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
