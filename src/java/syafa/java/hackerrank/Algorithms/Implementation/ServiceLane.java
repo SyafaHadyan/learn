fb7133805d5789f6e016e57252dee6329be73845
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 20:13:39 +0700

    Input highway length and test case

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ServiceLane.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ServiceLane.java
@@ -7,6 +7,7 @@ public class ServiceLane
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] highwayCar = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
