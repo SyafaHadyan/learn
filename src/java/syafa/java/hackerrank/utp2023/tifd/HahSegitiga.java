5d0ec69485fae166fd99ea29371df070091c76bb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 11:16:35 +0700

    Add comment

--- src/java/syafa/java/hackerrank/utp2023/tifd/HahSegitiga.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/HahSegitiga.java
@@ -5,6 +5,12 @@ public class HahSegitiga
 {
     public static void main(String[] args)
     {
+        /*
+         * Segitiga siku-siku
+         * Segitiga sama sisi
+         * Segitiga sama kaki
+         * Segitiga sembarang
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
