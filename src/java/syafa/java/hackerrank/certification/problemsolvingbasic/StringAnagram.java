66cca5fceaaea70223fbfceaa9295660062eef51
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 09:06:46 +0700

    Sort str

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
@@ -7,9 +7,13 @@ public class StringAnagram
     {
         Scanner input = new Scanner(System.in);
         int dictionarySize = Integer.parseInt(input.nextLine());
+        String[] dictonary = new String[dictionarySize];
         for (int i = 0; i < dictionarySize; i++)
         {
-            //
+            String tempString = input.nextLine();
+            char[] tempChar = tempString.toCharArray();
+            Arrays.sort(tempChar);
+            dictonary[i] = new String(tempChar);
         }
         input.close();
     }
