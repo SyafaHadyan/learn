853866afac57974291ae26ad39a67e738db45e0a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 19:02:57 +0700

    Input

--- src/java/syafa/java/hackerrank/utp2024/tifc/HutanLabirin.java
+++ src/java/syafa/java/hackerrank/utp2024/tifc/HutanLabirin.java
@@ -6,6 +6,13 @@ public class HutanLabirin
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int start = Integer.parseInt(input.nextLine());
+        int end = Integer.parseInt(input.nextLine());
+        int step = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = 0; i < step; i++)
+        {
+            //
+        }        
     }
 }
