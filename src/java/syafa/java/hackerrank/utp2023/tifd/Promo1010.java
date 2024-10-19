9ab138a0eda53663a8918e1aacd460afd51c288b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 10:06:07 +0700

    Input arr int coffee purchase

--- src/java/syafa/java/hackerrank/utp2023/tifd/Promo1010.java
+++ src/java/syafa/java/hackerrank/utp2023/tifd/Promo1010.java
@@ -1,9 +1,13 @@
 package syafa.java.hackerrank.utp2023.tifd;
+import java.util.*;
+import java.util.stream.*;
 
 public class Promo1010
 {
     public static void main(String[] args) 
     {
-        
+        Scanner input = new Scanner(System.in);
+        int[] purchaseCoffee = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        input.close();
     }
 }
