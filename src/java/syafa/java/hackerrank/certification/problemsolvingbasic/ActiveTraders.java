4e75aa8662abe2eb94d1b370968a5a6b69015ad1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 08:37:31 +0700

    For-i

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/ActiveTraders.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/ActiveTraders.java
@@ -6,6 +6,11 @@ public class ActiveTraders
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int customerAmount = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < customerAmount; i++)
+        {
+            //
+        }
         input.close();
     }
 }
