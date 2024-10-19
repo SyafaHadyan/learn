0eace18a184330fbd7e6a1f1ef9b882da93623a4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 09:08:57 +0700

    Reduce var

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
@@ -6,15 +6,15 @@ public class StringAnagram
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int dictionarySize = Integer.parseInt(input.nextLine());
-        String[] dictonary = new String[dictionarySize];
-        for (int i = 0; i < dictionarySize; i++)
+        String[] dictonary = new String[Integer.parseInt(input.nextLine())];
+        for (int i = 0; i < dictonary.length; i++)
         {
             String tempString = input.nextLine();
             char[] tempChar = tempString.toCharArray();
             Arrays.sort(tempChar);
             dictonary[i] = new String(tempChar);
         }
+        int[] match = new int[Integer.parseInt(input.nextLine())];
         input.close();
     }
 }
