8f2af0e321aa2739a078a99d29d749b357ed67c8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 23:19:49 +0700

    Remove comment

--- src/java/syafa/java/hackerrank/Algorithms/Strings/MarsExploration.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/MarsExploration.java
@@ -5,16 +5,7 @@ public class MarsExploration
 {
     public static void main(String[] args)
     {
-        /*
-         * SOS OOS OSO SOS OSS OSO SOS OSO SOS
-         *      x   x       x   x       x
-         * 
-         * SOS O O SOS O SOS O SS O SOS O SOS O SOS
-         *     x x     x     x  x x  x  x     x 
-         * 12
-         */
         Scanner input = new Scanner(System.in);
-        //char[] message = input.nextLine().toCharArray();
         String message = input.nextLine();
         input.close();
         int corruptMessage = 0;
