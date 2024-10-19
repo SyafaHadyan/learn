77ae8a0f43010bf7ad0492d04106f1cb509a0a17
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 19:18:02 +0700

    Arr

--- src/java/syafa/java/hackerrank/utp2024/tifc/UnitGawatDarurat.java
+++ src/java/syafa/java/hackerrank/utp2024/tifc/UnitGawatDarurat.java
@@ -1,11 +1,19 @@
 package syafa.java.hackerrank.utp2024.tifc;
 import java.util.*;
+//import java.util.stream.*;
 
 public class UnitGawatDarurat
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[] patientName = new String[3];
+        int[] patientAge = new int[3];
+        int[] patientOxygen = new int[3];
+        for (int index = 0; index < array.length; index++)
+        {
+            
+        }
         input.close();
     }
 }
