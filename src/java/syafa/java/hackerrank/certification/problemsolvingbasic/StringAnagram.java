f408f03f8c5302bf3529f3dfeffac2ad492cfd6b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 09:04:22 +0700

    Fori

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/StringAnagram.java
@@ -6,6 +6,11 @@ public class StringAnagram
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int dictionarySize = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < dictionarySize; i++)
+        {
+            //
+        }
         input.close();
     }
 }
