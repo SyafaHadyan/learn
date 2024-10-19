5f43b93df365aa70973aea89cd9fff37d076b9a5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 19:14:14 +0700

    Break

--- src/java/syafa/java/hackerrank/utp2024/tifc/HutanLabirin.java
+++ src/java/syafa/java/hackerrank/utp2024/tifc/HutanLabirin.java
@@ -8,20 +8,24 @@ public class HutanLabirin
         Scanner input = new Scanner(System.in);
         int start = Integer.parseInt(input.nextLine());
         int end = Integer.parseInt(input.nextLine());
-        int step = Integer.parseInt(input.nextLine());
+        //int step = Integer.parseInt(input.nextLine());
         input.close();
         int counter = 0;
         int number = start;
-        while (counter != step || number < 1000 || counter <= step)
+        while (counter != end || number < 1000 || counter <= end)
         {
             counter++;
             if (number == 0 || number % 2 == 0)
             {
                 number /= 2;
             }
-            else if (number % 2 != 0)
+            else if (number == 1 || number % 2 != 0)
             {
-                number *= 10;
+                number *= 100;
+            }
+            if (number > 1000 || counter == end)
+            {
+                break;
             }
         }
         if (number < 1000)
