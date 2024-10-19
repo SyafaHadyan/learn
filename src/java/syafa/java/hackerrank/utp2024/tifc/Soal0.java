e0d2a85dd6d9d0303d4be9bbdc3ac7a9e2dfa9b0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 15:01:17 +0700

    Create file

---
+++ src/java/syafa/java/hackerrank/utp2024/tifc/Soal0.java
@@ -0,0 +1,11 @@
+package syafa.java.hackerrank.utp2024.tifc;
+import java.util.*;
+
+public class Soal0
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
---
+++ src/java/syafa/java/hackerrank/utp2024/tifc/Soal2.java
@@ -0,0 +1,11 @@
+package syafa.java.hackerrank.utp2024.tifc;
+import java.util.*;
+
+public class Soal2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
