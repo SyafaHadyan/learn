b6a03ca19833ee50b4f2f6f0ea8bb56ddd8631ce
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 23:26:08 +0700

    Sort word char arr

--- src/java/syafa/java/hackerrank/Algorithms/Strings/Pangrams.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/Pangrams.java
@@ -6,6 +6,12 @@ public class Pangrams
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        char[] word = input.nextLine().toCharArray();
+        Arrays.sort(word);
         input.close();
+        for (int i = 0; i < word.length; i++)
+        {
+            
+        }
     }
 }
