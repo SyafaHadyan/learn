13792690b9244a9f7c1f2a83b213a21a7510bd91
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-19 07:53:46 +0700

    For-i

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/sampletest/FizzBuzz.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/sampletest/FizzBuzz.java
@@ -6,6 +6,11 @@ public class FizzBuzz
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
+        int number = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = 1; i <= number; i++)
+        {
+            //
+        }
     }
 }
