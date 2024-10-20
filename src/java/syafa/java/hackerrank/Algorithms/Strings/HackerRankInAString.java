f6dbdc2784426340484bb7ab8ec8750cbb3189f8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 11:02:44 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
@@ -5,28 +5,6 @@ public class HackerRankInAString
 {
     public static void main(String[] args)
     {
-        /*
-         * 2
-         * hereiamstackerrank
-         * hackerworld
-         * 
-         * YES
-         * NO
-         * 
-         * 1
-         * hereiamstackerrank
-         * 
-         * H
-         * A
-         * C
-         * K
-         * E
-         * R
-         * R
-         * A
-         * N
-         * K
-         */
         Scanner input = new Scanner(System.in);
         int check = Integer.parseInt(input.nextLine());
         String[] checkString = new String[check];
@@ -54,11 +32,6 @@ public class HackerRankInAString
             ArrayList<String> tempCheckString = new ArrayList<>(Arrays.asList(checkString[i].split("")));
             for (int j = 0; j < tempCheckString.size(); j++)
             {
-                /*
-                 * 2
-                 * hereiamstackerrank
-                 * hackerrank
-                 */
                 if (!(tempCheckString.get(j).equalsIgnoreCase(hackerrank[j])))
                 {
                     tempCheckString.remove(j);
