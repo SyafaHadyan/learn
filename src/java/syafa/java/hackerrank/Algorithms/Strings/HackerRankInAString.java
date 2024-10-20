65bb0da3ae02964fdc60a4eca597369d501ab5b2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 10:08:36 +0700

    Str to compare

--- src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
@@ -5,10 +5,35 @@ public class HackerRankInAString
 {
     public static void main(String[] args)
     {
+        /*
+         * H
+         * A
+         * C
+         * K
+         * E
+         * R
+         * R
+         * A
+         * N
+         * K
+         */
         Scanner input = new Scanner(System.in);
         int check = Integer.parseInt(input.nextLine());
         String[] checkString = new String[check];
         input.close();
+        String[] hackerrank =
+        {
+            "H",
+            "A",
+            "C",
+            "K",
+            "E",
+            "R",
+            "R",
+            "A",
+            "N",
+            "K"
+        };
         for (int i = 0; i < check; i++)
         {
             checkString[i] = input.nextLine();
