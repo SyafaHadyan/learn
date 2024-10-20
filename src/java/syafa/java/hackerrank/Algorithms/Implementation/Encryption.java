fd0a1eee40f461134f18450aa7c9d025679596f0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 13:02:29 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/Encryption.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/Encryption.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class Encryption {
-
+public class Encryption
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
