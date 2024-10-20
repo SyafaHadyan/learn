740e4373a21673c8e759e4d02c355bd6294579d9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 11:17:07 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Strings/MakingAnagrams.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/MakingAnagrams.java
@@ -5,6 +5,59 @@ public class MakingAnagrams
 {
     public static void main(String[] args)
     {
+        /*
+         * cde
+         * abc
+         * 
+         * 4
+         * 
+         * cde > delete d, e > c
+         * abc > delete a, b > c
+         * deletions = 4
+         * 
+         * abc
+         * amnop
+         * 
+         * 6
+         * 
+         * abc > delete b, c > a
+         * amnop > delete m, n, o, p > a
+         * deletions = 6
+         * 
+         * # Step
+         * 
+         * ## Sort array char
+         * 
+         * determine shortest char to prevent a full deletion
+         * 
+         * abc
+         * amnop
+         * 
+         * if i firstChar != i secondChar > delete firstChar[i] && secondChar[i]
+         * 
+         * ac
+         * anop
+         * 
+         * delete firstChar[i] && secondChar[i]
+         * a
+         * anop
+         * 
+         * delete secondChar[i]
+         * a
+         * aop
+         * 
+         * delete secondChar[i]
+         * a
+         * ap
+         * 
+         * delete secondChar[i]
+         * a
+         * a
+         * 
+         * total deletions = 6
+         * 
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
