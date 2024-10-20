ade35dd9f79cfd5965b567ef3f10a222ee881420
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 18:46:34 +0700

    Fix if budget is lower than cost

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/HalloweenSale.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/HalloweenSale.java
@@ -6,34 +6,15 @@ public class HalloweenSale
 {
     public static void main(String[] args)
     {
-        /*
-         * p = firstGamePrice
-         * d = discountPrevious (discount from previous game price)
-         * m = minimumGameCost (minimum game cost)
-         * s = budget (starting budget)
-         * 
-         * p  d m s
-         * 20 3 6 70
-         * 
-         * 5
-         * 
-         * 20 17 14 11 8 6 6 6 6 6
-         * 
-         * gameCount = 0;
-         * while (budget >= 0)
-         * {
-         *      budget -= firstGamePrice;
-         *      gameCount++;
-         *      if (firstGamePrice >= minimumGameCost)
-         *      {
-         *          firstGamePrice -= discountPrevious;
-         *      }
-         * }
-         */
         Scanner input = new Scanner(System.in);
         int[] initialSetup = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int gameCount = 0;
         input.close();
+        if (initialSetup[3] < initialSetup[0])
+        {
+            System.out.print(0);
+            return;
+        }
         while (initialSetup[3] >= 0)
         {
             initialSetup[3] -= initialSetup[0];
