0a23d6896cbdaf516e24d21f79a5177db2e30ef0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 14:17:53 +0700

    Add str number to word

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
@@ -13,5 +13,12 @@ public class TheTimeInWords
         int hour = Integer.parseInt(input.nextLine());
         int minute = Integer.parseInt(input.nextLine());
         input.close();
+        String[] baseNumber = { "", "one", "two", "three",
+        "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] tensNumber = { "", "ten", "twenty",
+        "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+        "ninety"};
     }
 }
