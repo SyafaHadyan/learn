963e2e4f027e02e926aed964ee3795a5b235031d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 12:42:53 +0700

    Input to array

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
@@ -7,6 +7,8 @@ public class FindTheMedian
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] number = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
