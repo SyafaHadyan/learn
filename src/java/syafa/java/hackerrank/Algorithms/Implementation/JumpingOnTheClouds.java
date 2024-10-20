12b05e964b175e11c064a7cefc0148585070dfea
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:26:19 +0700

    Remove player pos

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
@@ -6,26 +6,11 @@ public class JumpingOnTheClouds
 {
     public static void main(String[] args)
     {
-        /*
-         * 7
-         * 0 0 1 0 0 1 0
-         * 
-         * 4
-         * 
-         * 6
-         * 0 0 0 0 1 0
-         * 
-         * 3
-         * 
-         * Avoid 1
-         * Try jump 2, if 1, jump 1 then 2
-         */
         Scanner input = new Scanner(System.in);
         StringBuilder useless = new StringBuilder(input.nextLine());
         useless.delete(0,useless.length());
         int[] cloud = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
-        int playerPosition = 0;
         int totalJump = 0;
         for (int i = 0; i < cloud.length; i++)
         {
@@ -33,17 +18,15 @@ public class JumpingOnTheClouds
             {
                 if (cloud[i + 2] != 1) 
                 {
-                    playerPosition += 2;
                     totalJump++;
                     i++;
                 }
                 else if (cloud[i + 2] == 1)
                 {
-                    playerPosition += 3;
                     totalJump += 2;
                     i += 2;
                 }
-                if (cloud.length == playerPosition)
+                if (cloud.length == i)
                 {
                     System.out.print(totalJump);
                     return;
@@ -51,9 +34,9 @@ public class JumpingOnTheClouds
             }
             catch (IndexOutOfBoundsException e)
             {
-                if (cloud.length - playerPosition != 0)
+                if (cloud.length - i != 0)
                 {
-                    totalJump += (cloud.length - 1) - playerPosition;
+                    totalJump += (cloud.length - 1) - i;
                 }
                 System.out.print(totalJump);
                 return;
