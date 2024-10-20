93749ffdb8998fdf05f4b5504ed5f2ebf1e0a66d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:57:21 +0700

    Add note loop

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
@@ -22,9 +22,51 @@ public class ChocolateFeast
          * wrapperCounter = 5
          * chocolateWrapper = 2
          * 
+         * # Loop
+         * 
+         * chocolateCounter = 5
+         * wrapperCounter = 5
+         * chocolateWrapper = 2
+         * 
+         * while (wraperCounter >= chocolateWrapper)
+         * {
+         *      wrapperCounter - chocolateWrapper;
+         *      chocolateCounter++;
+         *      wrapperCounter++;
+         * }
+         * 
+         * ## first iterate
+         * 
+         * chocolateCounter = 6
+         * wrapperCounter = 5 - 2 = 3 + 1 = 4
+         * 
+         * ## second iterate
+         * 
+         * chocolateCounter = 7
+         * wrapperCounter = 4 - 2 = 2 + 1 = 3
+         * 
+         * ## third iterate
+         * 
+         * chocolateCounter = 8
+         * wrapperCounter = 3 - 2 = 1 + 1 = 2
+         * 
+         * ## fourth iterate
+         * 
+         * chocolateCounter = 9
+         * wrapperCounter = 2 - 1 = 1 + 1 = 1
+         * 
+         * *End of iteration*
+         * 
+         * chocolateCounter = 7
+         * wrapperCounter = 1
+         * chocolateWrapper = 2
+         * 
+         * 
+         * # Other
+         * 
          * convert wrapperCounter > chocolateCounter
          * wrapperCounter = wrapperCounter % (wrapperCounter / chocolateWrapper) = 1 (wrapperCounter now 1)
-         * chocolateCounter += wrapperCounterBefore - wrapperCounter
+         * chocolateCounter += 
          * 
          *  3       t = 3 (test cases)
          * 10 2 5  n = 10, c = 2, m = 5 (first test case)
