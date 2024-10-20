e9cfc1c3b6d51702f6da76fab4769a0f0eced429
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 14:15:15 +0700

    Input time

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
@@ -5,7 +5,13 @@ public class TheTimeInWords
 {
     public static void main(String[] args)
     {
+        /*
+         * string array each contains english word
+         * 
+         */
         Scanner input = new Scanner(System.in);
+        int hour = Integer.parseInt(input.nextLine());
+        int minute = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
