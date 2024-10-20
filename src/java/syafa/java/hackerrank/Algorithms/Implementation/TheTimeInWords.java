5723bb7f4d64ae11ce5a4029935b7389aa658b89
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 15:02:59 +0700

    If 45

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheTimeInWords.java
@@ -40,6 +40,7 @@ public class TheTimeInWords
             System.out.print(tensNumber[minute % 100 / 10] + ' ' + "past" + ' ' + baseNumber[hour % 100]);
             return;
         }
+        // fix later
         if (minute > 30 && minute <= 59 && minute != 45)
         {
             String minuteWord = "";
@@ -55,6 +56,10 @@ public class TheTimeInWords
                 minuteWord = tensNumber[(60 - minute) % 10] + minuteWord;
             }
             System.out.print(minuteWord + ' ' + "minutes to" + ' ' + baseNumber[(hour + 1) % 100]);
+        }
+        if (minute == 45)
+        {
+            System.out.print("quarter to" + ' ' + baseNumber[hour % 100]);
         }
     }
 }
