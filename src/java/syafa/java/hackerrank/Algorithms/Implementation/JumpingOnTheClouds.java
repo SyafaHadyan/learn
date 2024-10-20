f1df2beb97de10e68bf8a6254d7b7bc33078a4b1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:05:13 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
@@ -1,9 +1,25 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
 
 public class JumpingOnTheClouds
 {
     public static void main(String[] args)
     {
-        
+        /*
+         * 7
+         * 0 0 1 0 0 1 0
+         * 
+         * 4
+         * 
+         * 6
+         * 0 0 0 0 1 0
+         * 
+         * 3
+         * 
+         * Avoid 1
+         * Try jump 2, if 1, jump 1 then 2
+         */
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
