0955a5cb1cf1a326c5cea65e966ddd1f673c3706
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 17:50:32 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Strings/TwoStrings.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/TwoStrings.java
@@ -5,6 +5,34 @@ public class TwoStrings
 {
     public static void main(String[] args)
     {
+        /*
+         * testCase
+         * hello
+         * world
+         * hi
+         * world
+         * 
+         * YES
+         * NO
+         * 
+         * 
+         * testCase 0
+         * 
+         * hello
+         * world
+         * 
+         * both contains o and l
+         * YES
+         * 
+         * testCase1
+         * 
+         * hi
+         * world
+         * 
+         * both string does not contain any mutual character
+         * NO
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
