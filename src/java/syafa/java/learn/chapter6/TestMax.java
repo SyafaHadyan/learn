a048ed444d3c7e88d1fd917f0983b6a3d7735836
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 19:38:43 +0700

    Create file

---
+++ src/java/syafa/java/learn/chapter6/TestMax.java
@@ -0,0 +1,11 @@
+package syafa.java.learn.chapter6;
+import java.util.*;
+
+public class TestMax
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
