078d2fcbd03dbf7b41be5562424f9bdc9cb122b3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 06:35:31 +0700

    Input sighting amount

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -6,6 +6,7 @@ public class MigratoryBirds
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int sightingAmount = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
