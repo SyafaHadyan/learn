9fac19ffb0a2b695cbebd1088878c3a0d60b8cd3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 06:34:55 +0700

    Remove

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -6,43 +6,6 @@ public class MigratoryBirds
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int arrayLength = Integer.parseInt(input.nextLine());
-        int[] sightings = new int[arrayLength];
-        for (int i = 0; i < arrayLength; i++)
-        {
-            sightings[i] = Integer.parseInt(input.next());
-        }
         input.close();
-        Arrays.sort(sightings);
-        int mode = 0;
-        int currentMinVal = 0;
-        int currentMaxMode = 1;
-        int maxMode = 0;
-        for (int i = 0; i < sightings.length; i++)
-        {
-            try
-            {
-                if (i == 0)
-                {
-                    mode = sightings[i];
-                    maxMode = sightings[i];
-                }
-                if (sightings[i] == sightings[i + 1])
-                {
-                    currentMaxMode++;
-                }
-                if ((currentMaxMode > mode) && (sightings[i] > currentMinVal))
-                {
-                    mode = currentMaxMode;
-                    currentMinVal = sightings[i];
-                    maxMode = sightings[i];
-                    currentMaxMode = 1;
-                }
-            }
-            catch (IndexOutOfBoundsException e)
-            {
-                System.out.print(maxMode);
-            }
-        }
     }
 }
