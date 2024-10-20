d66ad5e864812195a0d2993673edb4ddb2f8a51f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 12:42:07 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Sorting;
+import java.util.*;
+import java.util.stream.*;
 
 public class FindTheMedian
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
