2cf378f7651f32c32d1556aa65d035244f918e4c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 10:47:05 +0700

    Add to arrlist str

--- src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
@@ -6,6 +6,16 @@ public class HackerRankInAString
     public static void main(String[] args)
     {
         /*
+         * 2
+         * hereiamstackerrank
+         * hackerworld
+         * 
+         * YES
+         * NO
+         * 
+         * 1
+         * hereiamstackerrank
+         * 
          * H
          * A
          * C
@@ -20,34 +30,40 @@ public class HackerRankInAString
         Scanner input = new Scanner(System.in);
         int check = Integer.parseInt(input.nextLine());
         String[] checkString = new String[check];
-        input.close();
-        String[] hackerrank =
+        char[] hackerrank =
         {
-            "H",
-            "A",
-            "C",
-            "K",
-            "E",
-            "R",
-            "R",
-            "A",
-            "N",
-            "K"
+            'H',
+            'A',
+            'C',
+            'K',
+            'E',
+            'R',
+            'R',
+            'A',
+            'N',
+            'K'
         };
-        int counter = hackerrank.length;
         for (int i = 0; i < check; i++)
         {
             checkString[i] = input.nextLine();
+            checkString[i] = checkString[i].toUpperCase();
         }
         for (int i = 0; i < checkString.length; i++)
         {
+            int counter = 0;
+            ArrayList<String> tempCheckString = new ArrayList<>(Arrays.asList(checkString[i].split("")));
             for (int j = 0; j < checkString[i].length(); j++)
             {
-                for (int k = counter; k < hackerrank.length;)
+                for (int k = 0; k < hackerrank.length; k++)
                 {
-                    //
+                    if ()
                 }
             }
+        }
+        input.close();
+        for (int i = 0; i < checkString.length; i++)
+        {
+            System.out.println(checkString[i]);
         }
     }
 }
