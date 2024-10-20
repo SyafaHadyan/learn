c56d28d0efc47873477404c191527fb91dd57c36
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 13:07:13 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/Encryption.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/Encryption.java
@@ -5,6 +5,33 @@ public class Encryption
 {
     public static void main(String[] args)
     {
+        /*
+         * haveaniceday
+         * 
+         * sqrt input is 3.46...
+         * arr row = Math.floor sqrt input = 3
+         * arr col = Math.ceil sqrt input = 4
+         * 
+         * have
+         * anic
+         * eday
+         * 
+         * output = hae and via ecy
+         * 
+         * 
+         * feedthedog
+         * 
+         * sqrt input = 3.16...
+         * arr row = Math.floor sqrt input = 3
+         * arr col = Math.ceil sqrt input = 4
+         * 
+         * feed
+         * thed
+         * og
+         * 
+         * output = fto ehg ee dd
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
