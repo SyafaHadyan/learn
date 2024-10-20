df4b834a3fcf4a8fbd8866d9ce6637b01e14bad5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 09:13:51 +0700

    Check if prev is larger

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/InsertionSortPart2.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/InsertionSortPart2.java
@@ -10,12 +10,19 @@ public class InsertionSortPart2
          * n = 7
          * arr = 3 4 7 5 6 2 1
          * 
-         * 3 4 7 5 6 2 1
-         * 3 4 7 5 6 2 1
-         * 3 4 5 7 6 2 1
-         * 3 4 5 6 7 2 1
-         * 2 3 4 5 6 7 1
-         * 1 2 3 4 5 6 7
+         * [x]3 4 7 5 6 2 1
+         * [x]3 4 7 5 6 2 1
+         * [x]3 4 5 7 6 2 1
+         * [x]3 4 5 6 7 2 1
+         * [ ]2 3 4 5 6 7 1
+         * [ ]1 2 3 4 5 6 7
+         * 
+         * [x]3 4 7 5 6 2 1
+         * [x]3 4 7 5 6 2 1
+         * [x]3 4 5 7 6 2 1
+         * [x]3 4 5 6 7 2 1
+         * [ ]3 4 5 6 2 7 1
+         * [ ]3 4 5 6 2 1 7
          */
         Scanner input = new Scanner(System.in);
         int[] number = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -25,16 +32,23 @@ public class InsertionSortPart2
         {
             try
             {
-                if (number[i] < number[i + 1])
+                if (number[counter] < number[counter + 1])
                 {
-                    //
+                    counter++;
+                    int temp = number[number.length];
                 }
-                else if (number[i] > number[i + 1])
+                else if (number[counter] > number[counter + 1])
                 {
-                    int[] temp = {number[i], number[i + 1]};
-                    number[i] = temp[1];
-                    number[i + 1] = temp[0];
-                    temp[0] = number[number.length];
+                    int[] temp = {number[counter], number[counter + 1]};
+                    number[counter] = temp[1];
+                    number[counter + 1] = temp[0];
+                    int tempCounter = counter;
+                    while (number[tempCounter] < number[tempCounter - 1])
+                    {
+                        temp[0] = number[tempCounter];
+                        temp[1] = number[tempCounter - 1];
+                        //
+                    }
                 }
             }
             catch (IndexOutOfBoundsException e)
