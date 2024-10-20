dedc6e2923e5f72f2a65a3fc496d0f5dcf8b9a0f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 08:51:32 +0700

    Add comment

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/InsertionSortPart2.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/InsertionSortPart2.java
@@ -5,6 +5,17 @@ public class InsertionSortPart2
 {
     public static void main(String[] args)
     {
+        /*
+         * n = 7
+         * arr = 3 4 7 5 6 2 1
+         * 
+         * 3 4 7 5 6 2 1
+         * 3 4 7 5 6 2 1
+         * 3 4 5 7 6 2 1
+         * 3 4 5 6 7 2 1
+         * 2 3 4 5 6 7 1
+         * 1 2 3 4 5 6 7
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
