988c407ddc8120de8f83564cc5c6dd572116dacd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 09:46:11 +0700

    If not late or early

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
@@ -6,9 +6,19 @@ public class LibraryFine
 {
     public static void main(String[] args)
     {
+        /*
+         * if same month and year, fine = (15 * days late)
+         * if different month but same year, fine = (500 * months late)
+         * if different year, fine = 10000
+         */
         Scanner input = new Scanner(System.in);
         int[] bookReturn = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] dueDate = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        if (bookReturn[0] <= dueDate[0] && bookReturn[1] <= dueDate[1] && bookReturn[2] <= dueDate[2])
+        {
+            System.out.print(0);
+            return;
+        }
     }
 }
