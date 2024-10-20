ac2173c753337daf0c92bfa3bdca30fd243c9133
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:51:03 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
@@ -5,7 +5,42 @@ public class ChocolateFeast
 {
     public static void main(String[] args)
     {
+        /*
+         * n = money
+         * c = chocolatePrice
+         * m = chocolateWrapper
+         * 
+         * chocolateCounter
+         * wrapperCounter
+         * 
+         * ex.
+         * 
+         * 15 3 2
+         * 
+         * money = 15 and chocolatePrice = 3
+         * chocolateCounter = 5
+         * wrapperCounter = 5
+         * chocolateWrapper = 2
+         * 
+         * convert wrapperCounter > chocolateCounter
+         * wrapperCounter = wrapperCounter % (wrapperCounter / chocolateWrapper) = 1 (wrapperCounter now 1)
+         * chocolateCounter += wrapperCounterBefore - wrapperCounter
+         * 
+         *  3       t = 3 (test cases)
+         * 10 2 5  n = 10, c = 2, m = 5 (first test case)
+         * 12 4 4  n = 12, c = 4, m = 4 (second test case)
+         * 6 2 2   n = 6,  c = 2, m = 2 (third test case)
+         * 
+         * 6
+         * 3
+         * 5
+         */
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < testCase; i++)
+        {
+            //
+        }
         input.close();
     }
 }
