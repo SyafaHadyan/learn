5987aa1f7dcafd2380beb0bd8f6469cd7c55b56f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 19:40:00 +0700

    Input

--- src/java/syafa/java/learn/chapter6/TestMax.java
+++ src/java/syafa/java/learn/chapter6/TestMax.java
@@ -6,6 +6,10 @@ public class TestMax
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.printf("%-20s%-2c","Enter first number",':');
+        int firstNumber = Integer.parseInt(input.nextLine());
+        System.out.printf("%-20s%-2c","Enter second number",':');
+        int secondNumber = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
