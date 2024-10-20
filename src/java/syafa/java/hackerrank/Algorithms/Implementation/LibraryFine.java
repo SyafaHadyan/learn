1a673216bb36fbb176162b0955258088546900e2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 09:59:09 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
@@ -6,11 +6,6 @@ public class LibraryFine
 {
     public static void main(String[] args)
     {
-        /*
-         * [x]if same month and year, fine = (15 * days late)
-         * [x]if different month but same year, fine = (500 * months late)
-         * [x]if different year, fine = 10000
-         */
         Scanner input = new Scanner(System.in);
         int[] bookReturn = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] dueDate = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
