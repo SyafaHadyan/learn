2f98effd678cc375972eea4421e79f591e7864f5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:35:15 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ChocolateFeast.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class ChocolateFeast {
-
+public class ChocolateFeast
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
