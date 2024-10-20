c091f7ba6f586eba42503263e8eceb654b08da4f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 10:03:19 +0700

    Var

--- src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
@@ -6,6 +6,8 @@ public class HackerRankInAString
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int check = Integer.parseInt(input.nextLine());
+        String checkString = input.nextLine();
         input.close();
     }
 }
