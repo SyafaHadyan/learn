e74bb352a9e725fb177d5e05d922e604634f087b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 12:50:12 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/FindTheMedian.java
@@ -7,15 +7,16 @@ public class FindTheMedian
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int arrayLength = Integer.parseInt(input.nextLine());
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0,useless.length());
         int[] number = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
         Arrays.sort(number);
         if (number.length % 2 == 0)
         {
-            System.out.printf("%.0f",(number[(number.length / 2) - 1]) + (number[number.length / 2] + 1) / 2.0);
+            System.out.printf("%d",(number[(number.length / 2) - 1]) + (number[number.length / 2] + 1) / 2.0);
             return;
         }
-        System.out.printf("%.0f",(number[number.length / 2]));
+        System.out.printf("%d",(number[number.length / 2]));
     }
 }
