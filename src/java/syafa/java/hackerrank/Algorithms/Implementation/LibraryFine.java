8185ce87e1d6a18101f3b8dd8424339d68357e6f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 09:39:33 +0700

    Input to arr int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/LibraryFine.java
@@ -1,11 +1,14 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class LibraryFine
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] bookReturn = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dueDate = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
