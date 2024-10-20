b17d0f808907c9c4ebdade5a9749d10af8d981ed
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 11:11:01 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Strings/MakingAnagrams.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/MakingAnagrams.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Strings;
 
-public class MakingAnagrams {
-
+public class MakingAnagrams
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
