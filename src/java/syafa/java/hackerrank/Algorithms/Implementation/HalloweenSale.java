e06fdd997a625332d49030b345193b1e1686b61a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 18:24:05 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/HalloweenSale.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/HalloweenSale.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class HalloweenSale {
-
+public class HalloweenSale
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
