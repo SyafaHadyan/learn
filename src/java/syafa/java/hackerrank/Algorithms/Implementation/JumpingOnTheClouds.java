b84a348942c2b7882848e90ebcf9f67e717a6d47
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 16:00:41 +0700

    Revert "Initialize"
    This reverts commit 986e0c19f15b47cd25e267a9a693f627c5ef6508.

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheClouds.java
@@ -1,9 +1,5 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class JumpingOnTheClouds
-{
-    public static void main(String[] args)
-    {
-        
-    }
+public class JumpingOnTheClouds {
+
 }
