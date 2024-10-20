7d4ed84bbf63fd6b4f1c575e8500bae1593c2f08
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 06:43:23 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MigratoryBirds.java
@@ -6,8 +6,21 @@ public class MigratoryBirds
 {
     public static void main(String[] args)
     {
+        /*
+         * 6
+         * 1 4 4 4 5 3
+         * 
+         * 4
+         * 
+         * 11
+         * 1 2 3 4 5 4 3 2 1 3 4
+         * 
+         * 3
+         * 
+         * sort reverse array of sightings (start from highest type) then when lower type has higher sightings, update mode to current type (which is lower)
+         */
         Scanner input = new Scanner(System.in);
-        int sightingAmount = Integer.parseInt(input.nextLine());
+        //int sightingAmount = Integer.parseInt(input.nextLine());
         int[] sighting = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
         Collections.reverse(Arrays.asList(sighting));
