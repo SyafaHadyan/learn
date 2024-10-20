36af393a82a8e47db284629a7bcaf126f764df3e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-20 13:53:14 +0700

    Input big int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ExtraLongFactorials.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ExtraLongFactorials.java
@@ -1,11 +1,13 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.math.*;
 
 public class ExtraLongFactorials
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        BigInteger number = input.nextBigInteger();
         input.close();
     }
 }
