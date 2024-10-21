78a8d2c8b5f1fa8b55e3a5745391940f18bcd935
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 08:29:44 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class StrangeCounter {
-
+public class StrangeCounter
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
