68c2be6263857515ca46227aa326351770223dbe
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 21:53:31 +0700

    Initialize scanner

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Greedy;
+import java.util.*;
+import java.util.stream.*;
 
 public class MarkAndToys
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
