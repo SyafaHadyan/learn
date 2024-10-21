feafc34f504434a6e8380ceff3e6c54a1405c89b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 11:17:04 +0700

    temp

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -11,21 +11,23 @@ public class StrangeCounter
         input.close();
         BigInteger initialValue = BigInteger.valueOf(3);
         BigInteger currentValue = BigInteger.valueOf(3);
+        BigInteger time = BigInteger.ONE;
         if (initialValue.intValue() == 1)
         {
             System.out.print(3);
             return;
         }
-        for (BigInteger i = BigInteger.ONE; (i.compareTo(requestTimeValue) < 0); i.add(BigInteger.ONE))
+        /* TODO: Fix this */
+        while (time.compareTo(requestTimeValue) <= 0)
         {
             currentValue.subtract(BigInteger.valueOf(1));
             if (currentValue == BigInteger.ONE)
             {
                 initialValue = initialValue.multiply(BigInteger.valueOf(2));
                 currentValue = initialValue;
-                i.add(BigInteger.ONE);
+                time.add(BigInteger.ONE);
             }
-            if (requestTimeValue.subtract(i) == BigInteger.ONE)
+            if (requestTimeValue.subtract(time) == BigInteger.ONE)
             {
                 System.out.print(currentValue);
                 return;
