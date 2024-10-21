5aed42622e3f9264cacd7842fd12bd8a9f04999b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 21:59:52 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
@@ -6,17 +6,6 @@ public class MarkAndToys
 {
     public static void main(String[] args)
     {
-        /*
-         * 7 50
-         * 1 12 5 111 200 1000 10
-         * 
-         * 4
-         * 
-         * 1 5 10 12
-         * 
-         * Sort price
-         * iterate > get lowest val with budget
-         */
         Scanner input = new Scanner(System.in);
         int[] budget = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] price = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
