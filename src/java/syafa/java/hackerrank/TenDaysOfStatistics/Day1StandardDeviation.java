fde62b2953f65c6b3c94b54e265953c637861910
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 06:25:51 +0700

    Add note

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1StandardDeviation.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1StandardDeviation.java
@@ -6,6 +6,23 @@ public class Day1StandardDeviation
 {
     public static void main(String[] args)
     {
+        /*
+         * 2 5 2 7 4
+         * sum = 20
+         * mean = 4
+         * 
+         * substract mean from each element > square the result > take sum > average 1 digit precision
+         * 
+         * 5
+         * 10 40 30 50 20
+         * 
+         * 14.1
+         * 
+         * STDIN           Function
+         * -----           --------
+         * 5               arr[] size n = 5
+         * 10 40 30 50 20  arr =[10, 40, 30, 50, 20]
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
