1a6041b698db2605c2d144d4fed4884c9ce53fe2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 20:46:24 +0700

    Input player

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
@@ -7,6 +7,8 @@ public class ClimbingTheLeaderboard
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] ranked = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] player = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
