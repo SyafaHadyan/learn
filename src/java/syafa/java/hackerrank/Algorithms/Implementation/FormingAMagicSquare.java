8f47b0227dbbde1f33bd3b237754578099e87acf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 20:10:16 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/FormingAMagicSquare.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/FormingAMagicSquare.java
@@ -5,6 +5,29 @@ public class FormingAMagicSquare
 {
     public static void main(String[] args)
     {
+        /*
+         * 5 3 4 1 5 8 6 4 2
+         * x x x x x x x x x
+         * 
+         * sort
+         * 1 2 3 4 4 5 5 6 8
+         * 
+         * iterate through element then add by val (no possible minimum increment)
+         * 
+         * 
+         * element  validElement    passValue?
+         * 1        1               pass
+         * 2        2               pass
+         * 3        3               pass
+         * 4        4               pass
+         * 4        5               not pass > counter += Math.abs(validElement - element) (value = 1)  total = 1
+         * 4        6               not pass > counter += Math.abs(validElement - element) (value = 2)  total = 3
+         * 5        7               not pass > counter += Math.abs(validElement - element) (value = 2)  total = 5
+         * 6        8               not pass > counter += Math.abs(validElement - element) (value = 2)  total = 7
+         * 8        9               not pass > counter += Math.abs(validElement - element) (value = 1)  total = 8
+         * 
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
