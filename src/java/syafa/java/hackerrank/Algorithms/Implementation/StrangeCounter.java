70a4255e8173fceb26ecb5f1dd1c988c4ddc6719
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 08:37:16 +0700

    Fix counter

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -10,7 +10,7 @@ public class StrangeCounter
         input.close();
         int initialValue = 3;
         int currentValue = initialValue;
-        for (int i = 1; i <= requestTimeValue; i++)
+        for (int i = 2; i < requestTimeValue; i++)
         {
             currentValue--;
             if (currentValue == 1)
@@ -19,5 +19,6 @@ public class StrangeCounter
                 currentValue = initialValue;
             }
         }
+        System.out.print(currentValue);
     }
 }
