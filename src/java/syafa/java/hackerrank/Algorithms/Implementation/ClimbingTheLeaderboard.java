0687c23a472b9f0554006878f25d7da0d0fa826c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 20:47:05 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
@@ -6,6 +6,28 @@ public class ClimbingTheLeaderboard
 {
     public static void main(String[] args)
     {
+        /*
+         * 7
+         * 100 100 50 40 40 20 10
+         * 4
+         * 5 25 50 120
+         * 
+         * 6
+         * 4
+         * 2
+         * 1
+         * 
+         * 6
+         * 100 90 90 80 75 60
+         * 5
+         * 50 65 77 90 102
+         * 
+         * 6
+         * 5
+         * 4
+         * 2
+         * 1
+         */
         Scanner input = new Scanner(System.in);
         int[] ranked = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] player = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
