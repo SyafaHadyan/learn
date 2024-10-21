a0b3ce8b29c84571a7f17c4430e6f95c2093166f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 21:54:53 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/MarkAndToys.java
@@ -6,6 +6,17 @@ public class MarkAndToys
 {
     public static void main(String[] args)
     {
+        /*
+         * 7 50
+         * 1 12 5 111 200 1000 10
+         * 
+         * 4
+         * 
+         * 1 5 10 12
+         * 
+         * Sort price
+         * iterate > get lowest val with budget
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
