d2a5a0ea0f6d7423a174f63d71c9f5a70e6ad0af
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 07:05:10 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CavityMap.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CavityMap.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 
-public class CavityMap {
-
+public class CavityMap
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
