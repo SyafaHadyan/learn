2f6377903eb38711b68711286a583a696246ed1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-21 08:31:35 +0700

    Initialize counter int

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -6,6 +6,13 @@ public class StrangeCounter
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int requestTimeValue = Integer.parseInt(input.nextLine());
         input.close();
+        int currentTime = 1;
+        int currentValue = 3;
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
     }
 }
