b62caef98f587a7ef73a9edf6c378e4d3ad8b7dd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 18:22:53 +0700

    Add to str arr

--- src/java/syafa/java/hackerrank/latsolpemdastif24/TicTacToe.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/TicTacToe.java
@@ -7,6 +7,11 @@ public class TicTacToe
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[][] board = new String[3][];
+        for (int i = 0; i < board.length; i++)
+        {
+            board[i] = input.nextLine().split(" ");
+        }
         input.close();
     }
 }
