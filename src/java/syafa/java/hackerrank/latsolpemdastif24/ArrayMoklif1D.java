6d0ad759fa3095a3f668faca36ad90c4a5fd324e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 16:50:47 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ArrayMoklif1D.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ArrayMoklif1D.java
@@ -1,9 +1,12 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
+import java.util.stream.*;
 
 public class ArrayMoklif1D
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(Systme.in);
+        input.close();
     }
 }
