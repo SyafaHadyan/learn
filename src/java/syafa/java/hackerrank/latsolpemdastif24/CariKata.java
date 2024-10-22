b0450ac7d9021bb54c8d6698d678c079ce744e57
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:24:47 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/latsolpemdastif24/CariKata.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/CariKata.java
@@ -5,18 +5,6 @@ public class CariKata
 {
     public static void main(String[] args)
     {
-        /*
-         * aku adalah dia dan dia adalah aku
-         * aku
-         * 
-         * aku ditemukan pada posisi ke-1.
-         * aku ditemukan pada posisi ke-7.
-         * 
-         * lorem ipsum dolor sit amet
-         * cinta
-         * 
-         * cinta tidak ditemukan.
-         */
         Scanner input = new Scanner(System.in);
         String[] dictionary = input.nextLine().split(" ");
         String search = input.nextLine();
