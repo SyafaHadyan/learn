d6d61e58629abd45864ee2a9f0bfd5e26bfa2f1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 18:23:37 +0700

    Add note

--- src/java/syafa/java/hackerrank/latsolpemdastif24/TicTacToe.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/TicTacToe.java
@@ -6,6 +6,23 @@ public class TicTacToe
 {
     public static void main(String[] args)
     {
+        /*
+         * X O O
+         * X O X
+         * X X O
+         * 
+         * X memenangkan permainan.
+         * 
+         * X O O
+         * X X O
+         * O X O
+         * 
+         * O memenangkan permainan.
+         * 
+         * else
+         * 
+         * Tidak ada pemenang.
+         */
         Scanner input = new Scanner(System.in);
         String[][] board = new String[3][];
         for (int i = 0; i < board.length; i++)
