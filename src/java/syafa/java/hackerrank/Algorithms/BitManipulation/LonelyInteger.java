c21578211ae1954bdb59c9f3448029e12324ce0e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 08:55:25 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/BitManipulation/LonelyInteger.java
+++ src/java/syafa/java/hackerrank/Algorithms/BitManipulation/LonelyInteger.java
@@ -5,6 +5,46 @@ public class LonelyInteger
 {
     public static void main(String[] args)
     {
+        /*
+         * 3
+         * 1 1 2
+         * 
+         * 2
+         * 
+         * 2 is the only unique integer (does not repeat inside)
+         * 
+         * 5
+         * 0 0 1 2 1
+         * 
+         * 2
+         * 
+         * 2 is the only unique integer
+         * 
+         * 1
+         * 1
+         * 
+         * 1
+         * 
+         * 1 is the only element inside
+         * 
+         * int[] input arr > sort >
+         * if (i == i + 1)
+         * {
+         *      currentNumber = i;
+         *      continue;
+         * }
+         * else if (i != i + 1)
+         * {
+         *      if (i != currentNumber)
+         *      {
+         *          if (i != 1 + 1)
+         *          {
+         *              print i;
+         *          }
+         *      }
+         * }
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
