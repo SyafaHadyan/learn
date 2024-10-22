817b2cc7732b14777bf63d8b2f36f28d0f5bdd9e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 13:25:37 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
@@ -33,7 +33,8 @@ public class Day1Quartiles
          * 3 5 7 8  12 13 14 18
          */
         Scanner input = new Scanner(System.in);
-        int arraySize = Integer.parseInt(input.nextLine());
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0,useless.length());
         int[] data = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
         Arrays.sort(data);
