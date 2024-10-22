54d7636d0b76fac359c83d40fd7ff4b3425283a8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 11:13:42 +0700

    Test length

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
@@ -31,6 +31,9 @@ public class Day1Quartiles
             double q2 = (data[((data.length / 2) - 1)] + data[(data.length / 2)]) / 2.0;
             double q3 = upperHalf[(upperHalf.length / 2)];
             System.out.println(q1 + "\n" + q2 + "\n" + q3);
+            return;
         }
+        int[] lowerHalf = Arrays.copyOfRange(data,0,(data.length / 2));
+        System.out.println(lowerHalf.length);
     }
 }
