0f837da68efa31732577084b493a93f85df1ffb1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 08:50:59 +0700

    Initialzie scanner

--- src/java/syafa/java/hackerrank/Algorithms/BitManipulation/LonelyInteger.java
+++ src/java/syafa/java/hackerrank/Algorithms/BitManipulation/LonelyInteger.java
@@ -1,9 +1,11 @@
 package syafa.java.hackerrank.Algorithms.BitManipulation;
+import java.util.*;
 
 public class LonelyInteger
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
