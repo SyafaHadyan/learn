ce68bb37364c95f07044ea81951eb67f6410e596
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 18:02:01 +0700

    Chg to int arr

--- src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
@@ -9,11 +9,15 @@ public class JumlahBarisDanKolom
         Scanner input = new Scanner(System.in);
         int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] array = new int[arraySize[3]][];
-        int rowSum = 0;
-        int colSum = 0;
+        int[] rowSum = new int[3];
+        int[] colSum = new int[3];
         for (int i = 0; i < arraySize.length; i++)
         {
-            
+            array[i] = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < array[i].length; j++)
+            {
+                
+            }
         }
         input.close();
     }
