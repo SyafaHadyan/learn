8bbfd37971a3ca4f9e5cb481ef06594d7db99ba4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 08:18:45 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/JimAndTheOrders.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/JimAndTheOrders.java
@@ -5,6 +5,34 @@ public class JimAndTheOrders
 {
     public static void main(String[] args)
     {
+        /*
+         * Customer     1	2	3	4	5
+         * Order #		8	5	6	2	4
+         * Prep time	3	6	2	3	3
+         * Calculate:
+         * Serve time	11	11	8	5	7
+         * 
+         * return customerServeOrder
+         * 
+         * 4 5 3 1 2
+         * 
+         * 3
+         * 1 3
+         * 2 3
+         * 3 3
+         * 
+         * 1 2 3
+         * 
+         * 5
+         * 8 1
+         * 4 2
+         * 5 6
+         * 3 1
+         * 4 3
+         * 
+         * 4 2 5 1 3
+         * 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
