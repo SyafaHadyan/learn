6d0515d161919b192d7eb60857b92a2221fc9f5c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:01:17 +0700

    Initialzie

--- src/java/syafa/java/hackerrank/latsolpemdastif24/RataRataNilaiPemdas.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/RataRataNilaiPemdas.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
+import java.util.stream.*;
 
-public class RataRataNilaiPemdas {
-
+public class RataRataNilaiPemdas
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
