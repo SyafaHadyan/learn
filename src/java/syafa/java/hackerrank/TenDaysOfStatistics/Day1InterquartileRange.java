552789f44bd73f76586ca5f4f35d9da34e3cef3b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 14:10:28 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1InterquartileRange.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1InterquartileRange.java
@@ -6,34 +6,6 @@ public class Day1InterquartileRange
 {
     public static void main(String[] args)
     {
-        /*
-         * STDIN           Function
-         * -----           --------
-         * 6               arrays size n = 6
-         * 6 12 8 10 20 16 values = [6, 12, 8, 10, 20, 16]
-         * 5 4 3 2 1 5     freqs = [5, 4, 3, 2, 1, 5]
-         * 
-         * 9.0
-         * 
-         * 6
-         * 6 12 8 10 20 16
-         * 5 4 3 2 1 5
-         * 
-         * sort
-         * 
-         * create 6 5 times
-         * create 8 3 times
-         * 
-         * Lower half (L): 6, 6, 6, 6, 6, 8, 8, 8, 10, 10
-         * Upper half (U): 12, 12, 12, 12, 16, 16, 16, 16, 16, 20
-         * 
-         * Q1 = 7.0
-         * Q2 = 16.0
-         * 
-         * interquartileRange = 16.0 - 7.0 = 9.0
-         * 
-         * 
-         */
         Scanner input = new Scanner(System.in);
         int dataSize = Integer.parseInt(input.nextLine());
         int[][] dataFrequency = new int[2][];
