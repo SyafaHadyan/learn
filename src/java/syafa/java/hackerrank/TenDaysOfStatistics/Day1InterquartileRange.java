5996fd3f0ea16bd418073d2701c4c6564f49f75a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 13:35:25 +0700

    Input data arr

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1InterquartileRange.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1InterquartileRange.java
@@ -6,6 +6,10 @@ public class Day1InterquartileRange
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int dataSize = Integer.parseInt(input.nextLine());
+        int[] data = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] frequency = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        input.close();
     }
 }
