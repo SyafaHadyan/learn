9eac83cf8aa9cc05f69b551b9191d3672fd781cc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:27:45 +0700

    Initialzie

---
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ArgmaxMatriks.java
@@ -0,0 +1,12 @@
+package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
+import java.util.stream.*;
+
+public class ArgmaxMatriks
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
