b004e9f68cc04e4afa7438af27134165c4f721f4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 16:58:21 +0700

    Input val

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ArrayKeSekian.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ArrayKeSekian.java
@@ -7,6 +7,8 @@ public class ArrayKeSekian
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] data = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] element = input.nextLine().split(" ");
         input.close();
     }
 }
