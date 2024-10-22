49c22329aa75d70e490cc2697a1c2db06354c3e6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:38:06 +0700

    Add if higher

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ArgmaxMatriks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ArgmaxMatriks.java
@@ -23,9 +23,17 @@ public class ArgmaxMatriks
         Scanner input = new Scanner(System.in);
         int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] array = new int[arraySize[0]][];
+        int[] maxValue = new int[3];
         for (int i = 0; i < args.length; i++)
         {
             array[i] = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < array[i].length; j++)
+            {
+                if (array[i][j] > maxValue[2])
+                {
+                    maxValue[2] = array[i][j];
+                }
+            }
         }
         input.close();
     }
