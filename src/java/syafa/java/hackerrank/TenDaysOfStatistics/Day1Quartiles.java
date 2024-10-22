be93415cb896ed7158d2b205c7efceee7c5997d1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 10:49:56 +0700

    Initialize

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
+import java.util.stream.*;
 
-public class Day1Quartiles {
-
+public class Day1Quartiles
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
