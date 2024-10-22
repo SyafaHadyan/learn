0bd876df170552cc2422296ef2b46c7d2c1da614
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:15:09 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/latsolpemdastif24/MengurutkanArray.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/MengurutkanArray.java
@@ -6,14 +6,9 @@ public class MengurutkanArray
 {
     public static void main(String[] args)
     {
-        /*
-         * 5
-         * 1 5 6 2 3
-         * 
-         * 1 2 3 5 6
-         */
         Scanner input = new Scanner(System.in);
-        int arraySize = Integer.parseInt(input.nextLine());
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0,useless.length());
         int[] data = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
         Arrays.sort(data);
