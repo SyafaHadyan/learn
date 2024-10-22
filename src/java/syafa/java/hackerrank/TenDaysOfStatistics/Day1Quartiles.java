28cea242e0263fb592a3c8d4ae6c57a87f5334c0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 10:50:50 +0700

    Input int arr

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1Quartiles.java
@@ -6,6 +6,9 @@ public class Day1Quartiles
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
+        int[] data = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        input.close();
     }
 }
