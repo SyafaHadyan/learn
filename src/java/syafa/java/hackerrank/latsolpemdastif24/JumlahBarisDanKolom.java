ac1dea84266929c1980aa36decd0e35d372951ab
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 18:17:37 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
@@ -6,36 +6,6 @@ public class JumlahBarisDanKolom
 {
     public static void main(String[] args)
     {
-        /*
-         * 3 3
-         * 10 15 21
-         * 11 12 17
-         * 31 22 15
-         * 
-         * Hasil penjumlahan pada baris adalah 46 40 68
-         * Hasil penjumlahan pada kolom adalah 52 49 53
-         * 
-         * 10 15 21 ---> 46
-         * 11 12 17 ---> 40
-         * 31 22 15 ---> 68
-         * | | |
-         * v v v
-         * 52 49 53
-         * 
-         * 2 3
-         * 5 1 2
-         * 3 4 5
-         * 
-         * Hasil penjumlahan pada baris adalah 8 12
-         * Hasil penjumlahan pada kolom adalah 8 5 7
-         * 
-         * 5 1 2 ---> 8
-         * 3 4 5 ---> 12
-         * | | |
-         * v v v
-         * 8 5 7 
-         * 
-         */
         Scanner input = new Scanner(System.in);
         int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] array = new int[arraySize[0]][];
