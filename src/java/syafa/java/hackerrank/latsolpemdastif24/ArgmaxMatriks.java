8bd4d8183c94c2ad43473c25d696c753eaf25930
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:42:56 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ArgmaxMatriks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ArgmaxMatriks.java
@@ -6,20 +6,6 @@ public class ArgmaxMatriks
 {
     public static void main(String[] args)
     {
-        /*
-         * 2 2
-         * 1 2
-         * 3 4
-         * 
-         * [1, 1]
-         * 
-         * Nilai maksimal terdapat pada baris ke-1 dan kolom ke-1 (indeks dimulai dari 0).
-         * 
-         * 1 2
-         * -1 1
-         * 
-         * [0, 1]
-         */
         Scanner input = new Scanner(System.in);
         int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] array = new int[arraySize[0]][];
