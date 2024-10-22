c2a073a3b9e1c4d95b99653417b305b3cccb8f94
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 08:15:30 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/JimAndTheOrders.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/JimAndTheOrders.java
@@ -1,5 +1,9 @@
 package syafa.java.hackerrank.Algorithms.Greedy;
 
-public class JimAndTheOrders {
-
+public class JimAndTheOrders
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
