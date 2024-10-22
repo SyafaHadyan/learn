3a412899bb2d8b84aa9972f685d4f54dc213c194
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:12:26 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/MengurutkanArray.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/MengurutkanArray.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
+import java.util.stream.*;
 
-public class MengurutkanArray {
-
+public class MengurutkanArray
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
