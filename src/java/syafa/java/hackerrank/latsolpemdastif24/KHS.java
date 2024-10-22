8f70721c19c17adfcac4a906c60f20d2a27c5c9c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:52:34 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/latsolpemdastif24/KHS.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/KHS.java
@@ -3,20 +3,6 @@ import java.util.*;
 
 public class KHS
 {
-    /*
-     * 5
-     * pemdas aok kalkulus pemlan asd
-     * A B+ B+ A B
-     * aok
-     * 
-     * Nilai dari mata kuliah aok adalah B+.
-     * 
-     * -10
-     * input tidak valid
-     * 
-     * if (x < 0)
-     * sout input tidak valid.
-     */
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
