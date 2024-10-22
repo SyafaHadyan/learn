c807e6357c8bd3869f6067d80f285dcc89ae578b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:59:49 +0700

    Input val

--- src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/JumlahBarisDanKolom.java
@@ -7,6 +7,11 @@ public class JumlahBarisDanKolom
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < arraySize.length; i++)
+        {
+            
+        }
         input.close();
     }
 }
