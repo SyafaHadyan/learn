88cb9608d01d42d56fef1604e2b874cf525a9ae7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 17:03:20 +0700

    Add note

--- src/java/syafa/java/hackerrank/latsolpemdastif24/RataRataNilaiPemdas.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/RataRataNilaiPemdas.java
@@ -6,9 +6,22 @@ public class RataRataNilaiPemdas
 {
     public static void main(String[] args)
     {
+        /*
+         * 5
+         * 90 87 92 100 88
+         * 
+         * 92 100
+         * 
+         * Rata-rata dari ke-5 bilangan tersebut adalah 91,4. Bilangan yang lebih besar dari 91,4 adalah 92 dan 100.
+         */
         Scanner input = new Scanner(System.in);
         int arraySize = Integer.parseInt(input.nextLine());
         int[] score = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        Arrays.sort(score);
+        for (int i = 0; i < score.length; i++)
+        {
+            
+        }
     }
 }
