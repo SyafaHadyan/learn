fd5aa8e6436dd537bcd0f1edda3f30071bf4ad24
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-22 16:40:40 +0700

    Initialize

--- src/java/syafa/java/Praktikum/Bab4/Praktikum0.java
+++ src/java/syafa/java/Praktikum/Bab4/Praktikum0.java
@@ -1,5 +1,11 @@
 package syafa.java.Praktikum.Bab4;
+import java.util.*;
+import java.util.stream.*;
 
-public class Praktikum0 {
-
+public class Praktikum0
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
