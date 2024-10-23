b76f11cdf29d9ffee6c424a3a15c423823246d83
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 11:27:08 +0700

    if next num is 1 higher

--- src/java/syafa/java/hackerrank/Algorithms/Strings/SeparateTheNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/SeparateTheNumbers.java
@@ -1,11 +1,30 @@
 package syafa.java.hackerrank.Algorithms.Strings;
 import java.util.*;
+import java.util.stream.*;
 
 public class SeparateTheNumbers
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String numberStr = input.nextLine();
+        String[] numberStrArr = input.nextLine().split("");
+        ArrayList<Integer> number = new ArrayList<>();
+        for (int i = 0; i < numberStr.length(); i++)
+        {
+            try
+            {
+                if (Integer.parseInt(String.valueOf(numberStr.charAt(i + 1))) - Integer.parseInt(String.valueOf(numberStr.charAt(i))) == 1)
+                {
+                    //
+                }
+            }
+            catch (IndexOutOfBoundsException e)
+            {
+                //
+            }
+        }
         input.close();
+        System.out.println();
     }
 }
