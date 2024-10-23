31c2cf5378b8cde221bd9337a8446d7541e3bfd2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:56:30 +0700

    Temp

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
@@ -17,13 +17,20 @@ public class KucingDalamKandang
          * 0 1 5
          * 0 2 4
          * 
-         * sort
+         * 
          * iterate 2
+         * 0 2 4
+         * check lowestAmount > highestAmount - (balancedValue - lowestAmount) = 2
+         * move [3] to [2]
+         * 0 3 3
+         * 
+         * 
+         * iterate 3
+         * move [1] to [0]
          * 1 2 3
          * 
-         * sort
-         * iterate 3
-         * 2 2 2
+         * iterate 4
+         * move
          */
         Scanner input = new Scanner(System.in);
         int cageAmount = Integer.parseInt(input.nextLine());
@@ -34,8 +41,10 @@ public class KucingDalamKandang
             Arrays.sort(cage);
             for (int i = 0; i < cage.length; i++)
             {
+                /*
                 cage[cage[cage.length - 1] - 1];
                 cage[cage[cage.length - 2]]
+                */
             }
         }
     }
