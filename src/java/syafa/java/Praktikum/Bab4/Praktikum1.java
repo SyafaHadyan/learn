2b53b996ba625a322bb77a633aceecb414ab9eb7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 22:00:10 +0700

    Initial data value

--- src/java/syafa/java/Praktikum/Bab4/Praktikum1.java
+++ src/java/syafa/java/Praktikum/Bab4/Praktikum1.java
@@ -6,6 +6,14 @@ public class Praktikum1
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[][] data =
+        {
+            {"Ekonomi", "Bisnis", "Eksekutif", "Pariwisata"},
+            {"50000", "100000", "200000", "300000"},
+            {"2", "5", "7", "10"},
+            {"Tidak ada", "Ada", "Ada", "Ada"},
+            {"Tidak ada", "Tidak ada", "Tidak ada", "Ada"}
+        };
         input.close();
     }
 }
