619fcf46d2f2f726f092caedaa7f56a8b7497c7e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 15:02:13 +0700

    Initialize

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/BudiDanBuahnya.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/BudiDanBuahnya.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.kualifikasisrifoton2024;
+import java.util.*;
+import java.util.stream.*;
 
-public class BudiDanBuahnya {
-
+public class BudiDanBuahnya
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
