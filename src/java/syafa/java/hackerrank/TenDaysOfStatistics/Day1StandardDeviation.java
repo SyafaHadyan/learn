077b5a7e9282f12d1846827626f51b9df5e983f4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 10:31:30 +0700

    Edit print

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1StandardDeviation.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day1StandardDeviation.java
@@ -50,7 +50,6 @@ public class Day1StandardDeviation
         {
             standardDeviation += Math.pow((data[i] - mean),2);
         }
-        //double result = (Math.sqrt(standardDeviation / data.length));
-        System.out.print(String.format("%.3g%n",(Math.sqrt(standardDeviation / data.length))));
+        System.out.printf("%.1f",(Math.sqrt(standardDeviation / data.length)));
     }
 }
