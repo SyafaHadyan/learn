9a8d64384dad70d3b856246e67a763b6d4e3919a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 11:37:09 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Search;
+import java.util.*;
+import java.util.stream.*;
 
-public class MinimumLoss {
-
+public class MinimumLoss
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
