e496ecc5e2991298ce3dda3051040f85a465648a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 11:40:16 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
@@ -6,6 +6,25 @@ public class MinimumLoss
 {
     public static void main(String[] args)
     {
+        /*
+         * 3
+         * 5 10 3
+         * 
+         * 2
+         * 
+         * 5
+         * 20 7 8 2 5
+         * 
+         * 2
+         * 
+         * int lowestPrice = Integer.MAX_VALUE;
+         * sort housePrice
+         * iterate through element
+         * if ((i + 1) - i < lowestPrice)
+         * {
+         *      lowestPrice = (i + 1) - i;
+         * }
+         */
         Scanner input = new Scanner(System.in);
         int arrayLength = Integer.parseInt(input.nextLine());
         int[] housePrice = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
