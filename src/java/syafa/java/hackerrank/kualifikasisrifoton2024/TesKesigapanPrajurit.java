b5432e1c359f9327e3e7e0b145737d5c812df429
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:22:17 +0700

    Calculate

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TesKesigapanPrajurit.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TesKesigapanPrajurit.java
@@ -8,7 +8,12 @@ public class TesKesigapanPrajurit
     {
         Scanner input = new Scanner(System.in);
         int[] weapon = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int counter = Integer.MIN_VALUE;
         input.close();
+        int counter = weapon[0] + weapon[2];
+        if (weapon[1] + weapon[3] > counter)
+        {
+            counter = weapon[1] + weapon[3];
+        }
+        System.out.print(counter);
     }
 }
