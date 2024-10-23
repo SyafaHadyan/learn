7e2abf001bfbd6e2d89869d5c33ee705118d0328
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:20:02 +0700

    Initial

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TesKesigapanPrajurit.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TesKesigapanPrajurit.java
@@ -7,6 +7,8 @@ public class TesKesigapanPrajurit
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] weapon = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int counter = Integer.MIN_VALUE;
         input.close();
     }
 }
