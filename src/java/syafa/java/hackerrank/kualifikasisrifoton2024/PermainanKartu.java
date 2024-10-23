df35f3d98ed93f850ba241de993831500b35b764
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 17:53:03 +0700

    Add note

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/PermainanKartu.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/PermainanKartu.java
@@ -5,6 +5,16 @@ public class PermainanKartu
 {
     public static void main(String[] args)
     {
+        /*
+         * H
+         * 3H 9S
+         * 
+         * 1
+         * 
+         * Jenis truf: H (Hati) Kartu pertama: "3H", yang memiliki peringkat 3 dan jenis Hati. Kartu kedua: "9S", yang memiliki peringkat 9 dan jenis Sekop.
+         * Kartu pertama adalah kartu truf karena jenisnya adalah Hati (H), yang merupakan jenis truf dan Kartu kedua adalah Sekop (S), yang bukan jenis truf.
+         * Berdasarkan aturan permainan, kartu truf selalu menang melawan kartu non-truf, tidak peduli berapa peringkatnya.
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
