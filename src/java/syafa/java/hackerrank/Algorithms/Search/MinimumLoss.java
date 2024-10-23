20a75b360131315387e7d729e24e75f0d9ebf3ae
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 11:38:07 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/MinimumLoss.java
@@ -7,6 +7,8 @@ public class MinimumLoss
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] housePrice = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
