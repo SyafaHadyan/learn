a535e9f20825d1795a4b6e020c97504e23536ced
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:19:02 +0700

    Initialize

---
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TesKesigapanPrajurit.java
@@ -0,0 +1,12 @@
+package syafa.java.hackerrank.kualifikasisrifoton2024;
+import java.util.*;
+import java.util.stream.*;;
+
+public class TesKesigapanPrajurit 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
