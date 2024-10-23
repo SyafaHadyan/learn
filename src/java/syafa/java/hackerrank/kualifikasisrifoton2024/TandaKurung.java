3d2033e677fc9f48656493dfbc1b6a03220f1c55
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 17:14:57 +0700

    Initialize

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TandaKurung.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/TandaKurung.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.kualifikasisrifoton2024;
+import java.util.*;
 
-public class TandaKurung {
-
+public class TandaKurung
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
