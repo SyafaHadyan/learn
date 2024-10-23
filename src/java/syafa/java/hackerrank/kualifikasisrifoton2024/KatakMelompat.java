b2981fbc49d5b25a16298c2e7088943278fb93bd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 14:45:08 +0700

    Initialize

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KatakMelompat.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KatakMelompat.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.kualifikasisrifoton2024;
+import java.util.*;
+import java.util.stream.*;
 
-public class KatakMelompat {
-
+public class KatakMelompat
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
