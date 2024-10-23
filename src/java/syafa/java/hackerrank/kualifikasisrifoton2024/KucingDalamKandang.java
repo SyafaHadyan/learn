c6e902de00ac41118d8b68d80b2e5b321b128639
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:44:02 +0700

    Add note

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
@@ -6,9 +6,37 @@ public class KucingDalamKandang
 {
     public static void main(String[] args)
     {
+        /*
+         * 3
+         * 1 0 5
+         * 
+         * 4
+         * 
+         * sort
+         * iterate 1
+         * 0 1 5
+         * 0 2 4
+         * 
+         * sort
+         * iterate 2
+         * 1 2 3
+         * 
+         * sort
+         * iterate 3
+         * 2 2 2
+         */
         Scanner input = new Scanner(System.in);
         int cageAmount = Integer.parseInt(input.nextLine());
         int[] cage = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        while (true)
+        {
+            Arrays.sort(cage);
+            for (int i = 0; i < cage.length; i++)
+            {
+                cage[cage[cage.length - 1] - 1];
+                cage[cage[cage.length - 2]]
+            }
+        }
     }
 }
