a18998a0c7f231afb6c492b2a703bcf1acd871d4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 16:12:44 +0700

    For loop check each word

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/AlienGagap.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/AlienGagap.java
@@ -7,5 +7,16 @@ public class AlienGagap
     {
         Scanner input = new Scanner(System.in);
         ArrayList<String> inputWord = new ArrayList<>(Arrays.asList(input.nextLine().split(" ")));
+        input.close();
+        for (int i = 0; i < inputWord.size(); i++)
+        {
+            for (int j = 0 + i; j < inputWord.size() - i; j++)
+            {
+                if (inputWord.get(i).equalsIgnoreCase(inputWord.get(j + 1)))
+                {
+                    
+                }
+            }
+        }
     }
 }
