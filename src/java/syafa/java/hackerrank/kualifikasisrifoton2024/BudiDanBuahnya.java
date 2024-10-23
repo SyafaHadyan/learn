ab83cba2bea36322c37bb25e7c0509a480f9c4d9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 15:09:28 +0700

    Add counter to int arr when no possible combination left

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/BudiDanBuahnya.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/BudiDanBuahnya.java
@@ -8,31 +8,37 @@ public class BudiDanBuahnya
     {
         Scanner input = new Scanner(System.in);
         int testCase = Integer.parseInt(input.nextLine());
+        int[] possibleDays = new int[testCase];
         for (int i = 0; i < testCase; i++)
         {
             int[] fruit = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int counter = 0;
             while (true)
             {
                 if (fruit[0] != 0 && fruit[1] != 0)
                 {
                     fruit[0]--;
                     fruit[1]--;
+                    counter++;
                     continue;
                 }
                 else if (fruit[0] != 0 && fruit[2] != 0)
                 {
                     fruit[0]--;
                     fruit[1]--;
+                    counter++;
                     continue;
                 }
                 else if (fruit[1] != 0 && fruit[2] != 0)
                 {
                     fruit[1]--;
                     fruit[2]--;
+                    counter++;
                     continue;
                 }
                 else
                 {
+                    possibleDays[i] = counter;
                     break;
                 }
             }
