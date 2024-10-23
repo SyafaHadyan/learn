e3c16dcc4decf6ad31ffe0e7de3cd4410ffbedfd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 18:12:58 +0700

    Change to private

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/PermainanKartu.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/PermainanKartu.java
@@ -5,7 +5,7 @@ public class PermainanKartu
 {
     static class game
     {
-        public static int checkCard(char turf,char firstCardType,char secondCardType,char firstCardValue,char secondCardValue)
+        private static int checkCard(char turf,char firstCardType,char secondCardType,char firstCardValue,char secondCardValue)
         {
             return 1;
         }
