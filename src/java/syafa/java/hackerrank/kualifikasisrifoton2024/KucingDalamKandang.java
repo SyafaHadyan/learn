0f4ba598aec3211f4f960024f57205b9e415dd5d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-23 20:38:30 +0700

    Initialize

--- src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
+++ src/java/syafa/java/hackerrank/kualifikasisrifoton2024/KucingDalamKandang.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.kualifikasisrifoton2024;
+import java.util.*;
+import java.util.stream.*;
 
-public class KucingDalamKandang {
-
+public class KucingDalamKandang
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
