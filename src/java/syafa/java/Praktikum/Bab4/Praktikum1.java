696f98621de6ca1c165ddeb947c70287491ead61
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-24 07:18:02 +0700

    Ask user input then store to temp passenger book

--- src/java/syafa/java/Praktikum/Bab4/Praktikum1.java
+++ src/java/syafa/java/Praktikum/Bab4/Praktikum1.java
@@ -135,7 +135,8 @@ public class Praktikum1
                         int ticketAmount = Integer.parseInt(input.nextLine());
                         for (int i = 0; i < ticketAmount; i++)
                         {
-                            //
+                            System.out.printf("\n%s%\n%s","Masukkan nama penumpang dan jenis tiket (Nama Jenis)","Contoh: Dengklek Pariwisata");
+                            String tempPassengerTicketBook = input.nextLine();
                         }
                         break;
                     case 5:
