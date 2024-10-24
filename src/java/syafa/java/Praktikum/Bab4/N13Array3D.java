d537490312f0779e70570313fe67306043800112
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-24 15:02:39 +0700

    Add ans

--- src/java/syafa/java/Praktikum/Bab4/N13Array3D.java
+++ src/java/syafa/java/Praktikum/Bab4/N13Array3D.java
@@ -7,6 +7,10 @@ public class N13Array3D
         int a[] = {2,7,6,8,1};
         int b[] = {3,4,2,8,9,7,2,7,5};
         int pjg;
+        int jumlahGenap = 0;
+        int jumlahGanjil = 0;
+        int banyakBilanganGenap = 0;
+        int banyakBilanganGanjil = 0;
         pjg = Math.max(a.length,b.length); 
         int c[] = new int[pjg];
         for (int i = 0; i < pjg; i++)
@@ -19,5 +23,29 @@ public class N13Array3D
                 c[i] = a[i] + b[i];
             System.out.print(c[i] + " ");
         }
+        for (int i = 0; i < c.length; i++)
+        {
+            if (c[i] % 2 == 0)
+            {
+                jumlahGenap += c[i];
+                banyakBilanganGenap++;
+            }
+            else if (c[i] % 2 != 0)
+            {
+                jumlahGanjil += c[i];
+                banyakBilanganGanjil++;
+            }
+        }
+        System.out.print
+        (
+            "\nJumlah bilangan genap" + "\n" +
+            jumlahGenap + "\n" +
+            "Jumlah bilangan ganjil" + "\n" +
+            jumlahGanjil + "\n" +
+            "Banyak bilangan genap" + "\n" +
+            banyakBilanganGenap + "\n" +
+            "Banyak bilangan ganjil" + "\n" +
+            banyakBilanganGanjil
+        );
     }
 }
