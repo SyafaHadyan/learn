4856ae4894772e409d3592ce93305c257f4daba3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 17:09:52 +0700

    Input data

--- src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
+++ src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
@@ -6,6 +6,13 @@ public class ManajemenAntrianVaksinasi
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int operation = Integer.parseInt(input.nextLine());
+        ArrayList<String> operationResult = new ArrayList<>();
+        ArrayList<String> patientRegister = new ArrayList<>();
+        for (int i = 0; i < operation; i++)
+        {
+            
+        }
         input.close();
     }
 }
