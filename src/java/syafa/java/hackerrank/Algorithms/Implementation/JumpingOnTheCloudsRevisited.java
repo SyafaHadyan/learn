6105f900e159648e624913821e218bce6ab9db14
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 20:04:31 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheCloudsRevisited.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheCloudsRevisited.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
+import java.util.stream.*;
 
-public class JumpingOnTheCloudsRevisited {
-
+public class JumpingOnTheCloudsRevisited
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
