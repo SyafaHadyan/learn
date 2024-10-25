da78b5cf64af42e4a41daf3666ec781c493b09e8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 20:07:51 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheCloudsRevisited.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/JumpingOnTheCloudsRevisited.java
@@ -6,6 +6,22 @@ public class JumpingOnTheCloudsRevisited
 {
     public static void main(String[] args)
     {
+        /*
+         * STDIN             Function
+         * -----             --------
+         * 8 2               n = 8, k = 2
+         * 0 0 1 0 0 1 1 0   c = [0, 0, 1, 0, 0, 1, 1, 0]
+         * 
+         * 92
+         * 
+         * clouds = 8
+         * jumpLength = 2
+         * 
+         * 0 -> 2 = 100 - 1 - 2 = 97 (land on thundercloud) (additional 2)
+         * 2 -> 4 = 97 - 1 = 96
+         * 4 -> 6 = 96 - 1 - 2 = 93 (land on thundercloud) (additional 2)
+         * 6 -> IndexOutOfBounds = 93 - 1 = 92
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
