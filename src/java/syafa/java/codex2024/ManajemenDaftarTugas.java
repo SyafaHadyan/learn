73739af4fe451ca5531d0074f60ae25639d46def
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 18:00:42 +0700

    Initialize

--- src/java/syafa/java/codex2024/ManajemenDaftarTugas.java
+++ src/java/syafa/java/codex2024/ManajemenDaftarTugas.java
@@ -1,5 +1,11 @@
 package syafa.java.codex2024;
+import java.util.*;
 
-public class ManajemenDaftarTugas {
-
+public class ManajemenDaftarTugas
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
