cfb906a56346184b7dd86da76a80dfa49c0bc665
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 16:45:13 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SubarrayDivision.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SubarrayDivision.java
@@ -6,6 +6,41 @@ public class SubarrayDivision
 {
     public static void main(String[] args)
     {
+        /*
+         * 5
+         * 1 2 1 3 2
+         * 3 2
+         * 
+         * 2
+         * 
+         * 2 squares summing 3
+         * possible combination:
+         * index    0 1
+         *          1 2
+         * 
+         * index    1 2
+         *          2 1
+         * 
+         * 
+         * 
+         * 6
+         * 1 1 1 1 1 1
+         * 3 2
+         * 
+         * 0
+         * 
+         * no possible combination of 2 squares summing 3
+         * 
+         * 1
+         * 4
+         * 4 1
+         * 
+         * 1
+         * 
+         * only 1 possible combination of 1 square summing 4
+         * 
+         * print 4
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
