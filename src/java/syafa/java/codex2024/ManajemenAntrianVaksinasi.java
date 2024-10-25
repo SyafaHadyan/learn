17cd7888901143e63a376ddb26d4114fe6d90f19
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 17:08:24 +0700

    Initialize

--- src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
+++ src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
@@ -1,5 +1,11 @@
 package syafa.java.codex2024;
+import java.util.*;
 
-public class ManajemenAntrianVaksinasi {
-
+public class ManajemenAntrianVaksinasi
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
