1bd18308b9596386edeec3d3afb47f36a73f829d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-25 18:00:03 +0700

    Remove comments

--- src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
+++ src/java/syafa/java/codex2024/ManajemenAntrianVaksinasi.java
@@ -5,41 +5,6 @@ public class ManajemenAntrianVaksinasi
 {
     public static void main(String[] args)
     {
-        /*
-         * 7
-         * DAFTAR 101
-         * DAFTAR 202
-         * CEKDEPAN
-         * LAYANI
-         * CEKDEPAN
-         * LAYANI
-         * CEKDEPAN
-         * 
-         * 101
-         * 202
-         * ANTRIAN KOSONG
-         * 
-         * 
-         * Orang dengan ID 101 mendaftar ke dalam antrian.
-         * Orang dengan ID 202 mendaftar setelahnya.
-         * Saat dicek, orang dengan ID 101 berada di depan antrian.
-         * Orang dengan ID 101 dilayani dan keluar dari antrian.
-         * Saat dicek kembali, orang dengan ID 202 berada di depan.
-         * Orang dengan ID 202 dilayani dan keluar dari antrian.
-         * Antrian kosong ketika dicek terakhir.
-         * 
-         * 7
-         * DAFTAR 101
-         * DAFTAR 202
-         * CEKDEPAN
-         * LAYANI
-         * CEKDEPAN
-         * LAYANI
-         * CEKDEPAN
-         * 101
-         * 202
-         * ANTRIAN KOSONG
-         */
         Scanner input = new Scanner(System.in);
         int operation = Integer.parseInt(input.nextLine());
         ArrayList<String> operationResult = new ArrayList<>();
