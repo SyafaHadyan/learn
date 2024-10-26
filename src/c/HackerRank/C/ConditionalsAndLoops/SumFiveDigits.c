46cf8fc88c5cda2dd0a892d748564d415c2d09d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 11:50:27 +0700

    For loop

---
+++ src/c/HackerRank/C/ConditionalsAndLoops/SumFiveDigits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int input;
+    int total;
+    scanf("%d",&input);
+    for (int i = 0; i < sizeof(input); i++)
+    {
+        /* code */
+    }
+    
+}
