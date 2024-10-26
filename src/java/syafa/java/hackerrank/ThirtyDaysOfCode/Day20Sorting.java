3b155f0413e2fb810c89261bc73b12e2459a0427
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:32:09 +0700

    Return result int arr

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
@@ -6,9 +6,9 @@ public class Day20Sorting
 {
     static class sortArray
     {
-        public static int bubbleSort(int[] array)
+        public static int[] bubbleSort(int[] array)
         {
-            int numberOfSwap = 0;
+            int[] result = new int[3];
             for (int i = 0; i < array.length - 1; i++)
             {
                 for (int j = 0; j < array.length - i - 1; j++)
@@ -18,11 +18,11 @@ public class Day20Sorting
                         int temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
-                        numberOfSwap++;
+                        result[2]++;
                     }
                 }
             }
-            return numberOfSwap;
+            return result;
         }
     }
     public static void main(String[] args)
@@ -31,6 +31,6 @@ public class Day20Sorting
         int arrayLength = Integer.parseInt(input.nextLine());
         int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
-        System.out.print("Array is sorted in" + " " + 0 + " " + "swaps" + "." + "\n" + "First element" + ":" + " " + 0 + "\n" + "Last element" + ":" + " " + 0);
+        System.out.print("Array is sorted in" + " " + sortArray.bubbleSort(array)[2] + " " + "swaps" + "." + "\n" + "First element" + ":" + " " + 0 + "\n" + "Last element" + ":" + " " + 0);
     }
 }
