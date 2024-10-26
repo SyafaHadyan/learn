1fc7bb0ad73e78955e1d678f9c6eb0cca16927b3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 11:29:43 +0700

    Convert to word

--- src/c/HackerRank/C/ConditionalsAndLoops/ConditionalStatements.c
+++ src/c/HackerRank/C/ConditionalsAndLoops/ConditionalStatements.c
@@ -8,5 +8,41 @@ int main()
     {
         printf("%s","One");
     }
+    else if (input == 2)
+    {
+        printf("%s","Two");
+    }
+    else if (input == 3)
+    {
+        printf("%s","Three");
+    }
+    else if (input == 4)
+    {
+        printf("%s","Four");
+    }
+    else if (input == 5)
+    {
+        printf("%s","Five");
+    }
+    else if (input == 6)
+    {
+        printf("%s","Six");
+    }
+    else if (input == 7)
+    {
+        printf("%s","Seven");
+    }
+    else if (input == 8)
+    {
+        printf("%s","Eight");
+    }
+    else if (input == 9)
+    {
+        printf("%s","Nine");
+    }
+    else
+    {
+        printf("%s","Greater than 9");
+    }
     return 0;
 }
