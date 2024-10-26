57981da58431229b3d1f27ecaceda8178d10cff6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 21:38:59 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/LinkedLists/PrintElementsLinkedList.java
+++ src/java/syafa/java/hackerrank/DataStructures/LinkedLists/PrintElementsLinkedList.java
@@ -6,6 +6,12 @@ public class PrintElementsLinkedList
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int inputAmount = Integer.parseInt(input.nextLine());
+        LinkedList<Integer> data = new LinkedList<>();
+        for (int i = 0; i < inputAmount; i++)
+        {
+            
+        }
         input.close();
     }
 }
