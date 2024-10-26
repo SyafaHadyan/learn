77d612a567fddd96a97c57727c3df540923a884a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 21:15:55 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/ArrayManipulation.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/ArrayManipulation.java
@@ -22,6 +22,11 @@ public class ArrayManipulation
          * The maximum value is 200.
          */
         Scanner input = new Scanner(System.in);
+        int[] arraySize = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < arraySize[1]; i++)
+        {
+            //
+        }
         input.close();
     }
 }
