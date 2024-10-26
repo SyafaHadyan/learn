3483ee22d783082aea526ac45dd52d12031af5a1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 13:32:58 +0700

    Print func

--- src/c/HackerRank/C/Functions/CalculateNTerm.c
+++ src/c/HackerRank/C/Functions/CalculateNTerm.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+int calculateAt(int a,int b,int c);
+
 int main()
 {
     int dataSize;
     int a,b,c;
     scanf("%d",&dataSize);
     scanf("%d" "%d" "%d",&a,&b,&c);
+    printf("%d",calculateAt(a,b,c));
+    return 0;
 }
