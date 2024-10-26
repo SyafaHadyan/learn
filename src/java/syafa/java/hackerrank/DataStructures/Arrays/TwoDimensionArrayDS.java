93c6a629de4b77dcf3a0e7458c2343c1e73bd3ee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:29:27 +0700

    Add from another file

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/TwoDimensionArrayDS.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/TwoDimensionArrayDS.java
@@ -6,6 +6,40 @@ public class TwoDimensionArrayDS
 {
     public static void main(String[] args)
     {
-        
+        int[][] hourGlass = new int[6][6];
+        int currentMaxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                hourGlass[i][j] = Integer.parseInt(input.next());
+            }
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (hourGlass[i][j] +
+                    hourGlass[i][j + 1] +
+                    hourGlass[i][j + 2] +
+                    hourGlass[i + 1][j + 1] +
+                    hourGlass[i + 2][j] +
+                    hourGlass[i + 2][j + 1] +
+                    hourGlass[i + 2][j + 2] >
+                    currentMaxValue)
+                {
+                    currentMaxValue =
+                    hourGlass[i][j] +
+                    hourGlass[i][j + 1] +
+                    hourGlass[i][j + 2] +
+                    hourGlass[i + 1][j + 1] +
+                    hourGlass[i + 2][j] +
+                    hourGlass[i + 2][j + 1] +
+                    hourGlass[i + 2][j + 2];
+                }
+            }
+        }
+        input.close();
+        System.out.print(currentMaxValue);
     }
 }
