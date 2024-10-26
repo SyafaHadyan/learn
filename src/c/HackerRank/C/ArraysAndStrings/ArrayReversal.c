017429df5a28e872b47814a4cd0e080f59d6bcb7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 13:08:09 +0700

    Input to arr

---
+++ src/c/HackerRank/C/ArraysAndStrings/ArrayReversal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int arraySize;
+    int array[arraySize];
+    scanf("%d",&arraySize);
+    for (int i = 0; i < arraySize; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    
+}
