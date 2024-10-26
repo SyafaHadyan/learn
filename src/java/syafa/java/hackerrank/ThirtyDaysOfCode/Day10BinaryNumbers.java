106c06350995b99ef2e2ac813a3f4c81ac8b9adc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:44:22 +0700

    Add note

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
@@ -1,11 +1,15 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 import java.util.*;
+import java.util.stream.*;
 
 public class Day10BinaryNumbers
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String binaryString = Integer.toBinaryString(Integer.parseInt(input.nextLine()));
+        // 10101010100110 - example
+        int[] binary = Integer.toBinaryString(Integer.parseInt(input.nextLine())).split("").mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
