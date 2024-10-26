88db2fec7249d2dfaf5123966d717d9e9dd61823
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 21:14:27 +0700

    Add note

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/ArrayManipulation.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/ArrayManipulation.java
@@ -6,6 +6,19 @@ public class ArrayManipulation
 {
     public static void main(String[] args)
     {
+        /*
+         * 5 3
+         * 1 2 100
+         * 2 5 100
+         * 3 4 100
+         * 
+         * 200
+         * 
+         * After the first update the list is 100 100 0 0 0.
+         * After the second update list is 100 200 100 100 100.
+         * After the third update list is 100 200 200 200 100.
+         * The maximum value is 200.
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
