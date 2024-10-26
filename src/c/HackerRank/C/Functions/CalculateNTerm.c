37e59770fee0d7c7d02bfdf8de8377da9f381755
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 14:13:09 +0700

    Fix set c

--- src/c/HackerRank/C/Functions/CalculateNTerm.c
+++ src/c/HackerRank/C/Functions/CalculateNTerm.c
@@ -13,11 +13,11 @@ int calculateAt(int a,int b,int c,int counter,int currentCount,int result)
 int main()
 {
     int dataSize;
-    int result = 0;
     int currentCount = 3;
     int a,b,c;
     scanf("%d",&dataSize);
     scanf("%d" "%d" "%d",&a,&b,&c);
+    int result = c;
     while (currentCount != dataSize)
     {
         result = a + b + c;
