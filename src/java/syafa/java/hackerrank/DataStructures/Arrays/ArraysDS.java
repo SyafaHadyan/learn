9f6f11a2ed4caab2866b1861bf5fd891ab0cd85f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:18:02 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/ArraysDS.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/ArraysDS.java
@@ -7,6 +7,8 @@ public class ArraysDS
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
+        int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
