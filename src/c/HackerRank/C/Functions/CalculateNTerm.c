17fab92d5a7e5ada72c32a9e4033138fba3779c7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 13:34:50 +0700

    Recursive

--- src/c/HackerRank/C/Functions/CalculateNTerm.c
+++ src/c/HackerRank/C/Functions/CalculateNTerm.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int calculateAt(int a,int b,int c);
+int calculateAt(int a,int b,int c,int counter)
+{
+    while (counter)
+    {
+        
+        calculateAt();
+    }
+    
+}
 
 int main()
 {
@@ -8,6 +16,6 @@ int main()
     int a,b,c;
     scanf("%d",&dataSize);
     scanf("%d" "%d" "%d",&a,&b,&c);
-    printf("%d",calculateAt(a,b,c));
+    printf("%d",calculateAt(a,b,c,dataSize));
     return 0;
 }
