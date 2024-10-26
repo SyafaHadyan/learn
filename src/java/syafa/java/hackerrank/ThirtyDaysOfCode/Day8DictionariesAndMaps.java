89086520fb2cc8076707596c0353f214edd2aa74
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 16:59:03 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8DictionariesAndMaps.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8DictionariesAndMaps.java
@@ -5,7 +5,6 @@ public class Day8DictionariesAndMaps
 {
     public static void main(String[] args)
     {
-        // TODO: Optimize the code
         Scanner input = new Scanner(System.in);
         int phoneBookSize = Integer.parseInt(input.nextLine());
         HashMap<String, Integer> phoneBook = new HashMap<String, Integer>();
@@ -25,35 +24,5 @@ public class Day8DictionariesAndMaps
             System.out.println("Not found");
         }
         input.close();
-        /*
-        int phoneBookInput = Integer.parseInt(input.nextLine());
-        String nameArray[] = new String[phoneBookInput];
-        int phoneNumberArray[] = new int[phoneBookInput];
-        String result[] = new String[phoneBookInput];
-        String name = "";
-        int phoneNumber = 0;
-        for (int i = 0; i < phoneBookInput; i++)
-        {
-            name = input.next();
-            nameArray[i] = name;
-            phoneNumber = input.nextInt();
-            phoneNumberArray[i] = phoneNumber;
-            input.nextLine();
-        }
-        for (int i = 0; i < phoneBookInput; i++)
-        {
-            String searchName = input.nextLine();
-            result[i] = searchName + "=" + phoneNumberArray[i];
-            if (!Arrays.stream(nameArray).anyMatch(searchName::equals))
-            {
-                result[i] = "Not found";
-            }
-        }
-        input.close();
-        for (int i = 0; i < phoneBookInput; i++)
-        {
-            System.out.println(result[i]);
-        }
-        */
     }
 }
