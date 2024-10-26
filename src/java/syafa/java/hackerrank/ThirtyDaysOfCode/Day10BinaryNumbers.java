df34e78c0d0756f2185f5b6a1eca33c638873d02
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:57:41 +0700

    Convert to int arr

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
@@ -9,7 +9,7 @@ public class Day10BinaryNumbers
         Scanner input = new Scanner(System.in);
         String binaryString = Integer.toBinaryString(Integer.parseInt(input.nextLine()));
         // 10101010100110 - example
-        int[] binary = for (int i = 0; (Integer.toBinaryString(Integer.parseInt(input.nextLine()))).toCharArray().length; i++) {binary[i] = (Integer.toBinaryString(Integer.parseInt(input.nextLine()))).toCharArray()[i]};
+        int[] binary = Integer.toString(Integer.parseInt(input.nextLine())).chars().map(Character::getNumericValue).toArray();
         input.close();
     }
 }
