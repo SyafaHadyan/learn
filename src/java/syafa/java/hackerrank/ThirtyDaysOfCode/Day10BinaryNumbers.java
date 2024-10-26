cfcd833611828d84e15e836b8ed7bedd4b021cab
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:40:49 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
 
-public class Day10BinaryNumbers {
-
+public class Day10BinaryNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
