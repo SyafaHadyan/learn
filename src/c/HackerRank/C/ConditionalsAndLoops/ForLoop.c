de60d9447932c8be241058cbbf25e4546557d0da
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 11:33:05 +0700

    Input for

---
+++ src/c/HackerRank/C/ConditionalsAndLoops/ForLoop.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int min;
+    int max;
+    scanf("%d\n%d",&min,&max);
+    for (int i = min; i <= max; i++)
+    {
+        //
+    }
+    if (min == 1)
+    {
+        printf("%s","one");
+    }
+    else if (min == 2)
+    {
+        printf("%s","two");
+    }
+    else if (min == 3)
+    {
+        printf("%s","three");
+    }
+    else if (min == 4)
+    {
+        printf("%s","four");
+    }
+    else if (min == 5)
+    {
+        printf("%s","five");
+    }
+    else if (min == 6)
+    {
+        printf("%s","six");
+    }
+    else if (min == 7)
+    {
+        printf("%s","seven");
+    }
+    else if (min == 8)
+    {
+        printf("%s","eight");
+    }
+    else if (min == 9)
+    {
+        printf("%s","nine");
+    }
+    else
+    {
+        printf("%s","Greater than 9");
+    }
+    return 0;
+}
