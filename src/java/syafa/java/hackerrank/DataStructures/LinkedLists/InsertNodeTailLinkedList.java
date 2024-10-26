1f9ffe881e8c84bb2560bb031e3509b330b90137
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 22:05:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/LinkedLists/InsertNodeTailLinkedList.java
+++ src/java/syafa/java/hackerrank/DataStructures/LinkedLists/InsertNodeTailLinkedList.java
@@ -6,6 +6,12 @@ public class InsertNodeTailLinkedList
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int dataAmount = Integer.parseInt(input.nextLine());
+        LinkedList<Integer> data = new LinkedList<>();
+        for (int i = 0; i < args.length; i++)
+        {
+            data.add(Integer.parseInt(input.nextLine()));
+        }
         input.close();
     }
 }
