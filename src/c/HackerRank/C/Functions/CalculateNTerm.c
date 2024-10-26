2632b964b0756679c108e3040bc2acda42df9e98
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 13:39:54 +0700

    If less than count

--- src/c/HackerRank/C/Functions/CalculateNTerm.c
+++ src/c/HackerRank/C/Functions/CalculateNTerm.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int calculateAt(int a,int b,int c,int counter)
+int calculateAt(int a,int b,int c,int counter,int currentCount,int result)
 {
     int result = 0;
-    int currentCount = 3;
-    while (currentCount != counter)
+    result += a + b + c;
+    a = b;
+    b = c;
+    c = result;
+    currentCount++;
+    if (currentCount != counter)
     {
-        calculateAt();
+        calculateAt(a,b,c,counter,currentCount,result);
     }
     return result;
 }
@@ -14,9 +18,11 @@ int calculateAt(int a,int b,int c,int counter)
 int main()
 {
     int dataSize;
+    int result = 0;
+    int currentCount = 3;
     int a,b,c;
     scanf("%d",&dataSize);
     scanf("%d" "%d" "%d",&a,&b,&c);
-    printf("%d",calculateAt(a,b,c,dataSize));
+    printf("%d",calculateAt(a,b,c,dataSize,currentCount,result));
     return 0;
 }
