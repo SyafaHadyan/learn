fadc0823cbdc28c4a719cef463c836aa235979b3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:34:42 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/LeftRotation.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/LeftRotation.java
@@ -7,6 +7,8 @@ public class LeftRotation
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] arrayRotation = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
