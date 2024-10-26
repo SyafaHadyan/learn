6f7338e9d6914135f1d7fd4e01bceaa0acd22a8b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 21:38:10 +0700

    Initialize

--- src/java/syafa/java/hackerrank/DataStructures/LinkedLists/PrintElementsLinkedList.java
+++ src/java/syafa/java/hackerrank/DataStructures/LinkedLists/PrintElementsLinkedList.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.DataStructures.LinkedLists;
+import java.util.*;
 
-public class PrintElementsLinkedList {
-
+public class PrintElementsLinkedList
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
