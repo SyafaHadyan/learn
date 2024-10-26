7cd60ac0fcf2006b53d5340d8be90abccc9066e9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:26:18 +0700

    Input data

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day20Sorting.java
@@ -1,10 +1,14 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 import java.util.*;
+import java.util.stream.*;
 
 public class Day20Sorting
 {
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        input.close();
     }
 }
