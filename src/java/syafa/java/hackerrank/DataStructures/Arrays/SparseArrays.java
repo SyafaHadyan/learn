03b717d9695902c1644020da92b04b42d1a81898
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:55:14 +0700

    Input data

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/SparseArrays.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/SparseArrays.java
@@ -6,6 +6,12 @@ public class SparseArrays
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int dataAmount = Integer.parseInt(input.nextLine());
+        String[] data = new String[dataAmount];
+        for (int i = 0; i < dataAmount; i++)
+        {
+            //
+        }
         input.close();
     }
 }
