3ab77269f9720362585c9289c28545cb76bc41b1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 13:13:52 +0700

    Input data

---
+++ src/c/HackerRank/C/ArraysAndStrings/java.java
@@ -0,0 +1,16 @@
+package c.HackerRank.C.ArraysAndStrings;
+import java.util.*;
+
+public class java
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String[] inputString = input.nextLine().split(" ");
+        input.close();
+        for (int i = 0; i < inputString.length; i++)
+        {
+            
+        }
+    }
+}
