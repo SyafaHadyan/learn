74898ad24facf8769002ba033d5eb646e42b31c4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 14:37:31 +0700

    Check char

--- src/c/HackerRank/C/Functions/StudentsMarksSum.c
+++ src/c/HackerRank/C/Functions/StudentsMarksSum.c
@@ -5,10 +5,19 @@ int main()
     int studentAmount;
     scanf("%d",&studentAmount);
     int studentGrade[studentAmount];
+    char sumGrade;
     for (int i = 0; i < studentAmount; i++)
     {
-        /* code */
+        scanf("%d",&studentGrade[i]);
     }
-    
+    scanf("%c",&sumGrade);
+    if (sumGrade == 'b')
+    {
+        //
+    }
+    else if (sumGrade = 'g')
+    {
+        //
+    }
     return 0;
 }
