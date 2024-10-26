bb1f1219b5873c4878babb5540d2944be9c756dc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 11:30:50 +0700

    Change to lowercase

--- src/c/HackerRank/C/ConditionalsAndLoops/ConditionalStatements.c
+++ src/c/HackerRank/C/ConditionalsAndLoops/ConditionalStatements.c
@@ -6,39 +6,39 @@ int main()
     scanf("%d",&input);
     if (input == 1)
     {
-        printf("%s","One");
+        printf("%s","one");
     }
     else if (input == 2)
     {
-        printf("%s","Two");
+        printf("%s","two");
     }
     else if (input == 3)
     {
-        printf("%s","Three");
+        printf("%s","three");
     }
     else if (input == 4)
     {
-        printf("%s","Four");
+        printf("%s","four");
     }
     else if (input == 5)
     {
-        printf("%s","Five");
+        printf("%s","five");
     }
     else if (input == 6)
     {
-        printf("%s","Six");
+        printf("%s","six");
     }
     else if (input == 7)
     {
-        printf("%s","Seven");
+        printf("%s","seven");
     }
     else if (input == 8)
     {
-        printf("%s","Eight");
+        printf("%s","eight");
     }
     else if (input == 9)
     {
-        printf("%s","Nine");
+        printf("%s","nine");
     }
     else
     {
