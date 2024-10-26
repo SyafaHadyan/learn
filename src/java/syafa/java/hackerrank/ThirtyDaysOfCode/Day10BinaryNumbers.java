4e9f54a11406d61bf8b6ee695665ff7c8a5d9899
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 09:01:13 +0700

    If cons

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
@@ -1,15 +1,28 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
 import java.util.*;
-import java.util.stream.*;
 
 public class Day10BinaryNumbers
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        String binaryString = Integer.toBinaryString(Integer.parseInt(input.nextLine()));
-        // 10101010100110 - example
         int[] binary = Integer.toString(Integer.parseInt(input.nextLine())).chars().map(Character::getNumericValue).toArray();
         input.close();
+        int consecutive = 0;
+        int currentConsecutive = 1;
+        for (int i = 0; i < binary.length; i++)
+        {
+            try
+            {
+                if (binary[i - 1] == 1)
+                {
+                    currentConsecutive++;
+                }
+            }
+            catch (Exception e)
+            {
+                currentConsecutive++;
+            }
+        }
     }
 }
