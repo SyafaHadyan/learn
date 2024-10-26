6b1b86262a1fc73da9a10f1647be1e095667752d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:29:17 +0700

    Initialize

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/TwoDimensionArrayDS.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/TwoDimensionArrayDS.java
@@ -1,6 +1,11 @@
 package syafa.java.hackerrank.DataStructures.Arrays;
+import java.util.*;
+import java.util.stream.*;
 
 public class TwoDimensionArrayDS
 {
-
+    public static void main(String[] args)
+    {
+        
+    }
 }
