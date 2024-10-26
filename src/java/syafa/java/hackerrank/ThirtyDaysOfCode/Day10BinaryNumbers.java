2d5325ce1015f4ae42da5aa2ddd47b76eaf6a8f4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 08:50:18 +0700

    Temp

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day10BinaryNumbers.java
@@ -9,7 +9,7 @@ public class Day10BinaryNumbers
         Scanner input = new Scanner(System.in);
         String binaryString = Integer.toBinaryString(Integer.parseInt(input.nextLine()));
         // 10101010100110 - example
-        int[] binary = Integer.toBinaryString(Integer.parseInt(input.nextLine())).split("").mapToInt(Integer::parseInt).toArray();
+        int[] binary = Integer.parseInt(Integer.toBinaryString(Integer.parseInt(input.nextLine())));
         input.close();
     }
 }
