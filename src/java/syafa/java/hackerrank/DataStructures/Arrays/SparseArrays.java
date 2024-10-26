4338d737c64729440174d88c92cf3f760af00134
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 20:53:58 +0700

    Initialize

--- src/java/syafa/java/hackerrank/DataStructures/Arrays/SparseArrays.java
+++ src/java/syafa/java/hackerrank/DataStructures/Arrays/SparseArrays.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.DataStructures.Arrays;
+import java.util.*;
 
-public class SparseArrays {
-
+public class SparseArrays
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
