a20b3cfb993271b0a2b220e6f03fcd37d10c38d4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 12:32:57 +0700

    Base loop

--- src/c/HackerRank/C/ConditionalsAndLoops/PatternLoop.c
+++ src/c/HackerRank/C/ConditionalsAndLoops/PatternLoop.c
@@ -4,4 +4,20 @@ int main()
 {
     int loop;
     scanf("%d",&loop);
+    for (int i = 0; i < loop; i++)
+    {
+        for (int j = 0; j < loop; j++)
+        {
+            printf("%d",loop);
+            if (loop - j != 1)
+            {
+                printf(" ");
+            }
+        }
+        if (loop - i != 1)
+        {
+            printf("\n");
+        }
+    }
+    
 }
