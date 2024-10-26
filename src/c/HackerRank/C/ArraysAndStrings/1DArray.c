b213aa54d34f8bdc431b0ebf5b307e1179fee09b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 12:55:31 +0700

    Dynamic arr int

--- src/c/HackerRank/C/ArraysAndStrings/1DArray.c
+++ src/c/HackerRank/C/ArraysAndStrings/1DArray.c
@@ -4,11 +4,11 @@ int main()
 {
     int arraySize;
     int total;
-    int array[arraySize];
+    int *array = (int*)malloc(arraySize * sizeof(int));
     scanf("%d",&arraySize);
     for (size_t i = 0; i < arraySize; i++)
     {
-        array[i] = scanf("%d");
+        array[i] = scanf("%d",&array[i]);
     }
     printf("%s","Test");
     return 0;
