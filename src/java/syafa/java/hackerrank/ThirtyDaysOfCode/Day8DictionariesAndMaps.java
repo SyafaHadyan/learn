a573531130c13eb2aeae8e74f4e6b1fa324fb294
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-26 17:00:47 +0700

    Change input

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8DictionariesAndMaps.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day8DictionariesAndMaps.java
@@ -6,11 +6,12 @@ public class Day8DictionariesAndMaps
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int phoneBookSize = Integer.parseInt(input.nextLine());
+        int phoneBookSize = input.nextInt();
+        input.nextLine();
         HashMap<String, Integer> phoneBook = new HashMap<String, Integer>();
         for (int i = 0; i < phoneBookSize; i++)
         {
-            phoneBook.put(input.next(),Integer.parseInt(input.next()));
+            phoneBook.put(input.next(),input.nextInt());
             input.nextLine();
         }
         for (int i = 0; i < phoneBook.size(); i++)
