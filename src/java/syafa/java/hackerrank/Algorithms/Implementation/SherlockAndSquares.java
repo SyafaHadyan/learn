885d84569152e38ba70f929b5b8ec3b62c8601d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 14:12:58 +0700

    Print result

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SherlockAndSquares.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SherlockAndSquares.java
@@ -23,9 +23,13 @@ public class SherlockAndSquares
             }
             result[i] = counter;
         }
-        for (int i = 0; i < args.length; i++)
+        for (int i = 0; i < result.length; i++)
         {
             System.out.print(result[i]);
+            if (result.length - i != 1)
+            {
+                System.out.print("\n");
+            }
         }
     }
 }
