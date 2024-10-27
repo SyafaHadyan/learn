a491f5b6ffc8079a3ab1c16ab31b7876329345c3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 20:11:45 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Search/IceCreamParlor.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/IceCreamParlor.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Search;
+import java.util.*;
+import java.util.stream.*;
 
-public class IceCreamParlor {
-
+public class IceCreamParlor
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
