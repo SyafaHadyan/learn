d370fdd9ba891bf9858b2a7f17fc67553840d5bc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 18:55:15 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/Algorithms/Strings/CaesarCipher.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/CaesarCipher.java
@@ -21,29 +21,9 @@ public class CaesarCipher
     }
     public static void main(String[] args)
     {
-        /*
-         * 11
-         * middle-Outz
-         * 2
-         * 
-         * okffng-Qwvb
-         * 
-         * Original alphabet:      abcdefghijklmnopqrstuvwxyz
-         * Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
-         * m -> o
-         * i -> k
-         * d -> f
-         * d -> f
-         * l -> n
-         * e -> g
-         * -    -
-         * O -> Q
-         * u -> w
-         * t -> v
-         * z -> b
-         */
         Scanner input = new Scanner(System.in);
-        int wordLength = Integer.parseInt(input.nextLine());
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0,useless.length());
         char[] originalLetter = input.nextLine().toCharArray();
         int rotation = Integer.parseInt(input.nextLine());
         StringBuilder result = new StringBuilder();
