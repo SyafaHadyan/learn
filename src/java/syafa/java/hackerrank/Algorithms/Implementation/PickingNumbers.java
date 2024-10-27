c9f386c2b3f7da6fc609ebbd60edfc7451dcd46a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 11:44:38 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/PickingNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/PickingNumbers.java
@@ -7,6 +7,9 @@ public class PickingNumbers
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] array = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        Arrays.sort(array);
     }
 }
