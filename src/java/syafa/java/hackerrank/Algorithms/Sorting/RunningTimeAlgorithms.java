351172a1bb60d815274fcfeb33a3f0b843bcf689
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 06:42:31 +0700

    Method insertion sort

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
@@ -4,6 +4,14 @@ import java.util.stream.*;
 
 public class RunningTimeAlgorithms
 {
+    static class sort
+    {
+        public static int insertionSort(int[] array)
+        {
+            int swapAmount = 0;
+            return swapAmount;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
