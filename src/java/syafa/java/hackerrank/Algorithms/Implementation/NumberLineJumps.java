9b6b7d37828bb63dfe005d4771f8310dbf21c790
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 11:18:42 +0700

    Whlie loop

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/NumberLineJumps.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/NumberLineJumps.java
@@ -1,11 +1,20 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class NumberLineJumps
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] kangarooJump = Stream.of(input.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        while (kangarooJump[0] != kangarooJump[2] && !(kangarooJump[0] > kangarooJump[2]))
+        {
+            for (int i = 0; i < kangarooJump.length; i++)
+            {
+                
+            }
+        }
     }
 }
