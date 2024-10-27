645bb18cabb7d1e862b97c8f8ea4e434bf1357f2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 11:03:11 +0700

    Turn page from back

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DrawingBook.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DrawingBook.java
@@ -9,13 +9,14 @@ public class DrawingBook
         int bookPage = Integer.parseInt(input.nextLine());
         int targetPage = Integer.parseInt(input.nextLine());
         input.close();
-        int minimumPageTurn = 0;
+        int minimumPageTurnFront = 0;
+        int minimumPageTurnBack = 0;
         int section = 1;
         for (int i = 1; i < targetPage; i++)
         {
             if (section == 1)
             {
-                minimumPageTurn++;
+                minimumPageTurnFront++;
                 section = 0;
                 continue;
             }
@@ -25,6 +26,20 @@ public class DrawingBook
                 continue;
             }
         }
-        System.out.print(minimumPageTurn);
+        for (int i = bookPage; i > targetPage; i--)
+        {
+            if (section == 1)
+            {
+                minimumPageTurnBack++;
+                section = 0;
+                continue;
+            }
+            if (section == 0)
+            {
+                section++;
+                continue;
+            }
+        }
+        System.out.print(minimumPageTurnFront);
     }
 }
