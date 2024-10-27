6d4e2be60bffdb73649fbfcc33b3cf5bf4e60c6a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 20:43:55 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Search/MissingNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/MissingNumbers.java
@@ -6,6 +6,14 @@ public class MissingNumbers
 {
     public static void main(String[] args)
     {
+        /*
+         * 10
+         * 203 204 205 206 207 208 203 204 205 206
+         * 13
+         * 203 204 204 205 206 207 205 208 203 206 205 206 204
+         * 
+         * 204 205 206
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
