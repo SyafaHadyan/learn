7c60fc8d8725e634b49244916889b41af653183e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 20:43:23 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Search/MissingNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/MissingNumbers.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Search;
+import java.util.*;
+import java.util.stream.*;
 
-public class MissingNumbers {
-
+public class MissingNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
