5178e4a5d65042ab16db6a68427773b81f717f2c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 06:44:37 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
@@ -33,6 +33,8 @@ public class RunningTimeAlgorithms
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
