d957a9ed59ccab3d9b4781f2d70beed939a62ec0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 10:53:39 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/DrawingBook.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/DrawingBook.java
@@ -6,6 +6,8 @@ public class DrawingBook
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int bookPage = Integer.parseInt(input.nextLine());
+        int targetPage = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
