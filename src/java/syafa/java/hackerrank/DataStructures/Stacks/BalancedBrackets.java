9e15659985b06eced2a8022e4884e1369a389bf5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 07:06:39 +0700

    Add note

--- src/java/syafa/java/hackerrank/DataStructures/Stacks/BalancedBrackets.java
+++ src/java/syafa/java/hackerrank/DataStructures/Stacks/BalancedBrackets.java
@@ -5,6 +5,18 @@ public class BalancedBrackets
 {
     public static void main(String[] args)
     {
+        /*
+         * STDIN           Function
+         * -----           --------
+         * 3               n = 3
+         * {[()]}          first s = '{[()]}'
+         * {[(])}          second s = '{[(])}'
+         * {{[[(())]]}}    third s ='{{[[(())]]}}'
+         * 
+         * YES
+         * NO
+         * YES
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
