57ce79a48d2b5b8b3fcaa31dcbe12434c974f2dd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 18:29:04 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Strings/CaesarCipher.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/CaesarCipher.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Strings;
+import java.util.*;
 
-public class CaesarCipher {
-
+public class CaesarCipher
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
