a0f5f1169c3ea6c719985dcf50d2c61b8a554b4b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 06:58:25 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/PriyankaAndToys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/PriyankaAndToys.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Greedy;
+import java.util.*;
+import java.util.stream.*;
 
-public class PriyankaAndToys {
-
+public class PriyankaAndToys
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
