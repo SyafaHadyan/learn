9cdcd7dad217328db662ee6942a1183aae49be1b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 14:07:42 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SherlockAndSquares.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SherlockAndSquares.java
@@ -6,6 +6,8 @@ public class SherlockAndSquares
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int minNumber = Integer.parseInt(input.nextLine());
+        int maxNumber = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
