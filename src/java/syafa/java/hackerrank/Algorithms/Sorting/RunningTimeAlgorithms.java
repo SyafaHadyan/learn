5afb5c5f8fe133e1ebf2ad70b7340008879d96da
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 06:43:37 +0700

    Add standard insertion sort algorithm

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
@@ -9,6 +9,24 @@ public class RunningTimeAlgorithms
         public static int insertionSort(int[] array)
         {
             int swapAmount = 0;
+            int n = array.length;
+            for (int i = 1; i < n; ++i)
+            {
+                int k = array[i];
+                int j = i - 1;
+    
+                // Move elements of arr[0..i-1], that are
+                // greater than key, to one position ahead
+                // of their current position
+                while (j >= 0 && array[j] > k)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                    swapAmount++;
+                }
+              
+                array[j + 1] = k;
+            }
             return swapAmount;
         }
     }
