b1c283b59a1bfcf998add520059d30962bdc25e1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-27 20:12:35 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Search/IceCreamParlor.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/IceCreamParlor.java
@@ -6,6 +6,24 @@ public class IceCreamParlor
 {
     public static void main(String[] args)
     {
+        /*
+         * STDIN       Function
+         * -----       --------
+         * 2           t = 2
+         * 4           k = 4
+         * 5           cost[] size n = 5
+         * 1 4 5 3 2   cost = [1, 4, 5, 3, 2]
+         * 4           k = 4
+         * 4           cost[] size n = 4
+         * 2 2 4 3     cost=[2, 2,4, 3]
+         * 
+         * 1 4
+         * 1 2
+         * 
+         * Two friends like to pool their money and go to the ice cream parlor.
+         * They always choose two distinct flavors and they spend all of their money.
+         * Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have. 
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
