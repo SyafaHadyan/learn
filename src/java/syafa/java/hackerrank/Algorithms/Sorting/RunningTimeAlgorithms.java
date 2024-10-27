7aa5d9ac0008406f7edcdabb74ab79f4ad40e44a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 06:41:29 +0700

    Initialzie

--- src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
+++ src/java/syafa/java/hackerrank/Algorithms/Sorting/RunningTimeAlgorithms.java
@@ -1,6 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Sorting;
+import java.util.*;
+import java.util.stream.*;
 
 public class RunningTimeAlgorithms
 {
-
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
