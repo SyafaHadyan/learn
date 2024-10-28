2236b575b233529a7ced48e9a181729ae5da741a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 14:16:05 +0700

    Add from hackerrank

---
+++ src/java/syafa/java/hackerrank/Algorithms/Debugging/Solution.java
@@ -0,0 +1,33 @@
+package syafa.java.hackerrank.Algorithms.Debugging;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static boolean isSmartNumber(int num) {
+        int val = (int) Math.sqrt(num);    
+        if(num / val == 1)
+            return true;
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        int test_cases;
+        Scanner in = new Scanner(System.in);
+        test_cases = in.nextInt();
+        int num;
+        for(int i = 0; i < test_cases; i++){
+            num = in.nextInt();
+            if(isSmartNumber(num)){
+                System.out.println("YES");
+            }
+            else System.out.println("NO");
+        }
+        in.close();
+    }
+}
+
+
+
