76875a63a283c2af4d16c8a57bcd0c4a0b32fc82
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 13:11:30 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TheBombermanGame.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TheBombermanGame.java
@@ -1,11 +1,18 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class TheBombermanGame
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] configuration = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        char[][] map = new char[configuration[0]][configuration[1]];
+        for (int i = 0; i < configuration[0]; i++)
+        {
+            //
+        }
         input.close();
     }
 }
