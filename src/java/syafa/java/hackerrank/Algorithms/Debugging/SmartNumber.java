f22816a51fcbb2dbe93e96523e7592cc3a5c5ff0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 14:16:37 +0700

    Rename

--- src/java/syafa/java/hackerrank/Algorithms/Debugging/SmartNumber.java
+++ src/java/syafa/java/hackerrank/Algorithms/Debugging/SmartNumber.java
@@ -5,22 +5,27 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
-    public static boolean isSmartNumber(int num) {
+public class SmartNumber
+{
+    public static boolean isSmartNumber(int num)
+    {
         int val = (int) Math.sqrt(num);    
         if(num / val == 1)
             return true;
         return false;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int test_cases;
         Scanner in = new Scanner(System.in);
         test_cases = in.nextInt();
         int num;
-        for(int i = 0; i < test_cases; i++){
+        for(int i = 0; i < test_cases; i++)
+        {
             num = in.nextInt();
-            if(isSmartNumber(num)){
+            if (isSmartNumber(num))
+            {
                 System.out.println("YES");
             }
             else System.out.println("NO");
