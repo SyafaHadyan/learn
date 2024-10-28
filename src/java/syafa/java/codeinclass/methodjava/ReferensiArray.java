00486fd0276262a411ecc43f19a56c7ead202c70
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 07:59:21 +0700

    Sum array

--- src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
+++ src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
@@ -4,9 +4,18 @@ import java.util.stream.*;;
 
 public class ReferensiArray
 {
+    static class arrayOperation
+    {
+        public static int sumArray(int[] array)
+        {
+            return IntStream.of(array).sum();
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        System.out.println("The sum is" + " " + arrayOperation.sumArray(array));
         input.close();
     }
 }
