28ea13509d0bb2cded6fda6e5b2211cbe1d6a6cd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 08:02:34 +0700

    Sort array

--- src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
+++ src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
@@ -10,12 +10,27 @@ public class ReferensiArray
         {
             return IntStream.of(array).sum();
         }
+        public static int[] sortArray(int[] array)
+        {
+            Arrays.sort(array);
+            return array;
+        }
     }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
-        System.out.println("The sum is" + " " + arrayOperation.sumArray(array));
+        System.out.printf("%-30s,%d","The sum is",arrayOperation.sumArray(array));
+        System.out.println("The sorted array is");
+        int[] sortedArray = arrayOperation.sortArray(array);
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(sortedArray);
+            if (array.length - i != 1)
+            {
+                System.out.print(' ');
+            }
+        }
         input.close();
     }
 }
