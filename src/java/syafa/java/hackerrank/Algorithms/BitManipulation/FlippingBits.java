f2bf3aa1d97efde6d7b32c567ed06acdba8843b5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-29 06:26:52 +0700

    Loop

--- src/java/syafa/java/hackerrank/Algorithms/BitManipulation/FlippingBits.java
+++ src/java/syafa/java/hackerrank/Algorithms/BitManipulation/FlippingBits.java
@@ -1,3 +1,17 @@
-public class FlippingBits {
+package syafa.java.hackerrank.Algorithms.BitManipulation;
+import java.util.*;
 
+public class FlippingBits
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        int[] result = new int[testCase];
+        for (int i = 0; i < testCase; i++)
+        {
+            //
+        }
+        input.close();
+    }
 }
