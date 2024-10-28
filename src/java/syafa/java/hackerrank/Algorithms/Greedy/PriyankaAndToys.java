30846f2f606e51c8d64157a003390ab912a6c0e9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 07:01:16 +0700

    Add note

--- src/java/syafa/java/hackerrank/Algorithms/Greedy/PriyankaAndToys.java
+++ src/java/syafa/java/hackerrank/Algorithms/Greedy/PriyankaAndToys.java
@@ -6,6 +6,17 @@ public class PriyankaAndToys
 {
     public static void main(String[] args)
     {
+        /*
+         * 8
+         * 1 2 3 21 7 12 14 21
+         * 
+         * 4
+         * 
+         * Container 1 (1 2 3)  Range 1     5
+         * Container 2 (21 21)  Range 21    25
+         * Container 3 (7)      Range 7     11
+         * Container 4 (12 14)  Range 12    14
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
