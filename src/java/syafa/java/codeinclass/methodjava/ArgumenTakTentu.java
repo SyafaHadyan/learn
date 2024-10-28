da7ada6e4cc9e6969314984ef6ef41deaed3c937
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 07:36:28 +0700

    Input unknown amount

--- src/java/syafa/java/codeinclass/methodjava/ArgumenTakTentu.java
+++ src/java/syafa/java/codeinclass/methodjava/ArgumenTakTentu.java
@@ -1,5 +1,21 @@
 package syafa.java.codeinclass.methodjava;
+import java.util.*;
+import java.util.stream.*;
 
-public class ArgumenTakTentu {
-
+public class ArgumenTakTentu
+{
+    static class numberOperation
+    {
+        public static double average(double... numbers)
+        {
+            double average = 0d;
+            return average;
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] number = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        input.close();
+    }
 }
