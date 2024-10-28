439a1fa20615ba5590127e47406144d2794d021c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-28 07:57:32 +0700

    Initialize

--- src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
+++ src/java/syafa/java/codeinclass/methodjava/ReferensiArray.java
@@ -1,5 +1,12 @@
 package syafa.java.codeinclass.methodjava;
+import java.util.*;
+import java.util.stream.*;;
 
-public class ReferensiArray {
-
+public class ReferensiArray
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
