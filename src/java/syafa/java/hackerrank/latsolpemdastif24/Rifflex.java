2977af6208b44bcb907e235c2f212ea0a5803436
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 20:18:07 +0700

    Add note

--- src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
@@ -24,8 +24,15 @@ public class Rifflex
          * A  B  C  D  E  F  G  H
          * 2S 3S 4S 5S 2D 3D 4D 5D
          * 
-         * G  B  E  D  F  A  H  C 
+         * G  B  E  D  F  A  H  C
          * 4D 3S 2D 5S 3D 2S 5D 4S
+         * 
+         * 
+         * A  B  C  D  E  F  G  H
+         * 2S 3S 4S 5S 2D 3D 4D 5D
+         * 
+         * 
+         * 4S 2D 3S 5S 4D 3D 2S 5D
          */
         Scanner input = new Scanner(System.in);
         String[] card = input.nextLine().split("\s");
