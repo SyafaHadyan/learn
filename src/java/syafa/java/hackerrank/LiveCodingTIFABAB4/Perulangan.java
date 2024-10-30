d86d38b7f96d4112174afda9054f2a692ea146d4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 18:29:26 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB4/Perulangan.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB4/Perulangan.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LiveCodingTIFABAB4;
+import java.util.*;
 
-public class Perulangan {
-
+public class Perulangan
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
