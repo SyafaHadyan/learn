5105567c2ee9d451924d07e2ff926c19112a3c90
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 15:52:57 +0700

    Add

---
+++ src/java/syafa/java/hackerrank/certification/basic/Braces.java
@@ -0,0 +1,29 @@
+package syafa.java.hackerrank.certification.basic;
+//import java.util.*;
+//import java.util.Scanner;
+// Write your code here. DO NOT use an access modifier in your class declaration.
+/*
+class Parse
+{
+    static String isBalanced(String s) 
+    {
+        if ()
+    }
+    
+}
+class Solution
+{
+	
+	public static void main(String[] args) {
+		Parser parser = new Parser();
+        
+		Scanner in = new Scanner(System.in);
+
+		while (in.hasNext()) {
+			System.out.println(parser.isBalanced(in.next()));
+		}
+        
+		in.close();
+	}
+}
+*/
