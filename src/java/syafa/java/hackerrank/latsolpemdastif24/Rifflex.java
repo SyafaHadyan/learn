2e8837ab3e12b0b3af4beb252a9453cf0968b94f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 20:05:47 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
+import java.util.stream.*;
 
-public class Rifflex {
-
+public class Rifflex
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
