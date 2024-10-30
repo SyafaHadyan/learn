6782ab6bc70e7011873b6fb8c9effe06ef198fd3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 20:07:14 +0700

    Add note

--- src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/Rifflex.java
@@ -6,6 +6,21 @@ public class Rifflex
 {
     public static void main(String[] args)
     {
+        /*
+         * 2S 3S 4S 5S
+         * 
+         * Diberikan setumpuk kartu remi. Tugas Anda adalah untuk mengacak tumpukan kartu secara selang-seling kemudian secara "3x+1".
+         * Pengacakan "3x+1" sendiri adalah cara mengacak tumpukan kartu dengan menukar kartu sekarang dengan kartu ke-(3x+1)
+         * apabila posisi sekarang ganjil dan dengan kartu ke-(x/2) apabila posisi sekarang genap untuk semua kartu.
+         * 
+         * Kartu berupa string; ada empat macam kartu, Spade (S), Diamond (D), Heart (H), dan Club (C). Input bisa saja kosong.
+         * Output Format
+         * Kartu-kartu yang dibatasi dengan spasi
+         * 
+         * 2S 3S 4S 5S 2D 3D 4D 5D
+         * 
+         * 4D 3S 2D 5S 3D 2S 5D 4S
+         */
         Scanner input = new Scanner(System.in);
         input.close();
     }
