a0013e0cec8a30f2e65bc26c7dbf804fdbaa3368
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-30 18:34:47 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LiveCodingTIFABAB4/LED.java
+++ src/java/syafa/java/hackerrank/LiveCodingTIFABAB4/LED.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.LiveCodingTIFABAB4;
+import java.util.*;
+import java.util.stream.*;
 
-public class LED {
-
+public class LED
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
