c8193e1699ae5456ee53fcc708486cfa21b4b4c9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 13:46:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
@@ -7,6 +7,11 @@ public class Day29BitwiseAND
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < testCase; i++)
+        {
+            int[] bitwise = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
