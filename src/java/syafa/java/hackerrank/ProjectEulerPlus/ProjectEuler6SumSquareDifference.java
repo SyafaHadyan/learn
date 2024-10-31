b40a03d871d9e37e2c3849151e8eb3e0149b9556
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 16:31:48 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler6SumSquareDifference.java
+++ src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler6SumSquareDifference.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.ProjectEulerPlus;
+import java.util.*;
 
-public class ProjectEuler6SumSquareDifference {
-
+public class ProjectEuler6SumSquareDifference
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
