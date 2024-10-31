5cff32e90e0203b6f4f0990e2e691cf76301a32a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 13:47:11 +0700

    Class

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
@@ -4,6 +4,13 @@ import java.util.stream.*;
 
 public class Day29BitwiseAND
 {
+    static class bitOperation
+    {
+        public static int maxBitwise(int[] bitwise)
+        {
+            return 0;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
