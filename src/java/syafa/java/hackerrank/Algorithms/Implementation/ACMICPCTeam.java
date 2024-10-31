808813a5ecd53b0974aacd46c2da73eb7951a702
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-01 06:35:57 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ACMICPCTeam.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ACMICPCTeam.java
@@ -7,6 +7,12 @@ public class ACMICPCTeam
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[][] team = new int[config[0]][config[1]];
+        for (int i = 0; i < config[0]; i++)
+        {
+            team[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
