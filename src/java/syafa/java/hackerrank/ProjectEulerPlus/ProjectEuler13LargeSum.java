80580cce83cd205a2a9e15ec0f9d1d44d5b9e229
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 16:48:46 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler13LargeSum.java
+++ src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler13LargeSum.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.ProjectEulerPlus;
+import java.util.*;
+import java.math.*;
 
-public class ProjectEuler13LargeSum {
-
+public class ProjectEuler13LargeSum
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        input.close();
+    }
 }
