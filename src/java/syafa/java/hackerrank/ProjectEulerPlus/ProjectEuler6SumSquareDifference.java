b8d6165a272b432b7b372563bcddd6f63a8dbb79
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 16:32:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler6SumSquareDifference.java
+++ src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler6SumSquareDifference.java
@@ -6,6 +6,11 @@ public class ProjectEuler6SumSquareDifference
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < testCase; i++)
+        {
+            
+        }
         input.close();
     }
 }
