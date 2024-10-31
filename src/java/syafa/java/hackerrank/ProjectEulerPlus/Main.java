903d9088c35f137c7f897eb0b9e7190d1c95e49c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 16:12:01 +0700

    Rename file

--- src/java/syafa/java/hackerrank/ProjectEulerPlus/Main.java
+++
@@ -1,5 +0,0 @@
-package syafa.java.hackerrank.ProjectEulerPlus;
-
-public class Main {
-    
-}
---
+++ src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler1MultiplesOf3And5.java
@@ -0,0 +1,10 @@
+package syafa.java.hackerrank.ProjectEulerPlus;
+import java.util.*;
+
+public class ProjectEuler1MultiplesOf3And5
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
