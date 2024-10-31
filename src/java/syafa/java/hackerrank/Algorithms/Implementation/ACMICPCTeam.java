efb0316e16bfb5e91d303b63e90625b906fdecd7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-01 06:34:28 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ACMICPCTeam.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ACMICPCTeam.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
+import java.util.stream.*;
 
-public class ACMICPCTeam {
-
+public class ACMICPCTeam
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
