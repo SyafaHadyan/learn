296531622db6d1cf3fc36a4ad42ba25939c48a88
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 16:13:38 +0700

    Class

--- src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler1MultiplesOf3And5.java
+++ src/java/syafa/java/hackerrank/ProjectEulerPlus/ProjectEuler1MultiplesOf3And5.java
@@ -3,11 +3,22 @@ import java.util.*;
 
 public class ProjectEuler1MultiplesOf3And5
 {
+    static class getMultiple
+    {
+        public static int multipleOfTen(int number)
+        {
+            return 0;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int testCase = Integer.parseInt(input.nextLine());
         int[] result = new int[testCase];
+        for (int i = 0; i < result.length; i++)
+        {
+            
+        }
         input.close();
     }
 }
