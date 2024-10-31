91f077c5a8b8782ae5525df2d0552c879eeb2ebf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 13:45:24 +0700

    Initialize

--- src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
+++ src/java/syafa/java/hackerrank/ThirtyDaysOfCode/Day29BitwiseAND.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.ThirtyDaysOfCode;
+import java.util.*;
+import java.util.stream.*;
 
-public class Day29BitwiseAND {
-
+public class Day29BitwiseAND
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
