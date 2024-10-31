77204ae793d2651f6d6e4639ee8c8b43c23062c9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-10-31 07:50:31 +0700

    For loop test case

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TaumAndBday.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TaumAndBday.java
@@ -7,6 +7,12 @@ public class TaumAndBday
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        int[] result = new int[testCase];
+        for (int i = 0; i < testCase; i++)
+        {
+            //
+        }
         input.close();
     }
 }
