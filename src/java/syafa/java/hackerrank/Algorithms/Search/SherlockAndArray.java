425e4dd61d040692f054ea7a135dfe853f1036a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-01 09:04:20 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Search/SherlockAndArray.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/SherlockAndArray.java
@@ -1,3 +1,12 @@
-public class SherlockAndArray {
+package syafa.java.hackerrank.Algorithms.Search;
+import java.util.*;
+import java.util.stream.*;
 
+public class SherlockAndArray
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
