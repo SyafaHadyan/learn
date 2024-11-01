80bb317dd6779800a30b553c3265fcb740979a9b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-01 09:07:58 +0700

    Print result

--- src/java/syafa/java/hackerrank/Algorithms/Search/SherlockAndArray.java
+++ src/java/syafa/java/hackerrank/Algorithms/Search/SherlockAndArray.java
@@ -22,5 +22,13 @@ public class SherlockAndArray
             result[i] = arrayUtil.equalSum(Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray());
         }
         input.close();
+        for (int i = 0; i < result.length; i++)
+        {
+            System.out.print(result[i]);
+            if (result.length - i != 1)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
