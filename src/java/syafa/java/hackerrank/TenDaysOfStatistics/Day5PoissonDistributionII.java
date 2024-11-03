cffe0d89688eb3fcb8dbaf03feccc36ef1a38b24
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:33:16 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
@@ -7,6 +7,7 @@ public class Day5PoissonDistributionII
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        double[] machineOperationCost = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
     }
 }
