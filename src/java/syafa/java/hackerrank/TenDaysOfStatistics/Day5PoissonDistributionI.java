b9a7bc13608f85cd8c7f88aa9f06f3993ad78082
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:17:45 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionI.java
@@ -6,6 +6,8 @@ public class Day5PoissonDistributionI
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        double averageNumberOfSuccess = Double.parseDouble(input.nextLine());
+        double probabilityValue = Double.parseDouble(input.nextLine());
         input.close();
     }
 }
