cfdd8b6bd7216497771a7459b88825c98eebfe0a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:22:54 +0700

    Calculate factorial

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionI.java
@@ -8,7 +8,12 @@ public class Day5PoissonDistributionI
         Scanner input = new Scanner(System.in);
         double averageNumberOfSuccess = Double.parseDouble(input.nextLine());
         double probabilityValue = Double.parseDouble(input.nextLine());
+        double factorialOfProbabilityValue = 1d;
+        for (int i = (int) probabilityValue; i > 0; i--)
+        {
+            factorialOfProbabilityValue *= i;
+        }
         input.close();
-        System.out.printf("%.3f",(((Math.pow(averageNumberOfSuccess,probabilityValue)) * (Math.pow(Math.E,(-averageNumberOfSuccess)))) / ()));
+        System.out.printf("%.3f",(((Math.pow(averageNumberOfSuccess,probabilityValue)) * (Math.pow(Math.E,(-averageNumberOfSuccess)))) / ));
     }
 }
