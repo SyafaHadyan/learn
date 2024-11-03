79ed15dec5c7fc132e7e424371cc66226e78769f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 13:11:51 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4GeometricDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4GeometricDistributionII.java
@@ -7,6 +7,8 @@ public class Day4GeometricDistributionII
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] probabilityOfDefect = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int defectAt = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
