aaad85a94770596b35713e2bc309e62e4cfba9ba
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:32:28 +0700

    Initialize

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
+import java.util.stream.*;
 
-public class Day5PoissonDistributionII {
-
+public class Day5PoissonDistributionII
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
