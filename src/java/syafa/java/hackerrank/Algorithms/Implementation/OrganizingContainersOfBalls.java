709255fc526885eb36b6fa62bd91464923462226
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 09:08:42 +0700

    Class

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/OrganizingContainersOfBalls.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/OrganizingContainersOfBalls.java
@@ -4,10 +4,21 @@ import java.util.stream.*;
 
 public class OrganizingContainersOfBalls
 {
+    static class ballContainer
+    {
+        public static String sortContainer(int[][] ball)
+        {
+            return "Impossible";
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < testCase; i++)
+        {
+            //
+        }
         input.close();
     }
 }
