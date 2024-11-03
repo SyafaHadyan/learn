fd9e6f317b6f5c2bc4085525f3cded34724258a8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:35:05 +0700

    Calculate cost

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
@@ -9,5 +9,7 @@ public class Day5PoissonDistributionII
         Scanner input = new Scanner(System.in);
         double[] machineOperationCost = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
+        double dailyCostA = (160 + (40 * (Math.pow(machineOperationCost[0],2))));
+        double dailyCostB = (128 + (40 * (Math.pow(machineOperationCost[1],2))));
     }
 }
