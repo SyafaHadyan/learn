29c0af942527194d751e6c354199ef46fdb40540
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:45:36 +0700

    No var

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
@@ -9,18 +9,6 @@ public class Day5PoissonDistributionII
         Scanner input = new Scanner(System.in);
         double[] machineOperationCost = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
-        double dailyCostA = (160 + (40 * (Math.pow(machineOperationCost[0],2))));
-        double dailyCostB = (128 + (40 * (Math.pow(machineOperationCost[1],2))));
-        double factorialDailyCostA = 1d;
-        double factorialDailyCostB = 1d;
-        for (int i = (int) dailyCostA; i > 0; i--)
-        {
-            factorialDailyCostA *= i;
-        }
-        for (int i = (int) dailyCostB; i > 0; i--)
-        {
-            factorialDailyCostB *= i;
-        }
-        System.out.printf("%.3f\n%.3f",(((Math.pow(machineOperationCost[0],dailyCostA)) * (Math.pow(Math.E,(-machineOperationCost[0])))) / factorialDailyCostA),(((Math.pow(machineOperationCost[1],dailyCostB)) * (Math.pow(Math.E,(-machineOperationCost[1])))) / factorialDailyCostB));
+        System.out.printf("%.3f\n%.3f",(160 + (40 * (machineOperationCost[0] + (Math.pow(machineOperationCost[0],2))))),(128 + (40 * (machineOperationCost[1] + (Math.pow(machineOperationCost[1],2))))));
     }
 }
