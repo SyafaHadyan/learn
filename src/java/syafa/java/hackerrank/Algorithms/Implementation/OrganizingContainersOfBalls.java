54431a3ffc02b0cac7db4c30d90262e8f9a4e2d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 09:05:02 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/OrganizingContainersOfBalls.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/OrganizingContainersOfBalls.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
+import java.util.stream.*;
 
-public class OrganizingContainersOfBalls {
-
+public class OrganizingContainersOfBalls
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        input.close();
+    }
 }
