70f95e7edf1fe7c916ef1bfede34f9080589e5b8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-03 20:37:53 +0700

    Calculate factorial

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day5PoissonDistributionII.java
@@ -11,5 +11,16 @@ public class Day5PoissonDistributionII
         input.close();
         double dailyCostA = (160 + (40 * (Math.pow(machineOperationCost[0],2))));
         double dailyCostB = (128 + (40 * (Math.pow(machineOperationCost[1],2))));
+        double factorialDailyCostA = 1d;
+        double factorialDailyCostB = 1d;
+        for (int i = (int) dailyCostA; i > 0; i--)
+        {
+            factorialDailyCostA *= i;
+        }
+        for (int i = (int) dailyCostB; i > 0; i--)
+        {
+            factorialDailyCostB *= i;
+        }
+        System.out.printf("%.3f\n%.3f",);
     }
 }
