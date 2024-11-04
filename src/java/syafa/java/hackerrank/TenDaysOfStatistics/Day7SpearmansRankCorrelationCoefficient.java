b8f46da088297bac87c90de2f9b2f255e8c34e06
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 14:31:20 +0700

    Initialize

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day7SpearmansRankCorrelationCoefficient.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day7SpearmansRankCorrelationCoefficient.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
+import java.util.stream.*;
 
-public class Day7SpearmansRankCorrelationCoefficient {
-
+public class Day7SpearmansRankCorrelationCoefficient
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
