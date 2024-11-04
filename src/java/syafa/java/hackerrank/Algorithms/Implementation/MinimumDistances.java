af2b044be8745624a6b4d80e900ca8d127d796d9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 16:16:11 +0700

    Delete useless input

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MinimumDistances.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MinimumDistances.java
@@ -7,7 +7,8 @@ public class MinimumDistances
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int arrayLength = Integer.parseInt(input.nextLine());
+        StringBuilder useless = new StringBuilder(input.nextLine());
+        useless.delete(0, useless.length());
         int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
         int minDistance = Integer.MAX_VALUE;
