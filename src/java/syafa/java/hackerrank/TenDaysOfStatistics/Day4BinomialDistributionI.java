4398be5dee2831d239b1d7aa64430b51746cf56e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 21:29:19 +0700

    Factorial method

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionI.java
@@ -4,25 +4,24 @@ import java.util.stream.*;
 
 public class Day4BinomialDistributionI
 {
+    public static double factorial(int number)
+    {
+        double result = 1d;
+        for (int i = number; i > 0; i--)
+        {
+            result *= i;
+        }
+        return result;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         double[] data = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
         double ratioBoy = data[0] / (data[0] + data[1]);
-        double factorialK = 6;
-        double[] factorialN =
-        {
-            720,
-            120,
-            24,
-            6,
-            2,
-            1
-        };
         double result = 0d;
         int counter = 0;
-        for (int i = 3; i <= 6; i++)
+        for (int i = 3; i < 7; i++)
         {
             double factorial = 1d;
             for (int j = i; j > 0; j--)
@@ -30,7 +29,7 @@ public class Day4BinomialDistributionI
                 factorial *= j;
             }
             double tempResult = 720 / factorial * factorialN[counter];
-            result += tempResult * Math.pow(data[0],i) * Math.pow((1 - data[0]),6 - i);
+            result += tempResult * Math.pow(ratioBoy,i) * Math.pow((1 - ratioBoy),6 - i);
             counter++;
         }
         System.out.printf("%.3f",result);
