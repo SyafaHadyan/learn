afd1fb4d783a9fff0b1766d2b59977ec6c2bdc06
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 21:05:38 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionI.java
@@ -7,6 +7,7 @@ public class Day4BinomialDistributionI
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        double[] data = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
     }
 }
