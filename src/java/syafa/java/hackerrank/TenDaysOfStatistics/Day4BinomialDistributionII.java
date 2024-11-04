288b8c781f07c7355923f55396513b2b8d802fca
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 06:42:33 +0700

    Double factorial

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionII.java
@@ -13,16 +13,37 @@ public class Day4BinomialDistributionII
         }
         return result;
     }
+    public static double factorial(double number)
+    {
+        double result = 1d;
+        for (int i = (int) number; i > 0; i--)
+        {
+            result *= i;
+        }
+        return result;
+    }
     public static void main(String[] args)
     {
+        /*
+         * 12 10
+         * 
+         * 0.891
+         * 0.342
+         */
         Scanner input = new Scanner(System.in);
         double[] data = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
         double result = 0d;
-        for (int i = 3; i < 7; i++)
+        for (int i = 1; i <= 2; i++)
+        {
+            result += (factorial(10) / (factorial(i) * factorial(data[0] / 100))) * Math.pow((data[1] / (data[0] + data[1])),i) * Math.pow(1 - (data[1] / (data[0] + data[1])),10 - i);
+        }
+        System.out.printf("%.3f",result);
+        result = 0d;
+        for (int i = 1; i <= 10; i++)
         {
             result += (720 / (factorial(i) * factorial(6 - i))) * Math.pow((data[0] / (data[0] + data[1])),i) * Math.pow(1 - (data[0] / (data[0] + data[1])),6 - i);
         }
-        System.out.printf("%.3f",result);
+        System.out.printf("\n%.3f",result);
     }
 }
