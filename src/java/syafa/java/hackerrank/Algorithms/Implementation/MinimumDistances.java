669fc6a7d3e2a0db5b47de3cd68dd05fc2349ba2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 16:08:03 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/MinimumDistances.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/MinimumDistances.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
+import java.util.stream.*;
 
-public class MinimumDistances {
-
+public class MinimumDistances
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        input.close();
+    }
 }
