f52a5acce283019195bb4895a34d34ab520fe180
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 08:37:46 +0700

    Edit

--- src/java/syafa/java/hackerrank/latsolpemdastif24/Kelas.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/Kelas.java
@@ -1,5 +1,18 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class Kelas {
-
+class car
+{
+    static String carManufacturer = "Car Manfc";
+    static String carBrand = "CarBrand Name";
+    static String carManufacureYear = "2000";
+    static String carColor = "White";
+}
+public class Kelas
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
