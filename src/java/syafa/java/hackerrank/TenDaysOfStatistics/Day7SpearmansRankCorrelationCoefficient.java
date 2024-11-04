88e34dba7ad47fc830ff4ed6e37a0a58a7123968
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 14:44:22 +0700

    Calculate rank diff

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day7SpearmansRankCorrelationCoefficient.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day7SpearmansRankCorrelationCoefficient.java
@@ -11,6 +11,7 @@ public class Day7SpearmansRankCorrelationCoefficient
         double[] firstSet = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         double[] secondSet = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
         input.close();
+        double rankDifference = 0d;
         HashMap<Double,Integer> firstSetMap = new HashMap<Double,Integer>();
         HashMap<Double,Integer> secondSetMap = new HashMap<Double,Integer>();
         for (int i = 0; i < (firstSet.length + secondSet.length) / 2; i++)
@@ -20,5 +21,10 @@ public class Day7SpearmansRankCorrelationCoefficient
         }
         Arrays.sort(firstSet);
         Arrays.sort(secondSet);
+        for (int i = 0; i < (firstSet.length + secondSet.length) / 2; i++)
+        {
+            rankDifference += Math.pow((firstSetMap.get(firstSet[i]) - secondSetMap.get(secondSet[i])),2);
+        }
+        System.out.print(rankDifference);
     }
 }
