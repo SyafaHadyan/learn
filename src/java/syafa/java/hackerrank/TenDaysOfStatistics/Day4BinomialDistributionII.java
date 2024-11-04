2ca9b62dcd7dbc7ab69a0a7aa6e35a1d7b6b5aaf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 06:31:33 +0700

    Add from another file

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day4BinomialDistributionII.java
@@ -1,3 +1,28 @@
-public class Day4BinomialDistributionII {
+package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
+import java.util.stream.*;
 
+public class Day4BinomialDistributionII
+{
+    public static double factorial(int number)
+    {
+        double result = 1d;
+        for (int i = number; i > 0; i--)
+        {
+            result *= i;
+        }
+        return result;
+    }
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double[] data = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
+        input.close();
+        double result = 0d;
+        for (int i = 3; i < 7; i++)
+        {
+            result += (720 / (factorial(i) * factorial(6 - i))) * Math.pow((data[0] / (data[0] + data[1])),i) * Math.pow(1 - (data[0] / (data[0] + data[1])),6 - i);
+        }
+        System.out.printf("%.3f",result);
+    }
 }
