26406ef3db52422e312f6f8dd2694945ef36fad5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-04 20:18:41 +0700

    If already added, increment value

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day0MeanMedianAndMode.java
@@ -9,17 +9,31 @@ public class Day0MeanMedianAndMode
         Scanner input = new Scanner(System.in);
         int dataLength = Integer.parseInt(input.nextLine());
         double[] data = Stream.of(input.nextLine().split("\s")).mapToDouble(Double::parseDouble).toArray();
+        input.close();
         double mean = Arrays.stream(data).average().orElse(0.0);
         double median = 0d;
-        int maxValue = 0;
         int mode = 0;
-        input.close();
+        Set<Integer> modeCount = new HashSet<>();
+        HashMap<Integer,Integer> modeHash = new HashMap<>();
         Arrays.sort(data);
         median = (data[(data.length / 2) - 1] + data[data.length / 2]) / 2.0;
         if (data.length % 2 != 0)
         {
             median = data[(data.length / 2)];
         }
+        for (int i = 0; i < data.length; i++)
+        {
+            if (modeHash.get((int) data[i]) != null)
+            {
+                modeHash.merge((int) data[i],1, Integer::sum);
+                continue;
+            }
+        }
+        for (int i = 0; i < data.length; i++)
+        {
+            //
+        }
+        /*
         for (int i = data.length - 1; i >= 0; i--)
         {
             int currentMaxVal = 0;
@@ -36,6 +50,7 @@ public class Day0MeanMedianAndMode
                 mode = (int) data[i];
             }
         }
+        */
         System.out.printf("%.1f\n%.1f\n%d",mean,median,mode);
     }
 }
