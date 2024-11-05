dc9ecd7f761c3e15e88fc4f4853f73dc548f8fa6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 20:16:02 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremI.java
@@ -6,6 +6,10 @@ public class Day6TheCentralLimitTheoremI
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        double elevatorMaximumWeight = Double.parseDouble(input.nextLine());
+        double amountOfBox = Double.parseDouble(input.nextLine());
+        double meanWeight = Double.parseDouble(input.nextLine());
+        double standardDeviation = Double.parseDouble(input.nextLine());
         input.close();
     }
 }
