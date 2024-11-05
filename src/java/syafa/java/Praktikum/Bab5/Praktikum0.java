65245dac3da504824f87356558a2c5779dc3f9de
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 06:57:13 +0700

    Default var

--- src/java/syafa/java/Praktikum/Bab5/Praktikum0.java
+++ src/java/syafa/java/Praktikum/Bab5/Praktikum0.java
@@ -1,24 +1,36 @@
 package syafa.java.Praktikum.Bab5;
 import java.util.*;
 
+
 public class Praktikum0
 {
+    static final String WELCOME_MESSAGE = "Program Penghitung Kombinasi dan Permutasi";
+    static final String[] OPTION =
+    {
+        "Menghitung Permutasi",
+        "Menghitung Kombinasi",
+        "Keluar"
+    };
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int menuChoose = 0;
-        if (menuChoose == 1)
+        while (menuChoose != 3)
         {
-            //
-        }
-        else if (menuChoose == 2)
-        {
-            //
-        }
-        else if (menuChoose == 3)
-        {
-            input.close();
-            System.exit(0);
+            System.out.println();
+            if (menuChoose == 1)
+            {
+                //
+            }
+            else if (menuChoose == 2)
+            {
+                //
+            }
+            else if (menuChoose == 3)
+            {
+                input.close();
+                System.exit(0);
+            }
         }
     }
 }
