0e48fcc49958ef6adda5f33cd3b1cafa6e965e8a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 07:59:39 +0700

    Add

--- src/java/syafa/java/codeinclass/oop/House.java
+++ src/java/syafa/java/codeinclass/oop/House.java
@@ -7,4 +7,28 @@ public class House
     public double length;
     public double width;
     public double height;
+
+    public static void openDoor()
+    {
+        System.out.println("Open Door");
+    }
+    public static void closeDoor()
+    {
+        System.out.println("Close Door");
+    }
+    public static void openWindow()
+    {
+        System.out.println("Open Window");
+    }
+    public static void closeWindow()
+    {
+        System.out.println("Close Window");
+    }
+    public static void main(String[] args)
+    {
+        openDoor();
+        closeDoor();
+        openWindow();
+        closeWindow();
+    }
 }
