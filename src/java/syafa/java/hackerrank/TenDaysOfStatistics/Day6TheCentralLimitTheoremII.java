ee92881ddf76dadd78527e41872083aa45c43cbc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 18:56:04 +0700

    Add note

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremII.java
@@ -5,6 +5,12 @@ public class Day6TheCentralLimitTheoremII
 {
     public static void main(String[] args)
     {
+        /*
+         * 250
+         * 100
+         * 2.4
+         * 2.0
+         */
         Scanner input = new Scanner(System.in);
         double availableTicket = Double.parseDouble(input.nextLine());
         double amountOfStudents = Double.parseDouble(input.nextLine());
