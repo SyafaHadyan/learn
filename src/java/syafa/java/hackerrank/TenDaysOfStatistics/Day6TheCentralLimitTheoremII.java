5f6e15d792b61bd8139be377d7b6fc22cd3b4d92
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 18:55:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremII.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremII.java
@@ -1,5 +1,15 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
 
-public class Day6TheCentralLimitTheoremII {
-
+public class Day6TheCentralLimitTheoremII
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double availableTicket = Double.parseDouble(input.nextLine());
+        double amountOfStudents = Double.parseDouble(input.nextLine());
+        double ticketPurchaseMean = Double.parseDouble(input.nextLine());
+        double standardDeviation = Double.parseDouble(input.nextLine());
+        input.close();
+    }
 }
