ae35b96eb1eb3482452662937df83272d0eb197a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 06:54:25 +0700

    Main

--- src/java/syafa/java/Praktikum/Bab5/Praktikum0.java
+++ src/java/syafa/java/Praktikum/Bab5/Praktikum0.java
@@ -6,6 +6,19 @@ public class Praktikum0
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        input.close();
+        int menuChoose = 0;
+        if (menuChoose == 1)
+        {
+            //
+        }
+        else if (menuChoose == 2)
+        {
+            //
+        }
+        else if (menuChoose == 3)
+        {
+            input.close();
+            System.exit(0);
+        }
     }
 }
