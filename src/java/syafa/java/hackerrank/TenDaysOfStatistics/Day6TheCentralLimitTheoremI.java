d13f0c169297caedc426f0ac1c96a31ce4cedf0f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 20:14:54 +0700

    Initialize

--- src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremI.java
+++ src/java/syafa/java/hackerrank/TenDaysOfStatistics/Day6TheCentralLimitTheoremI.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.TenDaysOfStatistics;
+import java.util.*;
 
-public class Day6TheCentralLimitTheoremI {
-
+public class Day6TheCentralLimitTheoremI
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
