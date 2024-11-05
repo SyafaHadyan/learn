96635bf9a6bb2ca8323a0856850cb78935e1fa04
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-05 08:50:16 +0700

    Edit

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
@@ -7,37 +7,6 @@ public class ClimbingTheLeaderboard
     public static void main(String[] args)
     {
         /*
-         * 7
-         * 100 100 50 40 40 20 10
-         * 4
-         * 5 25 50 120
-         * 
-         * input rank
-         * 100 100 50 40 40 20 10 (should be array + 1)
-         * 
-         * use set which can only contain distinct values > return index at > store at currentPlayerPosition[i]
-         * 
-         * keep index when
-         * sort player
-         * 5 25 50 120
-         * 
-         * first iterate
-         * 
-         *     1   1   2  3  3  4  5  6
-         * 5 > 100 100 50 40 40 20 10 5
-         * currentPlayerPosition[0] = 6
-         * 
-         * second iterate
-         * 
-         *      1   1   2  3  3  4  5  6
-         * 25 > 100 100 50 40 40 25 20 10
-         * currentPlayerPosition[1] = 4
-         * 
-         * 6
-         * 4
-         * 2
-         * 1
-         * 
          * 6
          * 100 90 90 80 75 60
          * 5
