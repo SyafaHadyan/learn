ec1eab58bb28d27a4d84f339ddaa315b5a47ed2a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 09:13:01 +0700

    Prime funcd check

--- src/java/syafa/java/Praktikum/Bab5/Praktikum1.java
+++ src/java/syafa/java/Praktikum/Bab5/Praktikum1.java
@@ -3,9 +3,15 @@ import java.util.*;
 
 public class Praktikum1
 {
+    public static String checkPrime(int number)
+    {
+        return "Prima";
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int number = Integer.parseInt(input.nextLine());
+        System.out.printf("%d%s",number,"a");
         input.close();
     }
 }
