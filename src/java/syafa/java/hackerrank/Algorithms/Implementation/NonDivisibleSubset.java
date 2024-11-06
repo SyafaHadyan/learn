fd8c4af3119e93d423d7949198547a11ee9b6555
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 10:30:44 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
@@ -7,6 +7,8 @@ public class NonDivisibleSubset
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] data = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
