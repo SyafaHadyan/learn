808f4aba8c8fb0771358e7bccb15d0d7faf577f8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 19:27:08 +0700

    Edit print input

--- src/java/syafa/java/Praktikum/Bab5/N04MethodVoid.java
+++ src/java/syafa/java/Praktikum/Bab5/N04MethodVoid.java
@@ -1,5 +1,62 @@
 package syafa.java.Praktikum.Bab5;
+import java.util.*;
 
-public class N04MethodVoid {
+public class N04MethodVoid
+{
+        static Scanner in = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        int pilih = 3;
+        do
+        {
+            System.out.println("Program Menghitung Sederhana ");
+            System.out.println("Pilih salah satu :");
+            System.out.println("1. Volume Prisma Segitiga");
+            System.out.println("2. Volume Balok");
+            System.out.println("3. Luas, keliling, dan sisi miring segitiga (siku-siku)");
+            System.out.println("4. Keluar");
+            System.out.print("Masukkan Pilihan Anda : ");
+            pilih = in.nextInt();
+            switch (pilih)
+            {
+                case 1:
+                    volPrismaSegitiga();
+                    break;
+                case 2:
+                    volBalok();
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        while (pilih != 4);
+        in.close();
+    }
 
+    private static void volPrismaSegitiga()
+    {
+        float alas, tinggiSegitiga, luas, tinggiPrisma, volume;
+        System.out.print("Masukkan nilai panjang alas :");
+        alas = in.nextFloat();
+        System.out.print("Masukkan nilai tinggi segitiga :");
+        tinggiSegitiga = in.nextFloat();
+        luas = (alas * tinggiSegitiga) / 2;
+        System.out.print("Masukkan nilai tinggi prisma :");
+        tinggiPrisma = in.nextFloat();
+        volume = (luas * tinggiPrisma);
+        System.out.println("Volume Prisma Segitiga :" + volume);
+    }
+
+    private static void volBalok()
+    {
+        float panjang, lebar, tinggi, volume;
+        System.out.print("Masukkan panjang balok : ");
+        panjang = in.nextFloat();
+        System.out.print("Masukkan lebar balok : ");
+        lebar = in.nextFloat();
+        System.out.print("Masukkan tinggi balok : ");
+        tinggi = in.nextFloat();
+        volume = panjang * lebar * tinggi;
+        System.out.println("Volume balok : " + volume);
+    }
 }
