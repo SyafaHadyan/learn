ad0b024db80ae1d767eba2eb9f64056d57edf4b5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 10:41:56 +0700

    Counter

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
@@ -6,21 +6,41 @@ public class NonDivisibleSubset
 {
     public static void main(String[] args)
     {
+        /*
+         * STDIN    Function
+         * -----    --------
+         * 4 3      S[] size n = 4, k = 3
+         * 1 7 2 4  S = [1, 7, 2, 4]
+         * 
+         * 3
+         * 
+         * 4 3
+         * 1 7 2 4
+         */
         Scanner input = new Scanner(System.in);
         int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         int[] data = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
-        HashMap<Integer,Boolean> nonDivisibleSet = new HashMap<>();
+        HashMap<Integer,Integer> nonDivisibleSet = new HashMap<>();
+        int counter = 0;
         for (int i = 0; i < config[0]; i++)
         {
             for (int j = 1 + i; j < config[0]; j++)
             {
-                if (data[i] + data[j] % config[1] != 0)
+                if ((data[i] + data[j]) % config[1] != 0)
                 {
-                    nonDivisibleSet.putIfAbsent(data[i],true);
-                    nonDivisibleSet.putIfAbsent(data[j],true);
+                    nonDivisibleSet.putIfAbsent(data[i],data[j]);
+                    counter++;
+                }
+                else if ((data[i] + data[j]) % config[1] == 0)
+                {
+                    System.out.println(nonDivisibleSet);
+                    nonDivisibleSet.remove(data[i],data[j]);
+                    System.out.println(nonDivisibleSet);
+                    System.out.println("debug");
                 }
             }
         }
+        System.out.println(nonDivisibleSet);
     }
 }
