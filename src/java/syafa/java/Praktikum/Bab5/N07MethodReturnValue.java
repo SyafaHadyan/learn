26e59821706161422c992451a7d9d6b26528bb2b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 20:38:47 +0700

    Input age

--- src/java/syafa/java/Praktikum/Bab5/N07MethodReturnValue.java
+++ src/java/syafa/java/Praktikum/Bab5/N07MethodReturnValue.java
@@ -8,27 +8,31 @@ public class N07MethodReturnValue
         Scanner in = new Scanner(System.in);
         System.out.println("Program Pendataan Mahasiswa");
         System.out.println();
-        String nim1, nama1, nim2, nama2, jur1, jur2;
+        String nim1, nama1, nim2, nama2, jur1, jur2, umur1, umur2;
         System.out.print("Masukkan NIM Mahasiswa 1 : ");
         nim1 = in.nextLine();
         System.out.print("Masukkan Nama Mahasiswa 1 : ");
         nama1 = in.nextLine();
         System.out.print("Masukkan Jurusan Mahasiswa 1 : ");
         jur1 = in.nextLine();
+        System.out.print("Masukkan Umur Mahasiswa 1 : ");
+        umur1 = in.nextLine();
         System.out.print("Masukkan NIM Mahasiswa 2 : ");
         nim2 = in.nextLine();
         System.out.print("Masukkan Nama Mahasiswa 2 : ");
         nama2 = in.nextLine();
         System.out.print("Masukkan Jurusan Mahasiswa 2 : ");
         jur2 = in.nextLine();
+        System.out.print("Masukkan Umur Mahasiswa 2 : ");
+        umur2 = in.nextLine();
         in.close();
         System.out.println("Data Mahasiswa : ");
         System.out.println(showData(nim1, nama1, jur1));
         System.out.print(showData(nim2, nama2, jur2));
     }
-    public static String showData(String nim, String nama, String jurusan)
+    public static String showData(String nim, String nama, String jurusan, String umur)
     {
-        String show = "\nnim : " + nim + "\nnama : " + nama + "\nJurusan : " + jurusan;
+        String show = "\nnim : " + nim + "\nnama : " + nama + "\nJurusan : " + jurusan + "\nUmur : ";
         return show;
     }
 }
