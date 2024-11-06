e36d84ee7a44c3c783f1b6338d036c1c29b547ab
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-06 10:32:07 +0700

    Hashmap store non divisible set

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/NonDivisibleSubset.java
@@ -10,5 +10,13 @@ public class NonDivisibleSubset
         int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         int[] data = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        HashMap<Integer,Boolean> nonDivisibleSet = new HashMap<>();
+        for (int i = 0; i < config[0]; i++)
+        {
+            for (int j = 1 + i; j < config[0]; j++)
+            {
+                //
+            }
+        }
     }
 }
