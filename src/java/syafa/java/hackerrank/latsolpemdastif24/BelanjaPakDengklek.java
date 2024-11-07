e7a02e348b390b528ac773c81796a9dce0c1ec93
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 15:52:37 +0700

    Input data

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
@@ -6,6 +6,12 @@ public class BelanjaPakDengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        double firstProduct = Double.parseDouble(input.nextLine());
+        firstProduct *= Double.parseDouble(input.nextLine());
+        double secondProduct = Double.parseDouble(input.nextLine());
+        secondProduct *= Double.parseDouble(input.nextLine());
+        double thirdProduct = Double.parseDouble(input.nextLine());
+        thirdProduct *= Double.parseDouble(input.nextLine());
         input.close();
     }
 }
