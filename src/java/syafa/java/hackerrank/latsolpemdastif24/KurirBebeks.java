80f1d49a998dda0f0091e8d4e108fd1e236d9e56
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 19:36:39 +0700

    Input data and quit if lt 0

--- src/java/syafa/java/hackerrank/latsolpemdastif24/KurirBebeks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/KurirBebeks.java
@@ -6,6 +6,14 @@ public class KurirBebeks
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String deliveryOption = input.nextLine();
+        int numberOfItems = Integer.parseInt(input.nextLine());
+        int totalWeight = Integer.parseInt(input.nextLine());
+        if (numberOfItems <= 0 || totalWeight <= 0)
+        {
+            System.out.print("Pengiriman tidak dapat dilakukan.");
+            System.exit(0);
+        }
         input.close();
     }
 }
