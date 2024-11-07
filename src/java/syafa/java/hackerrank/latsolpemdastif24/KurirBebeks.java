ebc726d8a4aa2721ebe2534016aac9ebc9a9e58b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 19:33:30 +0700

    Initialzie

--- src/java/syafa/java/hackerrank/latsolpemdastif24/KurirBebeks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/KurirBebeks.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class KurirBebeks {
-
+public class KurirBebeks
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
