25e1f2a362da5277f877cc6ac375ccdf4e7bf25a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 07:08:29 +0700

    Swap val

--- src/java/syafa/java/Praktikum/Bab5/N12MethodReturnValueBerparameterArray.java
+++ src/java/syafa/java/Praktikum/Bab5/N12MethodReturnValueBerparameterArray.java
@@ -35,6 +35,18 @@ public class N12MethodReturnValueBerparameterArray
     }
     private static int[] sortKecilBesar(int[] array)
     {
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array.length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
         return array;
     }
 }
