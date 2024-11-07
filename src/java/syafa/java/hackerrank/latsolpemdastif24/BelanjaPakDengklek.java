4e155582677a486829482b5c752fdc21870691a8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 15:50:16 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class BelanjaPakDengklek {
-
+public class BelanjaPakDengklek
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
