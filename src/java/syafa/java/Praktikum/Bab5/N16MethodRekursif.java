b612ae17a6bf9398e9c2e2bc00cc4ea8a7fd4261
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 08:14:10 +0700

    Take user input

--- src/java/syafa/java/Praktikum/Bab5/N16MethodRekursif.java
+++ src/java/syafa/java/Praktikum/Bab5/N16MethodRekursif.java
@@ -1,5 +1,23 @@
 package syafa.java.Praktikum.Bab5;
+import java.util.*;
 
-public class N16MethodRekursif {
-
+public class N16MethodRekursif
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan bilangan : ");
+        int bilangan = Integer.parseInt(input.nextLine());
+        input.close();
+        int hasil;
+        hasil = faktorial(bilangan);
+        System.out.println("Nilai dari " + bilangan + "! adalah " + hasil);
+    }
+    private static int faktorial(int bil)
+    {
+        if (bil == 1)
+            return 1;
+        else
+            return (bil * faktorial(bil-1));
+    }
 }
