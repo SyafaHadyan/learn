2608a240b32b940fd9e6aed9a4dc4050814315cf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 18:03:38 +0700

    Check prime

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
@@ -19,6 +19,13 @@ public class BilanganPrimaPt3
     }
   	public static boolean isPrima(int x)
     {
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+            {
+                return false;
+            }
+        }
     	return true;
     }
 }
