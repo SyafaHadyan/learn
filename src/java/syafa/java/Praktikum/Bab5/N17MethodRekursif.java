72ba72083262d73361718427b228721a4b7ba49a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 08:15:40 +0700

    Initialize

--- src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
+++ src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
@@ -1,5 +1,11 @@
 package syafa.java.Praktikum.Bab5;
+import java.util.*;
 
-public class N17MethodRekursif {
-
+public class N17MethodRekursif
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
