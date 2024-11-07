4f5d0ac2cb07bafac0cd50c54301febc49b66006
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 17:06:40 +0700

    Print with pre suf

--- src/java/syafa/java/hackerrank/latsolpemdastif24/PrintLine.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/PrintLine.java
@@ -6,10 +6,11 @@ public class PrintLine
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        print(input.nextLine());
         input.close();
     }
   	public static void print(String kalimat)
     {
-    	
+    	System.out.print('#' + kalimat + '#');
     }
 }
