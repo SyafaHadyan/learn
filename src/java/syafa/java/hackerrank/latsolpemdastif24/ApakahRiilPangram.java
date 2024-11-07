6eae7d96c76a08def43b10a52ef8a4a471ec0b87
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 06:48:59 +0700

    Test regex

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
@@ -1,11 +1,14 @@
 package syafa.java.hackerrank.latsolpemdastif24;
 import java.util.*;
+import java.util.regex.*;
 
 public class ApakahRiilPangram
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String word = input.nextLine().replaceAll("\\W+","");
         input.close();
+        System.out.print(word);
     }
 }
