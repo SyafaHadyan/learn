bb0048efa1f8c421363943259b6c7045350b6250
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 16:06:23 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/RestoGKM.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/RestoGKM.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class RestoGKM {
-
+public class RestoGKM
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
