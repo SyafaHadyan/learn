5edc3bb55928b76414f98fc68df25c91da8c3669
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 18:00:51 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
@@ -1,5 +1,15 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.Scanner;
 
-public class BilanganPrimaPt3 {
-
+public class BilanganPrimaPt3
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+  	public static boolean isPrima(int x)
+    {
+    	return true;
+    }
 }
