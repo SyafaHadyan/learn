b88e248bbf82fc1503c2540a39d65013ea3b0e47
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 18:16:02 +0700

    Input data

--- src/java/syafa/java/hackerrank/latsolpemdastif24/LaundryBebeks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/LaundryBebeks.java
@@ -6,6 +6,27 @@ public class LaundryBebeks
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String customerName = input.nextLine();
+        int customerID = Integer.parseInt(input.nextLine());
+        int startHour = input.nextInt();
+        int startMinute = input.nextInt();
+        input.nextLine();
+        String itemName0 = input.next();
+        int itemPrice0 = input.nextInt();
+        int itemQuantity0 = input.nextInt();
+        input.nextLine();
+        String itemName1 = input.next();
+        int itemPrice1 = input.nextInt();
+        int itemQuantity1 = input.nextInt();
+        input.nextLine();
+        String itemName2 = input.next();
+        int itemPrice2 = input.nextInt();
+        int itemQuantity2 = input.nextInt();
+        input.nextLine();
+        String itemName3 = input.next();
+        int itemPrice3 = input.nextInt();
+        int itemQuantity3 = input.nextInt();
+        input.nextLine();
         input.close();
     }
 }
