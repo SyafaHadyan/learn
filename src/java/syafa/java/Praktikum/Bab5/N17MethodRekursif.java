3071167affa70f231571a3f20e0a1975a4dcab61
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 08:19:19 +0700

    Calculate fibonacci

--- src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
+++ src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
@@ -3,9 +3,23 @@ import java.util.*;
 
 public class N17MethodRekursif
 {
+    public static void fibonacci(int max)
+    {
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int currentNumber = 0;
+        for (int i = 0; i < max; i++)
+        {
+            currentNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = currentNumber;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan batas bilangan fibonacci" + " : ");
+        fibonacci(Integer.parseInt(input.nextLine()));
         input.close();
     }
 }
