af55193afde51b26944883528f053af21c1bf0c0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 17:53:11 +0700

    Add from another file

--- src/java/syafa/java/hackerrank/latsolpemdastif24/Fibonacci101.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/Fibonacci101.java
@@ -1,5 +1,30 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class Fibonacci101 {
-
+public class Fibonacci101
+{
+    public static void fibonacci(int firstNumber,int secondNumber,int max,int currentCounter)
+    {
+        currentCounter++;
+        int currentNumber = firstNumber + secondNumber;
+        if (currentCounter != max)
+        {
+            if (currentCounter == 1)
+            {
+                System.out.println("0\n1\n1");
+            }
+            else
+            {
+                System.out.println(currentNumber);
+            }
+            fibonacci(secondNumber,currentNumber,max,currentCounter);
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan batas bilangan fibonacci" + " : ");
+        fibonacci(0,1,Integer.parseInt(input.nextLine()),0);
+        input.close();
+    }
 }
