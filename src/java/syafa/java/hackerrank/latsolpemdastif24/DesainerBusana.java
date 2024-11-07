d2a77b503b3d0dd6bfa3d3d617d77dbd28c89abf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 15:02:43 +0700

    Static final var

--- src/java/syafa/java/hackerrank/latsolpemdastif24/DesainerBusana.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/DesainerBusana.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class DesainerBusana {
-
+public class DesainerBusana
+{
+    static final double[] CLOTH = {1.2,1.5,2.0};
+    static final double[] RIBBON = {0.8,1.0,1.3};
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
