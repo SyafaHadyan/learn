222a7c4f4f02d24e1292152e55e09c4daeb1c7a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 18:12:43 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/LaundryBebeks.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/LaundryBebeks.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class LaundryBebeks {
-
+public class LaundryBebeks
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
