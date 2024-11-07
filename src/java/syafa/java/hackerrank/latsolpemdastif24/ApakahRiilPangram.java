140f69ede5576702a9f719ae40d54338fd15c846
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 06:42:43 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.*;
 
-public class ApakahRiilPangram {
-
+public class ApakahRiilPangram
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
