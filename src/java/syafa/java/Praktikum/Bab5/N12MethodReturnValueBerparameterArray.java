55b78ab80337d50fd6ced5d684d4a8f6ed5db096
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 07:12:47 +0700

    Fix sort

--- src/java/syafa/java/Praktikum/Bab5/N12MethodReturnValueBerparameterArray.java
+++ src/java/syafa/java/Praktikum/Bab5/N12MethodReturnValueBerparameterArray.java
@@ -44,7 +44,7 @@ public class N12MethodReturnValueBerparameterArray
     {
         for (int i = 0; i < array.length; i++)
         {
-            for (int j = 0; j < array.length; j++)
+            for (int j = 0 + i; j < array.length; j++)
             {
                 if (array[i] > array[j])
                 {
