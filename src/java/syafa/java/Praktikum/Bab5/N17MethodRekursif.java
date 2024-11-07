0d20d703deef78582e082a0edda1b073553d0134
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 08:27:04 +0700

    Recursive call

--- src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
+++ src/java/syafa/java/Praktikum/Bab5/N17MethodRekursif.java
@@ -3,13 +3,16 @@ import java.util.*;
 
 public class N17MethodRekursif
 {
-    public static void fibonacci(int max)
+    public static void fibonacci(int firstNumber,int secondNumber,int max,int currentCounter)
     {
-        int firstNumber = 1;
-        int secondNumber = 1;
-        int currentNumber = 1;
-        System.out.println(firstNumber);
-        System.out.print(secondNumber);
+        currentCounter++;
+        int currentNumber = firstNumber + secondNumber;
+        if (currentCounter != max)
+        {
+            System.out.println(currentNumber);
+            fibonacci(secondNumber,currentNumber,max,currentCounter);
+        }
+        /*
         for (int i = 0; i < max - 2; i++)
         {
             currentNumber = firstNumber + secondNumber;
@@ -18,12 +21,13 @@ public class N17MethodRekursif
             System.out.print("\n");
             System.out.print(currentNumber);
         }
+        */
     }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan batas bilangan fibonacci" + " : ");
-        fibonacci(Integer.parseInt(input.nextLine()));
+        fibonacci(1,1,Integer.parseInt(input.nextLine()),1);
         input.close();
     }
 }
