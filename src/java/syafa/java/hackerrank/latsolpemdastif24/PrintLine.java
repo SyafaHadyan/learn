69daaf53e60560887e5387820aa029ea1e528aaa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 17:05:20 +0700

    Initialize

--- src/java/syafa/java/hackerrank/latsolpemdastif24/PrintLine.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/PrintLine.java
@@ -1,5 +1,15 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.Scanner;
 
-public class PrintLine {
-
+public class PrintLine
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+  	public static void print(String kalimat)
+    {
+    	
+    }
 }
