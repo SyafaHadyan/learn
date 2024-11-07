4d2e28843a31bf400e34a38381ca0bce23fa3106
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 14:48:34 +0700

    Create file

---
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/RerataArray.java
@@ -0,0 +1,15 @@
+package syafa.java.hackerrank.latsolpemdastif24;
+import java.util.Scanner;
+
+public class RerataArray
+{
+    public static void main(String[] args)
+    {
+        //
+    }
+  
+  	public static double hitungRerata(int[] arr)
+    {
+    	return 0d;
+    }
+}
