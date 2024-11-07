3a4ce710ca29a385eac07ca990bd3e3c57df2b0e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 16:07:22 +0700

    Input point

--- src/java/syafa/java/hackerrank/latsolpemdastif24/RestoGKM.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/RestoGKM.java
@@ -6,6 +6,7 @@ public class RestoGKM
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int point = 31 - (Integer.parseInt(input.nextLine()));
         input.close();
     }
 }
