666bf63496b45092d2814d6c23d5bdeff8567602
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 16:01:46 +0700

    Add currency

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
@@ -17,12 +17,12 @@ public class BelanjaPakDengklek
         input.close();
         System.out.printf
         (
-            "%s\n%s%-2c%.2f\n%s%-2c%.2f\n%s%-2c%.2f\n%s%-2c%.2f",
+            "%s\n%s%-2c%1s%.2f\n%s%-2c%1s%.2f\n%s%-2c%1s%.2f\n%s%-2c%1s%.2f",
             "Halo Pak Dengklek!",
-            "Total harga sebelum pajak",':',(firstProduct + secondProduct + thirdProduct),
-            "Jumlah pajak",':',(0.1 * (firstProduct + secondProduct + thirdProduct)),
-            "Jumlah diskon",':',(0.05 * (firstProduct + secondProduct + thirdProduct)),
-            "Total harga yang harus dibayarkan",':',((firstProduct + secondProduct + thirdProduct) + (0.1 * (firstProduct + secondProduct + thirdProduct)) - (0.05 * (firstProduct + secondProduct + thirdProduct)))
+            "Total harga sebelum pajak",':',"Rp",(firstProduct + secondProduct + thirdProduct),
+            "Jumlah pajak",':',"Rp",(0.1 * (firstProduct + secondProduct + thirdProduct)),
+            "Jumlah diskon",':',"Rp",(0.05 * (firstProduct + secondProduct + thirdProduct)),
+            "Total harga yang harus dibayarkan",':',"Rp",((firstProduct + secondProduct + thirdProduct) + (0.1 * (firstProduct + secondProduct + thirdProduct)) - (0.05 * (firstProduct + secondProduct + thirdProduct)))
         );
     }
 }
