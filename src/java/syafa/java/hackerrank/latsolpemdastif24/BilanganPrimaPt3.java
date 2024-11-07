1c6be1aedd10daae04e6ae486b9aebf269f84868
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 18:02:52 +0700

    Print if prime

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BilanganPrimaPt3.java
@@ -6,6 +6,15 @@ public class BilanganPrimaPt3
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int primeStart = Integer.parseInt(input.nextLine());
+        for (int i = 2; i <= primeStart;)
+        {
+            if (isPrima(i))
+            {
+                System.out.print(' ');
+                System.out.print(i);
+            }
+        }
         input.close();
     }
   	public static boolean isPrima(int x)
