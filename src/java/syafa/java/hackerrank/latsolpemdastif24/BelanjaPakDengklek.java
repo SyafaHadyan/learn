5e5572570093edab3e291424386980746f8e0d3e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-07 15:59:42 +0700

    Fix print

--- src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/BelanjaPakDengklek.java
@@ -7,6 +7,7 @@ public class BelanjaPakDengklek
     {
         
         Scanner input = new Scanner(System.in);
+        input.nextLine();
         double firstProduct = Double.parseDouble(input.nextLine());
         firstProduct *= Double.parseDouble(input.nextLine());
         double secondProduct = Double.parseDouble(input.nextLine());
@@ -16,12 +17,12 @@ public class BelanjaPakDengklek
         input.close();
         System.out.printf
         (
-            "%s\n%s%-2c%.2f\n%s%-2c%.2f\n%s%-2c%.2f",
+            "%s\n%s%-2c%.2f\n%s%-2c%.2f\n%s%-2c%.2f\n%s%-2c%.2f",
             "Halo Pak Dengklek!",
             "Total harga sebelum pajak",':',(firstProduct + secondProduct + thirdProduct),
-            "Jumlah pajak",':',(0.01 * (firstProduct + secondProduct + thirdProduct)),
+            "Jumlah pajak",':',(0.1 * (firstProduct + secondProduct + thirdProduct)),
             "Jumlah diskon",':',(0.05 * (firstProduct + secondProduct + thirdProduct)),
-            "Total harga yang harus dibayarkan",':',(0d)
+            "Total harga yang harus dibayarkan",':',((firstProduct + secondProduct + thirdProduct) + (0.1 * (firstProduct + secondProduct + thirdProduct)) - (0.05 * (firstProduct + secondProduct + thirdProduct)))
         );
     }
 }
