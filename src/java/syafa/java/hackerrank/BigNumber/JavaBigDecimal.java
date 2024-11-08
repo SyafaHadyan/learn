574781bc83ad130623661888ba558e7b451c5f9c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 18:12:02 +0700

    Initialize

--- src/java/syafa/java/hackerrank/BigNumber/JavaBigDecimal.java
+++ src/java/syafa/java/hackerrank/BigNumber/JavaBigDecimal.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.BigNumber;
+import java.util.*;
+import java.math.*;
 
-public class JavaBigDecimal {
-
+public class JavaBigDecimal
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
