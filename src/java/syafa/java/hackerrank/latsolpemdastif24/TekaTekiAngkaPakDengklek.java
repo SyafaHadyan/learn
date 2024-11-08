d1d8944cb635035857dec96a07984b80ffcff04f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 09:29:59 +0700

    Input data

--- src/java/syafa/java/hackerrank/latsolpemdastif24/TekaTekiAngkaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/TekaTekiAngkaPakDengklek.java
@@ -6,6 +6,10 @@ public class TekaTekiAngkaPakDengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        int firstNumber = Integer.parseInt(input.nextLine());
+        int secondNumber = Integer.parseInt(input.nextLine());
+        int thirdNumber = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
