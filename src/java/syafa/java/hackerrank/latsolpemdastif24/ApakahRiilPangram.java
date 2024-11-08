9bf197e140cdc4d7a60c8587b8c084e38ea2a8b4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 07:19:01 +0700

    Match digit

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
@@ -1,4 +1,5 @@
 package syafa.java.hackerrank.latsolpemdastif24;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.*;
 
@@ -7,8 +8,10 @@ public class ApakahRiilPangram
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        String[] word = input.nextLine().replaceAll("\\W+","").toLowerCase().split("");
+        String[] word = input.nextLine().replaceAll("[\\W+\\d+]","").toLowerCase().split("");
         input.close();
+        Arrays.sort(word);
+        ArrayList<String> inputWord = new ArrayList<>(Arrays.asList(word));
         ArrayList<String> validAlphabetList = new ArrayList<>(Arrays.asList("abcdefghijklmnopqrstuvwxyz".split("")));
         ArrayList<String> missingAlphabet = new ArrayList<>();
         for (int i = 0; i < word.length; i++)
@@ -17,7 +20,10 @@ public class ApakahRiilPangram
             {
                 continue;
             }
-            missingAlphabet.add(validAlphabetList.get(i));
+            for (int j = 0; j < word.length; j++)
+            {
+                
+            }
         }
         System.out.println(validAlphabetList);
         System.out.println(missingAlphabet);
