fc0719fd16eb7ecf637993752b5bf777b176f207
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 09:43:10 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/latsolpemdastif24/TekaTekiAngkaPakDengklek.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/TekaTekiAngkaPakDengklek.java
@@ -5,19 +5,6 @@ public class TekaTekiAngkaPakDengklek
 {
     public static void main(String[] args)
     {
-        /*
-         * Dengklek
-         * 5
-         * 3
-         * 2
-         * 
-         * Halo Pak Dengklek!
-         * Hasil perhitungan pertama: 14
-         * Hasil perhitungan kedua: 2
-         * Apakah hasil pertama lebih besar dari hasil kedua? true
-         * Apakah a lebih besar dari b dan b lebih besar dari c? false
-         * Apakah a sama dengan b atau b sama dengan c? true
-         */
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         int firstNumber = Integer.parseInt(input.nextLine());
