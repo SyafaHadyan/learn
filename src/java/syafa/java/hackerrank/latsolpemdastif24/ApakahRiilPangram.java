28980d3912cc7e456dbe7f73af9c9ff2cffcc7ff
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 07:01:13 +0700

    Remove if match

--- src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
+++ src/java/syafa/java/hackerrank/latsolpemdastif24/ApakahRiilPangram.java
@@ -7,9 +7,20 @@ public class ApakahRiilPangram
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        char[] word = input.nextLine().replaceAll("\\W+","").toLowerCase().toCharArray();
+        String[] word = input.nextLine().replaceAll("\\W+","").toLowerCase().split("-");
         input.close();
+        ArrayList<String> validAlphabetList = new ArrayList<>(Arrays.asList("abcdefghijklmnopqrstuvwxyz".split("")));
+        boolean isPerfectPangram = true;
         Arrays.sort(word);
-        System.out.print(word);
+        int counter = 0;
+        for (int i = 0; i < word.length; i++)
+        {
+            if (word[i].equalsIgnoreCase(validAlphabetList.get(counter)))
+            {
+                validAlphabetList.remove(counter);
+            }
+            counter++;
+        }
+        System.out.print(Arrays.toString(word));
     }
 }
