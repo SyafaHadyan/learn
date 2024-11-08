ba67394041c91929a7f785d2799f30033ed27504
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-08 18:07:26 +0700

    Initialize

--- src/java/syafa/java/hackerrank/BigNumber/JavaBigInteger.java
+++ src/java/syafa/java/hackerrank/BigNumber/JavaBigInteger.java
@@ -1,5 +1,22 @@
 package syafa.java.hackerrank.BigNumber;
+import java.util.*;
+import java.math.*;
 
-public class JavaBigInteger {
-
+public class JavaBigInteger
+{
+    /*
+     * 1234
+     * 20
+     * 
+     * 1254
+     * 24680
+     * 
+     * +
+     * *
+     */
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
