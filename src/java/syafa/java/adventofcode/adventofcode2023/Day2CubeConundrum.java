fee13614648120d34edcf920947c83dd9c41369b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-10 06:49:33 +0700

    Initialize

--- src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
@@ -1,5 +1,12 @@
 package syafa.java.adventofcode.adventofcode2023;
+import java.util.*;
+import java.util.stream.*;
 
-public class Day2CubeConundrum {
-
+public class Day2CubeConundrum
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
