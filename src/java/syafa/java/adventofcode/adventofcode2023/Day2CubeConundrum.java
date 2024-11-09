ca2f12e014d85f7a8add6a67823fa1af8a84b53c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-10 06:50:52 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
@@ -4,9 +4,15 @@ import java.util.stream.*;
 
 public class Day2CubeConundrum
 {
+    static final int[] CUBE_CONFIG = {12,13,14};
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int possibleIdSum = 0;
+        while (input.hasNextLine())
+        {
+            
+        }
         input.close();
     }
 }
