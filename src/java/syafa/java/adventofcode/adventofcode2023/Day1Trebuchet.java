8c7390b671a68c80918a0e6aaec061ffc98b7535
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-09 19:25:13 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2023/Day1Trebuchet.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day1Trebuchet.java
@@ -1,11 +1,17 @@
 package syafa.java.adventofcode.adventofcode2023;
 import java.util.*;
+import java.util.stream.*;
 
 public class Day1Trebuchet
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int sum = 0;
+        while (input.hasNextLine())
+        {
+            int[] inputData = Stream.of(input.nextLine().replaceAll("\\D+","").split("")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
