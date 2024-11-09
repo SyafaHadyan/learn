619c1dba02a5261b4d064bace1520b4b9e56e587
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-09 19:19:50 +0700

    Create file

---
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day1Trebuchet.java
@@ -0,0 +1,11 @@
+package syafa.java.adventofcode.adventofcode2023;
+import java.util.*;
+
+public class Day1Trebuchet
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
