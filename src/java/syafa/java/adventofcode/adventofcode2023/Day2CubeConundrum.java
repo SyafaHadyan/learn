3e30f149a99e3ed6e1e2ae7c7e28af875409543d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-10 08:56:53 +0700

    Remove comments and remove unused import

--- src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
@@ -1,6 +1,5 @@
 package syafa.java.adventofcode.adventofcode2023;
 import java.util.*;
-import java.util.regex.*;
 
 public class Day2CubeConundrum
 {
@@ -13,20 +12,6 @@ public class Day2CubeConundrum
         );
     public static void main(String[] args)
     {
-        /*
-         * Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-         * Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-         * Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-         * Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-         * Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-         * 
-         * only 12 red cubes, 13 green cubes, and 14 blue cubes
-         */
-        /*
-         * TODO
-         * 
-         * Split based on game set
-         */
         Scanner input = new Scanner(System.in);
         int possibleIdSum = 0;
         while (input.hasNextLine())
