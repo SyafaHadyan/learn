5bc0acf8fbd473b93f547f729306c4b0e5d353af
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-10 07:14:51 +0700

    Store config in map

--- src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day2CubeConundrum.java
@@ -4,7 +4,13 @@ import java.util.stream.*;
 
 public class Day2CubeConundrum
 {
-    static final int[] CUBE_CONFIG = {12,13,14};
+    //static final int[] CUBE_CONFIG = {12,13,14};
+    static final Map<String,Integer> CUBE_CONFIG = Map.ofEntries
+        (
+            Map.entry("Red",12),
+            Map.entry("Green",13),
+            Map.entry("Blue",14)
+        );
     public static void main(String[] args)
     {
         /*
@@ -13,20 +19,36 @@ public class Day2CubeConundrum
          * Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
          * Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
          * Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+         * 
+         * only 12 red cubes, 13 green cubes, and 14 blue cubes
          */
         Scanner input = new Scanner(System.in);
         int possibleIdSum = 0;
+        System.out.println(CUBE_CONFIG);
         while (input.hasNextLine())
         {
             int gameId = 0;
-            int tempRed = 0;
-            int tempGreen = 0;
-            int tempBlue = 0;
+            int[] currentCubeConfig = new int[3];
             String[] currentGame = input.nextLine().split("\\W+");
             for (int i = 2; i < (currentGame.length - 2); i += 2)
             {
-                //
+                //if ()
+                /*
+                if (currentGame[i].equalsIgnoreCase("Red"))
+                {
+                    //
+                }
+                else if (currentGame[i].equalsIgnoreCase("Green"))
+                {
+                    //
+                }
+                else if (currentGame[i].equalsIgnoreCase("Blue"))
+                {
+                    //
+                }
+                */
             }
+            //if ()
         }
         input.close();
     }
