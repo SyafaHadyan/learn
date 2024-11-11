f9a2ceb1314c360a430ba8b0120c010a11228c56
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-11 07:51:51 +0700

    Create file

---
+++ src/java/syafa/java/codeinclass/javaconstructor/Rectangle.java
@@ -0,0 +1,12 @@
+package syafa.java.codeinclass.javaconstructor;
+import java.util.*;
+
+public class
+
+public class Rectangle
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
