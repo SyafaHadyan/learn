a6be4790cfbbfea2fb5ef5bfcf18b0f8639bff38
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-11 07:53:22 +0700

    Initialize

--- src/java/syafa/java/codeinclass/javaconstructor/Rectangle.java
+++ src/java/syafa/java/codeinclass/javaconstructor/Rectangle.java
@@ -1,12 +1,21 @@
 package syafa.java.codeinclass.javaconstructor;
 import java.util.*;
 
-public class
-
 public class Rectangle
 {
+    double width;
+    double height;
+    public Rectangle()
+    {
+        //
+    }
+    public double getArea()
+    {
+        return this.width * this.height;
+    }
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
