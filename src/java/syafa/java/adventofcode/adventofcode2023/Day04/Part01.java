5751e8d15387889c73679b7d4fbbfafbe5644a0c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-13 19:47:45 +0700

    Input winning numbers

--- src/java/syafa/java/adventofcode/adventofcode2023/Day04/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day04/Part01.java
@@ -1,5 +1,33 @@
 package syafa.java.adventofcode.adventofcode2023.Day04;
+import java.util.*;
+import java.util.stream.*;
 
-public class Part01 {
-
+public class Part01
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine())
+        {
+            String card = input.next();
+            String cardNumber = input.next();
+            ArrayList<Integer> winningNumbers = new ArrayList<>();
+            try
+            {
+                winningNumbers.add(input.nextInt());
+            }
+            catch (NumberFormatException e)
+            {
+                //
+            }
+            /*
+            int[] winningNumbers = new int[5];
+            for (int i = 0; i < winningNumbers.length; i++)
+            {
+                
+            }
+            */
+        }
+        input.close();
+    }
 }
