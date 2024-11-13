79bada883e702955605aeb4f726455405c9f2478
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-13 19:50:31 +0700

    Print result

--- src/java/syafa/java/adventofcode/adventofcode2023/Day04/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day04/Part01.java
@@ -7,8 +7,10 @@ public class Part01
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int cardPoint = 0;
         while (input.hasNextLine())
         {
+            int currentCardPoint = 1;
             String card = input.next();
             String cardNumber = input.next();
             ArrayList<Integer> winningNumbers = new ArrayList<>();
@@ -20,6 +22,17 @@ public class Part01
             {
                 //
             }
+            while (input.hasNextInt())
+            {
+                if (winningNumbers.contains(input.nextInt()))
+                {
+                    currentCardPoint *= 2;
+                }
+            }
+            if (currentCardPoint > 1)
+            {
+                cardPoint += currentCardPoint;
+            }
             /*
             int[] winningNumbers = new int[5];
             for (int i = 0; i < winningNumbers.length; i++)
@@ -29,5 +42,6 @@ public class Part01
             */
         }
         input.close();
+        System.out.print(cardPoint);
     }
 }
