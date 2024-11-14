981f5bc125e8db576e2c905c544565ee1738fff0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-14 14:02:41 +0700

    Temp

--- src/java/syafa/java/stp/regresi/RegresiLinierSatuVariabelBebas.java
+++ src/java/syafa/java/stp/regresi/RegresiLinierSatuVariabelBebas.java
@@ -26,11 +26,11 @@ public class RegresiLinierSatuVariabelBebas
             sumX = sumX.add(x[i]);
             sumY = sumY.add(y[i]);
             sumXPowTwo = sumXPowTwo.add(sumX.pow(2));
-            sumXY = sumXY.add(sumX.multiply(sumY));
+            sumXY = sumXY.add(y.multiply(y));
         }
         b = (BigDecimal.valueOf(dataSet).multiply(sumXY).subtract(sumX.multiply(sumY))).divide(BigDecimal.valueOf(dataSet).multiply(sumXPowTwo).subtract(sumX.pow(2)),ROUNDING_MODE);
         a = (sumY.subtract(b.multiply(sumX))).divide(BigDecimal.valueOf(dataSet));
         input.close();
-        System.out.printf("Nilai A %.3f\nNilai B %.3f",a,b);
+        System.out.printf("Nilai A: %.3f\nNilai B: %.3f",a,b);
     }
 }
