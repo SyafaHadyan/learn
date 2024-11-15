ab00fdd6f83afc368aad9737c7441cbd8d212dad
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 11:10:26 +0700

    Change index and remove unused var

--- src/java/syafa/java/adventofcode/adventofcode2023/Day02/Part02.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day02/Part02.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Part02
 {
-    static final int[] CUBE_CONFIG = {12,13,14};
     static final Map<String,Integer> CUBE_CONFIG_INDEX = Map.ofEntries
         (
             Map.entry("RED",0),
@@ -16,10 +15,8 @@ public class Part02
         int maxSum = 0;
         while (input.hasNextLine())
         {
-            //boolean possibleConfig = true;
             String rawInput = input.nextLine();
-            //int currentGameId = Integer.parseInt(rawInput.split("\s")[1].replace(":",""));
-            int[] maxColorCombination = new int[CUBE_CONFIG.length];
+            int[] maxColorCombination = new int[CUBE_CONFIG_INDEX.size()];
             int currentSum = 1;
             rawInput = rawInput.replaceAll("Game \\d+","");
             rawInput = rawInput.replaceAll(": ","");
@@ -27,7 +24,7 @@ public class Part02
             String[] currentGame = rawInput.split(";\\s*");
             for (int i = 0; i < currentGame.length; i++)
             {
-                int[] currentCubeConfig = new int[CUBE_CONFIG.length];
+                int[] currentCubeConfig = new int[CUBE_CONFIG_INDEX.size()];
                 String[] currentGameSet = currentGame[i].split("\s");
                 for (int j = 0; j < currentGameSet.length; j += 2)
                 {
@@ -37,21 +34,8 @@ public class Part02
                         maxColorCombination[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] = currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())];
                     }
                 }
-                /*
-                for (int j = 0; j < currentGameSet.length; j += 2)
-                {
-                    currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] += Integer.parseInt(currentGameSet[j]);
-                }
-                for (int j = 0; j < currentCubeConfig.length; j++)
-                {
-                    if (currentCubeConfig[j] > CUBE_CONFIG[j])
-                    {
-                        possibleConfig = false;
-                    }
-                }
-                */
             }
-            for (int i = 0; i < CUBE_CONFIG.length; i++)
+            for (int i = 0; i < CUBE_CONFIG_INDEX.size(); i++)
             {
                 currentSum *= maxColorCombination[i];
             }
