b05439a4bac2dd866fac922f13cdbd1ec0267300
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 16:41:52 +0700

    For loop convert to game board flat

--- src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
+++ src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
@@ -9,12 +9,30 @@ public class SnakesAndLadders
     static final int PLAYER_AMOUNT = 2;
     static int[][] gameBoard = new int[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
     static int[] gameBoardFlat = new int[GAME_BOARD_SIZE * GAME_BOARD_SIZE];
+    static int[] playerPosition = new int[PLAYER_AMOUNT];
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i < GAME_BOARD_SIZE; i++)
+        for (int i = 0; i < gameBoard.length; i++)
         {
             gameBoard[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        }
+        for (int i = 1; i <= gameBoard.length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < gameBoard[i].length; j++)
+                {
+                    //
+                }
+            }
+            else if (i % 2 != 0)
+            {
+                for (int j = gameBoard[i].length - 1; j >= 0; j--)
+                {
+                    //
+                }
+            }
         }
         input.close();
     }
