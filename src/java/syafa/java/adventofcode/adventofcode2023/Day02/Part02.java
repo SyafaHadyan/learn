89ceccadee6e57396f7eda924c2b1122743595eb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 11:06:12 +0700

    Calculate max pos

--- src/java/syafa/java/adventofcode/adventofcode2023/Day02/Part02.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day02/Part02.java
@@ -13,12 +13,14 @@ public class Part02
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int possibleIdSum = 0;
+        int maxSum = 0;
         while (input.hasNextLine())
         {
-            boolean possibleConfig = true;
+            //boolean possibleConfig = true;
             String rawInput = input.nextLine();
-            int currentGameId = Integer.parseInt(rawInput.split("\s")[1].replace(":",""));
+            //int currentGameId = Integer.parseInt(rawInput.split("\s")[1].replace(":",""));
+            int[] maxColorCombination = new int[CUBE_CONFIG.length];
+            int currentSum = 1;
             rawInput = rawInput.replaceAll("Game \\d+","");
             rawInput = rawInput.replaceAll(": ","");
             rawInput = rawInput.replaceAll(",","");
@@ -27,6 +29,15 @@ public class Part02
             {
                 int[] currentCubeConfig = new int[CUBE_CONFIG.length];
                 String[] currentGameSet = currentGame[i].split("\s");
+                for (int j = 0; j < currentGameSet.length; j += 2)
+                {
+                    currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] = Integer.parseInt(currentGameSet[j]);
+                    if (maxColorCombination[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] < currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())])
+                    {
+                        maxColorCombination[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] = currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())];
+                    }
+                }
+                /*
                 for (int j = 0; j < currentGameSet.length; j += 2)
                 {
                     currentCubeConfig[CUBE_CONFIG_INDEX.get(currentGameSet[j + 1].toUpperCase())] += Integer.parseInt(currentGameSet[j]);
@@ -38,13 +49,15 @@ public class Part02
                         possibleConfig = false;
                     }
                 }
+                */
             }
-            if (possibleConfig)
+            for (int i = 0; i < CUBE_CONFIG.length; i++)
             {
-                possibleIdSum += currentGameId;
+                currentSum *= maxColorCombination[i];
             }
+            maxSum += currentSum;
         }
         input.close();
-        System.out.print(possibleIdSum);
+        System.out.print(maxSum);
     }
 }
