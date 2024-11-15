ba544ac09c344aa649ccb9bdc5c775412a5ab500
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 13:45:09 +0700

    Check left part

--- src/java/syafa/java/adventofcode/adventofcode2023/Day03/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day03/Part01.java
@@ -30,18 +30,53 @@ public class Part01
         {
             int j = 0;
             boolean performSymbolCheck = false;
-            while (engine[i][j] != null)
+            int digitLength = 0;
+            while (engine[i][j].equalsIgnoreCase(null))
             {
+                if (engine[i][j].equalsIgnoreCase(null))
+                {
+                    j = 0;
+                }
                 StringBuilder numberStruct = new StringBuilder();
+                boolean isEngineSchematic = false;
                 while (isDigit(engine[i][j]))
                 {
                     performSymbolCheck = true;
                     numberStruct.append(engine[i][j]);
+                    digitLength++;
                     j++;
                 }
                 if (performSymbolCheck)
                 {
-                    //
+                    boolean first = true;
+                    for (int k = j - digitLength; k < j + digitLength; k++)
+                    {
+                        if (first)
+                        {
+                            for (int l = 0; l < 6; l++)
+                            {
+                                for (int m = i - 1; m < engine.length; m++)
+                                {
+                                    try
+                                    {
+                                        if (!(engine[m][j - 1].equalsIgnoreCase(".")))
+                                        {
+                                            isEngineSchematic = true;
+                                        }
+                                    }
+                                    catch (IndexOutOfBoundsException e)
+                                    {
+                                        //
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (isEngineSchematic)
+                    {
+                        engineSchematicSum += Integer.parseInt(String.valueOf(numberStruct));
+                    }
+                    continue;
                 }
                 j++;
             }
