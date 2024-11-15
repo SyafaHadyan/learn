c91edaef21a5292a6fccc637b23dbdfba5a7b35b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 10:08:22 +0700

    Remove comments

--- src/java/syafa/java/adventofcode/adventofcode2023/Day01/Part02.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day01/Part02.java
@@ -4,20 +4,6 @@ import java.util.stream.*;
 
 public class Part02
 {
-    /*
-    static final Map<String,Integer> NUMBER_WORD = Map.of
-    (
-        "one",1,
-        "two",2,
-        "three",3,
-        "four",4,
-        "five",5,
-        "six",6,
-        "seven",7,
-        "eight",8,
-        "nine",9
-    );
-    */
     static final String[] NUMBER_WORD =
     {
         "one",
