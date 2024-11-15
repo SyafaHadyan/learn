68a01f50aecb88024976067d727b4060f74c67b5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 16:37:28 +0700

    Initialize game setting

--- src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
+++ src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
@@ -1,11 +1,21 @@
 package syafa.java.codingquest.practice2022;
 import java.util.*;
+import java.util.stream.*;
 
 public class SnakesAndLadders
 {
+    static final int GAME_BOARD_SIZE = 6;
+    static final int GAME_ROUND = 20;
+    static final int PLAYER_AMOUNT = 2;
+    static int[][] gameBoard = new int[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+    static int[] gameBoardFlat = new int[GAME_BOARD_SIZE * GAME_BOARD_SIZE];
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        for (int i = 0; i < GAME_BOARD_SIZE; i++)
+        {
+            
+        }
         input.close();
     }
 }
