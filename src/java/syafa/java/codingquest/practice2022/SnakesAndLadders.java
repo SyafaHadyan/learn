41478d7d0f5c62212a28cf8a4479faeee150fcc4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-15 16:34:21 +0700

    Initialize

--- src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
+++ src/java/syafa/java/codingquest/practice2022/SnakesAndLadders.java
@@ -1,5 +1,11 @@
 package syafa.java.codingquest.practice2022;
+import java.util.*;
 
-public class SnakesAndLadders {
-
+public class SnakesAndLadders
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
