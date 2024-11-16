270eef55bfda1e3dc87a4685ba17b5456a2f6409
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 18:28:27 +0700

    Advance scanner

--- src/java/syafa/java/adventofcode/adventofcode2023/Day05/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2023/Day05/Part01.java
@@ -1,11 +1,21 @@
 package syafa.java.adventofcode.adventofcode2023.Day05;
 import java.util.*;
+import java.util.stream.*;
 
 public class Part01
 {
+    static Scanner input = new Scanner(System.in);
+    static HashMap<Integer,Integer> seedToDestination = new HashMap<>();
+    public static void advanceScanner()
+    {
+        input.nextLine();
+        input.nextLine();
+    }
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        input.next();
+        int[] seedToPlant = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        advanceScanner();
         input.close();
     }
 }
