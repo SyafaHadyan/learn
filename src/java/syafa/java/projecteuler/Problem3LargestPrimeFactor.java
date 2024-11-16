5bdd78136bb4f31e6119075653a2f19c0b034655
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 20:10:37 +0700

    If factor and prime

--- src/java/syafa/java/projecteuler/Problem3LargestPrimeFactor.java
+++ src/java/syafa/java/projecteuler/Problem3LargestPrimeFactor.java
@@ -4,8 +4,22 @@ import java.math.*;
 public class Problem3LargestPrimeFactor
 {
     static final BigInteger NUMBER = new BigInteger("13195");
+    public static boolean checkPrimeFactor(BigInteger number)
+    {
+        for (int i = 0; i < 0; i++)
+        {
+            //
+        }
+        return true;
+    }
     public static void main(String[] args)
     {
-        //
+        for (BigInteger i = BigInteger.ONE; i.compareTo(NUMBER) < 0; i = i.add(BigInteger.ONE))
+        {
+            if (NUMBER.divideAndRemainder(i)[1].compareTo(BigInteger.ZERO) == 0 && i.isProbablePrime(10))
+            {
+                //
+            }
+        }
     }
 }
