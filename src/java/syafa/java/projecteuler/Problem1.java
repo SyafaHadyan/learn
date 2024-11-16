44e22885769ca113b9a6162a4ff83e66c4b28681
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 18:45:31 +0700

    Initialize

--- src/java/syafa/java/projecteuler/Problem1.java
+++ src/java/syafa/java/projecteuler/Problem1.java
@@ -1,5 +1,11 @@
 package syafa.java.projecteuler;
+import java.util.*;
 
-public class Problem1 {
-
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
