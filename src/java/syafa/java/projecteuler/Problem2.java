16a2856e74a07651b75844d46bf9e13ef24155d4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 18:58:23 +0700

    If even number, add to sum

--- src/java/syafa/java/projecteuler/Problem2.java
+++ src/java/syafa/java/projecteuler/Problem2.java
@@ -6,9 +6,16 @@ public class Problem2
     static final BigInteger MAX_FIBONACCI_SEQUENCE_VALUE = new BigInteger("4000000");
     public static void main(String[] args)
     {
-        for (BigInteger i = BigInteger.ZERO; i.compareTo(MAX_FIBONACCI_SEQUENCE_VALUE) < 0; i.add(BigInteger.ONE));
+        BigInteger fibonacciSum = BigInteger.ZERO;
+        BigInteger firstNumber = BigInteger.ONE;
+        BigInteger secondNumber = BigInteger.TWO;
+        BigInteger currentNumber = firstNumber.add(secondNumber);
+        for (BigInteger i = BigInteger.TWO; i.compareTo(MAX_FIBONACCI_SEQUENCE_VALUE) < 0; i.add(BigInteger.ONE));
         {
-            //
+            if (currentNumber.divideAndRemainder(BigInteger.TWO)[1].compareTo(BigInteger.ZERO) == 0)
+            {
+                fibonacciSum = fibonacciSum.add(currentNumber);
+            }
         }
     }
 }
