a6c5d52933e77535f18a393f9454ae9760578514
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 20:04:49 +0700

    Create file

---
+++ src/java/syafa/java/projecteuler/Problem3LargestPrimeFactor.java
@@ -0,0 +1,11 @@
+package syafa.java.projecteuler;
+import java.math.*;
+
+public class Problem3LargestPrimeFactor
+{
+    static final BigInteger NUMBER = new BigInteger("13195");
+    public static void main(String[] args)
+    {
+        //
+    }
+}
