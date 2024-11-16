6b2f248a4897fc5414e0f45db68696ca2373431f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 18:59:53 +0700

    Print result

--- src/java/syafa/java/projecteuler/Problem2.java
+++ src/java/syafa/java/projecteuler/Problem2.java
@@ -3,19 +3,23 @@ import java.math.*;
 
 public class Problem2
 {
-    static final BigInteger MAX_FIBONACCI_SEQUENCE_VALUE = new BigInteger("4000000");
+    static final BigInteger MAX_FIBONACCI_SEQUENCE_VALUE = new BigInteger("10");
     public static void main(String[] args)
     {
         BigInteger fibonacciSum = BigInteger.ZERO;
         BigInteger firstNumber = BigInteger.ONE;
         BigInteger secondNumber = BigInteger.TWO;
         BigInteger currentNumber = firstNumber.add(secondNumber);
-        for (BigInteger i = BigInteger.TWO; i.compareTo(MAX_FIBONACCI_SEQUENCE_VALUE) < 0; i.add(BigInteger.ONE));
+        for (BigInteger i = BigInteger.TWO; i.compareTo(MAX_FIBONACCI_SEQUENCE_VALUE) < 0; i = i.add(BigInteger.ONE));
         {
             if (currentNumber.divideAndRemainder(BigInteger.TWO)[1].compareTo(BigInteger.ZERO) == 0)
             {
                 fibonacciSum = fibonacciSum.add(currentNumber);
             }
+            firstNumber = secondNumber;
+            secondNumber = currentNumber;
+            currentNumber = firstNumber.add(secondNumber);
         }
+        System.out.print(fibonacciSum);
     }
 }
