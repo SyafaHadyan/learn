cb2184311250956f95b04967bf9893081504dca2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 18:54:00 +0700

    Iterate through

--- src/java/syafa/java/projecteuler/Problem2.java
+++ src/java/syafa/java/projecteuler/Problem2.java
@@ -1,9 +1,14 @@
 package syafa.java.projecteuler;
+import java.math.*;
 
 public class Problem2
 {
+    static final BigInteger MAX_FIBONACCI_SEQUENCE_VALUE = new BigInteger("4000000");
     public static void main(String[] args)
     {
-        //
+        for (BigInteger i = BigInteger.ZERO; i.compareTo(MAX_FIBONACCI_SEQUENCE_VALUE) < 0; i.add(BigInteger.ONE));
+        {
+            //
+        }
     }
 }
