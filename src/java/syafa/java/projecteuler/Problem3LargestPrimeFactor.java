ccfb5646ebe52125f7752cbd6503ec8eed33b333
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 20:13:03 +0700

    Compare and add change val

--- src/java/syafa/java/projecteuler/Problem3LargestPrimeFactor.java
+++ src/java/syafa/java/projecteuler/Problem3LargestPrimeFactor.java
@@ -4,22 +4,16 @@ import java.math.*;
 public class Problem3LargestPrimeFactor
 {
     static final BigInteger NUMBER = new BigInteger("13195");
-    public static boolean checkPrimeFactor(BigInteger number)
-    {
-        for (int i = 0; i < 0; i++)
-        {
-            //
-        }
-        return true;
-    }
     public static void main(String[] args)
     {
+        BigInteger maxPrimeFactor = BigInteger.ZERO;
         for (BigInteger i = BigInteger.ONE; i.compareTo(NUMBER) < 0; i = i.add(BigInteger.ONE))
         {
             if (NUMBER.divideAndRemainder(i)[1].compareTo(BigInteger.ZERO) == 0 && i.isProbablePrime(10))
             {
-                //
+                maxPrimeFactor = i;
             }
         }
+        System.out.print(maxPrimeFactor);
     }
 }
