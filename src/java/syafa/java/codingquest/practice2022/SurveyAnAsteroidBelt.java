f2c39845d79589a8645e4c79b8b73ec01cf44bb6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 10:02:27 +0700

    Initialize

--- src/java/syafa/java/codingquest/practice2022/SurveyAnAsteroidBelt.java
+++ src/java/syafa/java/codingquest/practice2022/SurveyAnAsteroidBelt.java
@@ -1,5 +1,11 @@
 package syafa.java.codingquest.practice2022;
+import java.util.*;
 
-public class SurveyAnAsteroidBelt {
-
+public class SurveyAnAsteroidBelt
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
