26335ae80e7b8aad5e6543e8416069d4762abe21
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-16 10:06:59 +0700

    Int asteroid oc

--- src/java/syafa/java/codingquest/practice2022/SurveyAnAsteroidBelt.java
+++ src/java/syafa/java/codingquest/practice2022/SurveyAnAsteroidBelt.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class SurveyAnAsteroidBelt
 {
+    static final int SPACE_GRID = 8;
+    static int[][] asteroidOccupation = new int[SPACE_GRID][SPACE_GRID];
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        for (int i = 0; i < asteroidOccupation.length; i++)
+        {
+            for (int j = 0; j < asteroidOccupation[i].length; j++)
+            {
+                asteroidOccupation[i][j] = input.nextInt();
+            }
+            input.nextLine();
+        }
         input.close();
     }
 }
