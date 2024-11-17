8bc91997eec3db2a93fc478df44542f5a4502c0d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-17 15:57:39 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/FlatlandSpaceStations.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/FlatlandSpaceStations.java
@@ -7,6 +7,8 @@ public class FlatlandSpaceStations
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] cityConfig = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] stationConfig = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
