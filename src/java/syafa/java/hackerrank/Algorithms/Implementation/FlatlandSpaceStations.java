4f1e4424bcd26f8fa9f8c50fd7443bc12b60eeda
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-17 16:05:28 +0700

    Change city with stationto 1

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/FlatlandSpaceStations.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/FlatlandSpaceStations.java
@@ -8,7 +8,19 @@ public class FlatlandSpaceStations
     {
         Scanner input = new Scanner(System.in);
         int[] cityConfig = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
-        int[] stationConfig = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] stationConfig = new int[cityConfig[0]];
         input.close();
+        int maxCityDistance = Integer.MIN_VALUE;
+        for (int i = 0; i < cityConfig[1]; i++)
+        {
+            stationConfig[input.nextInt()] = 1;
+        }
+        for (int i = 0; i < cityConfig[0]; i++)
+        {
+            for (int j = 0; j < stationConfig.length; j++)
+            {
+                
+            }
+        }
     }
 }
