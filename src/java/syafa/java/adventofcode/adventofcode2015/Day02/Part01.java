0bcbcb2e517518b3ac5994bdbbf8a070a662c23e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 18:19:48 +0700

    Fix additional

--- src/java/syafa/java/adventofcode/adventofcode2015/Day02/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day02/Part01.java
@@ -4,14 +4,15 @@ import java.util.stream.*;
 
 public class Part01
 {
+    static final int MAX_BOX_INPUT = 1000;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int wrappingPaperSize = 0;
-        while (input.hasNextLine())
+        for (int i = 0; i < MAX_BOX_INPUT; i++)
         {
             int[] boxDimension = Stream.of(input.nextLine().split("x")).mapToInt(Integer::parseInt).toArray();
-            wrappingPaperSize += ((2 * boxDimension[0] * boxDimension[1]) + (2 * boxDimension[1] * boxDimension[2]) + (2 * boxDimension[2] * boxDimension[0]));
+            wrappingPaperSize += ((2 * boxDimension[0] * boxDimension[1]) + (2 * boxDimension[1] * boxDimension[2]) + (2 * boxDimension[2] * boxDimension[0]) + (boxDimension[0] * boxDimension[1]));
         }
         input.close();
         System.out.print(wrappingPaperSize);
