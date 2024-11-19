6d71f42b358bd0bf0bf1afe8901961c179f6a4c1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 17:17:45 +0700

    Map entry pr val

--- src/java/syafa/java/adventofcode/adventofcode2015/Day01/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day01/Part01.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class Part01
 {
+    static final Map<String,Integer> STAIRCASE_SYMBOL = Map.ofEntries
+    (
+        Map.entry("(",1),
+        Map.entry(")",-1)
+    );
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
