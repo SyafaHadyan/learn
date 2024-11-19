a93191f57244009da3a3b128a037d81751d1e188
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 18:13:35 +0700

    Calculate wrapping size for each box

--- src/java/syafa/java/adventofcode/adventofcode2015/Day02/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day02/Part01.java
@@ -1,11 +1,18 @@
 package syafa.java.adventofcode.adventofcode2015.Day02;
 import java.util.*;
+import java.util.stream.*;
 
 public class Part01
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int wrappingPaperSize = 0;
+        while (input.hasNextLine())
+        {
+            int[] boxDimension = Stream.of(input.nextLine().split("x")).mapToInt(Integer::parseInt).toArray();
+            wrappingPaperSize += ((2 * boxDimension[0] * boxDimension[1]) + (2 * boxDimension[1] * boxDimension[2]) + (2 * boxDimension[2] * boxDimension[0]));
+        }
         input.close();
     }
 }
