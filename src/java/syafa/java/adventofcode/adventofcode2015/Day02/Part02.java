e1fc4e944d9ba8e86b6088d3c65ea1f926cbfdeb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 18:38:39 +0700

    Add from part 1

---
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day02/Part02.java
@@ -0,0 +1,22 @@
+package syafa.java.adventofcode.adventofcode2015.Day02;
+import java.util.*;
+import java.util.stream.*;
+
+public class Part02
+{
+    static final int MAX_BOX_INPUT = 1000;
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int wrappingPaperSize = 0;
+        for (int i = 0; i < MAX_BOX_INPUT; i++)
+        {
+            int[] boxDimension = Stream.of(input.nextLine().split("x")).mapToInt(Integer::parseInt).toArray();
+            wrappingPaperSize += ((2 * boxDimension[0] * boxDimension[1]) + (2 * boxDimension[1] * boxDimension[2]) + (2 * boxDimension[2] * boxDimension[0]));
+            Arrays.sort(boxDimension);
+            wrappingPaperSize += (boxDimension[0] * boxDimension[1]);
+        }
+        input.close();
+        System.out.print(wrappingPaperSize);
+    }
+}
