b394170b46143498e844c66ce58b4688f4b233fa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 17:23:11 +0700

    Calculate floor

--- src/java/syafa/java/adventofcode/adventofcode2015/Day01/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day01/Part01.java
@@ -13,9 +13,11 @@ public class Part01
         Scanner input = new Scanner(System.in);
         char[] inputFloor = input.nextLine().toCharArray();
         input.close();
+        int floor = 0;
         for (int i = 0; i < inputFloor.length; i++)
         {
-            //
+            floor += STAIRCASE_SYMBOL.get(inputFloor[i]);
         }
+        System.out.print(floor);
     }
 }
