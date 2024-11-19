a9b453ff20de9140708268166eda8b1240aa8e5d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-19 19:04:57 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2015/Day03/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day03/Part01.java
@@ -6,6 +6,14 @@ public class Part01
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        char[] move = input.nextLine().toCharArray();
         input.close();
+        int houseVisitCount = 1;
+        int[] currentPosition = new int[2];
+        HashMap<String,Boolean> visitedHouse = new HashMap<>();
+        for (int i = 0; i < move.length; i++)
+        {
+            //
+        }
     }
 }
