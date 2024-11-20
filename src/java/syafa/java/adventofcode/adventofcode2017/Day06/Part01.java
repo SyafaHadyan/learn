f713b0c94438955f31025e7b171208baedc89648
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 19:00:18 +0700

    Choose highest block

--- src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
@@ -9,5 +9,17 @@ public class Part01
         Scanner input = new Scanner(System.in);
         int[] memoryBlock = Stream.of(input.nextLine().split("\s++")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        HashMap<String,Boolean> memoryBlockDistribution = new HashMap<>;
+        while (true)
+        {
+            int choosenBlock = 0;
+            for (int i = 0; i < memoryBlock.length; i++)
+            {
+                if (memoryBlock[i] > choosenBlock)
+                {
+                    choosenBlock = memoryBlock[i];
+                }
+            }
+        }
     }
 }
