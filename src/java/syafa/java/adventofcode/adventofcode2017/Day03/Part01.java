e65d3ef417940706d69a176de2e046e44525c158
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 15:43:27 +0700

    Add note

--- src/java/syafa/java/adventofcode/adventofcode2017/Day03/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day03/Part01.java
@@ -5,6 +5,13 @@ public class Part01
 {
     public static void main(String[] args)
     {
+        /*
+         * 17  16  15  14  13
+         * 18   5   4   3  12
+         * 19   6   1   2  11
+         * 20   7   8   9  10
+         * 21  22  23  24  25
+         */
         Scanner input = new Scanner(System.in);
         int numberRequest = Integer.parseInt(input.nextLine());
         input.close();
