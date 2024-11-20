48d59b3143e264173ca1bbeb97573e300266b31d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 15:40:01 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2017/Day03/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day03/Part01.java
@@ -6,6 +6,16 @@ public class Part01
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberRequest = Integer.parseInt(input.nextLine());
         input.close();
+        int step = 0;
+        int currentNumber = numberRequest;
+        int ring = (int) Math.ceil(Math.sqrt(numberRequest));
+        System.out.println(ring);
+        while (currentNumber != 1)
+        {
+            currentNumber--;
+            step++;
+        }
     }
 }
