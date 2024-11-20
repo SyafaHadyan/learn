ccf470dfa9dd2602c5ea7dec6971fe248ba4c878
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 16:37:44 +0700

    Move with index

--- src/java/syafa/java/adventofcode/adventofcode2017/Day05/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day05/Part01.java
@@ -7,14 +7,25 @@ public class Part01
     {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> instruction = new ArrayList<>();
+        int step = 0;
         while (input.hasNextLine())
         {
             instruction.add(Integer.parseInt(input.nextLine()));
         }
         input.close();
-        for (int i = 0; i < instruction.size(); i++)
+        int index = 0;
+        while (true)
         {
-            //
+            int instructionSet = 0;
+            try
+            {
+                instructionSet = instruction.get(index);
+                instruction.set(index,instruction.get(index) + 1);
+            }
+            catch (IndexOutOfBoundsException e)
+            {
+                System.out.println(step);
+            }
         }
     }
 }
