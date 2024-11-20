eead079eef6ec37402dcf9e6e725aaff54fb6873
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 20:05:39 +0700

    Temp

--- src/java/syafa/java/adventofcode/adventofcode2017/Day08/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day08/Part01.java
@@ -1,14 +1,22 @@
 package syafa.java.adventofcode.adventofcode2017.Day08;
 import java.util.*;
 
+@SuppressWarnings("unused")
 public class Part01
 {
+    static final Map<String,Integer> INSTRUCTION_LIST = Map.ofEntries
+    (
+        Map.entry("inc",1),
+        Map.entry("dec",-1)
+    );
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int largestRegister = 0;
+        HashMap<Character,Integer> register = new HashMap<>();
         while (input.hasNextLine())
         {
-            //
+            String[] instructionInput = input.nextLine().split("\s");
         }
         input.close();
     }
