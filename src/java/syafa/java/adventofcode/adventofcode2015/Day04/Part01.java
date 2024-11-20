6af8a15db23cbc2769bfde46293987a12f15b4f6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 10:34:47 +0700

    Initialize

--- src/java/syafa/java/adventofcode/adventofcode2015/Day04/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2015/Day04/Part01.java
@@ -1,5 +1,12 @@
 package syafa.java.adventofcode.adventofcode2015.Day04;
+import java.util.*;
+import java.security.*;
 
-public class Part01 {
-
+public class Part01
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
