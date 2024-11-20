2f400b28c1aca54caca8fafab5dc470bcc906514
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 11:05:18 +0700

    If current digit matches next digit

--- src/java/syafa/java/adventofcode/adventofcode2017/Day01/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day01/Part01.java
@@ -1,5 +1,21 @@
 package syafa.java.adventofcode.adventofcode2017.Day01;
+import java.util.*;
+import java.util.stream.*;
 
-public class Part01 {
-
+public class Part01
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] captchaDigit = Stream.of(input.nextLine().split("")).mapToInt(Integer::parseInt).toArray();
+        input.close();
+        int validDigitSum = 0;
+        for (int i = 0; i < captchaDigit.length; i += 2)
+        {
+            if (captchaDigit[i] == captchaDigit[i + 1])
+            {
+                validDigitSum++;`
+            }
+        }
+    }
 }
