040a66eceaa65b33a306e06aec3f84c42134f72f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 16:07:29 +0700

    Add from part1

---
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day04/Part02.java
@@ -0,0 +1,28 @@
+package syafa.java.adventofcode.adventofcode2017.Day04;
+import java.util.*;
+
+public class Part02
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int validPassphrase = 0;
+        while (input.hasNextLine())
+        {
+            ArrayList<String> passphraseInput = new ArrayList<>();
+            Set<String> passphraseNoDuplicate = new HashSet<>();
+            String[] tempInput = input.nextLine().split("\s");
+            for (int i = 0; i < tempInput.length; i++)
+            {
+                passphraseInput.add(tempInput[i]);
+                passphraseNoDuplicate.add(tempInput[i]);
+            }
+            if (passphraseInput.size() == passphraseNoDuplicate.size())
+            {
+                validPassphrase++;
+            }
+        }
+        input.close();
+        System.out.println(validPassphrase);
+    }
+}
