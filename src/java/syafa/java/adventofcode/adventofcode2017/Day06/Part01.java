7b617b40ce5fa02e34c96fe7b01125ffe8f34cd0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 18:58:15 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
@@ -1,5 +1,13 @@
 package syafa.java.adventofcode.adventofcode2017.Day06;
+import java.util.*;
+import java.util.stream.*;
 
-public class Part01 {
-
+public class Part01
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] memoryBlock = Stream.of(input.nextLine().split("\s++")).mapToInt(Integer::parseInt).toArray();
+        input.close();
+    }
 }
