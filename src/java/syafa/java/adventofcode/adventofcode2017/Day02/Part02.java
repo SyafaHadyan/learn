d35b2a84887f07ea311eb054430528219dc96b56
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 11:29:12 +0700

    Add from first part

---
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day02/Part02.java
@@ -0,0 +1,20 @@
+package syafa.java.adventofcode.adventofcode2017.Day02;
+import java.util.*;
+import java.util.stream.*;
+
+public class Part02
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int checkSum = 0;
+        while (input.hasNextLine())
+        {
+            int[] rowInput = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(rowInput);
+            checkSum += (rowInput[rowInput.length - 1] - rowInput[0]);
+        }
+        input.close();
+        System.out.println(checkSum);
+    }
+}
