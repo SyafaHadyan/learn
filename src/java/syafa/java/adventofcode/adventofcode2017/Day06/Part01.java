7d7988968308e177b130474b4ca705afbb8a19ad
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 19:12:20 +0700

    Fix block index val

--- src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day06/Part01.java
@@ -9,36 +9,37 @@ public class Part01
         Scanner input = new Scanner(System.in);
         int[] memoryBlock = Stream.of(input.nextLine().split("\s++")).mapToInt(Integer::parseInt).toArray();
         input.close();
-        int distributionLoop = 0;
+        int distributionLoop = 1;
         HashMap<String,Boolean> memoryBlockDistribution = new HashMap<>();
         while (true)
         {
-            int choosenBlock = 0;
+            int[] choosenBlock = {0,0};
             for (int i = 0; i < memoryBlock.length; i++)
             {
-                if (memoryBlock[i] > choosenBlock)
+                if (memoryBlock[i] > choosenBlock[1])
                 {
-                    choosenBlock = i;
+                    choosenBlock[0] = i;
+                    choosenBlock[1] = memoryBlock[i];
                 }
             }
             for (int i = 0; i < memoryBlock.length; i++)
             {
-                if (i != choosenBlock)
+                if (i != choosenBlock[0])
                 {
-                    memoryBlock[i] += memoryBlock[choosenBlock] / (memoryBlock.length - 1);
-                }
-                else
-                {
-                    memoryBlock[i] = memoryBlock[i] % (memoryBlock.length - 1);
+                    memoryBlock[i] += memoryBlock[choosenBlock[0]] / (memoryBlock.length - 1);
                 }
             }
+            memoryBlock[choosenBlock[0]] = memoryBlock[choosenBlock[0]] % (memoryBlock.length - 1);
             if (memoryBlockDistribution.get(Arrays.toString(memoryBlock)) == null)
             {
                 memoryBlockDistribution.put(Arrays.toString(memoryBlock),true);
+                distributionLoop++;
+                continue;
             }
             else
             {
                 System.out.println(distributionLoop);
+                return;
             }
         }
     }
