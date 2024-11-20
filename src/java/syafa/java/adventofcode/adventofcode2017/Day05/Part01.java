309abebc4f8b5f1cd907a469dbe1ccc3dba15a5c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 16:31:36 +0700

    Input data

--- src/java/syafa/java/adventofcode/adventofcode2017/Day05/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day05/Part01.java
@@ -6,6 +6,15 @@ public class Part01
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        ArrayList<Integer> instruction = new ArrayList<>();
+        while (input.hasNextLine())
+        {
+            instruction.add(Integer.parseInt(input.nextLine()));
+        }
         input.close();
+        for (int i = 0; i < instruction.size(); i++)
+        {
+            //
+        }
     }
 }
