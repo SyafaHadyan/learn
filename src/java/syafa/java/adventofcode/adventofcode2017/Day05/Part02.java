8b04839b0003e883d23ad64607af57860964ad74
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-20 18:21:11 +0700

    Add from part1

---
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day05/Part02.java
@@ -0,0 +1,34 @@
+package syafa.java.adventofcode.adventofcode2017.Day05;
+import java.util.*;
+
+public class Part02
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> instruction = new ArrayList<>();
+        int step = 0;
+        while (input.hasNextLine())
+        {
+            instruction.add(Integer.parseInt(input.nextLine()));
+        }
+        input.close();
+        int index = 0;
+        int previousIndex = 0;
+        while (true)
+        {
+            try
+            {
+                step++;
+                instruction.set(previousIndex,instruction.get(previousIndex) + 1);
+                previousIndex += index;
+                index = instruction.get(previousIndex);
+            }
+            catch (IndexOutOfBoundsException e)
+            {
+                System.out.println(step);
+                return;
+            }
+        }
+    }
+}
