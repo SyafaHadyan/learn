b71cf06510523621d75defe3363ad9f545098796
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-21 19:24:42 +0700

    Initialize

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RiffleShufflerString.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RiffleShufflerString.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.uappemrogramandasar2023tifa;
+import java.util.*;
 
-public class RiffleShufflerString {
-
+public class RiffleShufflerString
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
