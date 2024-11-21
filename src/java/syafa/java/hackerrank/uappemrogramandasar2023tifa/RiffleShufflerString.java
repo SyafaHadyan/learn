f0c6741a3585a1c3fdde27e721abaae44cc938f1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-21 19:36:06 +0700

    Second level separate

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RiffleShufflerString.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RiffleShufflerString.java
@@ -10,6 +10,10 @@ public class RiffleShufflerString
         input.close();
         String firstString = "";
         String secondString = "";
+        String firstStringFirst = "";
+        String secondStringFirst = "";
+        String firstStringSecond = "";
+        String secondStringSecond = "";
         for (int i = 0; i < (inputString.length / 2) + (inputString.length % 2); i++)
         {
             firstString = firstString.concat(inputString[i]);
@@ -19,5 +23,14 @@ public class RiffleShufflerString
             secondString = secondString.concat(inputString[i]);
         }
         System.out.println(firstString + "\n" + secondString);
+        for (int i = 0; i < (firstString.length() / 2) + (firstString.length() % 2); i++)
+        {
+            firstStringFirst = firstStringFirst.concat(String.valueOf(firstString.charAt(i)));
+        }
+        for (int i = (firstString.length() / 2) + (firstString.length() % 2); i < firstString.length(); i++)
+        {
+            secondStringFirst = secondStringFirst.concat(String.valueOf(firstString.charAt(i)));
+        }
+        System.out.println(firstStringFirst + "\n" + secondStringFirst);
     }
 }
