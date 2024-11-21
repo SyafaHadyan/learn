8705cc2a593be978cd582fdb6b35d04840fb54ee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-21 20:45:39 +0700

    Add note

--- src/java/syafa/java/eling/tugas/NilaiAkhirPemdas.java
+++ src/java/syafa/java/eling/tugas/NilaiAkhirPemdas.java
@@ -7,6 +7,16 @@ class Mahasiswa
 }
 public class NilaiAkhirPemdas
 {
+    /*
+     * 4
+     * nafi 90
+     * yoga 80
+     * toni 75
+     * azel 60
+     * 
+     * Rata-rata: 76.25
+     * Praktikan dengan nilai di atas rata-rata: yoga nafi
+     */
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
