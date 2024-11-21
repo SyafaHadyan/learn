4bce594430d44a1516f74871c252f1ccc66696e2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-21 21:49:58 +0700

    Remove comments

--- src/java/syafa/java/eling/tugas/NilaiAkhirPemdas.java
+++ src/java/syafa/java/eling/tugas/NilaiAkhirPemdas.java
@@ -63,16 +63,6 @@ class Student
 }
 public class NilaiAkhirPemdas
 {
-    /*
-     * 4
-     * nafi 90
-     * yoga 80
-     * toni 75
-     * azel 60
-     * 
-     * Rata-rata: 76.25
-     * Praktikan dengan nilai di atas rata-rata: yoga nafi
-     */
     public static void main(String[] args)
     {
         Student studentDataTransfer = new Student();
