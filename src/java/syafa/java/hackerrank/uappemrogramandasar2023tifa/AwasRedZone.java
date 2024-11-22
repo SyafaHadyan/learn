95678f748ecd4be55cd0c8e99e729320815ca8a5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 19:38:35 +0700

    Input data

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
@@ -1,5 +1,15 @@
 package syafa.java.hackerrank.uappemrogramandasar2023tifa;
+import java.util.*;
+import java.util.stream.*;
 
-public class AwasRedZone {
-
+public class AwasRedZone
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] firstCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] secondCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] currentCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        input.close();
+    }
 }
