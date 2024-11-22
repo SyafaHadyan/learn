6e87131c09c30d2d48fe7960c93828fa358e33df
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 12:26:35 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC1JamDengklek {
-
+public class LC1JamDengklek
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
