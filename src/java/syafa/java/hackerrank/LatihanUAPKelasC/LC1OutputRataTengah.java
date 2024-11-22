cb70105b5d5bdecf5ca23138a83528273dfc7ecf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 12:21:02 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1OutputRataTengah.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1OutputRataTengah.java
@@ -1,5 +1,13 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC1OutputRataTengah {
-
+public class LC1OutputRataTengah
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+        int space = Integer.parseInt(input.nextLine());
+        input.close();
+    }
 }
