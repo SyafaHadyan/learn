0bf902eb66d04a5e738515ed2f9d0f7101cb58fa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 16:28:09 +0700

    Input to hashmap

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
@@ -10,7 +10,13 @@ public class LC3LombaDengklek
         HashMap<String,Integer> participantWinAmount = new HashMap<>();
         for (int i = 0; i < participantAmount; i++)
         {
-            //
+            String tempInput = input.nextLine();
+            if (participantWinAmount.get(tempInput) == null)
+            {
+                participantWinAmount.put(tempInput,1);
+                continue;
+            }
+            participantWinAmount.merge(tempInput,1,Integer::sum);
         }
         input.close();
     }
