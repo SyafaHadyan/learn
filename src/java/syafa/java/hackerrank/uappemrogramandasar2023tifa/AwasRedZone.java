629ab31c153e0c1e3769b72049758eef22a49f6c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 19:49:34 +0700

    Temp

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
@@ -11,16 +11,18 @@ public class AwasRedZone
         int[] secondCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         int[] currentCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
-        for (int i = 0; i < currentCoordinate.length; i++)
+        if (firstCoordinate[0] + firstCoordinate[1] < secondCoordinate[0] + secondCoordinate[1])
         {
-            
+            int[] tempSwap = {secondCoordinate[0],secondCoordinate[1]};
+            secondCoordinate[0] = firstCoordinate[0];
+            secondCoordinate[1] = firstCoordinate[1];
+            firstCoordinate[0] = tempSwap[0];
+            firstCoordinate[1] = tempSwap[1];
         }
         if
         (
-            (currentCoordinate[0] <= firstCoordinate[0] ||
-            currentCoordinate[1] <= firstCoordinate[1]) &&
-            (currentCoordinate[0] >= secondCoordinate[0] ||
-            currentCoordinate[1] >= secondCoordinate[1])
+            (currentCoordinate[0] <= firstCoordinate[0] && currentCoordinate[1] <= firstCoordinate[1]) &&
+            (currentCoordinate[0] >= secondCoordinate[0] && currentCoordinate[1] >= secondCoordinate[1])
         )
         {
             System.out.println("Bahaya");
