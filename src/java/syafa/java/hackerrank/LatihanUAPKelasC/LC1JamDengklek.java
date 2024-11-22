da03fdb96bfe65be4712a9093e8242cfe294c5c1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 12:37:45 +0700

    Convert all to 2 digits

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
@@ -13,8 +13,10 @@ public class LC1JamDengklek
         input.close();
         int hourEnd = (hourStart + (duration / 60)) % 23;
         int minuteEnd = (minuteStart + duration) % 60;
+        String hourStartString = String.format("%02d",hourStart);
+        String minuteStartString = String.format("%02d",minuteStart);
         String hourEndString = String.format("%02d",hourEnd);
         String minuteEndString = String.format("%02d",minuteEnd);
-        System.out.println("Dengklek belajar pukul " + hourStart + ":" + minuteStart + " selama " + duration + " menit dan selesai pada pukul " + hourEndString + ":" + minuteEndString + ".");
+        System.out.println("Dengklek belajar pukul " + hourStartString + ":" + minuteStartString + " selama " + duration + " menit dan selesai pada pukul " + hourEndString + ":" + minuteEndString + ".");
     }
 }
