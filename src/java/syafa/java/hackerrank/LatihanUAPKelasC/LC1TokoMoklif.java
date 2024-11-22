0e77023380bed613d263a27ebb08fe8146e3acf7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 12:39:44 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1TokoMoklif.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1TokoMoklif.java
@@ -1,5 +1,12 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
+import java.math.*;
 
-public class LC1TokoMoklif {
-
+public class LC1TokoMoklif
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
