cd58dee405e741e46704172736500a474247c3ec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 17:06:45 +0700

    Update most participant

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
@@ -8,7 +8,8 @@ public class LC3LombaDengklek
     {
         Scanner input = new Scanner(System.in);
         int participantWinAmount = Integer.parseInt(input.nextLine());
-        int mostOccurence = 1;
+        int mostOccurence = 0;
+        String mostParticipant = "";
         String[] winningParticipant = new String[participantWinAmount];
         String[][] participantList = new String[PARTICIPANT_AMOUNT][2];
         for (int i = 0; i < participantWinAmount; i++)
@@ -24,26 +25,37 @@ public class LC3LombaDengklek
                 if (!(winningParticipant[j].equalsIgnoreCase(participantList[i][0])) && participantList[i] != null && index < participantList.length)
                 {
                     participantList[index][0] = winningParticipant[j];
+                    participantList[index][1] = String.valueOf(1);
                     index++;
                 }
-            }
-        }
-        System.out.println();
-        for (int i = 0; i < winningParticipant.length; i++)
-        {
-            for (int j = 0 + i; j < participantList.length; j++)
-            {
-                if (winningParticipant[i].equalsIgnoreCase(participantList[j][0]))
+                else if (winningParticipant[j].equalsIgnoreCase(participantList[i][0]))
                 {
-                    int currentOccurence = Integer.parseInt(participantList[j][1] + 1);
-                    participantList[j][1] = String.valueOf(currentOccurence);
+                    int currentOccurence = Integer.parseInt(participantList[i][1]) + 1;
+                    participantList[i][1] = String.valueOf(currentOccurence);
                     if (currentOccurence > mostOccurence)
                     {
                         mostOccurence = currentOccurence;
+                        mostParticipant = participantList[i][0];
                     }
-                    continue;
                 }
             }
         }
+        System.out.println(mostOccurence);
+        // for (int i = 0; i < winningParticipant.length; i++)
+        // {
+        //     for (int j = 0 + i; j < participantList.length; j++)
+        //     {
+        //         if (winningParticipant[i].equalsIgnoreCase(participantList[j][0]))
+        //         {
+        //             int currentOccurence = Integer.parseInt(participantList[j][1] + 1);
+        //             participantList[j][1] = String.valueOf(currentOccurence);
+        //             if (currentOccurence > mostOccurence)
+        //             {
+        //                 mostOccurence = currentOccurence;
+        //             }
+        //             continue;
+        //         }
+        //     }
+        // }
     }
 }
