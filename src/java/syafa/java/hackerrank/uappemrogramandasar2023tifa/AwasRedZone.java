36d79ea7205fb466ca72f845f6509099fc25741e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 19:43:55 +0700

    Compare with both coordinate

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/AwasRedZone.java
@@ -11,12 +11,16 @@ public class AwasRedZone
         int[] secondCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         int[] currentCoordinate = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        for (int i = 0; i < currentCoordinate.length; i++)
+        {
+            
+        }
         if
         (
-            currentCoordinate[0] <= firstCoordinate[0] &&
-            currentCoordinate[1] <= firstCoordinate[1] &&
-            currentCoordinate[0] >= secondCoordinate[0] &&
-            currentCoordinate[1] >= secondCoordinate[1]
+            (currentCoordinate[0] <= firstCoordinate[0] ||
+            currentCoordinate[1] <= firstCoordinate[1]) &&
+            (currentCoordinate[0] >= secondCoordinate[0] ||
+            currentCoordinate[1] >= secondCoordinate[1])
         )
         {
             System.out.println("Bahaya");
