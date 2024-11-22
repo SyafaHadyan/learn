fc7911a1f4d404a73aa6ce781b3f0a6cbecd0746
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 16:35:31 +0700

    Participant list

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
@@ -3,21 +3,21 @@ import java.util.*;
 
 public class LC3LombaDengklek
 {
+    static final int PARTICIPANT_AMOUNT = 4;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int participantAmount = Integer.parseInt(input.nextLine());
-        HashMap<String,Integer> participantWinAmount = new HashMap<>();
-        for (int i = 0; i < participantAmount; i++)
+        int participantWinAmount = Integer.parseInt(input.nextLine());
+        String[] winningParticipant = new String[participantWinAmount];
+        String[] participantList = new String[PARTICIPANT_AMOUNT];
+        for (int i = 0; i < participantWinAmount; i++)
         {
-            String tempInput = input.nextLine();
-            if (participantWinAmount.get(tempInput) == null)
-            {
-                participantWinAmount.put(tempInput,1);
-                continue;
-            }
-            participantWinAmount.merge(tempInput,1,Integer::sum);
+            winningParticipant[i] = input.nextLine();
         }
         input.close();
+        for (int i = 0; i < winningParticipant.length; i++)
+        {
+            //
+        }
     }
 }
