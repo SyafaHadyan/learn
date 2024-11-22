8a8173cadf3f9599f5b9ce242a7bb9bd80b88eb8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 12:27:13 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1JamDengklek.java
@@ -6,6 +6,10 @@ public class LC1JamDengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int hourStart = input.nextInt();
+        int minuteStart = input.nextInt();
+        input.nextLine();
+        int duration = Integer.parseInt(input.nextLine());
         input.close();
     }
 }
