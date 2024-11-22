27f5d9e291e41e569bad3bbd7b3277664ea9edec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-22 13:30:06 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1TokoMoklif.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC1TokoMoklif.java
@@ -28,26 +28,6 @@ public class LC1TokoMoklif
     }
     public static void main(String[] args)
     {
-        /*
-         * ========================================
-         *               Toko Moklif
-         *            14 September 2024
-         * ----------------------------------------
-         * Name     : Dengklek
-         * NIM      : 245150200000001
-         * ----------------------------------------
-         * Nama Barang                     Harga($)
-         * ----------------------------------------
-         * Mikrosoft 345                  365000.00
-         * Abode Cloud                    200000.00
-         * Windos 9                       300000.00
-         * ----------------------------------------
-         * subTotal                       865000.00
-         * Total                          951500.00
-         * Bayar                         1000000.00
-         * Kembali                         48500.00
-         * ========================================
-         */
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         BigInteger NIM = input.nextBigInteger(); input.nextLine();
