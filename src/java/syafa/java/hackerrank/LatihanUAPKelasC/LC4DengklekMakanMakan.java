1788432d5ae56b862df8951fcd5fdd4e5faa76b0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 09:37:45 +0700

    Input guest list

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
@@ -6,6 +6,12 @@ public class LC4DengklekMakanMakan
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int guestAmount = Integer.parseInt(input.nextLine());
+        String[] guestList = new String[guestAmount];
+        for (int i = 0; i < args.length; i++)
+        {
+            guestList[i] = input.nextLine();
+        }
         input.close();
     }
 }
