c27da73d7f7fc89abf91a0cfe07c6fe711426dd7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 08:59:29 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
@@ -6,6 +6,17 @@ public class LC5PrimaAtauFibonacci
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        int[] numberSequence = new int[numberAmount];
+        for (int i = 0; i < numberSequence.length; i++)
+        {
+            numberSequence[i] = input.nextInt();
+        }
         input.close();
     }
+    /*
+     * Prime
+     * Fibonacci
+     * Arithmetic
+     */
 }
