026422d94a2613d93fc7e20d0d7831fcbf13808d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 18:38:37 +0700

    Initialize

---
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RankingPemain.java
@@ -0,0 +1,11 @@
+package syafa.java.hackerrank.uappemrogramandasar2023tifa;
+import java.util.*;
+
+public class RankingPemain
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
