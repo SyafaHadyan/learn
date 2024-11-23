5cc4cd9f26b2ce931faf026600282f6a0d09dd37
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 16:10:51 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
@@ -7,6 +7,21 @@ public class LC3LombaDengklek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int participantWinAmount = Integer.parseInt(input.nextLine());
+        int mostParticipantOccurence = 0;
+        String mostParticipant = "";
+        String[] winningParticipant = new String[participantWinAmount];
+        for (int i = 0; i < participantWinAmount; i++)
+        {
+            winningParticipant[i] = input.nextLine();
+        }
         input.close();
+        for (int i = 0; i < winningParticipant.length; i++)
+        {
+            for (int j = 1 + i; j < winningParticipant.length; j++)
+            {
+                //
+            }
+        }
     }
 }
