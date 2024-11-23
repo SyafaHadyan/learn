8291a50cb9f1967c7c97721fe8ab41205ae63906
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 08:21:12 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DataPenjualanHarian.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DataPenjualanHarian.java
@@ -3,9 +3,15 @@ import java.util.*;
 
 public class LC3DataPenjualanHarian
 {
+    static final int SALE_DAY_INPUT = 7;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] saleEntry = new int[SALE_DAY_INPUT];
+        for (int i = 0; i < saleEntry.length; i++)
+        {
+            saleEntry[i] = Integer.parseInt(input.nextLine());
+        }
         input.close();
     }
 }
