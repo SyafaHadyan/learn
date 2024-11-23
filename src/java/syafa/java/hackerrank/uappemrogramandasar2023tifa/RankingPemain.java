3deb68e74ec3b6f1471102d4936fbf3f1a79efa2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 18:41:36 +0700

    Input data

--- src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RankingPemain.java
+++ src/java/syafa/java/hackerrank/uappemrogramandasar2023tifa/RankingPemain.java
@@ -6,6 +6,25 @@ public class RankingPemain
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int playerAmount = Integer.parseInt(input.nextLine());
+        int[] playerOffensive = new int[playerAmount];
+        int[] playerDefensive = new int[playerAmount];
+        for (int i = 0; i < playerOffensive.length; i++)
+        {
+            playerOffensive[i] = input.nextInt();
+        }
+        input.nextLine();
+        for (int i = 0; i < playerDefensive.length; i++)
+        {
+            playerDefensive[i] = input.nextInt();
+        }
+        input.nextLine();
+        int managerInstructionAmount = Integer.parseInt(input.nextLine());
+        String[] instructionList = new String[managerInstructionAmount];
+        for (int i = 0; i < instructionList.length; i++)
+        {
+            instructionList[i] = input.nextLine();
+        }
         input.close();
     }
 }
