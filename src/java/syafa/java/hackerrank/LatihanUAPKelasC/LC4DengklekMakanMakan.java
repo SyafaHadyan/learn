d247030035dcbc3e1920899bae643f85ecd755b9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 09:40:39 +0700

    Check duplicates

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
@@ -8,10 +8,21 @@ public class LC4DengklekMakanMakan
         Scanner input = new Scanner(System.in);
         int guestAmount = Integer.parseInt(input.nextLine());
         String[] guestList = new String[guestAmount];
+        String[] guestAbove = new String[guestAmount];
         for (int i = 0; i < args.length; i++)
         {
             guestList[i] = input.nextLine();
         }
         input.close();
+        for (int i = 0; i < guestList.length; i++)
+        {
+            for (int j = 1 + i; j < guestList.length; j++)
+            {
+                if (guestList[i].equalsIgnoreCase(guestList[j]))
+                {
+                    //
+                }
+            }
+        }
     }
 }
