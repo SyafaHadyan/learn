d42e32a7e7bdb15edf68e8d85e9492f773495887
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 09:01:12 +0700

    Check prime sequence

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
@@ -3,6 +3,32 @@ import java.util.*;
 
 public class LC5PrimaAtauFibonacci
 {
+    public static boolean isPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean primeSequence(int[] number)
+    {
+        for (int i = 0; i < number.length; i++)
+        {
+            if (!(isPrime(number[i])))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
