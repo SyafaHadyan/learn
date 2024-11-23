54ff2f7f4fec9e44d7e39074423146730c23091a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 08:10:26 +0700

    While input

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3BilanganPrimaPt2.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3BilanganPrimaPt2.java
@@ -6,6 +6,13 @@ public class LC3BilanganPrimaPt2
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int primeAt = Integer.parseInt(input.nextLine());
         input.close();
+        int currentPrimeAt = 1;
+        int currentPrimeNumber = 2;
+        while (currentPrimeAt != primeAt)
+        {
+            //
+        }
     }
 }
