2c2b0d8c4074792393408f1cbfb87d852ef1b0bc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 09:59:28 +0700

    Fix null check

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DengklekMakanMakan.java
@@ -7,7 +7,7 @@ public class LC4DengklekMakanMakan
     {
         for (int i = 0; i < guestAbove.length; i++)
         {
-            if (guestAbove[i].equalsIgnoreCase(guestCheck))
+            if (guestAbove[i] != null && guestAbove[i].equalsIgnoreCase(guestCheck))
             {
                 return false;
             }
@@ -20,19 +20,27 @@ public class LC4DengklekMakanMakan
         int guestAmount = Integer.parseInt(input.nextLine());
         String[] guestList = new String[guestAmount];
         String[] guestAbove = new String[guestAmount];
-        for (int i = 0; i < args.length; i++)
+        for (int i = 0; i < guestList.length; i++)
         {
             guestList[i] = input.nextLine();
         }
         input.close();
+        System.err.println("Guest above:");
         int index = 0;
         for (int i = 0; i < guestList.length; i++)
         {
             for (int j = 1 + i; j < guestList.length; j++)
             {
-                if (guestList[i].equalsIgnoreCase(guestList[j]) && isNotAdded(guestAbove,guestList[i]))
+                if (index != 0 && guestList[i].equalsIgnoreCase(guestList[j]) && isNotAdded(guestAbove,guestList[i]) && index != 0)
                 {
                     guestAbove[index] = guestList[i];
+                    System.err.println(guestAbove[index]);
+                    index++;
+                }
+                else if (index == 0 && guestList[i].equalsIgnoreCase(guestList[j]))
+                {
+                    guestAbove[index] = guestList[i];
+                    System.err.println(guestAbove[index]);
                     index++;
                 }
             }
