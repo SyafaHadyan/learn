a44f6c3da762ca99a6be7ffb51832c96fe71645a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 08:57:42 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5PrimaAtauFibonacci.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC5PrimaAtauFibonacci {
-
+public class LC5PrimaAtauFibonacci
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
