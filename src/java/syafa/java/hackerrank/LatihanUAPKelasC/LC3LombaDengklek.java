2a170314dc705b0f5f8d16d5dfe7bc67deee87a7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 16:26:25 +0700

    Remove unused var

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3LombaDengklek.java
@@ -8,15 +8,10 @@ public class LC3LombaDengklek
     {
         Scanner input = new Scanner(System.in);
         int participantWinAmount = Integer.parseInt(input.nextLine());
-        int mostParticipantOccurence = 0;
         int currrentMostOccurence = 0;
-        int previousMostOccurence = 0;
         int mostOccurence = 0;
-        int duplicate = 0;
         String mostParticipant = "";
-        String[] winningParticipant = new String[participantWinAmount];
         HashMap<String,Integer> participantList = new HashMap<>();
-        ArrayList<Integer> occurence = new ArrayList<>();
         for (int i = 0; i < participantWinAmount; i++)
         {
             // winningParticipant[i] = input.nextLine();
