f34a0ed546f8327ccd61684a6517e6eddb3191b1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 08:12:12 +0700

    Check prime and inc counter

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3BilanganPrimaPt2.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3BilanganPrimaPt2.java
@@ -3,16 +3,31 @@ import java.util.*;
 
 public class LC3BilanganPrimaPt2
 {
+    public static boolean isPrime(int number)
+    {
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int primeAt = Integer.parseInt(input.nextLine());
         input.close();
         int currentPrimeAt = 1;
-        int currentPrimeNumber = 2;
+        int currentNumber = 1;
         while (currentPrimeAt != primeAt)
         {
-            //
+            currentNumber++;
+            if (isPrime(currentNumber))
+            {
+                currentPrimeAt++;
+            }
         }
     }
 }
