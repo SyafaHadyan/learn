9f2cf392d4b8a93db6bd1b8fcacfbbe093f6697b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-23 18:20:05 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SequenceEquation.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SequenceEquation.java
@@ -1,11 +1,14 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class SequenceEquation
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int sequenceAmount = Integer.parseInt(input.nextLine());
+        int[] sequence = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
