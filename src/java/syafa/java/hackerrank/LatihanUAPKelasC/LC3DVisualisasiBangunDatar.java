a9a326913ad8347a2c2d3e2c36287f2a693298e4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:01:02 +0700

    Initialzie

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC3DVisualisasiBangunDatar {
-
+public class LC3DVisualisasiBangunDatar
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
