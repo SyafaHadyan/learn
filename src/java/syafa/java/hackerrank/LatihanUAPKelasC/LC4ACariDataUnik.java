793213002764cd3ec86872fe435716d5054c473b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:59:27 +0700

    Compare data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ACariDataUnik.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ACariDataUnik.java
@@ -10,8 +10,20 @@ public class LC4ACariDataUnik
         int[] data = new int[dataAmount];
         for (int i = 0; i < data.length; i++)
         {
-            
+            data[i] = input.nextInt();
         }
         input.close();
+        for (int i = 0; i < data.length; i++)
+        {
+            boolean unique = true;
+            for (int j = 1 + i; j < data.length; j++)
+            {
+                if (data[i] == data[j])
+                {
+                    unique = false;
+                    break;
+                }
+            }
+        }
     }
 }
