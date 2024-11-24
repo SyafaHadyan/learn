d0af5860f6cd6ef2932c5b756b41088188c4b5ec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 12:09:54 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
@@ -6,6 +6,14 @@ public class LC3DNoGameNoLoop
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int platformSize = Integer.parseInt(input.nextLine());
+        int startPosition = Integer.parseInt(input.nextLine());
+        int finishPosition = Integer.parseInt(input.nextLine());
+        int coinLocation = Integer.parseInt(input.nextLine());
+        while (input.hasNextLine())
+        {
+            //
+        }
         input.close();
     }
 }
