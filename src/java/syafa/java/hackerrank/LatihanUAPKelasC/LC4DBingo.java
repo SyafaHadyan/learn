08fd2fe97e09085d6dbe88d2d709926cec99c493
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 20:10:37 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DBingo.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DBingo.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC4DBingo {
-
+public class LC4DBingo
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
