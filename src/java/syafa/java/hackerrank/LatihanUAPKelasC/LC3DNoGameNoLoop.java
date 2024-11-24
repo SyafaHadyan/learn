bf1f126567b62c21931dc2bda106963a137f54e8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 12:58:06 +0700

    If pos

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
@@ -15,12 +15,7 @@ public class LC3DNoGameNoLoop
         int playerPosition = Integer.parseInt(input.nextLine());
         int finishPosition = Integer.parseInt(input.nextLine());
         int coinLocation = Integer.parseInt(input.nextLine());
-        ArrayList<String> gameStatus = new ArrayList<>();
-        Map<Integer,String> gameMapStatus = Map.ofEntries
-        (
-            Map.entry(coinLocation,"Koin didapatkan"),
-            Map.entry(finishPosition,"Game finish")
-        );
+        boolean coinStatus = false;
         System.out.println("Game dimulai!");
         for (int i = 0; i < platformSize; i++)
         {
@@ -32,13 +27,17 @@ public class LC3DNoGameNoLoop
             String tempInput = input.nextLine();
             int previousPosition = playerPosition;
             playerPosition += MOVE_INSTRUCTION.get(tempInput);
-            if (playerPosition < 0 || playerPosition > platformSize - 1)
+            if (playerPosition < 0 || playerPosition > platformSize - 1 || playerPosition == finishPosition)
             {
+                if (playerPosition == finishPosition)
+                {
+                    previousPosition = playerPosition;
+                }
                 playerPosition = previousPosition;
             }
-            if (gameMapStatus.containsKey(playerPosition) && gameStatus.size() <= 2)
+            if (playerPosition == coinLocation)
             {
-                gameStatus.add(gameMapStatus.get(playerPosition));
+                coinStatus = true;
             }
             for (int i = 0; i < platformSize; i++)
             {
@@ -47,9 +46,6 @@ public class LC3DNoGameNoLoop
             System.out.print("\n");
         }
         input.close();
-        for (int i = 0; i < gameStatus.size(); i++)
-        {
-            System.out.println(gameStatus.get(i));
-        }
+        System.out.println("Game finish!" + ((coinStatus) ? "\n" + "Koin didapatkan!" : ""));
     }
 }
