20e530733429a4f76b6d052904d4ef9b66ed0451
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:53:18 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3ESulungBungsu.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3ESulungBungsu.java
@@ -1,5 +1,18 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC3ESulungBungsu {
-
+public class LC3ESulungBungsu
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int childAmount = Integer.parseInt(input.nextLine());
+        int lowest = Integer.MAX_VALUE;
+        int highest = Integer.MIN_VALUE;
+        for (int i = 0; i < childAmount; i++)
+        {
+            //
+        }
+        input.close();
+    }
 }
