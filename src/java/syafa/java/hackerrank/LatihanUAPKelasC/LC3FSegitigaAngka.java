215eb5d4ac1adfb0debfce8f65966ee62a54eb32
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 14:34:24 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3FSegitigaAngka.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3FSegitigaAngka.java
@@ -3,9 +3,16 @@ import java.util.*;
 
 public class LC3FSegitigaAngka
 {
+    static final int NUMBER_LIMIT = 9;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int staircaseSize = Integer.parseInt(input.nextLine());
         input.close();
+        int counter = 0;
+        for (int i = 0; i < staircaseSize; i++)
+        {
+            //
+        }
     }
 }
