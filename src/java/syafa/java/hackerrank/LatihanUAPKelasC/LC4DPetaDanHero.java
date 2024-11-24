1ea4866d3f489d4d66f0227aad5b1d4c82c4a147
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 15:24:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DPetaDanHero.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DPetaDanHero.java
@@ -1,5 +1,22 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC4DPetaDanHero {
-
+public class LC4DPetaDanHero
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int col = input.nextInt();
+        int heroCount = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
+        boolean print = true;
+        if (row <= 0 || col <= 0)
+        {
+            print = false;
+        }
+    }
 }
