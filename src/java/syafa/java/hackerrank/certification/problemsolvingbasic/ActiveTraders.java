229c9e3e20032a369c286f66a0fdac1db65c8462
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 08:28:45 +0700

    Add to hashmap

--- src/java/syafa/java/hackerrank/certification/problemsolvingbasic/ActiveTraders.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingbasic/ActiveTraders.java
@@ -6,6 +6,20 @@ public class ActiveTraders
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int traderAmount = Integer.parseInt(input.nextLine());
+        HashMap<String,Integer> traderList = new HashMap<>();
+        for (int i = 0; i < traderAmount; i++)
+        {
+            String tempInput = input.nextLine();
+            if (traderList.containsKey(tempInput))
+            {
+                traderList.merge(tempInput,1,Integer::sum);
+            }
+            else
+            {
+                traderList.put(tempInput,1);
+            }
+        }
         input.close();
     }
 }
