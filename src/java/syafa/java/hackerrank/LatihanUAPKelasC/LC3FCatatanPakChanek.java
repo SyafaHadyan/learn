635802675e079aa2b803d9b2d52879bcd09da577
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 15:00:19 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3FCatatanPakChanek.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3FCatatanPakChanek.java
@@ -6,6 +6,14 @@ public class LC3FCatatanPakChanek
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String firstWord = input.next();
+        String secondWord = input.next();
+        int firstWordOccurence = 0;
+        int secondWordOccurence = 0;
+        while (input.hasNextLine())
+        {
+            //
+        }
         input.close();
     }
 }
