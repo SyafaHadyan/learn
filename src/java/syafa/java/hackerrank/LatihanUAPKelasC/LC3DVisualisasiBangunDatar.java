87d6e1cce4bdf4e71f6943d930519b396f71dcc0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:03:35 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
@@ -3,9 +3,15 @@ import java.util.*;
 
 public class LC3DVisualisasiBangunDatar
 {
+    public static void shapeBuilder(int shapeSize,String shapeRequest)
+    {
+        System.out.println(shapeSize);
+        System.out.println(shapeRequest);
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        shapeBuilder(Integer.parseInt(input.nextLine()),input.nextLine().toLowerCase());
         input.close();
     }
 }
