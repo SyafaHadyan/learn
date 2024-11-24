a412e134857596e8c06b0a3cacd353c9f9330c4e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 12:08:13 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DNoGameNoLoop.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC3DNoGameNoLoop {
-
+public class LC3DNoGameNoLoop
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
