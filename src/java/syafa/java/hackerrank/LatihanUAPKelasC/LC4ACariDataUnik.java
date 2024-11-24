ee5f22d69da7a625d8210b08d95be284c6cb8f7d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:58:09 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ACariDataUnik.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ACariDataUnik.java
@@ -1,5 +1,17 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC4ACariDataUnik {
-
+public class LC4ACariDataUnik
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int dataAmount = Integer.parseInt(input.nextLine());
+        int[] data = new int[dataAmount];
+        for (int i = 0; i < data.length; i++)
+        {
+            
+        }
+        input.close();
+    }
 }
