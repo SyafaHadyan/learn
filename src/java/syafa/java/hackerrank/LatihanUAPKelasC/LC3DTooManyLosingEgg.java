04b111286d0480a372607e2a72289c5852892497
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 12:01:08 +0700

    Increment if above min quality

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DTooManyLosingEgg.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DTooManyLosingEgg.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class LC3DTooManyLosingEgg
 {
+    static final int MINIMUM_EGG_QUALITY = 6;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int eggAmount = Integer.parseInt(input.nextLine());
+        int minimumQuality = 0;
+        for (int i = 0; i < eggAmount; i++)
+        {
+            if (Integer.parseInt(input.nextLine()) >= MINIMUM_EGG_QUALITY)
+            {
+                minimumQuality++;
+            }
+        }
         input.close();
     }
 }
