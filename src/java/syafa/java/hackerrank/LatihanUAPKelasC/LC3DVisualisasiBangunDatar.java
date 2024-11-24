c0d71c153ce4132e4b490930bf3d4a1872f486a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:23:51 +0700

    Remove comments

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3DVisualisasiBangunDatar.java
@@ -35,14 +35,6 @@ public class LC3DVisualisasiBangunDatar
     }
     public static void shapeBiulderParallelogram(int shapeSize)
     {
-        /*
-         *      * * * * * * 
-         *     * * * * * * 
-         *    * * * * * * 
-         *   * * * * * * 
-         *  * * * * * * 
-         * * * * * * *
-         */
         int spaceBefore = shapeSize - 1;
         for (int i = 0; i < shapeSize; i++)
         {
