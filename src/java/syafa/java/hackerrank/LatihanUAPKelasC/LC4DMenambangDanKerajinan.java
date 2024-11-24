10c08a3680f8f7fd11cd8e6f57c42567b5cdbd71
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 16:33:42 +0700

    Valid block list

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
@@ -3,9 +3,23 @@ import java.util.*;
 
 public class LC4DMenambangDanKerajinan
 {
+    static final Map<String,Boolean> VALID_BLOCK = Map.ofEntries
+    (
+        Map.entry("dirt",true),
+        Map.entry("grass",true),
+        Map.entry("wood",true),
+        Map.entry("stone",true)
+    );
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int z = input.nextInt(); input.nextLine();
+        while (input.hasNextLine())
+        {
+            //
+        }
         input.close();
     }
 }
