97611fb1131943e58b437f41b9cffd0f679e69e5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-24 13:46:05 +0700

    If mod 3

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EAyamAyam.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EAyamAyam.java
@@ -1,5 +1,20 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC3EAyamAyam {
-
+public class LC3EAyamAyam
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int initialChickenAmount = Integer.parseInt(input.nextLine());
+        int dayAmount = Integer.parseInt(input.nextLine());
+        input.close();
+        for (int i = 1; i <= dayAmount; i++)
+        {
+            if (i % 3 == 0)
+            {
+                initialChickenAmount -= (initialChickenAmount / 4);
+            }
+        }
+    }
 }
