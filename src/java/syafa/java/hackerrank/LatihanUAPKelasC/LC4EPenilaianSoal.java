66e385a01f8c5699a79b741467dd25e99725a542
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 07:52:32 +0700

    Add answer key

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EPenilaianSoal.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EPenilaianSoal.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class LC4EPenilaianSoal
 {
+    static final char[] ANSWER_KEY =
+    {
+        'D','B','D','C','C','C','D','A','E','A','D'
+    };
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
