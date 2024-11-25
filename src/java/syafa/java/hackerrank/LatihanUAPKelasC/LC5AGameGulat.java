7c5c61cfb9e0e31696d675cef41f5eb92c994206
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 15:08:53 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
@@ -6,6 +6,13 @@ public class LC5AGameGulat
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int monsterHealth = Integer.parseInt(input.nextLine());
+        int playerHealth = Integer.parseInt(input.nextLine());
+        int moveAmount = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < moveAmount; i++)
+        {
+            
+        }
         input.close();
     }
 }
