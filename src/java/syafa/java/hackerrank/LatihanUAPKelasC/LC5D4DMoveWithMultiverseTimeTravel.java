70f444b955cda30e9e3f90c43ea8fce805405e9c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 19:59:26 +0700

    Counter vars

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
@@ -16,19 +16,35 @@ public class LC5D4DMoveWithMultiverseTimeTravel
          * step 2
          * move down
          * stop
+         * 
+         * move {direction}  
+         * step {indexStep}  
+         * world {indexWorld}  
+         * stop
          */
         Scanner input = new Scanner(System.in);
         String[][][][] worldData = new String[WORLD_DUPLICATE_LIMIT][STEP_LIMIT][WORLD_SIZE_LIMIT][WORLD_SIZE_LIMIT];
         int worldCounter = 0;
-        int stepCounter = 0;
+        int[][] stepCounter = new int[WORLD_DUPLICATE_LIMIT][STEP_LIMIT];
         while (input.hasNextLine())
         {
             String[] tempInput = input.nextLine().split("\s");
             if (!(tempInput[0].equalsIgnoreCase("Stop")))
             {
-                //
+                if (tempInput[0].equalsIgnoreCase("Move"))
+                {
+                    //
+                }
+                if (tempInput[0].equalsIgnoreCase("Step"))
+                {
+                    //
+                }
+                if (tempInput[0].equalsIgnoreCase("World"))
+                {
+                    //
+                }
             }
-            else if (tempInput[0].equalsIgnoreCase("Stop"))
+            else
             {
                 //
             }
