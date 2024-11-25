2e14d1ee60ceec6cf58ed581e5d0e60a8ce574b5
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 12:07:27 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ELaporanKaryawan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ELaporanKaryawan.java
@@ -6,6 +6,15 @@ public class LC4ELaporanKaryawan
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int employeeAmount = Integer.parseInt(input.nextLine());
+        String[][] employeeList = new String[employeeAmount][2];
+        for (int i = 0; i < employeeList.length; i++)
+        {
+            for (int j = 0; j < employeeList[i].length; j++)
+            {
+                employeeList[i][j] = input.nextLine();
+            }
+        }
         input.close();
     }
 }
