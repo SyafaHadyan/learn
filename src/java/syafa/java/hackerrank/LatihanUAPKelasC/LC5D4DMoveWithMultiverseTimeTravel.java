e15367a35cc82f33e96b2e0ce1d605ced251d36e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 19:51:16 +0700

    4d arr

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
@@ -3,9 +3,14 @@ import java.util.*;
 
 public class LC5D4DMoveWithMultiverseTimeTravel
 {
+    static final int WORLD_SIZE_LIMIT = 3;
+    static final int STEP_LIMIT = 16;
+    static final int WORLD_DUPLICATE_LIMIT = 8;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[][][][] worldData = new String[WORLD_DUPLICATE_LIMIT][STEP_LIMIT][WORLD_SIZE_LIMIT][WORLD_SIZE_LIMIT];
+
         input.close();
     }
 }
