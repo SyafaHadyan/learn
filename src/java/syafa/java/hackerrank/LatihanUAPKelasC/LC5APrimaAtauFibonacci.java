949e1b367e9e6df92a96aed3df765abebc898f94
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 13:52:06 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5APrimaAtauFibonacci.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5APrimaAtauFibonacci.java
@@ -6,6 +6,16 @@ public class LC5APrimaAtauFibonacci
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        int[] numberList = new int[numberAmount];
+        for (int i = 0; i < numberList.length; i++)
+        {
+            numberList[i] = Integer.parseInt(input.nextLine());
+        }
         input.close();
+        for (int i = 0; i < numberList.length; i++)
+        {
+            //
+        }
     }
 }
