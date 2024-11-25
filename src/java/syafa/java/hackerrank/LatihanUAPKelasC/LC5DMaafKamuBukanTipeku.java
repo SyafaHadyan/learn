70a8ca2035c454e6d022822011bbea6eda751e2b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:02:41 +0700

    Increment

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
@@ -3,9 +3,16 @@ import java.util.*;
 
 public class LC5DMaafKamuBukanTipeku
 {
+    static final int OPERATION_AMOUNT = 5;
+    static double initialNumber;
+    public static double increment(int number)
+    {
+        return initialNumber + number;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        initialNumber = Double.parseDouble(input.nextLine());
         input.close();
     }
 }
