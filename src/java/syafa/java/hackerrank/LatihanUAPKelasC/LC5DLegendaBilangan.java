66ba5dfa2a47e9a5fb6d1b86814c56574fd4e5f6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:35:00 +0700

    Fix result Print

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
@@ -6,9 +6,20 @@ public class LC5DLegendaBilangan
     public static boolean checkKaprekar(int number)
     {
         number *= number;
-        int firstNumber = Integer.parseInt(String.valueOf(number).substring(0,(String.valueOf(number).length()) / 2));
-        int secondNumber = Integer.parseInt(String.valueOf(number).substring((String.valueOf(number).length() / 2 + ((String.valueOf(number).length() % 2 != 0) ? - 1 : 0)),String.valueOf(number).length()));
-        return (firstNumber + secondNumber) == number;
+        String firstNumber = String.valueOf(number);
+        String secondNumber = String.valueOf(number);
+        if (String.valueOf(number).length() % 2 == 0)
+        {
+            firstNumber = firstNumber.substring(0,(firstNumber.length() / 2));
+            secondNumber = secondNumber.substring((secondNumber.length() / 2),secondNumber.length());
+            System.err.println("\nFN " + firstNumber);
+            System.err.println("SN\n " + secondNumber);
+            return Integer.parseInt(firstNumber) + Integer.parseInt(secondNumber) == number;
+        }
+        return true;
+        //int firstNumber = Integer.parseInt(String.valueOf(number).substring(0,(String.valueOf(number).length()) / 2));
+        //int secondNumber = Integer.parseInt(String.valueOf(number).substring((String.valueOf(number).length() / 2 + ((String.valueOf(number).length() % 2 != 0) ? - 1 : 0)),String.valueOf(number).length()));
+        //return (firstNumber + secondNumber) == number;
     }
     public static void main(String[] args)
     {
