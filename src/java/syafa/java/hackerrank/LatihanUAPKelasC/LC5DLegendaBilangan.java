d8051c62e952b6602ca57773c4a0bb80997ad88d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:16:30 +0700

    Split number

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
@@ -3,9 +3,22 @@ import java.util.*;
 
 public class LC5DLegendaBilangan
 {
+    public static boolean checkKaprekar(int number)
+    {
+        number *= number;
+        int firstNumber = Integer.parseInt(String.valueOf(number).substring(0,(String.valueOf(number).length()) / 2));
+        int secondNumbere = Integer.parseInt(String.valueOf(number).substring((String.valueOf(number).length() / 2 + ((String.valueOf(number).length() % 2 != 0) ? - 1 : 0)),String.valueOf(number).length()));
+        
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        boolean[] kaprekarValidity = new boolean[numberAmount];
+        for (int i = 0; i < kaprekarValidity.length; i++)
+        {
+            //
+        }
         input.close();
     }
 }
