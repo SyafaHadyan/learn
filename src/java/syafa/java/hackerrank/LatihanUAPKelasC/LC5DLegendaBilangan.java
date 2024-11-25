424a81a51cf89a168ab7b5f9fff497ea0ee227cb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:19:21 +0700

    Store result

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DLegendaBilangan.java
@@ -7,18 +7,31 @@ public class LC5DLegendaBilangan
     {
         number *= number;
         int firstNumber = Integer.parseInt(String.valueOf(number).substring(0,(String.valueOf(number).length()) / 2));
-        int secondNumbere = Integer.parseInt(String.valueOf(number).substring((String.valueOf(number).length() / 2 + ((String.valueOf(number).length() % 2 != 0) ? - 1 : 0)),String.valueOf(number).length()));
-        
+        int secondNumber = Integer.parseInt(String.valueOf(number).substring((String.valueOf(number).length() / 2 + ((String.valueOf(number).length() % 2 != 0) ? - 1 : 0)),String.valueOf(number).length()));
+        return (firstNumber + secondNumber) == number;
     }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int numberAmount = Integer.parseInt(input.nextLine());
         boolean[] kaprekarValidity = new boolean[numberAmount];
+        boolean validInput = true;
         for (int i = 0; i < kaprekarValidity.length; i++)
         {
-            //
+            int tempInput = Integer.parseInt(input.nextLine());
+            if (tempInput < 1)
+            {
+                validInput = false;
+            }
+            kaprekarValidity[i] = checkKaprekar(tempInput);
         }
         input.close();
+        if (validInput)
+        {
+            for (int i = 0; i < kaprekarValidity.length; i++)
+            {
+                
+            }
+        }
     }
 }
