ffc8d89e46dfe870f97916ac571e7a85fe88eed8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 15:13:14 +0700

    Change to method and int arr

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
@@ -3,28 +3,31 @@ import java.util.*;
 
 public class LC5AGameGulat
 {
+    public static int[] move(String move,int playerHealth,int monsterHealth)
+    {
+        if (move.equalsIgnoreCase("Punch"))
+        {
+            monsterHealth -= 30;
+        }
+        if (move.equalsIgnoreCase("Kick"))
+        {
+            monsterHealth -= (0.1d * (double) monsterHealth);
+        }
+        if (move.equalsIgnoreCase("Slam"))
+        {
+            monsterHealth -= 50;
+            playerHealth -= 10;
+        }
+        return new int[]{playerHealth,monsterHealth};
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int monsterHealth = Integer.parseInt(input.nextLine());
-        int playerHealth = Integer.parseInt(input.nextLine());
+        int[] playerData = {Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine())};
         int moveAmount = Integer.parseInt(input.nextLine());
         for (int i = 0; i < moveAmount; i++)
         {
-            String tempInput = input.nextLine();
-            if (tempInput.equalsIgnoreCase("Punch"))
-            {
-                monsterHealth -= 30;
-            }
-            if (tempInput.equalsIgnoreCase("Kick"))
-            {
-                monsterHealth -= (0.1d * (double) monsterHealth);
-            }
-            if (tempInput.equalsIgnoreCase("Slam"))
-            {
-                monsterHealth -= 50;
-                playerHealth -= 10;
-            }
+            
         }
         input.close();
     }
