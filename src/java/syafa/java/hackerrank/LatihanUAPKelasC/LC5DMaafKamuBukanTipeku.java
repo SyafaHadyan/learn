c74c077b59b56b41a309593ddeb2370b598ca5b2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:00:31 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC5DMaafKamuBukanTipeku {
-
+public class LC5DMaafKamuBukanTipeku
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
