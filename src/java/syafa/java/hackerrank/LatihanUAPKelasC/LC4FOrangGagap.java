f2ac3e55bf10436019d3563df02e9bb076aee3e6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 13:48:36 +0700

    Change to normal string

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4FOrangGagap.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4FOrangGagap.java
@@ -6,13 +6,15 @@ public class LC4FOrangGagap
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        char[] word = input.nextLine().toCharArray();
+        String[] word = input.nextLine().split("");
         input.close();
-        StringBuilder spell = new StringBuilder();
+        String spell = "";
         for (int i = 0; i < word.length; i++)
         {
-            spell.append(word[i]);
+            System.err.println(word[i]);
+            spell = spell.concat(word[i]);
             System.out.print(spell);
         }
+        System.err.println();
     }
 }
