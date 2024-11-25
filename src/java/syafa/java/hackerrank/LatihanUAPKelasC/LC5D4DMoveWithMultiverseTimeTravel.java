494e3cac50845b59daef7e7786f55608f111322f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 19:47:34 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5D4DMoveWithMultiverseTimeTravel.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC5D4DMoveWithMultiverseTimeTravel {
-
+public class LC5D4DMoveWithMultiverseTimeTravel
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
