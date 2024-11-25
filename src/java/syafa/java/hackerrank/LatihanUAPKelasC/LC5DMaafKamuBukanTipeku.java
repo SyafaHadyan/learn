d8f6ed0c130ae71270d8019b64730d0769bbb6bb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 16:06:46 +0700

    Fix calc

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5DMaafKamuBukanTipeku.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class LC5DMaafKamuBukanTipeku
 {
-    static final int OPERATION_AMOUNT = 5;
     static double initialNumber;
     public static double increment(int number)
     {
@@ -25,10 +24,10 @@ public class LC5DMaafKamuBukanTipeku
     {
         Scanner input = new Scanner(System.in);
         initialNumber = Double.parseDouble(input.nextLine());
-        increment(Integer.parseInt(input.nextLine()));
-        decrement(Integer.parseInt(input.nextLine()));
-        multiplication(Integer.parseInt(input.nextLine()));
-        division(Integer.parseInt(input.nextLine()));
+        initialNumber = increment(Integer.parseInt(input.nextLine()));
+        initialNumber = decrement(Integer.parseInt(input.nextLine()));
+        initialNumber = multiplication(Integer.parseInt(input.nextLine()));
+        initialNumber = division(Integer.parseInt(input.nextLine()));
         System.out.printf("%.2f\n",(initialNumber / 2));
         input.close();
     }
