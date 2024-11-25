693d194be003725b657f1fa0f353da2c615f6c09
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 15:11:02 +0700

    Move

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
@@ -11,7 +11,20 @@ public class LC5AGameGulat
         int moveAmount = Integer.parseInt(input.nextLine());
         for (int i = 0; i < moveAmount; i++)
         {
-            
+            String tempInput = input.nextLine();
+            if (tempInput.equalsIgnoreCase("Punch"))
+            {
+                monsterHealth -= 30;
+            }
+            if (tempInput.equalsIgnoreCase("Kick"))
+            {
+                monsterHealth -= (0.1d * (double) monsterHealth);
+            }
+            if (tempInput.equalsIgnoreCase("Slam"))
+            {
+                monsterHealth -= 50;
+                playerHealth -= 10;
+            }
         }
         input.close();
     }
