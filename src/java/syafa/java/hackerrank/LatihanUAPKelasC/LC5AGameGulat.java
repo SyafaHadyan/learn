05cc4b7e8ea248af1550bfe0e4a24ba1556970a0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 15:06:19 +0700

    Move file

---
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5AGameGulat.java
@@ -0,0 +1,11 @@
+package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
+
+public class LC5AGameGulat
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
