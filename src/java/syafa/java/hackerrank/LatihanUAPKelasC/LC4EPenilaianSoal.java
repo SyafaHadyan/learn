b73c28454f473c3092768eb63e8cb3bde461a98b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-25 11:55:21 +0700

    Change to str

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EPenilaianSoal.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EPenilaianSoal.java
@@ -3,27 +3,26 @@ import java.util.*;
 
 public class LC4EPenilaianSoal
 {
-    static final char[] ANSWER_KEY =
+    static final String[] ANSWER_KEY =
     {
-        'D','B','D','C','C','C','D','A','E','A','D'
+        "D","B","D","C","C","C","D","A","E","A","D"
     };
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int studentAmount = Integer.parseInt(input.nextLine());
-        int[][] studentGradeList = new int[studentAmount][ANSWER_KEY.length];
+        String[][] studentAnswerList = new String[studentAmount][ANSWER_KEY.length];
         int[] studentGrade = new int[studentAmount];
-        for (int i = 0; i < studentGradeList.length; i++)
+        for (int i = 0; i < studentAnswerList.length; i++)
         {
-            for (int j = 0; j < studentGradeList[i].length; j++)
+            studentAnswerList[i] = input.nextLine().split("\s");
+            for (int j = 0; j < studentAnswerList[i].length; j++)
             {
-                studentGradeList[i][j] = input.nextInt();
-                if (studentGradeList[i][j] == ANSWER_KEY[j])
+                if (studentAnswerList[i][j].equalsIgnoreCase(ANSWER_KEY[j]))
                 {
                     studentGrade[i]++;
                 }
             }
-            input.nextLine();
         }
         input.close();
         for (int i = 0; i < studentGrade.length; i++)
