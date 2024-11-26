32dca4931210f5fa385bac40a9c62a86465f398d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 14:30:10 +0700

    If power of two

--- src/java/syafa/java/hackerrank/certification/problemsolvingintermediate/BitwiseAND.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingintermediate/BitwiseAND.java
@@ -6,6 +6,36 @@ public class BitwiseAND
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        int[] number = new int[numberAmount];
+        int bitwiseAndCount = 0;
+        for (int i = 0; i < number.length; i++)
+        {
+            number[i] = Integer.parseInt(input.nextLine());
+        }
         input.close();
+        for (int i = 0; i < number.length; i++)
+        {
+            for (int j = 1 + i; j < number.length; j++)
+            {
+                int numberAnd = number[i] & number[j];
+                boolean powerOfTwo = true;
+                while (numberAnd >= 1)
+                {
+                    try
+                    {
+                        numberAnd = Integer.parseInt(String.valueOf(Math.sqrt(numberAnd)));
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        powerOfTwo = false;
+                    }
+                }
+                if (powerOfTwo)
+                {
+                    bitwiseAndCount++;
+                }
+            }
+        }
     }
 }
