68a5e795bced51d323b2da5b815f12609a5a0fc8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 10:44:35 +0700

    Temp

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5FOperasiString.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5FOperasiString.java
@@ -3,14 +3,19 @@ import java.util.*;
 
 public class LC5FOperasiString
 {
-    public static void moveSwap(String[] inputString)
+    static String[] inputString;
+    public static void moveSwap(int swapAmount)
     {
-        inputString[0] = inputString[inputString.length - 1];
-        for (int i = inputString.length - 1; i > 0; i--)
-        {
-            inputString[i] = inputString[i - 1];
-        }
         System.err.println(Arrays.toString(inputString));
+        for (int i = 0; i < swapAmount; i++)
+        {
+            inputString[0] = inputString[inputString.length - 1];
+            for (int j = inputString.length - 1; j > 0; j--)
+            {
+                inputString[j] = inputString[j - 1];
+            }
+            System.err.println(Arrays.toString(inputString));
+        }
     }
     public static void reverse(String inputString)
     {
@@ -19,14 +24,14 @@ public class LC5FOperasiString
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        String[] inputString = input.nextLine().split("");
+        inputString = input.nextLine().split("");
         int operationAmount = Integer.parseInt(input.nextLine());
-        for (int i = 0; i < operationAmount; i++)
+        for (int j = 0; j < operationAmount; j++)
         {
             String[] operation = input.nextLine().split("\s");
             if (operation[0].equalsIgnoreCase("Geser"))
             {
-                moveSwap(inputString);
+                moveSwap(Integer.parseInt(operation[1]));
             }
             if (operation[0].equalsIgnoreCase("Balik"))
             {
