811eebda36e293b4dd56ac24d51584dbe05026ee
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 11:51:40 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
@@ -6,6 +6,18 @@ public class LC3EKartuHasilStudi
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int amount = Integer.parseInt(input.nextLine());
+        double[][] data = new double[amount][2];
+        double result = 0d;
+        for (int i = 0; i < data.length; i++)
+        {
+            input.next();
+            for (int j = 0; j < data[i].length; j++)
+            {
+                data[i][j] = input.nextDouble();
+            }
+            input.nextLine();
+        }
         input.close();
     }
 }
