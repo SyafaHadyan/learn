925d2d73a22fe36e070ae96806030b1d434617b0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 08:17:08 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5EPerpustakaanE.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5EPerpustakaanE.java
@@ -6,6 +6,8 @@ public class LC5EPerpustakaanE
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int dayLate = Integer.parseInt(input.nextLine());
+        boolean isVIPMember = Boolean.parseBoolean(input.nextLine());
         input.close();
     }
 }
