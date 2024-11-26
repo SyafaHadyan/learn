07af95f5eeef3d21f4be55af7315d6e24ef4e7d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 09:10:54 +0700

    Count digit

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5ECekPassword.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5ECekPassword.java
@@ -3,9 +3,33 @@ import java.util.*;
 
 public class LC5ECekPassword
 {
+    static final int MIN_CHARACTER_LENGTH = 8;
+    static final int MIN_CAPITAL_CHARACTER = 1;
+    static final int MIN_DIGIT = 2;
+    static final int MIN_SPECIAL_CHARACTER = 1;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        while (true)
+        {
+            String[] tempInput = input.nextLine().split("");
+            int characterLength = 0;
+            int capitalCharacterCount = 0;
+            int digitCount = tempInput.length;
+            int specialCharacterCount = 0;
+            for (int i = 0; i < tempInput.length; i++)
+            {
+                characterLength++;
+                try
+                {
+                    Integer.parseInt(tempInput[i]);
+                }
+                catch (NumberFormatException e)
+                {
+                    digitCount--;
+                }
+            }
+        }
         input.close();
     }
 }
