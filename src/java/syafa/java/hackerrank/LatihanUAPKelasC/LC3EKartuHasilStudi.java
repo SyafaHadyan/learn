c05ebbf330d30394591e4adea7a1a15c9dd9c4cd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 11:48:00 +0700

    Initialize

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC3EKartuHasilStudi {
-
+public class LC3EKartuHasilStudi
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
