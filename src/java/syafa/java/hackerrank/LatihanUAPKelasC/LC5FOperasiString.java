6c3e002a6e204708de13660b46ff96369d330880
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-26 09:36:03 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5FOperasiString.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC5FOperasiString.java
@@ -1,5 +1,17 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
+import java.util.*;
 
-public class LC5FOperasiString {
-
+public class LC5FOperasiString
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String[] inputString = input.nextLine().split("");
+        int operationAmount = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < operationAmount; i++)
+        {
+            String operation = input.nextLine();
+        }
+        input.close();
+    }
 }
