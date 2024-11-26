cb1eddb07c78e17286bd0edbe6cf18da7c41e66a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 05:27:11 +0700

    Fix swap index

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ARifflex.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ARifflex.java
@@ -5,8 +5,8 @@ public class LC4ARifflex
 {
     static final double[][] SHUFFLE_RULE =
     {
-        {3,1},
-        {0.5,0}
+        {0.5,0},
+        {3,1}
     };
     public static void main(String[] args)
     {
@@ -17,14 +17,24 @@ public class LC4ARifflex
         int index = 0;
         for (int i = 0; i < card.length; i += 2)
         {
+            System.err.println("Swap " + i + " with " + index);
             initialShuffle[i] = card[index];
             index++;
         }
         for (int i = 1; i < card.length; i += 2)
         {
+            System.err.println("Swap " + i + " with " + index);
             initialShuffle[i] = card[index];
             index++;
         }
-        System.out.println(Arrays.toString(initialShuffle));
+        System.err.println(Arrays.toString(initialShuffle));
+        for (int i = 0; i < initialShuffle.length; i++)
+        {
+            String tempSwap = initialShuffle[i];
+            int indexSwap = (int) ((i * SHUFFLE_RULE[i % 2][0]) + SHUFFLE_RULE[i % 2][1]) % initialShuffle.length;
+            initialShuffle[i] = initialShuffle[indexSwap];
+            initialShuffle[indexSwap] = tempSwap;
+            System.err.println(Arrays.toString(initialShuffle));
+        }
     }
 }
