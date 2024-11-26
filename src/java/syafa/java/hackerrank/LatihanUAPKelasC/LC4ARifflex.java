99d51dd6f62e403b153a0d02ba69211fc5789f49
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 05:12:42 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ARifflex.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4ARifflex.java
@@ -3,9 +3,15 @@ import java.util.*;
 
 public class LC4ARifflex
 {
+    static final double[][] SHUFFLE_RULE =
+    {
+        {3,1},
+        {0.5,0}
+    };
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        String[] card = input.nextLine().split("\s");
         input.close();
     }
 }
