e21cd02db436587f2a1dd5f31f909af2fa29bb9e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 05:01:09 +0700

    Add sks table

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC3EKartuHasilStudi.java
@@ -3,9 +3,12 @@ import java.util.*;
 
 public class LC3EKartuHasilStudi
 {
-    static final double[] SKS_THRESHOLD =
+    static final double[][] SKS_THRESHOLD =
     {
-
+        {3,24},
+        {2.5,21},
+        {2.18},
+        {1.5,15},
     };
     public static void main(String[] args)
     {
@@ -14,6 +17,7 @@ public class LC3EKartuHasilStudi
         double[][] data = new double[amount][2];
         double result = 0d;
         int SKS = 0;
+        int nextSKS = 12;
         for (int i = 0; i < data.length; i++)
         {
             input.next();
@@ -29,6 +33,14 @@ public class LC3EKartuHasilStudi
         {
             result += data[i][0] * data[i][1] / SKS;
         }
-        System.out.printf("%s%-2c%.2f\n%s%-2c%-9s%d %s\n","IP Semester",':',(result),"Beban Studi",':',"maksimum",(0),"sks");
+        for (int i = 0; i < SKS_THRESHOLD.length; i++)
+        {
+            if (result >= SKS_THRESHOLD[i][0])
+            {
+                nextSKS = (int) SKS_THRESHOLD[i][1];
+                break;
+            }
+        }
+        System.out.printf("%s%-2c%.2f\n%s%-2c%-9s%d %s\n","IP Semester",':',(result),"Beban Studi",':',"maksimum",(nextSKS),"sks");
     }
 }
