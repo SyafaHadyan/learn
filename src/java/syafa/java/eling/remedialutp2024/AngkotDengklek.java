9468432411d7cad2f5176f578a198bf353b73073
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 14:13:54 +0700

    Add currency

--- src/java/syafa/java/eling/remedialutp2024/AngkotDengklek.java
+++ src/java/syafa/java/eling/remedialutp2024/AngkotDengklek.java
@@ -67,8 +67,8 @@ public class AngkotDengklek
         System.out.printf("%-28s%-2c%d %s\n","Durasi operasi",':',(hourEnd - hourStart),"jam");
         System.out.printf("%-28s%-2c%d %s\n","Jumlah penumpang dewasa",':',(adultPassengerCounter),"orang");
         System.out.printf("%-28s%-2c%d %s\n","Jumlah penumpang anak kecil",':',(childPassengerCounter),"orang");
-        System.out.printf("%-28s%-2c%s%d%c%d%s\n","Total pendapatan",':',"Rp",(currentIncome),'.',(00),",-");
-        System.out.printf("%-28s%-2c%s%d%c%d%s\n","Pengeluaran bensin",':',"Rp",(gasCost),'.',(00),",-");
-        System.out.print("Pak Dengklek " + ((finalIncome < 0) ? "mengalami kerugian" : "mendapat keuntungan") +  " sebesar " + ((finalIncome < 0) ? finalIncome * -1 : finalIncome) + ".00,-");
+        System.out.printf("%-28s%-2c%s%d%c%s%s\n","Total pendapatan",':',"Rp",(currentIncome),'.',"00",",-");
+        System.out.printf("%-28s%-2c%s%d%c%s%s\n","Pengeluaran bensin",':',"Rp",(gasCost),'.',"00",",-");
+        System.out.print("Pak Dengklek " + ((finalIncome < 0) ? "mengalami kerugian" : "mendapat keuntungan") +  " sebesar Rp" + ((finalIncome < 0) ? finalIncome * -1 : finalIncome) + ".00,-");
     }
 }
