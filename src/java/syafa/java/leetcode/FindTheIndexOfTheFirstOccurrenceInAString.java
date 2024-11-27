76a8286b36ca19283dc0e574a574f11a1cc98df7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 18:20:23 +0700

    Add answer

--- src/java/syafa/java/leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/java/syafa/java/leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,5 +1,23 @@
 package syafa.java.leetcode;
 
-public class FindTheIndexOfTheFirstOccurrenceInAString {
-
+public class FindTheIndexOfTheFirstOccurrenceInAString
+{
+    class Solution
+    {
+        public int strStr(String haystack,String needle)
+        {
+            if (haystack.equalsIgnoreCase(needle))
+            {
+                return 0;
+            }
+            for (int i = 0; i <= haystack.length() - needle.length(); i++)
+            {
+                if (haystack.substring(i,needle.length() + i).equalsIgnoreCase(needle))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
 }
