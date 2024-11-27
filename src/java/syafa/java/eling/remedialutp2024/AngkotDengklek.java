8da495293f1e53d4a050119851d5aca2f1aab957
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 13:37:13 +0700

    Input data

--- src/java/syafa/java/eling/remedialutp2024/AngkotDengklek.java
+++ src/java/syafa/java/eling/remedialutp2024/AngkotDengklek.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 
 public class AngkotDengklek
 {
+    static final long ADULT_TICKET_COST = 12000;
+    static final long CHILD_TICKET_COST = 8000;
+    static final int GAS_TANK = 20;
+    static final long GAS_CONSUMPTION = 2;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int hourStart = input.nextInt(); input.nextLine();
+        int hourEnd = input.nextInt(); input.nextLine();
+        int passengerAmount = input.nextInt(); input.nextLine();
+        for (int i = 0; i < passengerAmount; i++)
+        {
+            //
+        }
         input.close();
     }
 }
