b9e9b4747a7ee869075871c913b13c07ed7648f3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 10:11:42 +0700

    Print bottom

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EMatriksSpiral.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EMatriksSpiral.java
@@ -13,20 +13,29 @@ public class LC4EMatriksSpiral
         int leftBound = 0;
         int rightBound = matrixSize;
         int[][] matrix = new int[matrixSize][matrixSize];
-        for (int i = 0; i < matrix.length; i++)
+        while (true)
         {
-            matrix[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            for (int i = 0; i < matrix.length; i++)
+            {
+                matrix[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            }
+            input.close();
+            for (int i = 0; i < rightBound; i++)
+            {
+                System.out.print(matrix[upperBound][i] + " ");
+            }
+            upperBound++;
+            for (int i = upperBound; i < lowerBound; i++)
+            {
+                System.out.print(matrix[i][rightBound - 1] + " ");
+            }
+            rightBound--;
+            for (int i = rightBound; i > leftBound; i--)
+            {
+                System.out.print(matrix[lowerBound - 1][i - 1] + " ");
+            }
+            lowerBound--;
+            break;
         }
-        input.close();
-        for (int i = 0; i < rightBound; i++)
-        {
-            System.out.print(matrix[upperBound][i] + " ");
-        }
-        upperBound++;
-        for (int i = 0; i < lowerBound; i++)
-        {
-            System.out.print(matrix[i][rightBound - 1] + " ");
-        }
-        rightBound--;
     }
 }
