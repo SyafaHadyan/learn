a41f075d74194825bf078dbb6009eafcfcd08eaf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 11:05:31 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/EqualizeTheArray.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/EqualizeTheArray.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
 
-public class EqualizeTheArray {
-
+public class EqualizeTheArray
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
