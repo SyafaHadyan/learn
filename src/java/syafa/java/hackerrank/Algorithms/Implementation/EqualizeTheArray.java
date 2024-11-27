e54e0897f652070e312af98de696f69c2bce6c0d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 11:06:38 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/EqualizeTheArray.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/EqualizeTheArray.java
@@ -1,11 +1,18 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class EqualizeTheArray
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int numberAmount = Integer.parseInt(input.nextLine());
+        HashMap<Integer,Integer> number = new HashMap<>();
+        for (int i = 0; i < numberAmount; i++)
+        {
+            //
+        }
         input.close();
     }
 }
