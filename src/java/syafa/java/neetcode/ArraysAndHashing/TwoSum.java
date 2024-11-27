970ad9d0695aaf1a369aa10edb53818fcad8f367
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 19:27:10 +0700

    Add twosum

---
+++ src/java/syafa/java/neetcode/ArraysAndHashing/TwoSum.java
@@ -0,0 +1,22 @@
+package syafa.java.neetcode.ArraysAndHashing;
+
+public class TwoSum
+{
+    class Solution
+    {
+        public int[] twoSum(int[] nums,int target)
+        {
+            for (int i = 0; i < nums.length; i++)
+            {
+                for (int j = 1 + i; j < nums.length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[]{i,j};
+                    }
+                }
+            }
+            return new int[]{};
+        }
+    }
+}
