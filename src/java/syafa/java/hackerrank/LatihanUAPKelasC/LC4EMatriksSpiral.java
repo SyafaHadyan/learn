c8aeda583b08929d9949f34ceadaa42fb7afa20f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 09:55:07 +0700

    Input data

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EMatriksSpiral.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4EMatriksSpiral.java
@@ -1,11 +1,18 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
 import java.util.*;
+import java.util.stream.*;
 
 public class LC4EMatriksSpiral
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int matrixSize = Integer.parseInt(input.nextLine());
+        int[][] matrix = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrix.length; i++)
+        {
+            matrix[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
