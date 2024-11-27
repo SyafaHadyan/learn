873029edc915cee19b62e6585aaf3caf6ae094fc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 14:31:44 +0700

    Fix calc

--- src/java/syafa/java/eling/remedialutp2024/HutanLabirin.java
+++ src/java/syafa/java/eling/remedialutp2024/HutanLabirin.java
@@ -3,9 +3,29 @@ import java.util.Scanner;
 
 public class HutanLabirin
 {
+    static final int VALUE_LIMIT = 1000;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int initialValue = input.nextInt(); input.nextLine();
+        int stepLimit = input.nextInt();
         input.close();
+        for (int i = 1; i <= stepLimit; i++)
+        {
+            if (initialValue % 2 == 0)
+            {
+                initialValue /= 2;
+            }
+            else
+            {
+                initialValue *= 10;
+            }
+            if (initialValue > VALUE_LIMIT)
+            {
+                System.out.print("Berhasil keluar pada langkah ke" + "-" + i);
+                return;
+            }
+        }
+        System.out.print("Terjebak setelah" + " " + stepLimit + " " + "langkah");
     }
 }
