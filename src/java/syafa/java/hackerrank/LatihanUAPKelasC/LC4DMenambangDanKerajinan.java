8ba8f68ec500aa24ce1e84e527ce0f1b7ca86a2b
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 09:15:11 +0700

    Put to hashmap with list int as key and block name as val

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
@@ -16,10 +16,22 @@ public class LC4DMenambangDanKerajinan
     {
         Scanner input = new Scanner(System.in);
         int[] coordinateLimit = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
-        HashMap<Integer,String> registeredBlock = new HashMap<>();
+        HashMap<List<Integer>,String> registeredBlock = new HashMap<>();
         while (input.hasNextLine())
         {
-            //
+            String[] tempInput = input.nextLine().split("\s");
+            if (!(tempInput[0].equalsIgnoreCase("Stop")))
+            {
+                registeredBlock.put
+                (
+                    Collections.unmodifiableList
+                    (
+                        Arrays.asList(Integer.parseInt(tempInput[1]),Integer.parseInt(tempInput[2],Integer.parseInt(tempInput[3])))
+                    ),
+                    tempInput[0]
+                );
+            }
+
         }
         input.close();
     }
