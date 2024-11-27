921a11d2440091c82c2db933b955f1e505006fe6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-27 09:10:25 +0700

    Block list

--- src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
+++ src/java/syafa/java/hackerrank/LatihanUAPKelasC/LC4DMenambangDanKerajinan.java
@@ -1,11 +1,26 @@
 package syafa.java.hackerrank.LatihanUAPKelasC;
 import java.util.*;
+import java.util.stream.*;
 
 public class LC4DMenambangDanKerajinan
 {
+    static final ArrayList<String> VALID_BLOCK_LIST = new ArrayList<>(Arrays.asList
+    (
+        "air",
+        "dirt",
+        "grass",
+        "wood",
+        "stone"
+    ));
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] coordinateLimit = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        HashMap<Integer,String> registeredBlock = new HashMap<>();
+        while (input.hasNextLine())
+        {
+            //
+        }
         input.close();
     }
 }
