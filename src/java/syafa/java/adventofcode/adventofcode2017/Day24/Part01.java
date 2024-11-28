031cb6ddcf17d7770e66502b0dd17931446e5486
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-28 19:47:18 +0700

    Input bridge data

--- src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
@@ -1,11 +1,18 @@
 package syafa.java.adventofcode.adventofcode2017.Day24;
 import java.util.*;
+import java.util.stream.*;
 
 public class Part01
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        HashMap<List<Integer>,Integer> bridge = new HashMap<>();
+        while (input.hasNextLine())
+        {
+            int[] tempInput = Stream.of(input.nextLine().split("/")).mapToInt(Integer::parseInt).toArray();
+            bridge.put(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])),(tempInput[0] + tempInput[1]));
+        }
         input.close();
     }
 }
