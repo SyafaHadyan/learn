e5759806e172b3af6138d17497a8bc0e522f05fc
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-28 19:55:38 +0700

    Iterate through find possible key

--- src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
@@ -8,11 +8,31 @@ public class Part01
     {
         Scanner input = new Scanner(System.in);
         HashMap<List<Integer>,Integer> bridge = new HashMap<>();
+        int largestPort = 0;
+        int bridgeStrength = 0;
         while (input.hasNextLine())
         {
             int[] tempInput = Stream.of(input.nextLine().split("/")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(tempInput);
             bridge.put(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])),(tempInput[0] + tempInput[1]));
+            for (int i = 0; i < tempInput.length; i++)
+            {
+                if (tempInput[i] > largestPort)
+                {
+                    largestPort = tempInput[i];
+                }
+            }
         }
         input.close();
+        for (List<Integer> i : bridge.keySet())
+        {
+            for (int j = 1; j <= largestPort; j++)
+            {
+                if (bridge.containsKey(Collections.unmodifiableList(Arrays.asList(i.getFirst(),j))))
+                {
+                    //
+                }
+            }
+        }
     }
 }
