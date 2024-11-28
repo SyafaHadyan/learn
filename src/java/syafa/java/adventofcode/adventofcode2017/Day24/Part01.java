197f562cceafde18256484b1964801158fe51570
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-28 20:08:14 +0700

    If higher val strength

--- src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
@@ -18,31 +18,46 @@ public class Part01
             bridge.put(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])),(tempInput[0] + tempInput[1]));
             if (tempInput[0] == 0)
             {
-                possibleKeySet.add(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])));
+                System.err.println("Possible first port " +  tempInput[0] + " " + tempInput[1]);
             }
+            possibleKeySet.add(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])));
             for (int i = 0; i < tempInput.length; i++)
             {
                 if (tempInput[i] > largestPort)
                 {
+                    System.err.print("Largest port from " + largestPort);
                     largestPort = tempInput[i];
+                    System.err.println(" to " + largestPort);
                 }
             }
+            System.out.println("Current largest port " + largestPort + "\n");
         }
         input.close();
         for (List<Integer> i : possibleKeySet)
         {
+            int currentBridgeStrength = 0;
             for (int j = 1; true; j++)
             {
                 List<Integer> getKey = Collections.unmodifiableList(Arrays.asList(i.getFirst(),j));
                 if (bridge.containsKey(getKey) && bridge.get(getKey) > bridgeStrength)
                 {
-                    bridgeStrength = bridge.get(getKey);
+                    currentBridgeStrength += bridge.get(getKey);
+                    System.err.println(getKey);
+                    System.err.println("Match higher value at " + bridgeStrength);
                 }
-                else if (j == largestPort)
+                if (j == largestPort)
                 {
+                    System.err.println("Break");
                     break;
                 }
             }
+            if (currentBridgeStrength > bridgeStrength)
+            {
+                System.err.println("Current higher " + currentBridgeStrength);
+                bridgeStrength = currentBridgeStrength;
+                System.err.println("Current bridge strength " + bridgeStrength);
+            }
+            System.err.println();
         }
         System.out.println(bridgeStrength);
     }
