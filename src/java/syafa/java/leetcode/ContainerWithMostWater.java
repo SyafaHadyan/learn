051a688f41dbbc4568aadec6964b81ab009acbd0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-28 12:42:39 +0700

    Fix math calc

--- src/java/syafa/java/leetcode/ContainerWithMostWater.java
+++ src/java/syafa/java/leetcode/ContainerWithMostWater.java
@@ -19,15 +19,21 @@ public class ContainerWithMostWater
         {
             for (int j = leftIndex; j < rightIndex; j++)
             {
-                int currentWater = Math.min(height[j],height[rightIndex - 1]) * rightIndex - j - 1;
+                System.err.println("CRIDX " + height[j] + " CRLMT " + height[rightIndex - 1]);
+                System.err.println("CRIDXMN " + Math.min(height[j],height[rightIndex - 1]));
+                System.err.println("CRMTP " + (rightIndex - j - 1));
+                int currentWater = Math.min(height[j],height[rightIndex - 1]) * (rightIndex - j - 1);
+                System.err.println("CRWT " + currentWater);
                 if (currentWater > maxWater)
                 {
                     maxWater = currentWater;
+                    System.err.println("WTMX " + maxWater);
                 }
+                System.err.println();
             }
             for (int j = rightIndex - 1; j > leftIndex; j--)
             {
-                int currentWater = Math.min(height[j],height[leftIndex]) * j - leftIndex;
+                int currentWater = Math.min(height[j],height[leftIndex]) * (j - leftIndex);
                 if (currentWater > maxWater)
                 {
                     maxWater = currentWater;
