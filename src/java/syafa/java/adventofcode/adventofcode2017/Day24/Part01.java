6ef706ca3b3c8f573895ee5624a1d660c36d319c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-29 07:22:34 +0700

    Add to possible combination and add to brige only if not start at 0

--- src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day24/Part01.java
@@ -8,18 +8,31 @@ public class Part01
     {
         Scanner input = new Scanner(System.in);
         HashMap<List<Integer>,Integer> bridge = new HashMap<>();
-        List<List<Integer>> possibleKeySet = new ArrayList<>();
+        List<List<Integer>> startingPosition = new ArrayList<>();
+        ArrayList<Integer> possibleCombination = new ArrayList<>();
         int largestPort = 0;
         int bridgeStrength = 0;
         while (input.hasNextLine())
         {
             int[] tempInput = Stream.of(input.nextLine().split("/")).mapToInt(Integer::parseInt).toArray();
             Arrays.sort(tempInput);
-            bridge.putIfAbsent(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])),(tempInput[0] + tempInput[1]));
+            if (tempInput[0] != 0)
+            {
+                bridge.putIfAbsent(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])),(tempInput[0] + tempInput[1]));
+            }
+            for (int i = 0; i < tempInput.length; i++)
+            {
+                if (!(possibleCombination.contains(tempInput[i])) && tempInput[i] != 0)
+                {
+                    System.err.println("Combination " + tempInput[i]);
+                    possibleCombination.add(tempInput[i]);
+                }
+            }
+            System.err.println("Current possible combination " + possibleCombination);
             if (tempInput[0] == 0)
             {
                 System.err.println("Possible first port " +  tempInput[0] + " " + tempInput[1]);
-                possibleKeySet.add(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])));
+                startingPosition.add(Collections.unmodifiableList(Arrays.asList(tempInput[0],tempInput[1])));
             }
             if (tempInput[1] > largestPort)
             {
@@ -27,37 +40,41 @@ public class Part01
                 largestPort = tempInput[1];
                 System.err.println(" to " + largestPort);
             }
-            System.out.println("Current largest port " + largestPort + "\n");
+            System.out.println("Current largest port " + largestPort);
+            System.err.println("Bridge "+ bridge);
+            System.err.println("Starting position "+ startingPosition);
+            System.err.println("Possible combination "+ possibleCombination + "\n");
         }
         input.close();
-        System.err.println(bridge);
-        System.err.println(possibleKeySet);
-        for (List<Integer> i : possibleKeySet)
+        Collections.sort(possibleCombination);
+        System.err.println("Bridge "+ bridge);
+        System.err.println("Starting position "+ startingPosition);
+        System.err.println("Possible combination sorted "+ possibleCombination);
+        for (List<Integer> i : startingPosition)
         {
+            System.err.println("Starting position " + i);
             int currentBridgeStrength = 0;
-            for (int counter = 0, j = 1; j <= largestPort && counter < bridge.size(); j++)
+            for (int j = 1; j < bridge.size(); j++)
             {
-                ArrayList<Integer> getKey = new ArrayList<Integer>(Arrays.asList(i.getLast(),j));
+                ArrayList<Integer> getKey = new ArrayList<Integer>(Arrays.asList(i.getLast(),possibleCombination.get(j)));
                 System.err.println(getKey);
                 if (bridge.containsKey(getKey))
                 {
                     currentBridgeStrength += getKey.getLast();
                     System.err.println(getKey);
-                    System.err.println("Match higher value at " + currentBridgeStrength);
+                    System.err.println("Match value at " + currentBridgeStrength + " with key " + getKey);
                     getKey.set(0,getKey.getLast());
                     System.err.println(getKey);
-                    for (int k = 1; k <= largestPort; k++)
+                    for (int k = 1; k <= possibleCombination.size(); k++)
                     {
-                        for (int l = getKey.getFirst(), currentCounter = 0; l <= largestPort && currentCounter < bridge.size(); l++, counter++)
+                        for (int l = possibleCombination.getFirst(); l < possibleCombination.getLast(); l++)
                         {
                             getKey.set(1,l);
                             System.err.println(getKey);
                             if (bridge.containsKey(getKey))
                             {
                                 currentBridgeStrength += bridge.get(getKey);
-                                k++;
                                 System.err.println(currentBridgeStrength);
-                                System.err.println(getKey);
                                 System.err.println(bridge);
                             }
                         }
