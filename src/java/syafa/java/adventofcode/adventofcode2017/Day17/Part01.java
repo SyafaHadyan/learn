b1ce65f5ae5379cc284f1a4d8152ac72d69d3814
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-29 13:33:58 +0700

    Loop through index

--- src/java/syafa/java/adventofcode/adventofcode2017/Day17/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day17/Part01.java
@@ -3,9 +3,18 @@ import java.util.*;
 
 public class Part01
 {
+    static final int SPINLOCK = 3;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int operationLookup = Integer.parseInt(input.nextLine());
         input.close();
+        for (int i = 0; i < operationLookup; i++)
+        {
+            for (int j = i; j < i + SPINLOCK; j++)
+            {
+                //
+            }
+        }
     }
 }
