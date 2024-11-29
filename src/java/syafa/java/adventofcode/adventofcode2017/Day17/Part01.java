03d2439cf78c81b43e779c6447accc850a8bb598
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-11-29 13:54:28 +0700

    Spin lock cycle

--- src/java/syafa/java/adventofcode/adventofcode2017/Day17/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2017/Day17/Part01.java
@@ -3,18 +3,29 @@ import java.util.*;
 
 public class Part01
 {
-    static final int SPINLOCK = 3;
+    static final int SPINLOCK_CYCLE = 3;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int operationLookup = Integer.parseInt(input.nextLine());
         input.close();
-        for (int i = 0; i < operationLookup; i++)
+        ArrayList<Integer> spinlock = new ArrayList<>();
+        spinlock.add(0);
+        System.err.println(spinlock);
+        for (int i = 1, previousIndex = 0; i <= operationLookup; i++)
         {
-            for (int j = i; j < i + SPINLOCK; j++)
+            int nextIndex = previousIndex + SPINLOCK_CYCLE;
+            previousIndex = nextIndex;
+            try
             {
-                //
+                spinlock.add(nextIndex,i);
             }
+            catch (IndexOutOfBoundsException e)
+            {
+                spinlock.add(i);
+            }
+            System.err.println(spinlock);
         }
+        System.err.println(spinlock);
     }
 }
