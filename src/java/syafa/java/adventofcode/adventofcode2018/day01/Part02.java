0435cf430372926679172e90ad7989445490d187
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-01 18:53:37 +0700

    Input values to arrlist

--- src/java/syafa/java/adventofcode/adventofcode2018/day01/Part02.java
+++ src/java/syafa/java/adventofcode/adventofcode2018/day01/Part02.java
@@ -1,17 +1,23 @@
 package syafa.java.adventofcode.adventofcode2018.day01;
 import java.util.*;
 
-public class Part01
+public class Part02
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        HashMap<Long,Long> registeredValues = new HashMap<>();
+        ArrayList<Long> inputValues = new ArrayList<>();
         long frequency = 0l;
         while (input.hasNextLine())
         {
-            frequency += Long.parseLong(input.nextLine().replace("+",""));
+            inputValues.add(Long.parseLong(input.nextLine().replace("+","")));
         }
         input.close();
+        for (int i = 0; i < args.length; i++)
+        {
+            
+        }
         System.out.println(frequency);
     }
 }
