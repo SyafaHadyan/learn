bac1302b0bc84a5c81522b34a7d00420176985c9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-02 16:39:11 +0700

    Temp input data

--- src/java/syafa/java/adventofcode/adventofcode2024/day01/Part01.java
+++ src/java/syafa/java/adventofcode/adventofcode2024/day01/Part01.java
@@ -1,11 +1,19 @@
 package syafa.java.adventofcode.adventofcode2024.day01;
 import java.util.*;
+import java.util.stream.*;
 
 public class Part01
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int difference = 0;
+        ArrayList<Integer> firstSet = new ArrayList<>();
+        ArrayList<Integer> secondSet = new ArrayList<>();
+        while (input.hasNextLine())
+        {
+            int[] tempInput = Stream.of(input.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
