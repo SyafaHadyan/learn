1935b831d6c036ed14db7d7d98b890aedb090c9c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-05 20:28:12 +0700

    Fix rank input

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ClimbingTheLeaderboard.java
@@ -8,13 +8,13 @@ public class ClimbingTheLeaderboard
         Scanner input = new Scanner(System.in);
         int rankedAmount = Integer.parseInt(input.nextLine());
         HashMap<Integer,Integer> ranked = new HashMap<>();
-        for (int i = 1; i <= rankedAmount;)
+        int counter = 1;
+        for (int i = 0; i < rankedAmount; i++)
         {
             int tempInput = input.nextInt();
             if (!(ranked.containsKey(tempInput)))
             {
-                ranked.put(tempInput,i);
-                i++;
+                ranked.put(tempInput,counter++);
             }
         }
         input.nextLine();
