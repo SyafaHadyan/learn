3a865473da1f83af2f2c7edf7ecaaa9ecadc0e87
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-05 21:37:11 +0700

    Modulo left

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/RepeatedString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/RepeatedString.java
@@ -10,10 +10,12 @@ public class RepeatedString
         String stringInput = input.nextLine();
         int targetCharacterAmount = stringInput.replaceAll("[^" + TARGET_CHARACTER + "]","").length();
         long repeat = Long.parseLong(input.nextLine());
+        long remainingAdd = repeat % stringInput.length();
         repeat /= stringInput.length();
         input.close();
         System.err.println("TC " + targetCharacterAmount);
         System.err.println("RP " + repeat);
+        System.err.println("MD " + remainingAdd);
         System.out.println((repeat) * targetCharacterAmount);
     }
 }
