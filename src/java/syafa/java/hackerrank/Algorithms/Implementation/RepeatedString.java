5310807ba6ce1c823f4b3769f54b8557c3078db2
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-05 21:08:33 +0700

    Replace non match target

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/RepeatedString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/RepeatedString.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class RepeatedString
 {
+    static final String TARGET_CHARACTER = "a";
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int targetCharacterAmount = input.nextLine().replaceAll("[^" + TARGET_CHARACTER + "]","").length();
         input.close();
     }
 }
