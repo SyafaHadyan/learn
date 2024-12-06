f5f9a0fedc5bd18fd7c4a928990c35b62524c3b0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-06 16:22:13 +0700

    Loop from lower bound to upper bound

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class ModifiedKaprekarNumbers
 {
+    private static boolean isKaprekar(long number)
+    {
+        return false;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        long lowerBound = Long.parseLong(input.nextLine());
+        long upperBound = Long.parseLong(input.nextLine());
         input.close();
+        for (long i = lowerBound; i <= upperBound; i++)
+        {
+            //
+        }
     }
 }
