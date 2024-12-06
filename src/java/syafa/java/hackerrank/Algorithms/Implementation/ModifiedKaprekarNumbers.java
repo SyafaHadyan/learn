997fbc09b027586c6aa65bbc8547de12d11b8ec3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-06 16:27:31 +0700

    Kaprekar number even length

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
@@ -5,6 +5,18 @@ public class ModifiedKaprekarNumbers
 {
     private static boolean isKaprekar(long number)
     {
+        String numberString = String.valueOf(Math.abs(number));
+        long leftNumber = 0;
+        long rightNumber = 0;
+        if (numberString.length() % 2 == 0)
+        {
+            leftNumber = Long.parseLong(numberString.substring(0,numberString.length() / 2));
+            rightNumber = Long.parseLong(numberString.substring(numberString.length() / 2,numberString.length()));
+        }
+        else
+        {
+            //
+        }
         return false;
     }
     public static void main(String[] args)
