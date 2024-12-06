86eb2037c7b39bac7e5cb7002e5f59c661580dd8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-06 16:20:29 +0700

    Initialize

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/ModifiedKaprekarNumbers.java
@@ -1,5 +1,11 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
+import java.util.*;
 
-public class ModifiedKaprekarNumbers {
-
+public class ModifiedKaprekarNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
