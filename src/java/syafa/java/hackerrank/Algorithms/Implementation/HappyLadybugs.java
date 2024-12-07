78b88204445711c3285372294a92dcea2791cca9
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 08:07:16 +0700

    Add to hashmap and check if not even

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/HappyLadybugs.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/HappyLadybugs.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class HappyLadybugs
 {
+    static final String EMPTY_CELL = "_";
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
@@ -11,11 +12,27 @@ public class HappyLadybugs
         for (int i = 0; i < possible.length; i++)
         {
             int cellSize = Integer.parseInt(input.nextLine());
+            boolean isPossible = true;
             String[] cell = input.nextLine().split("");
             HashMap<String,Integer> bug = new HashMap<>();
-            for (int j = 0; j < cell.length; j++)
+            for (int j = 0; j < cellSize; j++)
             {
-                //
+                if (!(cell[i].equalsIgnoreCase(EMPTY_CELL)))
+                {
+                    bug.merge(cell[i],1,Integer::sum);
+                }
+            }
+            for (String j : bug.keySet())
+            {
+                if (bug.get(j) % 2 != 0)
+                {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if (isPossible)
+            {
+                possible[i] = true;
             }
         }
         input.close();
