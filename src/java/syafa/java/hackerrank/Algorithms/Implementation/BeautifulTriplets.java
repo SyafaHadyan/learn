c6cfd32e710bedc3997e1a76c5643d149c761a8d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 07:22:41 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulTriplets.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulTriplets.java
@@ -7,6 +7,8 @@ public class BeautifulTriplets
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
     }
 }
