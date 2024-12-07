db3084a0d48e8c8b72162724a3dd4a5a8258e6e7
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 08:02:48 +0700

    Loop game amount

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/HappyLadybugs.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/HappyLadybugs.java
@@ -6,6 +6,12 @@ public class HappyLadybugs
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int gameAmount = Integer.parseInt(input.nextLine());
+        boolean[] possible = new boolean[gameAmount];
+        for (int i = 0; i < possible.length; i++)
+        {
+            //
+        }
         input.close();
     }
 }
