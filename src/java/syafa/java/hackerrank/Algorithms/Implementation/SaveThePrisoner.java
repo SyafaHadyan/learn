311eb065ce0909098b31c6ee9aa9c3c5d5abab65
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 20:41:47 +0700

    Calculate end

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/SaveThePrisoner.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/SaveThePrisoner.java
@@ -7,6 +7,17 @@ public class SaveThePrisoner
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        int[] result = new int[testCase];
+        for (int i = 0; i < testCase; i++)
+        {
+            int[] data = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            result[i] = (data[2] + data[1]) % data[0];
+        }
         input.close();
+        for (int i = 0; i < result.length; i++)
+        {
+            System.out.println(result[i]);
+        }
     }
 }
