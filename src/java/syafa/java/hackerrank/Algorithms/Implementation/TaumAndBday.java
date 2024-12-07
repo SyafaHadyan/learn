93a2ce21cb83374aafee15e3e2b6cbb8266171b0
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 17:04:25 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/TaumAndBday.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/TaumAndBday.java
@@ -1,12 +1,18 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
-import java.util.stream.*;;
+import java.util.stream.*;
 
 public class TaumAndBday
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int testCase = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < testCase; i++)
+        {
+            int[] giftAmount = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            int[] giftCost = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        }
         input.close();
     }
 }
