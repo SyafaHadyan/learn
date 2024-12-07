877d7fb41ebec5271c5417d678434590b9e1f340
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 15:48:14 +0700

    Check each index

--- src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
+++ src/java/syafa/java/hackerrank/Algorithms/Strings/HackerRankInAString.java
@@ -3,53 +3,23 @@ import java.util.*;
 
 public class HackerRankInAString
 {
+    static final char[] STRING_ARRAY = "HACKERRANK".toCharArray();
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int check = Integer.parseInt(input.nextLine());
-        String[] checkString = new String[check];
-        String[] hackerrank =
+        int testCase = Integer.parseInt(input.nextLine());
+        boolean[] containString = new boolean[testCase];
+        for (int i = 0; i < testCase; i++)
         {
-            "H",
-            "A",
-            "C",
-            "K",
-            "E",
-            "R",
-            "R",
-            "A",
-            "N",
-            "K"
-        };
-        for (int i = 0; i < check; i++)
-        {
-            checkString[i] = input.nextLine();
-            checkString[i] = checkString[i].toUpperCase();
-        }
-        input.close();
-        for (int i = 0; i < checkString.length; i++)
-        {
-            ArrayList<String> tempCheckString = new ArrayList<>(Arrays.asList(checkString[i].split("")));
-            for (int j = 0; j < tempCheckString.size(); j++)
+            char[] tempInput = input.nextLine().toCharArray();
+            for (int j = 0, counter = 0; j < tempInput.length; j++)
             {
-                if (!(tempCheckString.get(j).equalsIgnoreCase(hackerrank[j])))
+                if (tempInput[i] == STRING_ARRAY[counter])
                 {
-                    tempCheckString.remove(j);
-                    j--;
+                    counter++;
                 }
             }
-            if (tempCheckString.equals(Arrays.asList(hackerrank)))
-            {
-                checkString[i] = "YES";
-            }
-            else
-            {
-                checkString[i] = "NO";
-            }
         }
-        for (int i = 0; i < checkString.length; i++)
-        {
-            System.out.println(checkString[i]);
-        }
+        input.close();
     }
 }
