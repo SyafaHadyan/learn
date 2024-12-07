56720c88021237ced844b4a3e60064e567f95afa
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 07:27:38 +0700

    Find if triplet

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulTriplets.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/BeautifulTriplets.java
@@ -10,5 +10,21 @@ public class BeautifulTriplets
         int[] config = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
         input.close();
+        int tripletCounter = 0;
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 1 + i; j < array.length; j++)
+            {
+                for (int k = 1 + j; k < array.length; k++)
+                {
+                    if (array[j] - array[i] == config[1] && array[k] - array[j] == config[1])
+                    {
+                        System.err.println(i + " " + j + " " + k);
+                        tripletCounter++;
+                    }
+                }
+            }
+        }
+        System.out.println(tripletCounter);
     }
 }
