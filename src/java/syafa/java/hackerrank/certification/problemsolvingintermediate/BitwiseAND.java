45b4f30ec4744159c2301a4b9ee73dd0ac2a285e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-07 13:53:30 +0700

    Input data

--- src/java/syafa/java/hackerrank/certification/problemsolvingintermediate/BitwiseAND.java
+++ src/java/syafa/java/hackerrank/certification/problemsolvingintermediate/BitwiseAND.java
@@ -6,6 +6,19 @@ public class BitwiseAND
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int arrayLength = Integer.parseInt(input.nextLine());
+        int[] data = new int[arrayLength];
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i] = Integer.parseInt(input.nextLine());
+        }
         input.close();
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 1 + i; j < data.length; j++)
+            {
+                //
+            }
+        }
     }
 }
