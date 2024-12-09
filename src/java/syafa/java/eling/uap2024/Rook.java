3e08fc9769878b1514ce6f854e5306529c71dcbd
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 15:53:48 +0700

    Initialize

--- src/java/syafa/java/eling/uap2024/Rook.java
+++ src/java/syafa/java/eling/uap2024/Rook.java
@@ -1,5 +1,11 @@
 package syafa.java.eling.uap2024;
+import java.util.*;
 
-public class Rook {
-
+public class Rook
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
