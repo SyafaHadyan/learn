b71a496b5c3cb6d926cbcccf1e54469cc05d4abf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 15:56:28 +0700

    Input data

--- src/java/syafa/java/eling/uap2024/Rook.java
+++ src/java/syafa/java/eling/uap2024/Rook.java
@@ -6,6 +6,15 @@ public class Rook
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int chessBoardWidth = Integer.parseInt(input.nextLine());
+        int chessBoardLength = Integer.parseInt(input.nextLine());
+        int rookAmount = Integer.parseInt(input.nextLine());
+        HashMap<Integer,Integer> rookPositionHorizontal = new HashMap();
+        HashMap<Integer,Integer> rookPositionVertical = new HashMap<>();
+        for (int i = 0; i < rookAmount; i++)
+        {
+            //
+        }
         input.close();
     }
 }
