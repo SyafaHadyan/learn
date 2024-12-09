03c9e148b5241988b27c1f9615d172a6d4e3217e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 16:16:27 +0700

    Input data

--- src/java/syafa/java/eling/uap2024/RotateArray.java
+++ src/java/syafa/java/eling/uap2024/RotateArray.java
@@ -1,5 +1,19 @@
 package syafa.java.eling.uap2024;
+import java.util.*;
+import java.util.stream.*;
 
-public class RotateArray {
-
+public class RotateArray
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int arraySize = Integer.parseInt(input.nextLine());
+        int[][] array = new int[arraySize][arraySize];
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+            
+        }
+        input.close();
+    }
 }
