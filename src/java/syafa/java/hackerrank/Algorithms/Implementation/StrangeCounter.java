49de9659a999d1642b189a172dc7fd5b28f634af
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 12:55:40 +0700

    Delete

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -6,30 +6,6 @@ public class StrangeCounter
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        double requestTimeValue = Double.parseDouble(input.nextLine());
         input.close();
-        double initialValue = 3;
-        double currentValue = initialValue;
-        if (requestTimeValue == 1)
-        {
-            System.out.print(3);
-            return;
-        }
-        for (double i = 1; i < requestTimeValue; i++)
-        {
-            currentValue--;
-            if (currentValue == 1)
-            {
-                initialValue *= 2;
-                currentValue = initialValue;
-                i++;
-            }
-            if (requestTimeValue - i == 1)
-            {
-                System.out.printf("%.0f",currentValue);
-                return;
-            }
-        }
-        System.out.print(1);
     }
 }
