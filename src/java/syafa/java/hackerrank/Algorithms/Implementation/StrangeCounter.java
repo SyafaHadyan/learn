e8c3edc0e8f9fead4f72f748fd19be9bbd35cd00
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 12:59:03 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/StrangeCounter.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class StrangeCounter
 {
+    static final long COUNTER_INCREASE = 2l;
+    static final long COUNTER_START = 3l;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        long timeRequest = Long.parseLong(input.nextLine());
         input.close();
+        long currentTime = 1;
+        long currentIterate = 0;
+        long curretnValue = COUNTER_START;
+        while (currentTime < timeRequest)
+        {
+            currentIterate++;
+        }
     }
 }
