2902360dbc1867476986dfae444a946c589059d8
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 16:40:58 +0700

    Swap diagonal

--- src/java/syafa/java/eling/uap2024/RotateArray.java
+++ src/java/syafa/java/eling/uap2024/RotateArray.java
@@ -4,16 +4,33 @@ import java.util.stream.*;
 
 public class RotateArray
 {
+    private static void rotateArray(int[][] array)
+    {
+        for (int i = 0, j = 0, counter = 0; i < array.length -2; i++, j++)
+        {
+            while (i != j)
+            {
+                int temp = array[counter][j];
+                array[counter][j] = array[counter][i];
+                array[counter][i] = temp;
+            }
+            counter++;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int arraySize = Integer.parseInt(input.nextLine());
+        int rotationAmount = Integer.parseInt(input.nextLine());
         int[][] array = new int[arraySize][arraySize];
         for (int i = 0; i < array.length; i++)
         {
             array[i] = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
-            
         }
         input.close();
+        for (int i = 0; i < rotationAmount; i++)
+        {
+            rotateArray(array);
+        }
     }
 }
