34aacee3f052ff883618379c8cecbd701048bd66
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-09 19:48:41 +0700

    Copy array return value

--- src/java/syafa/java/eling/uap2024/RotateArray.java
+++ src/java/syafa/java/eling/uap2024/RotateArray.java
@@ -4,59 +4,75 @@ import java.util.stream.*;
 
 public class RotateArray
 {
+    private static void rotateArrayReturn(int[][] array)
+    {
+        int[][] rotatedArray = new int[array.length][array.length];
+        for (int i = 0; i < rotatedArray.length; i++)
+        {
+            for (int j = 0; j < rotatedArray.length; j++)
+            {
+                rotatedArray[i][j] = array[rotatedArray.length - 1 - j][i];
+            }
+        }
+        for (int i = 0; i < rotatedArray.length; i++)
+        {
+            array[i] = rotatedArray[i].clone();
+        }
+    }
     private static void rotateArray(int[][] array)
     {
-        int[] horizontalPositionUp = new int[2];
-        int[] horizontalPositionBottom = {array.length - 1,array.length - 1};
-        int[] verticalPositionUp = new int[2];
-        int[] verticalPositionBottom = {array.length - 1,array.length - 1};
-        for (int i = 0; i < array.length; i++)
-        {
-            do
-            {
-                int tempSwap = array[verticalPositionUp[1]][verticalPositionUp[0]];
-                array[verticalPositionUp[1]][verticalPositionUp[0]] = array[horizontalPositionUp[1]][horizontalPositionUp[0]];
-                array[horizontalPositionUp[1]][horizontalPositionUp[0]] = tempSwap;
-                horizontalPositionUp[1]++;
-                verticalPositionUp[0]++;
-            }
-            while
-            (
-                (horizontalPositionUp[1] != verticalPositionUp[0])
-            );
-            horizontalPositionUp[1] = 0;
-            verticalPositionUp[0] = 0;
-            horizontalPositionUp[0]++;
-            verticalPositionUp[1]++;
-            if (array.length - i != 1)
-            {
-                do
-                {
-                    int tempSwap = array[verticalPositionBottom[1]][verticalPositionBottom[0]];
-                    array[verticalPositionBottom[1]][verticalPositionBottom[0]] = array[horizontalPositionBottom[1]][horizontalPositionBottom[0]];
-                    array[horizontalPositionBottom[1]][horizontalPositionBottom[0]] = tempSwap;
-                    horizontalPositionBottom[1]--;
-                    verticalPositionBottom[0]--;
-                }
-                while
-                (
-                    (horizontalPositionBottom[1] != verticalPositionBottom[0])
-                );
-            }
-            horizontalPositionBottom[1] = array.length - 1;
-            verticalPositionBottom[0] = array.length - 1;
-            horizontalPositionBottom[0]--;
-            verticalPositionBottom[1]--;
-        }
-        for (int i = 0; i < array.length / 2; i++)
-        {
-            for (int j = 0; j < array.length; j++)
-            {
-                int tempSwap = array[j][array.length - 1 - i];
-                array[j][array.length - 1 - i] = array[j][i];
-                array[j][i] = tempSwap;
-            }
-        }
+        rotateArrayReturn(array);
+        // int[] horizontalPositionUp = new int[2];
+        // int[] horizontalPositionBottom = {array.length - 1,array.length - 1};
+        // int[] verticalPositionUp = new int[2];
+        // int[] verticalPositionBottom = {array.length - 1,array.length - 1};
+        // for (int i = 0; i < array.length; i++)
+        // {
+        //     do
+        //     {
+        //         int tempSwap = array[verticalPositionUp[1]][verticalPositionUp[0]];
+        //         array[verticalPositionUp[1]][verticalPositionUp[0]] = array[horizontalPositionUp[1]][horizontalPositionUp[0]];
+        //         array[horizontalPositionUp[1]][horizontalPositionUp[0]] = tempSwap;
+        //         horizontalPositionUp[1]++;
+        //         verticalPositionUp[0]++;
+        //     }
+        //     while
+        //     (
+        //         (horizontalPositionUp[1] != verticalPositionUp[0])
+        //     );
+        //     horizontalPositionUp[1] = 0;
+        //     verticalPositionUp[0] = 0;
+        //     horizontalPositionUp[0]++;
+        //     verticalPositionUp[1]++;
+        //     if (array.length - i != 1)
+        //     {
+        //         do
+        //         {
+        //             int tempSwap = array[verticalPositionBottom[1]][verticalPositionBottom[0]];
+        //             array[verticalPositionBottom[1]][verticalPositionBottom[0]] = array[horizontalPositionBottom[1]][horizontalPositionBottom[0]];
+        //             array[horizontalPositionBottom[1]][horizontalPositionBottom[0]] = tempSwap;
+        //             horizontalPositionBottom[1]--;
+        //             verticalPositionBottom[0]--;
+        //         }
+        //         while
+        //         (
+        //             (horizontalPositionBottom[1] != verticalPositionBottom[0])
+        //         );
+        //     }
+        //     horizontalPositionBottom[1] = array.length - 1;
+        //     verticalPositionBottom[0] = array.length - 1;
+        //     horizontalPositionBottom[0]--;
+        //     verticalPositionBottom[1]--;
+        // }
+        // for (int i = 0; i < array.length / 2; i++)
+        // {
+        //     for (int j = 0; j < array.length; j++)
+        //     {
+        //         int tempSwap = array[j][array.length - 1 - i];
+        //         array[j][array.length - 1 - i] = array[j][i];
+        //         array[j][i] = tempSwap;
+        //     }
+        // }
     }
     public static void main(String[] args)
     {
