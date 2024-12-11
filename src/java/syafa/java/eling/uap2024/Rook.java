8ef5a388978de5bf916509f43835ebab6fc9ab52
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-11 13:53:25 +0700

    Change input type

--- src/java/syafa/java/eling/uap2024/Rook.java
+++ src/java/syafa/java/eling/uap2024/Rook.java
@@ -3,27 +3,32 @@ import java.util.*;
 
 public class Rook
 {
+    static final int CHESS_BOARD_SIZE = 8;
+    static final String ROOK_MARK = "#";
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int chessBoardWidth = input.nextInt();
-        int chessBoardLength = input.nextInt(); input.nextLine();
-        int rookAmount = Integer.parseInt(input.nextLine());
         int freeCount = 0;
         HashMap<Integer,Integer> rookPositionHorizontal = new HashMap<>();
         HashMap<Integer,Integer> rookPositionVertical = new HashMap<>();
-        for (int i = 0; i < rookAmount; i++)
+        for (int i = 0; i < CHESS_BOARD_SIZE; i++)
         {
-            rookPositionHorizontal.putIfAbsent(input.nextInt(),i);
-            rookPositionVertical.putIfAbsent(input.nextInt(),i);
-            input.nextLine();
+            String[] tempInput = input.nextLine().split("");
+            for (int j = 0; j < tempInput.length; j++)
+            {
+                if (tempInput[j].equalsIgnoreCase(ROOK_MARK))
+                {
+                    rookPositionHorizontal.putIfAbsent(j,j);
+                    rookPositionVertical.putIfAbsent(i,i);
+                }
+            }
         }
         input.close();
-        for (int i = 0; i < chessBoardLength; i++)
+        for (int i = 0; i <     CHESS_BOARD_SIZE; i++)
         {
             if (!(rookPositionVertical.containsKey(i)))
             {
-                for (int j = 0; j < chessBoardWidth; j++)
+                for (int j = 0; j < CHESS_BOARD_SIZE; j++)
                 {
                     if (!(rookPositionHorizontal.containsKey(j)))
                     {
