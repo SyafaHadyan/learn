7971bfe1362aa26839ce51e8113876d51ac487ec
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-11 15:18:09 +0700

    Print result

--- src/java/syafa/java/eling/uap2024/DataPekerja.java
+++ src/java/syafa/java/eling/uap2024/DataPekerja.java
@@ -5,22 +5,31 @@ public class DataPekerja
 {
     static final int MIN_WORK_HOUR = 8;
     static final int MAX_WORK_HOUR = 12;
-    static final long HOUR_SALARY = 15000;
+    static final double HOUR_SALARY = 15000d;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int workerAmount = input.nextInt();
         int dayAmount = input.nextInt(); input.nextLine();
         int[] workHour = new int[workerAmount];
-        long[] salary = new long[workerAmount];
+        double[] salary = new double[workerAmount];
         for (int i = 0; i < workerAmount; i++)
         {
             for (int j = 0; j < dayAmount; j++)
             {
-                workHour[i] = input.nextInt();
+                workHour[i] += input.nextInt();
             }
-            input.nextLine();
+            if (workerAmount - i != 1)
+            {
+                input.nextLine();
+            }
         }
         input.close();
+        for (int i = 0; i < workerAmount; i++)
+        {
+            System.out.println("Tukang Bangunan " + (i + 1) + ":");
+            System.out.println("- Total jam kerja: " + workHour[i] + " jam");
+            System.out.printf("%s%.1f\n","- Gaji: Rp ",salary[i]);
+        }
     }
 }
