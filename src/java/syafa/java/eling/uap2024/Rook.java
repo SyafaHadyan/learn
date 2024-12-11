5c412f917604dab24fa3852d9dd9864c3be5a19f
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-11 17:30:53 +0700

    Horizontal position check

--- src/java/syafa/java/eling/uap2024/Rook.java
+++ src/java/syafa/java/eling/uap2024/Rook.java
@@ -6,13 +6,33 @@ public class Rook
     static final int CHESS_BOARD_SIZE = 8;
     static final String ROOK_MARK = "#";
     static int[][] rookPosition = new int[CHESS_BOARD_SIZE][2];
+    static int rookPositionCounter = 0;
+    private static boolean verticalCheck(int verticalPosition)
+    {
+        for (int i = 0; i < rookPositionCounter; i++)
+        {
+            if (rookPosition[i][1] == verticalPosition)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean horizontalCheck(int horizontalPosition)
+    {
+        for (int i = 0; i < rookPositionCounter; i++)
+        {
+            if (rookPosition[i][0] == horizontalPosition)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int freeCount = 0;
-        int rookPositionCounter = 0;
-        HashMap<Integer,Integer> rookPositionHorizontal = new HashMap<>();
-        HashMap<Integer,Integer> rookPositionVertical = new HashMap<>();
         for (int i = 0; i < CHESS_BOARD_SIZE; i++)
         {
             String[] tempInput = input.nextLine().split("");
@@ -22,19 +42,17 @@ public class Rook
                 {
                     rookPosition[rookPositionCounter][0] = j;
                     rookPosition[rookPositionCounter++][1] = i;
-                    rookPositionHorizontal.putIfAbsent(j,j);
-                    rookPositionVertical.putIfAbsent(i,i);
                 }
             }
         }
         input.close();
         for (int i = 0; i < CHESS_BOARD_SIZE; i++)
         {
-            if (!(rookPositionVertical.containsKey(i)))
+            if (!(verticalCheck(i)))
             {
                 for (int j = 0; j < CHESS_BOARD_SIZE; j++)
                 {
-                    if (!(rookPositionHorizontal.containsKey(j)))
+                    if (!(horizontalCheck(j)))
                     {
                         freeCount++;
                     }
