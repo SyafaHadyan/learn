21a8b26bc3c9ec92cf8128c225ec8c01d1975d58
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 10:34:59 +0700

    Odd even op

--- src/java/syafa/java/cses/problemset/WeirdAlgorithm.java
+++ src/java/syafa/java/cses/problemset/WeirdAlgorithm.java
@@ -6,6 +6,21 @@ public class WeirdAlgorithm
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int initialNumber = Integer.parseInt(input.nextLine());
         input.close();
+        System.out.print(initialNumber);
+        for (int i = 0; initialNumber != 1; i++)
+        {
+            if (initialNumber % 2 != 0)
+            {
+                initialNumber = (initialNumber * 3) + 1;
+            }
+            else
+            {
+                initialNumber /= 2;
+            }
+            System.out.print(" " + initialNumber);
+        }
+        System.out.println();
     }
 }
