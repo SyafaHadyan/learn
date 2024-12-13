6004451cf9660451b5417e5bdfe307a7d475d624
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 10:29:29 +0700

    Initialize

--- src/java/syafa/java/cses/problemset/WeirdAlgorithm.java
+++ src/java/syafa/java/cses/problemset/WeirdAlgorithm.java
@@ -1,5 +1,11 @@
 package syafa.java.cses.problemset;
+import java.util.*;
 
-public class WeirdAlgorithm {
-
+public class WeirdAlgorithm
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
