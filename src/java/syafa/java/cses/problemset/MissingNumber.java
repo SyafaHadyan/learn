964e5c46e45c3fe49eaf189fae56cd3422838b09
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 10:47:02 +0700

    Initialize

--- src/java/syafa/java/cses/problemset/MissingNumber.java
+++ src/java/syafa/java/cses/problemset/MissingNumber.java
@@ -1,5 +1,11 @@
 package syafa.java.cses.problemset;
+import java.util.Scanner;
 
-public class MissingNumber {
-
+public class MissingNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
 }
