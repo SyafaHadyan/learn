a0e2e1a906b4e37a7984e9007513087ea0af6bf6
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 08:21:57 +0700

    Input data

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
@@ -1,11 +1,20 @@
 package syafa.java.hackerrank.Algorithms.Implementation;
 import java.util.*;
+import java.util.stream.*;
 
 public class CircularArrayRotation
 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        int elementCount = input.nextInt();
+        int rotationAmount = input.nextInt();
+        int queryCount = input.nextInt(); input.nextLine();
+        int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < queryCount; i++)
+        {
+            System.out.println(array[i]);
+        }
         input.close();
     }
 }
