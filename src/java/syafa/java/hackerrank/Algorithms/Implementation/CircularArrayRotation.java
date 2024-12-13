552b5a109fb2816f140a90780cb6d0efc97d02e3
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 08:45:05 +0700

    Swap array

--- src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
+++ src/java/syafa/java/hackerrank/Algorithms/Implementation/CircularArrayRotation.java
@@ -4,16 +4,35 @@ import java.util.stream.*;
 
 public class CircularArrayRotation
 {
+    static int rotationAmount;
+    static int[] tempArray;
+    private static void rotateArray(int[] array)
+    {
+        tempArray = new int[array.length];
+        System.err.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++)
+        {
+            System.err.println();
+            int tempIndexSwap = (rotationAmount + i) % (array.length);
+            System.err.println("TIDX " + tempIndexSwap);
+            System.err.println("AIDX " + array[i]);
+            tempArray[tempIndexSwap] = array[i];
+            System.err.println(Arrays.toString(tempArray));
+            System.err.println();
+        }
+        System.err.println(Arrays.toString(tempArray));
+    }
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        int elementCount = input.nextInt();
-        int rotationAmount = input.nextInt();
+        input.nextInt();
+        rotationAmount = input.nextInt();
         int queryCount = input.nextInt(); input.nextLine();
         int[] array = Stream.of(input.nextLine().split("\s")).mapToInt(Integer::parseInt).toArray();
+        rotateArray(array);
         for (int i = 0; i < queryCount; i++)
         {
-            System.out.println(array[i]);
+            System.out.println(tempArray[Integer.parseInt(input.nextLine())]);
         }
         input.close();
     }
