a95911c04528d1fc2e3d0f794c0f673f7b89b7f1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-13 10:59:17 +0700

    Calculate numbersum

--- src/java/syafa/java/cses/problemset/MissingNumber.java
+++ src/java/syafa/java/cses/problemset/MissingNumber.java
@@ -6,6 +6,14 @@ public class MissingNumber
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
+        long numberSum = Long.parseLong(input.nextLine());
+        long tempCalculate = ((numberSum - ((numberSum % 2 == 0) ? 1 : 0)) / 2) + 1;
+        if (numberSum % 2 == 0)
+        {
+            numberSum++;
+        }
+        numberSum *= tempCalculate;
         input.close();
+        System.out.println(numberSum);
     }
 }
