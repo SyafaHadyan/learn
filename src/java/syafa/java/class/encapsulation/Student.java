2ffde69150f5d21d8805dfda28de95d7ce2e88bb
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-13 09:28:21 +0700

    Add student

---
+++ src/java/syafa/java/class/encapsulation/Student.java
@@ -0,0 +1,22 @@
+public class Student
+{
+    private String name = "";
+    private int age = 0;
+
+    String getName()
+    {
+        return this.name;
+    }
+    int getAge()
+    {
+        return this.age;
+    }
+    void setName(String name)
+    {
+        this.name = name;
+    }
+    void setAge(int age)
+    {
+        this.age = age;
+    }
+}
